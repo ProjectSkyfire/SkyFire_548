90b21ec043d7a8fd7fbe668ca1ffa67127c0ce0d
SkyFire <admin@projectskyfire.org>
2013-11-13 18:30:29 -0600

    Turn off building scripts by default. Not needed right now.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -9,7 +9,7 @@
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 option(SERVERS          "Build worldserver and authserver"                            1)
-option(SCRIPTS          "Build core with scripts included"                            1)
+option(SCRIPTS          "Build core with scripts included"                            0)
 option(TOOLS            "Build map/vmap/mmap extraction/assembler tools"              0)
 option(USE_SCRIPTPCH    "Use precompiled headers when compiling scripts"              1)
 option(USE_COREPCH      "Use precompiled headers when compiling servers"              1)
