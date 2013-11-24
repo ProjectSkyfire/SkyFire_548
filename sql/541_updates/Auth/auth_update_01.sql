864b4a17beda201b356f9e47ab5f27a62b8ef5a8
SkyFire <admin@projectskyfire.org>
2013-11-24 17:49:57 -0600

    Next time populate the data needed too ;)
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- sql/541_updates/Auth/auth_update_01.sql
+++ sql/541_updates/Auth/auth_update_01.sql
@@ -6,6 +6,18 @@ CREATE TABLE `realm_classes` (
   PRIMARY KEY (`realmId`,`class`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO realm_classes VALUES ('1', '1', '0');
+INSERT INTO realm_classes VALUES ('1', '2', '0');
+INSERT INTO realm_classes VALUES ('1', '3', '0');
+INSERT INTO realm_classes VALUES ('1', '4', '0');
+INSERT INTO realm_classes VALUES ('1', '5', '0');
+INSERT INTO realm_classes VALUES ('1', '6', '2');
+INSERT INTO realm_classes VALUES ('1', '7', '0');
+INSERT INTO realm_classes VALUES ('1', '8', '0');
+INSERT INTO realm_classes VALUES ('1', '9', '0');
+INSERT INTO realm_classes VALUES ('1', '10', '4');
+INSERT INTO realm_classes VALUES ('1', '11', '0');
+
 DROP TABLE IF EXISTS `realm_races`;
 CREATE TABLE `realm_races` (
   `realmId` int(11) NOT NULL,
@@ -13,3 +25,19 @@ CREATE TABLE `realm_races` (
   `expansion` tinyint(4) NOT NULL COMMENT 'Expansion for race activation',
   PRIMARY KEY (`realmId`,`race`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO realm_races VALUES ('1', '1', '0');
+INSERT INTO realm_races VALUES ('1', '2', '0');
+INSERT INTO realm_races VALUES ('1', '3', '0');
+INSERT INTO realm_races VALUES ('1', '4', '0');
+INSERT INTO realm_races VALUES ('1', '5', '0');
+INSERT INTO realm_races VALUES ('1', '6', '0');
+INSERT INTO realm_races VALUES ('1', '7', '0');
+INSERT INTO realm_races VALUES ('1', '8', '0');
+INSERT INTO realm_races VALUES ('1', '9', '3');
+INSERT INTO realm_races VALUES ('1', '10', '1');
+INSERT INTO realm_races VALUES ('1', '11', '1');
+INSERT INTO realm_races VALUES ('1', '22', '3');
+INSERT INTO realm_races VALUES ('1', '24', '4');
+INSERT INTO realm_races VALUES ('1', '25', '4');
+INSERT INTO realm_races VALUES ('1', '26', '4');
