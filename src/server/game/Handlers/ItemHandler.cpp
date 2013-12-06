d34ea2d8f81ed7378c32de1612c46f35f3481a96
AriDEV <aridev666@gmail.com>
2013-12-06 20:48:55 +0100

    Misc: Fixed My Merge Conflicts Before
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- src/server/game/Handlers/ItemHandler.cpp
+++ src/server/game/Handlers/ItemHandler.cpp
@@ -788,7 +788,7 @@ void WorldSession::HandleBuyBankSlotOpcode(WorldPacket& recvData)
 
     ObjectGuid guid;
 
-	guid[0] = recvData.ReadBit();
+    guid[0] = recvData.ReadBit();
     guid[6] = recvData.ReadBit();
     guid[5] = recvData.ReadBit();
     guid[2] = recvData.ReadBit();
