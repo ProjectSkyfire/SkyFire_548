85eb612159dc03ecfe61a5dcfd942d33c844d72d
projectcore <arcaniafr@gmail.com>
2013-12-20 11:44:48 +0100

    Add CMSG_TRAINER_LIST

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -554,7 +554,7 @@ enum Opcodes
     CMSG_TOGGLE_PVP                                          = 0x0,
     CMSG_TOTEM_DESTROYED                                     = 0x0,
     CMSG_TRAINER_BUY_SPELL                                   = 0x0,
-    CMSG_TRAINER_LIST                                        = 0x0,
+    CMSG_TRAINER_LIST                                        = 0x0B45, // 5.4.1 17538
     CMSG_TRANSMOGRIFY_ITEMS                                  = 0x0,
     CMSG_TRIGGER_CINEMATIC_CHEAT                             = 0x0,
     CMSG_TURN_IN_PETITION                                    = 0x0,
