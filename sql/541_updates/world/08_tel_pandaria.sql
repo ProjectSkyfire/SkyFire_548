a56ab7e1fb3726fc69203d9d731b259774b178ac
gegge6265 <luigibalotta@live.it>
2013-12-20 17:31:50 +0100

    Update 08_tel_pandaria.sql
    Added Shrine of Two Moons and Shrine of Seven Stars Teleport!

--- sql/541_updates/world/08_tel_pandaria.sql
+++ sql/541_updates/world/08_tel_pandaria.sql
@@ -1,3 +1,9 @@
 DELETE FROM game_tele WHERE id =1523;
 INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
 (1523, 3001.38, -542.47, 248.18, 5.1, 870, 'Pandaria');
+DELETE FROM game_tele WHERE id =1524;
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
+(1524, 1665.493164, 929.158813, 471.178284, 3.332851, 870, 'shrinesevenstars');
+DELETE FROM game_tele WHERE id =1525;
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
+(1525, 821.489197, 253.009247, 503.919586, 0.598881, 870, 'shrinetwomoons');
