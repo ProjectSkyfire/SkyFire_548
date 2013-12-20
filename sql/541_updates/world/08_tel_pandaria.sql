ddeaef81087d6e34ad220f69a444da1ceb21c80b
gegge6265 <luigibalotta@live.it>
2013-12-20 18:34:46 +0100

    Update 08_tel_pandaria.sql
    I opened another pull request because the first one was wrong, i never used github, now i understand how it works :) Use these fixes!

--- sql/541_updates/world/08_tel_pandaria.sql
+++ sql/541_updates/world/08_tel_pandaria.sql
@@ -1,3 +1,21 @@
+DELETE FROM game_tele WHERE id =1524;
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
+(1524, 1665.493164, 929.158813, 471.178284, 3.332851, 870, 'shrinetwomoons');
+DELETE FROM game_tele WHERE id =1525;
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
+(1525, 821.489197, 253.009247, 503.919586, 0.598881, 870, 'shrinesevenstars');
 DELETE FROM game_tele WHERE id =1523;
 INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
 (1523, 3001.38, -542.47, 248.18, 5.1, 870, 'Pandaria');
+DELETE FROM game_tele WHERE id =1526;
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
+(1526,-296.103027,-1767.736206,61.592773, 5.601865, 870, 'pandon');
+DELETE FROM game_tele WHERE id =1527;
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
+(1527, 960.715393, -2463.432373, 471.178284, 180.573486, 870, 'jadeserpenttemple');
+DELETE FROM game_tele WHERE id =1528;
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
+(1528,6031.405273,1117.957031,55.934971, 3.332851, 870, 'isleofgiants');
+DELETE FROM game_tele WHERE id =1529;
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES 
+(1529,-698.285095,-5040.817871, -6.276751, 4.588705, 870, 'timelessisland');
