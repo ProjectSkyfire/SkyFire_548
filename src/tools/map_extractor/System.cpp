3c4b81d61154782df9f0ed4c0bbb994a0ed31dec
AriDEV <aridev666@gmail.com>
2013-12-21 12:19:27 +0100

    Tools/Extractor: One Last Update to the extractor i forgot to push
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- src/tools/map_extractor/System.cpp
+++ src/tools/map_extractor/System.cpp
@@ -95,7 +95,7 @@ float CONF_float_to_int16_limit = 2048.0f;   // Max accuracy = val/65536
 float CONF_flat_height_delta_limit = 0.005f; // If max - min less this value - surface is flat
 float CONF_flat_liquid_delta_limit = 0.001f; // If max - min less this value - liquid surface is flat
 
-uint32 CONF_TargetBuild = 17538;              // 5.4.1 17538
+uint32 CONF_TargetBuild = 17688;              // 5.4.2 17688
 
 // List MPQ for extract maps from
 char const* CONF_mpq_list[] =
