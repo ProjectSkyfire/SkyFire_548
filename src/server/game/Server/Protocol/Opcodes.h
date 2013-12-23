c60ca5daf9d4bb1ae69c3155ad6dfead606276cf
AriDEV <aridev666@gmail.com>
2013-12-23 08:39:47 +0100

    Server/PacketIO: Added Structure and Opcode for CMSG_BUY_BANK_SLOT
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -96,7 +96,7 @@ enum Opcodes
     CMSG_BUG                                                 = 0x0,
     CMSG_BUSY_TRADE                                          = 0x0,
     CMSG_BUYBACK_ITEM                                        = 0x0,
-    CMSG_BUY_BANK_SLOT                                       = 0x0,
+    CMSG_BUY_BANK_SLOT                                       = 0x01AD, // 5.4.2 17688
     CMSG_BUY_ITEM                                            = 0x0,
     CMSG_CALENDAR_ADD_EVENT                                  = 0x0,
     CMSG_CALENDAR_ARENA_TEAM                                 = 0x0,
