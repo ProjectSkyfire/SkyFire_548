29b6d7bf6ce2094e5fe551bb4e05d26b2cc1c7f1
SkyFire <admin@projectskyfire.org>
2013-12-28 20:56:18 -0600

    Starting to cleanup some of these deprecated scripts.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/scripts/EasternKingdoms/zone_duskwood.cpp
+++ src/server/scripts/EasternKingdoms/zone_duskwood.cpp
@@ -1,10 +1,12 @@
 /*
- * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2011-2014 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2006-2014 ScriptDev2 <https://github.com/scriptdev2/scriptdev2/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
+ * Free Software Foundation; either version 3 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -23,124 +25,7 @@ SDComment: Quest Support:8735
 SDCategory: Duskwood
 EndScriptData */
 
-#include "ScriptMgr.h"
-#include "ScriptedCreature.h"
-#include "Player.h"
-
-enum Yells
-{
-    YELL_TWILIGHTCORRUPTOR_RESPAWN                                  = 0,
-    YELL_TWILIGHTCORRUPTOR_AGGRO                                    = 1,
-    YELL_TWILIGHTCORRUPTOR_KILL                                     = 2,
-};
-
-
-/*######
-# at_twilight_grove
-######*/
-
-class at_twilight_grove : public AreaTriggerScript
-{
-public:
-    at_twilight_grove() : AreaTriggerScript("at_twilight_grove") { }
-
-    bool OnTrigger(Player* player, const AreaTriggerEntry* /*at*/) OVERRIDE
-    {
-        if (player->HasQuestForItem(21149))
-        {
-            if (Unit* TCorrupter = player->SummonCreature(15625, -10328.16f, -489.57f, 49.95f, 0, TEMPSUMMON_MANUAL_DESPAWN, 60000))
-            {
-                TCorrupter->setFaction(14);
-                TCorrupter->SetMaxHealth(832750);
-            }
-            if (Creature* CorrupterSpeaker = player->SummonCreature(1, player->GetPositionX(), player->GetPositionY(), player->GetPositionZ()-1, 0, TEMPSUMMON_TIMED_DESPAWN, 15000))
-            {
-                CorrupterSpeaker->SetName("Twilight Corrupter");
-                CorrupterSpeaker->SetVisible(true);
-                CorrupterSpeaker->AI()->Talk(YELL_TWILIGHTCORRUPTOR_RESPAWN, player->GetGUID());
-            }
-        }
-        return false;
-    };
-};
-
-/*######
-# boss_twilight_corrupter
-######*/
-
-enum TwilightCorrupter
-{
-    SPELL_SOUL_CORRUPTION           = 25805,
-    SPELL_CREATURE_OF_NIGHTMARE     = 25806,
-    SPELL_LEVEL_UP                  = 24312
-};
-
-class boss_twilight_corrupter : public CreatureScript
-{
-public:
-    boss_twilight_corrupter() : CreatureScript("boss_twilight_corrupter") { }
-
-    CreatureAI* GetAI(Creature* creature) const OVERRIDE
-    {
-        return new boss_twilight_corrupterAI(creature);
-    }
-
-    struct boss_twilight_corrupterAI : public ScriptedAI
-    {
-        boss_twilight_corrupterAI(Creature* creature) : ScriptedAI(creature) { }
-
-        uint32 SoulCorruption_Timer;
-        uint32 CreatureOfNightmare_Timer;
-        uint8 KillCount;
-
-        void Reset() OVERRIDE
-        {
-            SoulCorruption_Timer = 15000;
-            CreatureOfNightmare_Timer = 30000;
-            KillCount = 0;
-        }
-        void EnterCombat(Unit* /*who*/) OVERRIDE
-        {
-            Talk(YELL_TWILIGHTCORRUPTOR_AGGRO);
-        }
-
-        void KilledUnit(Unit* victim) OVERRIDE
-        {
-            if (victim->GetTypeId() == TYPEID_PLAYER)
-            {
-                ++KillCount;
-                Talk(YELL_TWILIGHTCORRUPTOR_KILL, victim->GetGUID());
-
-                if (KillCount == 3)
-                {
-                    DoCast(me, SPELL_LEVEL_UP, true);
-                    KillCount = 0;
-                }
-            }
-        }
-
-        void UpdateAI(uint32 diff) OVERRIDE
-        {
-            if (!UpdateVictim())
-                return;
-            if (SoulCorruption_Timer <= diff)
-            {
-                DoCastVictim(SPELL_SOUL_CORRUPTION);
-                SoulCorruption_Timer = rand()%4000+15000; //gotta confirm Timers
-            } else SoulCorruption_Timer-=diff;
-
-            if (CreatureOfNightmare_Timer <= diff)
-            {
-                DoCastVictim(SPELL_CREATURE_OF_NIGHTMARE);
-                CreatureOfNightmare_Timer = 45000; //gotta confirm Timers
-            } else CreatureOfNightmare_Timer-=diff;
-            DoMeleeAttackIfReady();
-        };
-    };
-};
-
 void AddSC_duskwood()
 {
-    new boss_twilight_corrupter();
-    new at_twilight_grove();
+
 }
