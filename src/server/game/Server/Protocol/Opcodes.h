1fcde44774f2b86bd2b1701857231aa6004c2136
projectcore <arcaniafr@gmail.com>
2013-12-30 20:37:12 +0100

    Update opcode SMSG_TRAINER_LIST

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -1345,7 +1345,7 @@ enum Opcodes
     SMSG_TRADE_STATUS_EXTENDED                               = 0x0,
     SMSG_TRAINER_BUY_FAILED                                  = 0x0,
     SMSG_TRAINER_BUY_SUCCEEDED                               = 0x0,
-    SMSG_TRAINER_LIST                                        = 0x0,
+    SMSG_TRAINER_LIST                                        = 0x026D, // 5.4.2 17688
     SMSG_TRANSFER_ABORTED                                    = 0x0,
     SMSG_TRANSFER_PENDING                                    = 0x0819, // 5.4.2 17688
     SMSG_TRIGGER_CINEMATIC                                   = 0x1320, // 5.4.2 17688
