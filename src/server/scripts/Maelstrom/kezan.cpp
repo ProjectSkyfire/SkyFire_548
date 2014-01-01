c8889ea25a66b0c5e1175e900d7f9d6bffa6b9b3
SkyFire <admin@projectskyfire.org>
2014-01-01 13:27:09 -0600

    Update copyrights to 2014. Making Uniform. Part 2
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/scripts/Maelstrom/kezan.cpp
+++ src/server/scripts/Maelstrom/kezan.cpp
@@ -1,4 +1,8 @@
 /*
+ * Copyright (C) 2011-2014 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation; either version 3 of the License, or (at your
