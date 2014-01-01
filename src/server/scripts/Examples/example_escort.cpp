c44be09f196e38fc9f168f096c2bdf08bd134514
SkyFire <admin@projectskyfire.org>
2014-01-01 14:24:35 -0600

    Update copyrights to 2014. Making Uniform. Remaining scripts Part 4
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/scripts/Examples/example_escort.cpp
+++ src/server/scripts/Examples/example_escort.cpp
@@ -2,6 +2,7 @@
  * Copyright (C) 2011-2014 Project SkyFire <http://www.projectskyfire.org/>
  * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2006-2014 ScriptDev2 <https://github.com/scriptdev2/scriptdev2/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -16,7 +17,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
+ 
 /* ScriptData
 SDName: Example_Escort
 SD%Complete: 100
--- src/server/scripts/Outland/TempestKeep/Eye/the_eye.h
+++ src/server/scripts/Outland/TempestKeep/Eye/the_eye.h
@@ -1,10 +1,12 @@
 /*
- * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2011-2014 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2006-2014 ScriptDev2 <https://github.com/scriptdev2/scriptdev2/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
+ * Free Software Foundation; either version 3 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
