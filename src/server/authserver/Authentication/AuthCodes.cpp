86c023736af55c8408162d889dbe143c38dc69c2
Sovak <Sovak007@gmail.com>
2014-02-22 11:44:13 +0100

    Updated build to 17930
    Signed-off-by: Sovak <Sovak007@gmail.com>

--- src/server/authserver/Authentication/AuthCodes.cpp
+++ src/server/authserver/Authentication/AuthCodes.cpp
@@ -24,7 +24,8 @@ namespace AuthHelper
 {
     static RealmBuildInfo const PostBcAcceptedClientBuilds[] =
     {
-        {17898, 5, 4, 7, ' '},        // Last version MoP
+        {17930, 5, 4, 7, ' '},        // Last version MoP
+        {17898, 5, 4, 7, ' '},
         {17688, 5, 4, 2, 'a'},
         {17658, 5, 4, 2, ' '},
         {17538, 5, 4, 1, ' '},
--- src/server/game/Entities/Object/Updates/UpdateFields.h
+++ src/server/game/Entities/Object/Updates/UpdateFields.h
@@ -20,7 +20,7 @@
 #ifndef _UPDATEFIELDS_H
 #define _UPDATEFIELDS_H
 
-// Auto generated for version 5, 4, 1, 17538
+// Auto generated for version 5.4.7 17930
 
 enum EObjectFields
 {
