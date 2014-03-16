2679de35461de6faad3665fcb81d65befd0a81b0
Rawaho <neiko.ambrose@gmail.com>
2014-03-16 23:44:29 +1300

    CMSG_BUY_BANK_SLOT structure and opcode by @Raknar

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -98,7 +98,7 @@ enum Opcodes
    CMSG_BUG                                                 = 0x0,
    CMSG_BUSY_TRADE                                          = 0x0,
    CMSG_BUYBACK_ITEM                                        = 0x0,
-   CMSG_BUY_BANK_SLOT                                       = 0x0,
+   CMSG_BUY_BANK_SLOT                                       = 0x00FE, // 5.4.7 18019
    CMSG_BUY_ITEM                                            = 0x1077, // 5.4.7 18019
    CMSG_CALENDAR_ADD_EVENT                                  = 0x0,
    CMSG_CALENDAR_ARENA_TEAM                                 = 0x0,
