7b99d800605a41949d29e3bcb40ecabec47282af
SkyFire <admin@projectskyfire.org>
2014-04-06 10:25:42 -0500

    Cleanup tabs
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/game/Handlers/QuestHandler.cpp
+++ src/server/game/Handlers/QuestHandler.cpp
@@ -36,9 +36,9 @@
 void WorldSession::HandleQuestgiverStatusQueryOpcode(WorldPacket& recvData)
 {
     ObjectGuid guid;
-	
-	guid[2] = recvData.ReadBit();
-	guid[7] = recvData.ReadBit();
+ 
+    guid[2] = recvData.ReadBit();
+    guid[7] = recvData.ReadBit();
     guid[3] = recvData.ReadBit();
     guid[1] = recvData.ReadBit();
     guid[6] = recvData.ReadBit();
@@ -101,8 +101,8 @@ void WorldSession::HandleQuestgiverHelloOpcode(WorldPacket& recvData)
 {
     ObjectGuid guid;
 
-	guid[2] = recvData.ReadBit();
-	guid[3] = recvData.ReadBit();
+    guid[2] = recvData.ReadBit();
+    guid[3] = recvData.ReadBit();
     guid[5] = recvData.ReadBit();
     guid[4] = recvData.ReadBit();
     guid[7] = recvData.ReadBit();
