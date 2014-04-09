aa2210259ecf00b401af426554c000773034c88a
gegge6265 <luigibalotta@live.it>
2014-04-09 14:09:05 +0200

    Update pet_levelstats_data.sql

--- sql/updates/world/pet_levelstats_data.sql
+++ sql/updates/world/pet_levelstats_data.sql
@@ -1,3 +1,17 @@
+DROP TABLE IF EXIST `pet_levelstats`;
+CREATE TABLE `pet_levelstats` (
+  `creature_entry` mediumint(8) unsigned NOT NULL,
+  `level` tinyint(3) unsigned NOT NULL,
+  `hp` smallint(5) unsigned NOT NULL,
+  `mana` smallint(5) unsigned NOT NULL,
+  `armor` int(10) unsigned NOT NULL DEFAULT '0',
+  `str` smallint(5) unsigned NOT NULL,
+  `agi` smallint(5) unsigned NOT NULL,
+  `sta` smallint(5) unsigned NOT NULL,
+  `inte` smallint(5) unsigned NOT NULL,
+  `spi` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`creature_entry`,`level`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 PACK_KEYS=0 COMMENT='pet levels stats.';
 INSERT INTO `pet_levelstats` (`creature_entry`, `level`, `hp`, `mana`, `armor`, `str`, `agi`, `sta`, `inte`, `spi`) VALUES
 (1, 1, 40, 1, 20, 12, 14, 1, 1, 25),
 (1, 2, 49, 1, 40, 14, 15, 2, 1, 26),
