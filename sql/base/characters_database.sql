9f058fd0b3dcb923e1fec10ce73ababc1d66b114
Palabola <hodobalazs@gmail.com>
2014-04-18 07:40:10 +0200

    Update characters DB to support reforge and transmog changes.

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -2206,6 +2206,8 @@ CREATE TABLE `item_instance` (
   `flags` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `enchantments` text NOT NULL,
   `randomPropertyId` smallint(5) NOT NULL DEFAULT '0',
+  `reforgeID` int (10) NOT NULL DEFAULT '0',,
+  `transmogrifyId` int (10) NOT NULL DEFAULT '0',,
   `durability` smallint(5) unsigned NOT NULL DEFAULT '0',
   `playedTime` int(10) unsigned NOT NULL DEFAULT '0',
   `text` text,
