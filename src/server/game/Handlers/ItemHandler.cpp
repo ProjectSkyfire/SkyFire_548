00fb51bb4aa8f792f42a4c76f89da40e928000bf
SkyFire <admin@projectskyfire.org>
2014-04-19 17:39:30 -0500

    Reverted changes by https://github.com/ProjectSkyfire/SkyFire_5xx/commit/cef781a9f602dcd512de6fdc9f96164f8c20e1f2 Will rewrite this. Fixes opening of vendors and trainers.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/game/Handlers/ItemHandler.cpp
+++ src/server/game/Handlers/ItemHandler.cpp
@@ -780,8 +780,13 @@ void WorldSession::SendListInventory(uint64 vendorGuid)
             itemsData << uint32(vendorItem->Type);                  // 1 is items, 2 is currency
             itemsData << uint32(itemTemplate->BuyCount);
 
-	    if (vendorItem->ExtendedCost != 0)
-		itemsData << uint32(vendorItem->ExtendedCost);
+	    if (vendorItem->ExtendedCost)
+            {
+                hasExtendedCost[slot] = true;
+                itemsData << uint32(vendorItem->ExtendedCost);
+            }
+            else
+                hasExtendedCost[slot] = false;
 
             itemsData << uint32(price);
             itemsData << uint32(vendorItem->item);
