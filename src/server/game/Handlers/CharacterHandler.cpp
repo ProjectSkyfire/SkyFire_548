c8a8b18c09739010f5cfd8fc5a511ae68d6010da
Rawaho <neiko.ambrose@gmail.com>
2014-05-22 15:51:19 +1200

    Core/PacketIO: Updated CMSG_CHAR_CREATE structure and opcode. Updated SMSG_CHAR_CREATE opcode. Updated CMSG/SMSG_RANDOMIZE_CHAR_NAME opcodes. Updated CMSG_CHAR_DELETE structure and opcode. Updated SMSG_CHAR_DELETE opcode.

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -299,8 +299,8 @@ void WorldSession::HandleCharCreateOpcode(WorldPacket& recvData)
 {
     uint8 hairStyle, face, facialHair, hairColor, race_, class_, skin, gender, outfitId;
 
-    recvData >> outfitId >> facialHair >> skin >> race_;
-    recvData >> hairStyle >> class_ >> face >> gender >> hairColor;
+    recvData >> outfitId >> hairStyle >> class_ >> skin;
+    recvData >> face >> race_ >> facialHair >> gender >> hairColor;
 
     uint32 nameLength = recvData.ReadBits(6);
     uint8 unk = recvData.ReadBit();
@@ -730,22 +730,22 @@ void WorldSession::HandleCharDeleteOpcode(WorldPacket& recvData)
 {
     ObjectGuid guid;
 
-    guid[6] = recvData.ReadBit();
-    guid[4] = recvData.ReadBit();
-    guid[5] = recvData.ReadBit();
     guid[1] = recvData.ReadBit();
-    guid[7] = recvData.ReadBit();
     guid[3] = recvData.ReadBit();
     guid[2] = recvData.ReadBit();
+    guid[7] = recvData.ReadBit();
+    guid[4] = recvData.ReadBit();
+    guid[6] = recvData.ReadBit();
     guid[0] = recvData.ReadBit();
+    guid[5] = recvData.ReadBit();
 
+    recvData.ReadByteSeq(guid[7]);
     recvData.ReadByteSeq(guid[1]);
-    recvData.ReadByteSeq(guid[2]);
+    recvData.ReadByteSeq(guid[6]);
+    recvData.ReadByteSeq(guid[0]);
     recvData.ReadByteSeq(guid[3]);
     recvData.ReadByteSeq(guid[4]);
-    recvData.ReadByteSeq(guid[0]);
-    recvData.ReadByteSeq(guid[7]);
-    recvData.ReadByteSeq(guid[6]);
+    recvData.ReadByteSeq(guid[2]);
     recvData.ReadByteSeq(guid[5]);
 
     TC_LOG_DEBUG("network", "Character (Guid: %u) deleted", GUID_LOPART(guid));
