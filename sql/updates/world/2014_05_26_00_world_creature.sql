421bbc3c3efc4d0513311a6ee31d0877c710a7ad
AriDEV <aridev666@gmail.com>
2014-05-26 02:35:00 +0200

    Database/Spawn: Added More Spawns
    -- Shado-Pan Wallwatcher
    -- Golden Lotus Guard
    -- Dreadspinner Tender
    -- Sungraze Mushan
    -- Mutated Silkmoth <Brood of Mothallus>
    -- Grainhunter Hawk
    -- Black Bengal Goat <Brood of Mothallus>
    -- Halfhill Villager
    
    - [ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2014_05_26_00_world_creature.sql
@@ -0,0 +1,72 @@
+-- Shado-Pan Wallwatcher (SPAWN) -- GUID GAPS TAKEN FROM LATEST SKYFIREDB
+-- Golden Lotus Guard
+-- Dreadspinner Tender
+-- Sungraze Mushan
+DELETE FROM creature WHERE guid IN (1108, 1109, 1110, 1111, 1112, 1113, 1114, 1115, 1116, 1117, 1118, 1119, 1120, 1121, 1122, 1123, 1124, 1125, 1126, 1127, 1128);
+INSERT INTO creature (guid, id, map, spawnMask, phaseMask, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, MovementType) VALUES
+(1108, 64263, 870, 1, 1, 976.914, 2179.79, 363.145, 0.942363, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1109, 64263, 870, 1, 1, 938.75, 2205.66, 363.228, 3.52203, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1110, 64263, 870, 1, 1, 942.811, 2202.87, 363.228, 3.30379, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1111, 64263, 870, 1, 1, 948.151, 2191.61, 363.228, 3.11017, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1112, 64263, 870, 1, 1, 931.002, 2171.05, 362.904, 2.16315, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1113, 64263, 870, 1, 1, 928.952, 2201.93, 363.145, 2.92557, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1114, 64263, 870, 1, 1, 971.055, 2186.81, 363.228, 0.0892947, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1115, 64263, 870, 1, 1, 985.214, 2203.58, 362.821, 1.69939, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1116, 64263, 870, 1, 1, 985.019, 2197.37, 362.821, 1.40254, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1117, 64263, 870, 1, 1, 455.217, 2167.29, 352.295, 1.50359, 120, 0, 0), -- Shado-Pan Wallwatcher
+(1118, 63939, 870, 1, 1, 1257.48, 1705.92, 359.062, 1.88665, 120, 0, 0), -- Golden Lotus Guard
+(1119, 63939, 870, 1, 1, 1248.74, 1739.47, 364.599, 2.24911, 120, 0, 0), -- Golden Lotus Guard
+(1120, 63939, 870, 1, 1, 1242.29, 1735.17, 364.599, 2.04913, 120, 0, 0), -- Golden Lotus Guard
+(1121, 63939, 870, 1, 1, 1254.99, 1773, 346.28, 4.02518, 120, 0, 0), -- Golden Lotus Guard
+(1122, 63939, 870, 1, 1, 1213.05, 1748.39, 364.672, 0.924759, 120, 0, 0), -- Golden Lotus Guard
+(1123, 61981, 870, 1, 1, 429.264, 2282.42, 238.999, 5.84805, 120, 0, 0), -- Dreadspinner Tender
+(1124, 61981, 870, 1, 1, 398.886, 2310.51, 237.04, 3.49397, 120, 0, 0), -- Dreadspinner Tender
+(1125, 61981, 870, 1, 1, 386.186, 2479.44, 220.844, 5.30411, 120, 0, 0), -- Dreadspinner Tender
+(1126, 58893, 870, 1, 1, -898.163, 433.643, 155.704, 4.94023, 120, 0, 0), -- Sungraze Mushan
+(1127, 58893, 870, 1, 1, -908.755, 431.604, 155.954, 4.82155, 120, 0, 0), -- Sungraze Mushan
+(1128, 58893, 870, 1, 1, -901.272, 422.894, 155.045, 4.87771, 120, 0, 0); -- Sungraze Mushan
+
+-- Mutated Silkmoth <Brood of Mothallus> (SPAWN) -- GUID GAPS TAKEN FROM LATEST SKYFIREDB
+-- Grainhunter Hawk
+DELETE FROM creature WHERE guid IN (1129, 1130, 1131, 1132, 1133, 1134, 1135, 1136, 1137, 1138, 1139, 1140, 1141, 1142, 1143, 1144, 1145, 1146, 1147, 1148, 1149, 1150, 1151, 1152);
+INSERT INTO creature (guid, id, map, spawnMask, phaseMask, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, MovementType) VALUES
+(1129, 57422, 870, 1, 1, -361.447, 52.0921, 171.97, 0.608943, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1130, 57422, 870, 1, 1, -297.966, 34.6178, 164.562, 1.86796, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1131, 57422, 870, 1, 1, -321.107, 32.1473, 168.79, 2.1373, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1132, 57422, 870, 1, 1, -302.823, 163.1, 185.585, 4.24835, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1133, 57422, 870, 1, 1, -405.431, 52.0452, 165.577, 6.26731, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1134, 57422, 870, 1, 1, -313.026, 180.265, 168.135, 4.14717, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1135, 57422, 870, 1, 1, -389.273, 116.951, 189.904, 0.277047, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1136, 57422, 870, 1, 1, -382.593, 134.301, 158.42, 0.536412, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1137, 57422, 870, 1, 1, -461.154, 119.627, 172.642, 3.70797, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1138, 57422, 870, 1, 1, -516.025, 175.964, 173.113, 2.75484, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1139, 57422, 870, 1, 1, -303.074, 66.4815, 171.07, 5.79639, 120, 5, 1), -- Mutated Silkmoth <Brood of Mothallus>
+(1140, 57422, 870, 1, 1, -382.337, 73.7135, 158.572, 2.07694, 120, 0, 0), -- Mutated Silkmoth <Brood of Mothallus>
+(1141, 57422, 870, 1, 1, -326.746, 71.5208, 158.547, 2.73018, 120, 0, 0), -- Mutated Silkmoth <Brood of Mothallus>
+(1142, 57422, 870, 1, 1, -503.439, 182.747, 162.114, 2.81803, 120, 0, 0), -- Mutated Silkmoth <Brood of Mothallus>
+(1143, 57422, 870, 1, 1, -478.418, 87.7704, 162.071, 4.21526, 120, 0, 0), -- Mutated Silkmoth <Brood of Mothallus>
+(1144, 57215, 870, 1, 1, -689.62, 353.867, 150.446, 5.93773, 120, 0, 0), -- Grainhunter Hawk
+(1145, 57215, 870, 1, 1, -754.957, 403.016, 136.193, 3.63029, 120, 0, 0), -- Grainhunter Hawk
+(1146, 57215, 870, 1, 1, -765.099, 449.953, 135.883, 3.63029, 120, 0, 0), -- Grainhunter Hawk
+(1147, 57215, 870, 1, 1, -755.049, 526.536, 135.878, 3.28122, 120, 0, 0), -- Grainhunter Hawk
+(1148, 57215, 870, 1, 1, -746.783, 511.469, 137.227, 3.56047, 120, 0, 0), -- Grainhunter Hawk
+(1149, 57215, 870, 1, 1, -705.173, 498.141, 152.122, 4.48588, 120, 0, 0), -- Grainhunter Hawk
+(1150, 57215, 870, 1, 1, -693.701, 612.759, 141.477, 2.32129, 120, 0, 0), -- Grainhunter Hawk
+(1151, 57215, 870, 1, 1, -708.309, 615.144, 135.749, 2.32129, 120, 0, 0), -- Grainhunter Hawk
+(1152, 57215, 870, 1, 1, -691.551, 573.463, 155.315, 4.64282, 120, 0, 0); -- Grainhunter Hawk
+
+-- Black Bengal Goat <Brood of Mothallus> (SPAWN) -- GUID GAPS TAKEN FROM LATEST SKYFIREDB
+-- Halfhill Villager
+DELETE FROM creature WHERE guid IN (1153, 1154, 1155, 1156, 1157, 1158, 1159, 1160, 1161, 1162, 1163);
+INSERT INTO creature (guid, id, map, spawnMask, phaseMask, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, MovementType) VALUES
+(1153, 58448, 870, 1, 1, 1618.75, 1060.09, 487.1, 3.3411, 120, 0, 0), -- Black Bengal Goat
+(1154, 58448, 870, 1, 1, 1594.2, 1078.56, 477.228, 0.613504, 120, 0, 0), -- Black Bengal Goat
+(1155, 58448, 870, 1, 1, 1335.24, 1541.47, 416.502, 4.39823, 120, 0, 0), -- Black Bengal Goat
+(1156, 58448, 870, 1, 1, 1412.46, 1579.14, 376.17, 3.35103, 120, 0, 0), -- Black Bengal Goat
+(1157, 58448, 870, 1, 1, 1205.24, 1732.41, 374.184, 3.12414, 120, 0, 0), -- Black Bengal Goat
+(1158, 58448, 870, 1, 1, 1196.51, 1749.78, 357.155, 3.9968, 120, 0, 0), -- Black Bengal Goat
+(1159, 59644, 870, 1, 1, -286.278, 500.405, 172.195, 2.70526, 120, 0, 0), -- Halfhill Villager
+(1160, 59644, 870, 1, 1, -264.908, 463.533, 166.282, 4.24115, 120, 0, 0), -- Halfhill Villager
+(1161, 59644, 870, 1, 1, -261.863, 461.788, 166.012, 2.75762, 120, 0, 0), -- Halfhill Villager
+(1162, 59644, 870, 1, 1, -179.951, 483.396, 187.553, 6.16001, 120, 0, 0), -- Halfhill Villager
+(1163, 59644, 870, 1, 1, -174.793, 436.564, 167.976, 1.19485, 120, 0, 0); -- Halfhill Villager
