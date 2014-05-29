3df5b9c6b2c7309942fe4c5f0e653b9e2a80385e
AriDEV <aridev666@gmail.com>
2014-05-29 22:38:38 +0200

    DB/GameTeleport: Added Peaks of Serenity to DB.
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2014_05_29_00_world_game_tele.sql
@@ -0,0 +1,3 @@
+DELETE FROM game_tele WHERE id=1524;
+INSERT INTO game_tele (id, position_x, position_y, position_z, orientation, map, name) VALUES
+(1524, 3826.87, 1795.74, 950.352, 0.299085, 870, 'PeakOfSerenity');
