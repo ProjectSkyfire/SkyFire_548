f88e943701bb924a72928ffdc27c132c906c6d57
AriDEV <aridev666@gmail.com>
2014-06-25 13:27:20 +0200

    Database/CreatureAddonTemplate: Updated Auras Spells that where merged together so the server could not read it correctly. Fixed DB Errors.
    -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2014_06_25_07_world_creature_template_addon.sql
@@ -0,0 +1,6 @@
+UPDATE `creature_template_addon` SET `auras`='29266 28559' WHERE `entry`=55464; -- NPC: Hozen Corpse; AURA: Permanent Feign Death, Untrackable;
+UPDATE `creature_template_addon` SET `auras`='29266 28559' WHERE `entry`=58533; -- NPC: Dustback Mushan; AURA: Permanent Feign Death, Untrackable;
+UPDATE `creature_template_addon` SET `auras`='29266 28559' WHERE `entry`=61806; -- NPC: Liu of the Thousand Blows; AURA: Permanent Feign Death, Untrackable;
+UPDATE `creature_template_addon` SET `auras`='29266 28559' WHERE `entry`=61808; -- NPC: Shado-Master Zhiyao; AURA: Permanent Feign Death, Untrackable;
+UPDATE `creature_template_addon` SET `auras`='29266 28559' WHERE `entry`=61810; -- NPC: Shiya Boldblade; AURA: Permanent Feign Death, Untrackable;
+UPDATE `creature_template_addon` SET `auras`='131531 8279' WHERE `entry`=66928; -- NPC: Sha Shooter; AURA: Sha Shooter Spawn [DNT], Stealth Detection;
