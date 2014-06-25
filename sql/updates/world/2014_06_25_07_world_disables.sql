fe0d4be03f4dab06df690f41a38aa99647827460
AriDEV <aridev666@gmail.com>
2014-06-25 14:37:42 +0200

    Database/Disables: Added Some Quests to Disables, Should Fix +- 50 Rows of DB Errors.
    -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2014_06_25_07_world_disables.sql
@@ -0,0 +1,107 @@
+DELETE FROM disables WHERE sourceType=1 AND entry=4734;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 4734, 'Disable Quest 4734: Egg Freezing');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=2969;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 2969, 'Disable Quest 2969: Freedom for All Creatures');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=5383;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 5383, 'Disable Quest 5383: Krastinov''s Bag of Horrors');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=30520;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 30520, 'Disable Quest 30520: Tummy Trouble');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9942;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9942, 'Disable Quest 9942: Tracking Down the Culprits');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9657;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9657, 'Disable Quest 9657: Deprecated: Offensive Channeling');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=29259;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 29259, 'Disable Quest 29259: [DEPRECATED] Hide and Seek');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=328;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 328, 'Disable Quest 328: The Hidden Key');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=7962;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 7962, 'Disable Quest 7962: [DEPRECATED] Wabbit Pelts');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=25076;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 25076, 'Disable Quest 25076: Taming the Beast');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=30965;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 30965, 'Disable Quest 30965: Herding Cats');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=498;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 498, 'Disable Quest 498: [DEPRECATED] The Rescue');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=503;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 503, 'Disable Quest 503: [DEPRECATED] Gol''dir');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=613;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 613, 'Disable Quest 613: Cracking Maury''s Foot');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9655;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9655, 'Disable Quest 9655: Deprecated: Healing Amplification');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9347;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9347, 'Disable Quest 9347: Voidwalkers Gone Wild');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9656;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9656, 'Disable Quest 9656: Deprecated: Lethality First');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=14000;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 14000, 'Disable Quest 14000: Hunt or Be Hunted');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=14427;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 14427, 'Disable Quest 14427: Item Quest');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=11934;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 11934, 'Disable Quest 11934: Unlocking Your Potential [PH]');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9411;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9411, 'Disable Quest 9411: <UNUSED>Infused Bandages');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=5511;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 5511, 'Disable Quest 5511: The Key to Scholomance');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9481;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9481, 'Disable Quest 9481: <UNUSED>Light Consumable');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=31099;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 31099, 'Disable Quest 31099: Klaxx Attack');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=3825;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 3825, 'Disable Quest 3825: Ogre Head On A Stick = Party');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9614;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9614, 'Disable Quest 9614: Deprecated: Reforge a Short Blade');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=29046;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 29046, 'Disable Quest 29046: Chef''s Challenge: Steelback');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=2359;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 2359, 'Disable Quest 2359: Klaven''s Tower');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=2841;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 2841, 'Disable Quest 2841: Rig Wars');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=2059;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 2059, 'Disable Quest 2059: <UNUSED>');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=3201;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 3201, 'Disable Quest 3201: At Last!');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=3628;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 3628, 'Disable Quest 3628: You Are Rakh''likh, Demon');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=3802;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 3802, 'Disable Quest 3802: Dark Iron Legacy');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9976;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9976, 'Disable Quest 9976: Primal Magic');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=5505;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 5505, 'Disable Quest 5505: The Key to Scholomance');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=26151;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 26151, 'Disable Quest 26151: Dark Letter');
