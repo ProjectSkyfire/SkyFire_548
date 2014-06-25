c12d9ee475ae215769ed115d7ab6426ef4d5b8f8
AriDEV <aridev666@gmail.com>
2014-06-25 22:42:14 +0200

    Database/Disables: Added Some More Quests to Disables, Should Fix +- 70 Rows of DB Errors.
    -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2014_06_25_09_world_disables.sql
@@ -0,0 +1,96 @@
+
+DELETE FROM disables WHERE sourceType=1 AND entry=7521;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 7521, 'Disable Quest 7521: Thunderaan the Windseeker');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=7522;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 7522, 'Disable Quest 7522: Examine the Vessel');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9459;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9459, 'Disable Quest 9459: <UNUSED>Anti-crit Potion');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9482;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9482, 'Disable Quest 9482: <UNUSED>Nature Resist Potion');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9478;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9478, 'Disable Quest 9478: <UNUSED>Portable Healing Font');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=29048;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 29048, 'Disable Quest 29048: Chef''s Challenge: Grumpfin');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=10029;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 10029, 'Disable Quest 10029: The Spirits Are Calling');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=12001;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 12001, 'Disable Quest 12001: zzOLDHunting Tarot: Lesser Beasts');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=10244;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 10244, 'Disable Quest 10244: R.T.F.R.C.M.');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9975;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9975, 'Disable Quest 9975: Primal Magic');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=32375;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 32375, 'Disable Quest 32375: A Dash of That');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=10133;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 10133, 'Disable Quest 10133: Mission: Kill the Messenger');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=10135;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 10135, 'Disable Quest 10135: Mission: Be the Messenger');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=10147;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 10147, 'Disable Quest 10147: Mission: Kill the Messenger');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=10148;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 10148, 'Disable Quest 10148: Mission: Be the Messenger');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9650;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9650, 'Disable Quest 9650: Deprecated: Magical Augmentation');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9654;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9654, 'Disable Quest 9654: Deprecated: A Well Rounded Defense');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9479;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9479, 'Disable Quest 9479: <UNUSED>Glorious Standard of the Alliance');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9308;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9308, 'Disable Quest 9308: <DEPRECATED>Blood Elf Remains?');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9660;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9660, 'Disable Quest 9660: Deprecated: Lethality First');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=14441;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 14441, 'Disable Quest 14441: Garrosh''s Autograph');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=260;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 260, 'Disable Quest 260: <UNUSED>');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=316;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 316, 'Disable Quest 316: <UNUSED 1>');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=6201;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 6201, 'Disable Quest 6201: <UNUSED> The Legacy of the Ashbringer');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=12626;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 12626, 'Disable Quest 12626: Company of the Damned');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=9231;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 9231, 'Disable Quest 9231: reuse');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=912;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 912, 'Disable Quest 912: Stonesplinter Trogg Disguise');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=2058;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 2058, 'Disable Quest 2058: <UNUSED>');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=3111;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 3111, 'Disable Quest 3111: <UNUSED> [DEPRECATED]');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=29550;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 29550, 'Disable Quest 29550: Saving Snowpuff');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=4299;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 4299, 'Disable Quest 4299: <NYI> <TXT>The Tomb of the Seven');
+
+DELETE FROM disables WHERE sourceType=1 AND entry=7561;
+INSERT INTO disables (sourceType, entry, comment) VALUES (1, 7561, 'Disable Quest 7561: Rise, Thunderfury!');
