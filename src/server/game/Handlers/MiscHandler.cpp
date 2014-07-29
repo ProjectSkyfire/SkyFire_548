b439f8b2275ee7513fddd9a8adab091c04e72499
AriDEV <aridev666@gmail.com>
2014-07-30 01:27:19 +0200

    Server/PacketIO: Sending SMSG_SET_DUNGEON_DIFFICULTY After Recv CMSG_SET_DUNGEON_DIFFICULTY When Not In Group. *Now u should be able to change Difficulty even if u not in Group.
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -1828,6 +1828,7 @@ void WorldSession::HandleSetDungeonDifficultyOpcode(WorldPacket& recvData)
     {
         _player->ResetInstances(INSTANCE_RESET_CHANGE_DIFFICULTY, false);
         _player->SetDungeonDifficulty(Difficulty(mode));
+        _player->SendDungeonDifficulty(true);
     }
 }
 
