b1929529f74012254fc38ceda3c2e1d8f499a102
AriDEV <aridev666@gmail.com>
2014-08-05 04:08:47 +0200

    Tools/Extractor: Updated Map Extractor With Shaurens Extractor Update For MoP, Special Thanks To @Shauren. Minor Thanks to Faelon For Some Minor Changes and CleanUP.
    Close #190
    Close #304
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- src/tools/map_extractor/dbcfile.cpp
+++ src/tools/map_extractor/dbcfile.cpp
@@ -33,54 +33,34 @@ bool DBCFile::open()
 
     DWORD readBytes = 0;
     SFileReadFile(_file, header, 4, &readBytes, NULL);
-    if (readBytes != 4)
-	{                                         // Number of records
+    if (readBytes != 4)                                         // Number of records
         return false;
-		printf("Error at 1", _file);
-	}
 
     if (header[0] != 'W' || header[1] != 'D' || header[2] != 'B' || header[3] != 'C')
-	{
         return false;
-		printf("Error at 2", _file);
-	}
+
     SFileReadFile(_file, &na, 4, &readBytes, NULL);
-    if (readBytes != 4)
-	{                                         // Number of records
+    if (readBytes != 4)                                         // Number of records
         return false;
-		printf("Error at 3", _file);
-	}
 
     SFileReadFile(_file, &nb, 4, &readBytes, NULL);
-    if (readBytes != 4)
-	{                                         // Number of records
+    if (readBytes != 4)                                         // Number of fields
         return false;
-		printf("Error at 4", _file);
-	}
 
     SFileReadFile(_file, &es, 4, &readBytes, NULL);
-    if (readBytes != 4)
-	{                                         // Number of records
+    if (readBytes != 4)                                         // Size of a record
         return false;
-		printf("Error at 5", _file);
-	}
 
     SFileReadFile(_file, &ss, 4, &readBytes, NULL);
-    if (readBytes != 4)
-	{                                         // Number of records
+    if (readBytes != 4)                                         // String size
         return false;
-		printf("Error at 6", _file);
-	}
 
     _recordSize = es;
     _recordCount = na;
     _fieldCount = nb;
     _stringSize = ss;
     if (_fieldCount * 4 != _recordSize)
-    {                                         // Number of records
         return false;
-		printf("Error at 7", _file);
-	}
 
     _data = new unsigned char[_recordSize * _recordCount + _stringSize];
     _stringTable = _data + _recordSize*_recordCount;
@@ -88,10 +68,7 @@ bool DBCFile::open()
     size_t data_size = _recordSize * _recordCount + _stringSize;
     SFileReadFile(_file, _data, data_size, &readBytes, NULL);
     if (readBytes != data_size)
-	{                                         // Number of records
         return false;
-		printf("Error at 8");
-	}
 
     return true;
 }
@@ -130,3 +107,4 @@ DBCFile::Iterator DBCFile::end()
     assert(_data);
     return Iterator(*this, _stringTable);
 }
+
--- src/tools/map_extractor/loadlib/loadlib.h
+++ src/tools/map_extractor/loadlib/loadlib.h
@@ -21,6 +21,8 @@
 #define LOAD_LIB_H
 
 #include "StormLib.h"
+#include <map>
+#include <string>
 
 #ifdef _WIN32
 typedef __int64            int64;
@@ -50,6 +52,8 @@ typedef uint8_t            uint8;
 
 #define FILE_FORMAT_VERSION    18
 
+#pragma pack(push, 1)
+
 union u_map_fcc
 {
     char   fcc_txt[4];
@@ -69,19 +73,39 @@ struct file_MVER
     uint32 ver;
 };
 
+class FileChunk
+{
+public:
+    ~FileChunk();
 
-class FileLoader{
+    uint8* data;
+    uint32 size;
+
+    template<class T>
+    T* As() { return (T*)data; }
+    void parseSubChunks();
+    std::multimap<std::string, FileChunk*> subchunks;
+    FileChunk* GetSubChunk(std::string const& name);
+};
+
+class ChunkedFile{
+public:
     uint8  *data;
     uint32  data_size;
-public:
-    virtual bool prepareLoadedData();
     uint8 *GetData()     {return data;}
     uint32 GetDataSize() {return data_size;}
 
-    file_MVER *version;
-    FileLoader();
-    ~FileLoader();
+    ChunkedFile();
+    virtual ~ChunkedFile();
+    bool prepareLoadedData();
     bool loadFile(HANDLE mpq, char *filename, bool log = true);
-    virtual void free();
+    void free();
+
+    void parseChunks();
+    std::multimap<std::string, FileChunk*> chunks;
+    FileChunk* GetChunk(std::string const& name);
 };
+
+#pragma pack(pop)
+
 #endif
