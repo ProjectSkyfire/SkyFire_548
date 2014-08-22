4bc6502526fa83cbca25b50704a7f22503e6b0c3
AriDEV <aridev666@gmail.com>
2014-08-22 22:53:21 +0200

    DB/World: Game Graveyard Zones Added For Map 870. *Now if u die in Pandaria u will not be ported to Azeroth Graveyard.
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2014_08_22_01_world_game_graveyard_zone.sql
@@ -0,0 +1,134 @@
+-- Jade Forest GY
+DELETE FROM game_graveyard_zone WHERE ghost_zone=5785;
+INSERT INTO game_graveyard_zone (id, ghost_zone, faction) VALUES
+(3168, 5785, 469), -- The Jade Forest, Alliance GY
+(3568, 5785, 0), -- The Jade Forest, Arboretum GY 90+
+(3566, 5785, 0), -- The Jade Forest, Cliff Village GY
+(4150, 5785, 0), -- The Jade Forest, Cliff Village GY
+(4371, 5785, 0), -- The Jade Forest, Cliff Village GY
+(3567, 5785, 0), -- The Jade Forest, Dawnblossom GY
+(3565, 5785, 0), -- The Jade Forest, East Temple GY
+(4298, 5785, 0), -- The Jade Forest, East Temple GY
+(3572, 5785, 0), -- The Jade Forest, Fishing Village GY
+(3564, 5785, 0), -- The Jade Forest, Fox Grove GY
+(3170, 5785, 67), -- The Jade Forest, Horde GY
+(4391, 5785, 67), -- The Jade Forest, Horde GY
+(4402, 5785, 67), -- The Jade Forest, Horde GY
+(3571, 5785, 0), -- The Jade Forest, Mogu Ruins GY
+(3569, 5785, 0), -- The Jade Forest, Spire of Solitude GY
+(3560, 5785, 0), -- The Jade Forest, Swamp GY East
+(3570, 5785, 0); -- The Jade Forest, Training Temple GY
+
+-- Vale of Eternal Blossoms GY
+DELETE FROM game_graveyard_zone WHERE ghost_zone=5840;
+INSERT INTO game_graveyard_zone (id, ghost_zone, faction) VALUES
+(4052, 5840, 0), -- Vale of Eternal Blossoms, Temp GY
+(4276, 5840, 469), -- Vale of Eternal Blossoms - Alliance GY
+(4277, 5840, 67), -- Vale of Eternal Blossoms - Horde GY
+(4278, 5840, 0), -- Vale of Eternal Blossoms - Guo-Lai GY
+(4279, 5840, 0), -- Vale of Eternal Blossoms - Guo-Lai 02 GY
+(4280, 5840, 0), -- Vale of Eternal Blossoms - Great Wall GY
+(4281, 5840, 0), -- Vale of Eternal Blossoms - Mistfall Village GY
+(4356, 5840, 0), -- Vale of Eternal Blossoms - Great Wall GY
+(4407, 5840, 0), -- Vale of Eternal Blossoms - Ancestral Rise GY
+(4575, 5840, 0); -- Vale of Eternal Blossoms - Vale Raid GY
+
+-- Kun-Lai Summit GY
+DELETE FROM game_graveyard_zone WHERE ghost_zone=5841;
+INSERT INTO game_graveyard_zone (id, ghost_zone, faction) VALUES
+(3573, 5841, 0), -- KLS - Binan Village GY
+(3574, 5841, 0), -- KLS - Northern Temple GY
+(3575, 5841, 0), -- KLS - Northern Balloon Village GY
+(3576, 5841, 0), -- KLS - Troll Village GY
+(3578, 5841, 0), -- KLS - Mountain Valley GY
+(3579, 5841, 0), -- KLS - Monkaris GY
+(3580, 5841, 0), -- KLS - Top of the World GY
+(3581, 5841, 0), -- KLS - Shadowpan Hideout GY
+(3582, 5841, 0), -- KLS - West Mountain Valley GY
+(3583, 5841, 0), -- KLS - Shadowpan Battleground GY
+(3590, 5841, 0), -- KLS - Alliance/Horde GY
+(3591, 5841, 0), -- KLS - West Fishing Village GY
+(3593, 5841, 0), -- KLS - Valley of the Kings GY
+(4270, 5841, 0), -- KLS - Valley of the Kings GY
+(4396, 5841, 0), -- KLS - Great Wall GY - Western Span
+(4397, 5841, 0), -- KLS - Great Wall GY - Middle Span
+(4399, 5841, 0), -- KLS - Great Wall GY - Eastern Span
+(4401, 5841, 0); -- KLS - West Mountain Valley GY
+
+-- Townlong Steppes GY
+DELETE FROM game_graveyard_zone WHERE ghost_zone=5842;
+INSERT INTO game_graveyard_zone (id, ghost_zone, faction) VALUES
+(4095, 5842, 0), -- Townlong Steppes, Temp GY
+(4106, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4325, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4326, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4327, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4328, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4337, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4357, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4380, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4381, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4383, 5842, 0), -- Townlong Steppes, Osul Mesa GY
+(4387, 5842, 0); -- Townlong Steppes, Osul Mesa GY
+
+-- Krasarang Wilds GY
+DELETE FROM game_graveyard_zone WHERE ghost_zone=6134;
+INSERT INTO game_graveyard_zone (id, ghost_zone, faction) VALUES
+(4101, 6134, 0), -- Krasarang Wilds - Kea Krak GY
+(4102, 6134, 0), -- Krasarang Wilds - The Deepwild GY
+(4103, 6134, 0), -- Krasarang Wilds - Dojani River GY
+(4104, 6134, 0), -- Krasarang Wilds - The Forbidden Junlge
+(4408, 6134, 0), -- Krasarang Wilds - Narsong Spire - Beach GY
+(4443, 6134, 0); -- Krasarang Wilds - Kea Krak GY
+-- Krasarang Wilds TSGY missing
+
+-- Dread Wastes GY
+DELETE FROM game_graveyard_zone WHERE ghost_zone=6138;
+INSERT INTO game_graveyard_zone (id, ghost_zone, faction) VALUES
+(4107, 6138, 0), -- Dread Wastes, Dread Wastes, GY
+(4299, 6138, 0), -- Dread Wastes, Dread Wastes, GY
+(4300, 6138, 0), -- Dread Wastes, Dread Wastes, GY
+(4301, 6138, 0), -- Dread Wastes, Dread Wastes, GY
+(4302, 6138, 0), -- Dread Wastes, Dread Wastes, GY
+(4303, 6138, 0), -- Dread Wastes, Dread Wastes, GY
+(4304, 6138, 0), -- Dread Wastes, Dread Wastes, GY
+(4305, 6138, 0), -- Dread Wastes, Dread Wastes, GY
+(4390, 6138, 0), -- Dread Wastes, Dread Wastes, GY
+(4405, 6138, 0); -- Dread Wastes, Dread Wastes, Amber Hibernal Teleport Pad
+
+-- Timeless Isle GY
+DELETE FROM game_graveyard_zone WHERE ghost_zone=6757;
+INSERT INTO game_graveyard_zone (id, ghost_zone, faction) VALUES
+(4558, 6757, 469), -- Timeless Isle - Alliance Landing gy
+(4559, 6757, 67), -- Timeless Isle - Horde Landing gy
+(4622, 6757, 0), -- Timeless Isle - Grotto gy
+(4623, 6757, 0), -- Timeless Isle - Ordon Sanc GY
+(4629, 6757, 0), -- Timeless Isle - Three-Breeze/Misty Strand
+(4630, 6757, 0), -- Timeless Isle - Pi'jiu GY
+(4631, 6757, 0), -- Timeless Isle - Celestial Court GY
+(4632, 6757, 0), -- Timeless Isle - Celestial Court GY 2
+(4633, 6757, 0), -- Timeless Isle - Firewalker Ruins GY
+(4634, 6757, 0), -- Timeless Isle - Firewalker's Path Lower
+(4635, 6757, 0); -- Timeless Isle - Firewalker's Path Upper
+
+-- Valley of the Four Winds GY
+DELETE FROM game_graveyard_zone WHERE ghost_zone=8505;
+INSERT INTO game_graveyard_zone (id, ghost_zone, faction) VALUES
+(3253, 8505, 0), -- Valley of the Four Winds, Temp GY
+(4098, 8505, 0), -- Valley of the Four Winds, Fruited Fields GY
+(4099, 8505, 0), -- Valley of the Four Winds, Stoneplow Fields GY
+(4100, 8505, 0), -- Valley of the Four Winds, Thirsty Alley GY
+(4105, 8505, 0), -- Valley of the Four Winds, Zhu Province GY
+(4147, 8505, 0), -- Valley of the Four Winds, Zhu Province GY
+(4148, 8505, 0), -- Valley of the Four Winds, Zhu Province GY
+(4149, 8505, 0), -- Valley of the Four Winds, Zhu Province GY
+(4369, 8505, 0), -- Valley of the Four Winds, Skyrange GY 2
+(4370, 8505, 0), -- Valley of the Four Winds, Skyrange GY 1
+(4389, 8505, 0); -- Valley of the Four Winds, Zhu Province GY
+
+-- Isle of Giants GY
+DELETE FROM game_graveyard_zone WHERE ghost_zone=6661;
+INSERT INTO game_graveyard_zone (id, ghost_zone, faction) VALUES
+(4469, 6661, 0); -- Isle of Giants - GY
+
+
