9dbb220b5c23fb63c2859d1a655a6050708c8a87
AriDEV <aridev666@gmail.com>
2014-08-24 00:33:30 +0200

    DB/Quest: Fixed Q: Orchard-Supplied Hardware (29580)
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2014_08_24_00_world_misc.sql
@@ -0,0 +1,4 @@
+-- Gameobject Loot: Orchard Tool
+DELETE FROM gameobject_loot_template WHERE entry=40278;
+INSERT INTO gameobject_loot_template (entry, item, ChanceOrQuestChance) VALUES
+(40278, 72133, -100); -- Orchard Tool
