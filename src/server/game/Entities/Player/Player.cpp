fbbb8ad80acd88b5ee685eb61907abdf9c40e621
Arci <arciatanthoria@gmail.com>
2014-08-24 12:19:24 +0200

    Player/Talents: fixed secondary spec talents

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -26477,7 +26477,7 @@ void Player::BuildPlayerTalentsInfoData(WorldPacket* data)
         }
 
         data->PutBits(wpos[i], talentCount, 23);
-        *data << uint32(GetTalentSpecialization(GetActiveSpec()));
+        *data << uint32(GetTalentSpecialization(i));
     }
 
     delete[] wpos;
