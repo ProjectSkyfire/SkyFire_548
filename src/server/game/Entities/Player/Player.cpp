7093a81ed3deb7cd4ee01808cd0319759f227bcc
Rawaho <neiko.ambrose@gmail.com>
2014-08-25 19:17:20 +1200

    Core/PacketIO: Updated SMSG_CLEAR_COOLDOWNS structure and opcode.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -26813,29 +26813,29 @@ void Player::SendClearAllCooldowns(Unit* target)
     uint32 spellCount = m_spellCooldowns.size();
     ObjectGuid guid = target ? target->GetGUID() : 0;
 
-    WorldPacket data(SMSG_CLEAR_COOLDOWNS, 4+8+1+spellCount*4);
-    data.WriteBit(guid[6]);
-    data.WriteBit(guid[4]);
-    data.WriteBits(spellCount, 22); // Spell Count
+    WorldPacket data(SMSG_CLEAR_COOLDOWNS, 1 + 8 + 3 + (spellCount * 4));
     data.WriteBit(guid[5]);
-    data.WriteBit(guid[0]);
+    data.WriteBit(guid[6]);
+    data.WriteBit(guid[7]);
     data.WriteBit(guid[3]);
     data.WriteBit(guid[2]);
-    data.WriteBit(guid[7]);
+    data.WriteBits(spellCount, 22); // Spell Count
     data.WriteBit(guid[1]);
-
+    data.WriteBit(guid[0]);
+    data.WriteBit(guid[4]);
     data.FlushBits();
 
-    data.WriteByteSeq(guid[3]);
     data.WriteByteSeq(guid[0]);
     data.WriteByteSeq(guid[1]);
+    data.WriteByteSeq(guid[7]);
     data.WriteByteSeq(guid[4]);
+    data.WriteByteSeq(guid[3]);
     data.WriteByteSeq(guid[5]);
+    data.WriteByteSeq(guid[6]);
+
     for (SpellCooldowns::const_iterator itr = m_spellCooldowns.begin(); itr != m_spellCooldowns.end(); ++itr)
         data << uint32(itr->first); // Spell ID
 
-    data.WriteByteSeq(guid[6]);
-    data.WriteByteSeq(guid[7]);
     data.WriteByteSeq(guid[2]);
 
     SendDirectMessage(&data);
