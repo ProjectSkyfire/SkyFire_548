ed3d8d11800539d9f5e5a40a20b2dbb371b193b5
Zerix <tkalmbach18@gmail.com>
2014-11-08 13:03:32 -0500

    Revert last change. Fucking wrong patch. 5.4.7 is not the supported patch.

--- src/server/game/Handlers/ChatHandler.cpp
+++ src/server/game/Handlers/ChatHandler.cpp
@@ -751,29 +751,29 @@ void WorldSession::HandleTextEmoteOpcode(WorldPacket& recvData)
         return;
     }
 
-	uint32 text_emote, emoteNum;
+    ObjectGuid guid;
+    uint32 text_emote, emoteNum;
 
-	recvData >> text_emote;
-	recvData >> emoteNum;
+    recvData >> text_emote;
+    recvData >> emoteNum;
 
-	ObjectGuid guid;
-	guid[2] = recvData.ReadBit();
-	guid[3] = recvData.ReadBit();
-	guid[0] = recvData.ReadBit();
-	guid[7] = recvData.ReadBit();
-	guid[4] = recvData.ReadBit();
 	guid[6] = recvData.ReadBit();
+	guid[7] = recvData.ReadBit();
+	guid[3] = recvData.ReadBit();
+	guid[2] = recvData.ReadBit();
+	guid[0] = recvData.ReadBit();
 	guid[5] = recvData.ReadBit();
 	guid[1] = recvData.ReadBit();
+	guid[4] = recvData.ReadBit();
 
 	recvData.ReadByteSeq(guid[0]);
-	recvData.ReadByteSeq(guid[6]);
 	recvData.ReadByteSeq(guid[5]);
-	recvData.ReadByteSeq(guid[7]);
-	recvData.ReadByteSeq(guid[3]);
-	recvData.ReadByteSeq(guid[4]);
 	recvData.ReadByteSeq(guid[1]);
+	recvData.ReadByteSeq(guid[4]);
 	recvData.ReadByteSeq(guid[2]);
+	recvData.ReadByteSeq(guid[3]);
+	recvData.ReadByteSeq(guid[7]);
+	recvData.ReadByteSeq(guid[6]);
 
     sScriptMgr->OnPlayerTextEmote(GetPlayer(), text_emote, emoteNum, guid);
 
