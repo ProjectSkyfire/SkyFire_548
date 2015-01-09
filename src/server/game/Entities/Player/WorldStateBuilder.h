03a35f7fc3cadf2a0e21f4ac304d42d88cf0d84c
SkyFire <admin@projectskyfire.org>
2015-01-09 16:33:54 -0600

    Update the copyrights. Hello 2015.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/game/Entities/Player/WorldStateBuilder.h
+++ src/server/game/Entities/Player/WorldStateBuilder.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2014 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2011-2015 Project SkyFire <http://www.projectskyfire.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
--- src/server/scripts/Pandaria/zone_the_veiled_stair.cpp
+++ src/server/scripts/Pandaria/zone_the_veiled_stair.cpp
@@ -1,7 +1,7 @@
 /*
-* Copyright (C) 2011-2014 Project SkyFire <http://www.projectskyfire.org/>
-* Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
-* Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
+* Copyright (C) 2011-2015 Project SkyFire <http://www.projectskyfire.org/>
+* Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
+* Copyright (C) 2005-2015 MaNGOS <http://getmangos.com/>
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
