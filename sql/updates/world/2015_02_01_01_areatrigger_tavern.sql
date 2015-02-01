ac5a49e327d0d9a02dd05456f1a48f3e8a77fd60
AriDEV <aridev666@gmail.com>
2015-02-01 03:23:59 +0100

    SQL: Added Some AreaTrigger Tavern Data

---
+++ sql/updates/world/2015_02_01_01_areatrigger_tavern.sql
@@ -0,0 +1,5 @@
+DELETE FROM `areatrigger_tavern` WHERE `id` IN (7983, 8442, 8579);
+INSERT INTO `areatrigger_tavern` (`id`, `name`) VALUES
+(7983, 'Jade Temple Grounds - Inn'),
+(8442, 'Tavern in the Mists - Inn'),
+(8579, 'Honeydew Village - Inn');
