ca89bf2db41e3dd9b4e83341ce2957ef69e8c77a
AriDEV <aridev666@gmail.com>
2015-02-12 12:33:31 -0600

    Fix Mistake in my Last Commit.
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- sql/updates/world/2015_02_12_03_gameobject_loot_template.sql
+++ sql/updates/world/2015_02_12_03_gameobject_loot_template.sql
@@ -1,4 +1,4 @@
-SET @GOBJECT := 211452; -- Bronze Gift of the Crane
+SET @GOBJECT := 41602; -- Bronze Gift of the Crane
 SET @ITEM := 80938; -- Gift of the Great Crane
 DELETE FROM `gameobject_loot_template` WHERE `entry`=@GOBJECT;
 INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES
