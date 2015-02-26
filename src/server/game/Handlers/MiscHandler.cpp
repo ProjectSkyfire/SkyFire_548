4adeab51e7812d648353c7140a4ccde5bae2136e
projectcoredevs <projectcore.devs@gmail.com>
2015-02-26 21:19:06 +0100

    Core/packet update ACCOUNT_DATA opcode
    CMSG_REQUEST_ACCOUNT_DATA,
    SMSG_UPDATE_ACCOUNT_DATA

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -1170,8 +1170,7 @@ void WorldSession::HandleRequestAccountData(WorldPacket& recvData)
 {
     TC_LOG_DEBUG("network", "WORLD: Received CMSG_REQUEST_ACCOUNT_DATA");
 
-    uint32 type;
-    recvData >> type;
+    uint32 type = recvData.ReadBits(3);
 
     TC_LOG_DEBUG("network", "RAD: type %u", type);
 
@@ -1195,33 +1194,33 @@ void WorldSession::HandleRequestAccountData(WorldPacket& recvData)
 
     dest.resize(destSize);
 
-    WorldPacket data(SMSG_UPDATE_ACCOUNT_DATA, 8+4+4+4+destSize);
+    WorldPacket data(SMSG_UPDATE_ACCOUNT_DATA, 8 + 4 + 4 + 4 + destSize);
 
     ObjectGuid guid;
 
-    data << uint32(size);                                   // decompressed length
-    data << uint32(destSize);
-    data.append(dest);
-    data << uint32(adata->Time);                            // unix time
-    data.WriteBit(guid[7]);
-    data.WriteBits(type, 3);                                 // type (0-7)
-    data.WriteBit(guid[3]);
-    data.WriteBit(guid[6]);
-    data.WriteBit(guid[1]);
+    data.WriteBits(type, 3);       // type (0-7)
     data.WriteBit(guid[5]);
+    data.WriteBit(guid[1]);
+    data.WriteBit(guid[3]);
+    data.WriteBit(guid[7]);
     data.WriteBit(guid[0]);
     data.WriteBit(guid[4]);
     data.WriteBit(guid[2]);
+    data.WriteBit(guid[6]);
 
-    data.WriteByteSeq(guid[6]);
-    data.WriteByteSeq(guid[7]);
-    data.WriteByteSeq(guid[4]);
+    data.WriteByteSeq(guid[3]);
     data.WriteByteSeq(guid[1]);
     data.WriteByteSeq(guid[5]);
+    data << uint32(size);         // decompressed length
+    data << uint32(destSize);
+    data.append(dest);
+    data.WriteByteSeq(guid[7]);
+    data.WriteByteSeq(guid[4]);
     data.WriteByteSeq(guid[0]);
-    data.WriteByteSeq(guid[3]);
+    data.WriteByteSeq(guid[6]);
     data.WriteByteSeq(guid[2]);
-
+    data << uint32(adata->Time); // unix time
+    
     SendPacket(&data);
 }
 
