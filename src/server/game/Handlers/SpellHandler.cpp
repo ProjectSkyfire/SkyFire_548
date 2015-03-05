3d47b1f06f5f709dbf33b70191e9421fb045670e
AriDEV <aridev666@gmail.com>
2015-03-05 15:02:49 +0100

    Server/PacketIO: Move SMSG_TOTEM_CREATED to SpellHandler.
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- src/server/game/Handlers/SpellHandler.cpp
+++ src/server/game/Handlers/SpellHandler.cpp
@@ -1511,3 +1511,32 @@ void WorldSession::HandleRequestCategoryCooldowns(WorldPacket& /*recvPacket*/)
 
     SendPacket(&data);
 }
+
+void WorldSession::SendTotemCreated(ObjectGuid TotemGUID, uint32 Duration, uint32 SpellID, uint8 Slot)
+{
+    WorldPacket data(SMSG_TOTEM_CREATED, 17);
+    data.WriteBit(TotemGUID[6]);
+    data.WriteBit(TotemGUID[1]);
+    data.WriteBit(TotemGUID[2]);
+    data.WriteBit(TotemGUID[5]);
+    data.WriteBit(TotemGUID[3]);
+    data.WriteBit(TotemGUID[4]);
+    data.WriteBit(TotemGUID[7]);
+    data.WriteBit(TotemGUID[0]);
+
+    data << uint32(Duration);
+    data << uint32(SpellID);
+
+    data.WriteByteSeq(TotemGUID[3]);
+    data.WriteByteSeq(TotemGUID[4]);
+    data.WriteByteSeq(TotemGUID[5]);
+    data.WriteByteSeq(TotemGUID[6]);
+    data.WriteByteSeq(TotemGUID[0]);
+    data.WriteByteSeq(TotemGUID[2]);
+
+    data << uint8(Slot);
+
+    data.WriteByteSeq(TotemGUID[1]);
+    data.WriteByteSeq(TotemGUID[7]);
+    _player->SendDirectMessage(&data);
+}
