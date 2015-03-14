ff013cfbe8480d74693408b33930855ef9bea5e8
AriDEV <aridev666@gmail.com>
2015-03-14 01:43:39 +0100

    Database: Added Quest: The People's Militia To Disables
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2015_03_13_01_disables.sql
@@ -0,0 +1,6 @@
+SET @SOURCETYPE := 1 -- Quest
+SET @QUESTID    := 14 -- The People's Militia
+
+DELETE FROM `disables` WHERE `sourceType`=@SOURCETYPE AND `entry`=@QUESTID;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(@SOURCETYPE, @QUESTID, 'Disable Quest 14: The People\'s Militia');
