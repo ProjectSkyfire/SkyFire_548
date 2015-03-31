e13c40747a72252d49c7ce7f5ea166e5b9a05125
RedSonja <redsonja@trinitycore.contrib>
2015-03-31 21:36:01 +0200

    Database: Fixed Missing Semicolon, i have forgotten. Thx @RedSonja for Pointing out.
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- sql/updates/world/2015_03_13_01_disables.sql
+++ sql/updates/world/2015_03_13_01_disables.sql
@@ -1,5 +1,5 @@
-SET @SOURCETYPE := 1 -- Quest
-SET @QUESTID    := 14 -- The People's Militia
+SET @SOURCETYPE := 1; -- Quest
+SET @QUESTID    := 14; -- The People's Militia
 
 DELETE FROM `disables` WHERE `sourceType`=@SOURCETYPE AND `entry`=@QUESTID;
 INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
