aea9d88ca08e6d4fbc832ac22f794f30e5db0031
AriDEV <aridev666@gmail.com>
2015-05-03 22:42:43 +0200

    Database/Quests: Added Player Faction Change Quests
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2015_05_03_00_player_factionchange_quests.sql
@@ -0,0 +1,398 @@
+SET @AQUESTID := 32578; -- Among the Bones (A)
+SET @HQUESTID := 32293; -- Among the Bones (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32571; -- A Wing to Fly On (A)
+SET @HQUESTID := 32506; -- A Wing to Fly On (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32525; -- Ashes of the Enemy (A)
+SET @HQUESTID := 32218; -- Ashes of the Enemy (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 31376; -- Attack At The Temple of the Jade Serpent (A)
+SET @HQUESTID := 31377; -- Attack At The Temple of the Jade Serpent (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32485; -- Bolstering the Defenses (A)
+SET @HQUESTID := 32288; -- Bolstering the Defenses (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32634; -- Breaking Down the Defenses (A)
+SET @HQUESTID := 32269; -- Breaking Down the Defenses (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 31378; -- Challenge At The Temple of the Red Crane (A)
+SET @HQUESTID := 31379; -- Challenge At The Temple of the Red Crane (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32627; -- Charged Moganite (A)
+SET @HQUESTID := 32265; -- Charged Moganite (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32551; -- Compy Stomp (A)
+SET @HQUESTID := 32282; -- Compy Stomp (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32543; -- Dangers of Za'Tual (A)
+SET @HQUESTID := 32200; -- Dangers of Za'Tual (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32539; -- Dark Offerings (A)
+SET @HQUESTID := 32217; -- Dark Offerings (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32537; -- De-Constructed (A)
+SET @HQUESTID := 32255; -- De-Constructed (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32639; -- Deconstruction (A)
+SET @HQUESTID := 32302; -- Deconstruction (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 31382; -- Defense At Niuzao Temple (A)
+SET @HQUESTID := 31383; -- Defense At Niuzao Temple (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32554; -- Dino Might (A)
+SET @HQUESTID := 32298; -- Dino Might (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32553; -- Direhorn vs Devilsaur (A)
+SET @HQUESTID := 32297; -- Direhorn or Devilsaur (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32573; -- Enemies Beneath the Tower (A)
+SET @HQUESTID := 32287; -- Enemies Beneath the Tower (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32607; -- Extended Shore Leave (A)
+SET @HQUESTID := 32676; -- Extended Shore Leave (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32527; -- Grave Circumstances (A)
+SET @HQUESTID := 32201; -- Grave Circumstances (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32540; -- Harbingers of the Loa (A)
+SET @HQUESTID := 32252; -- Harbingers of the Loa (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32538; -- Heinous Sacrifice (A)
+SET @HQUESTID := 32215; -- Heinous Sacrifice (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32631; -- High Recognition (A)
+SET @HQUESTID := 32304; -- High Recognition (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32732; -- Ihgaluk Crag (A)
+SET @HQUESTID := 32730; -- Ihgaluk Crag (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32528; -- Into the Crypts (A)
+SET @HQUESTID := 32226; -- Into the Crypts (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32546; -- Just Some Light Clean-Up Work (A)
+SET @HQUESTID := 32299; -- Just Some Light Clean-Up Work (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32548; -- Left To Rot (A)
+SET @HQUESTID := 32491; -- Left To Rot (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32552; -- Loa-saur (A)
+SET @HQUESTID := 32283; -- Loa-saur (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32632; -- Made for War (A)
+SET @HQUESTID := 32303; -- Made for War (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32638; -- Mana Scavengers (A)
+SET @HQUESTID := 32266; -- Mana Manifestations (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32536; -- Manipulating the Saurok (A)
+SET @HQUESTID := 32254; -- Manipulating the Saurok (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32586; -- Maximum Capacitor (A)
+SET @HQUESTID := 32208; -- Maximum Capacitor (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32301; -- Old Enemies (A)
+SET @HQUESTID := 32213; -- Old Enemies (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32637; -- Overpowered (A)
+SET @HQUESTID := 32305; -- Overpowered (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32541; -- Preventing a Future Threat (A)
+SET @HQUESTID := 32227; -- Preventing a Future Threat (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32544; -- Pterrible Ptorment (A)
+SET @HQUESTID := 32216; -- Pterrible Ptorment (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32608; -- Raiding the Vault (A)
+SET @HQUESTID := 32677; -- Raiding the Vault (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32582; -- Raining Bones (A)
+SET @HQUESTID := 32294; -- Raining Bones (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32532; -- Rise No More! (A)
+SET @HQUESTID := 32224; -- Rise No More! (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32550; -- Saur Loser (A)
+SET @HQUESTID := 32207; -- Saur Loser (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32526; -- Soul Surrender (A)
+SET @HQUESTID := 32220; -- Soul Surrender (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32633; -- Spellbound (A)
+SET @HQUESTID := 32264; -- Spellbound (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32533; -- Stone Cold (A)
+SET @HQUESTID := 32219; -- Stone Cold (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32606; -- Subtle Encouragement (A)
+SET @HQUESTID := 32605; -- Subtle Encouragement (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32542; -- Surgical Strike (A)
+SET @HQUESTID := 32275; -- Surgical Death (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32628; -- Tactical Mana Bombs (A)
+SET @HQUESTID := 32268; -- Tactical Mana Bombs (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32567; -- The Beast Pens (A)
+SET @HQUESTID := 32523; -- The Beast Pens (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32530; -- The Bloodletter (A)
+SET @HQUESTID := 32495; -- The Bloodletter (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32529; -- The Call of Thunder (A)
+SET @HQUESTID := 32225; -- The Call of Thunder (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32531; -- The Conquest of Stone (A)
+SET @HQUESTID := 32517; -- The Conquest of Stone (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32731; -- The Court of Bones (A)
+SET @HQUESTID := 32728; -- The Court of Bones (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32547; -- The Creeping Carpet of Ihgaluk (A)
+SET @HQUESTID := 32489; -- The Creeping Carpet of Ihgaluk (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32545; -- The Residents of Ihgaluk (A)
+SET @HQUESTID := 32274; -- The Residents of Ihgaluk (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32574; -- The Shuddering Moor (A)
+SET @HQUESTID := 32228; -- The Shuddering Moor (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32535; -- The Skumblade Threat (A)
+SET @HQUESTID := 32204; -- The Skumblade Threat (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32572; -- The Sleepless Legion (A)
+SET @HQUESTID := 32285; -- The Sleepless Legion (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 32575; -- The Zandalari Colossus (A)
+SET @HQUESTID := 32230; -- The Zandalari Colossus (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
+
+SET @AQUESTID := 31380; -- Trial At The Temple of the White Tiger (A)
+SET @HQUESTID := 31381; -- Trial At The Temple of the White Tiger (H)
+
+DELETE FROM `player_factionchange_quests` WHERE `alliance_id`=@AQUESTID AND `horde_id`=@HQUESTID;
+INSERT INTO `player_factionchange_quests` (`alliance_id`, `horde_id`) VALUES
+(@AQUESTID, @HQUESTID);
