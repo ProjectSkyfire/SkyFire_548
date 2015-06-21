85b422b6404ac914ae3bdb32fd1d5a47ed17524e
AriDEV <aridev666@gmail.com>
2015-06-22 00:47:43 +0200

    SQL: blackmarket sql template i hate git extensions...
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- sql/updates/world/2015_06_22_00_blackmarket_template.sql
+++ sql/updates/world/2015_06_22_00_blackmarket_template.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS `blackmarket_auctions`;
-CREATE TABLE `blackmarket_auctions` (
-  `auctionId` int(10) unsigned NOT NULL DEFAULT '0',
-  `templateId` int(10) unsigned NOT NULL DEFAULT '0',
-  `startTime` int(10) unsigned NOT NULL DEFAULT '0',
-  `currentBidder` int(10) unsigned NOT NULL DEFAULT '0',
-  `currentBid` int(10) unsigned NOT NULL DEFAULT '0',
-  `minIncrement` int(10) unsigned NOT NULL DEFAULT '0',
-  `numBids` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`auctionId`),
-  UNIQUE KEY `templateId` (`templateId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `blackmarket_template`;
+CREATE TABLE `blackmarket_template` (
+  `Id` int(10) unsigned NOT NULL DEFAULT '0',
+  `MarketId` int(10) unsigned NOT NULL DEFAULT '0',
+  `SellerID` int(10) unsigned NOT NULL DEFAULT '0',
+  `ItemEntry` int(10) unsigned NOT NULL DEFAULT '0',
+  `Quantity` int(10) unsigned NOT NULL DEFAULT '0',
+  `MinBid` INT(10) NOT NULL DEFAULT '0',
+  `Duration` int(10) unsigned NOT NULL DEFAULT '0',
+  `Chance` FLOAT DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
