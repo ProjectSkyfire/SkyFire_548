475ff037cf56634ee4817f3c103a13ce3f787964
AriDEV <aridev666@gmail.com>
2015-06-26 21:43:36 +0200

    SQL: Fixed Some DB Errors.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2015_06_26_00_creature_template.sql
@@ -0,0 +1,42 @@
+-- Yakrat
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=63957;
+-- Blackfuse Bombling
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=73352;
+-- Droplet of Y'Shaarj
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=73350;
+-- Mongoose Pup
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=63954;
+-- Filthling
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=71201;
+-- Personal World Destroyer
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=43800;
+-- Gahz'rooki
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=71159;
+-- Mechanical Pandaren Dragonling
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=64899;
+-- Anubisath Idol
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=68659;
+-- Frigid Frostling
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=40198;
+-- Zandalari Footslasher
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=70452;
+-- Tiny Green Carp
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=70259;
+-- Infected Fawn
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=61827;
+-- Perky Pug
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=37865;
+-- Darkmoon Hatchling
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=67319;
+-- Living Fluid
+UPDATE creature_template SET faction_A=188, faction_H=188, unit_class=1 WHERE entry=71199;
+
+
+-- Water Elemental
+UPDATE creature_template SET faction_A=91, faction_H=91, unit_class=8 WHERE entry=510;
+
+-- Treant
+UPDATE creature_template SET faction_A=14, faction_H=14, unit_class=1 WHERE entry=1964;
+
+-- Searing Totem
+UPDATE creature_template SET faction_A=58, faction_H=58, unit_class=1 WHERE entry=2523;
