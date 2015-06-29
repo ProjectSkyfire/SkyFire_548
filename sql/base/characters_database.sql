8e5c555bccdcaef6d0fc2598526449c620bf235a
AriDEV <aridev666@gmail.com>
2015-06-29 14:12:26 +0200

    Database: Fixed SQL Syntax. and removed keys that are not needed.
    Fixed #583
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -312,11 +312,9 @@ CREATE TABLE `blackmarket_auctions` (
   `templateId` int(10) unsigned NOT NULL DEFAULT '0',
   `startTime` int(10) unsigned NOT NULL DEFAULT '0',
   `currentBidder` int(10) unsigned NOT NULL DEFAULT '0',
-  `currentBid` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `currentBid` int(10) unsigned NOT NULL DEFAULT '0',
   `minIncrement` int(10) unsigned NOT NULL DEFAULT '0',
-  `numBids` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`auctionId`),
-  UNIQUE KEY `templateId` (`templateId`)
+  `numBids` int(10) unsigned NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
--- sql/updates/characters/2015_06_22_00_char_blackmarket_auctions.sql
+++ sql/updates/characters/2015_06_22_00_char_blackmarket_auctions.sql
@@ -1,11 +1,12 @@
 DROP TABLE IF EXISTS `blackmarket_auctions`;
 CREATE TABLE `blackmarket_auctions` (
-  `auctionId` int(10) unsigned NOT NULL DEFAULT '0',
-  `templateId` int(10) unsigned NOT NULL DEFAULT '0',
-  `startTime` int(10) unsigned NOT NULL DEFAULT '0',
-  `currentBidder` int(10) unsigned NOT NULL DEFAULT '0',
-  `currentBid` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `minIncrement` int(10) unsigned NOT NULL DEFAULT '0',
-  `numBids` int(10) unsigned NOT NULL DEFAULT '0',
+  `auctionId` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+  `templateId` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+  `startTime` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+  `currentBidder` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+  `currentBid` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+  `minIncrement` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+  `numBids` INT(10) UNSIGNED NOT NULL DEFAULT '0'
 )
+COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
