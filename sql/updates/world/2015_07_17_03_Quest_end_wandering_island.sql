41e05db57d112ea94a5f853774cf8d635ae5e444
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-07-17 02:46:25 +0200

    DB/Update quest starter & ender for wandering island

---
+++ sql/updates/world/2015_07_17_03_Quest_end_wandering_island.sql
@@ -0,0 +1,130 @@
+-- Parsed Creature quest for Creature id : 53566 
+DELETE FROM `creature_questender` WHERE `Id` = 53566 ;
+INSERT INTO `creature_questender` VALUES ( '53566', '29406'); -- The Lesson of the Sandy Fist
+INSERT INTO `creature_questender` VALUES ( '53566', '29408'); -- The Lesson of the Burning Scroll
+INSERT INTO `creature_questender` VALUES ( '53566', '29409'); -- The Disciple's Challenge
+INSERT INTO `creature_questender` VALUES ( '53566', '29524'); -- The Lesson of Stifled Pride
+INSERT INTO `creature_questender` VALUES ( '53566', '30027'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_questender` VALUES ( '53566', '30033'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_questender` VALUES ( '53566', '30034'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_questender` VALUES ( '53566', '30035'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_questender` VALUES ( '53566', '30036'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_questender` VALUES ( '53566', '30037'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_questender` VALUES ( '53566', '30038'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_questender` VALUES ( '53566', '30039'); -- Much to Learn
+INSERT INTO `creature_questender` VALUES ( '53566', '30040'); -- Much to Learn
+INSERT INTO `creature_questender` VALUES ( '53566', '30041'); -- Much to Learn
+INSERT INTO `creature_questender` VALUES ( '53566', '30042'); -- Much to Learn
+INSERT INTO `creature_questender` VALUES ( '53566', '30043'); -- Much to Learn
+INSERT INTO `creature_questender` VALUES ( '53566', '30044'); -- Much to Learn
+INSERT INTO `creature_questender` VALUES ( '53566', '30045'); -- Much to Learn-- Parsed Creature quest for Creature id : 54135 
+DELETE FROM `creature_questender` WHERE `Id` = 54135 ;
+INSERT INTO `creature_questender` VALUES ( '54135', '29420'); -- The Spirit's Guardian
+INSERT INTO `creature_questender` VALUES ( '54135', '29421'); -- Only the Worthy Shall Pass
+INSERT INTO `creature_questender` VALUES ( '54135', '29664'); -- The Challenger's Fires-- Parsed Creature quest for Creature id : 54568 
+DELETE FROM `creature_questender` WHERE `Id` = 54568 ;
+INSERT INTO `creature_questender` VALUES ( '54568', '29417'); -- The Way of the Huojin
+INSERT INTO `creature_questender` VALUES ( '54568', '29418'); -- Kindling the Fire
+INSERT INTO `creature_questender` VALUES ( '54568', '29522'); -- Ji of the Huojin
+INSERT INTO `creature_questender` VALUES ( '54568', '29523'); -- Fanning the Flames
+-- Parsed Creature quest for Creature id : 54608 
+DELETE FROM `creature_questender` WHERE `Id` = 54608 ;
+INSERT INTO `creature_questender` VALUES ( '54608', '29414'); -- The Way of the Tushui-- Parsed Creature quest for Creature id : 54786 
+DELETE FROM `creature_questender` WHERE `Id` = 54786 ;
+INSERT INTO `creature_questender` VALUES ( '54786', '29423'); -- The Passion of Shen-zin Su
+INSERT INTO `creature_questender` VALUES ( '54786', '29775'); -- The Spirit and Body of Shen-zin Su
+-- Parsed Creature quest for Creature id : 54787 
+DELETE FROM `creature_questender` WHERE `Id` = 54787 ;
+INSERT INTO `creature_questender` VALUES ( '54787', '29422'); -- Huo, the Spirit of Fire-- Parsed Creature quest for Creature id : 54943 
+DELETE FROM `creature_questender` WHERE `Id` = 54943 ;
+INSERT INTO `creature_questender` VALUES ( '54943', '29410'); -- Aysa of the Tushui
+INSERT INTO `creature_questender` VALUES ( '54943', '29419'); -- The Missing Driver
+INSERT INTO `creature_questender` VALUES ( '54943', '29424'); -- Items of Utmost Importance
+-- Parsed Creature quest for Creature id : 54975 
+DELETE FROM `creature_questender` WHERE `Id` = 54975 ;
+INSERT INTO `creature_questender` VALUES ( '54975', '29521'); -- The Singing Pools
+INSERT INTO `creature_questender` VALUES ( '54975', '29661'); -- The Lesson of Dry Fur
+INSERT INTO `creature_questender` VALUES ( '54975', '29663'); -- The Lesson of the Balanced Rock
+INSERT INTO `creature_questender` VALUES ( '54975', '29679'); -- A New Friend-- Parsed Creature quest for Creature id : 55020 
+DELETE FROM `creature_questender` WHERE `Id` = 55020 ;
+INSERT INTO `creature_questender` VALUES ( '55020', '29666'); -- The Sting of Learning
+INSERT INTO `creature_questender` VALUES ( '55020', '29676'); -- Finding an Old Friend
+INSERT INTO `creature_questender` VALUES ( '55020', '29677'); -- The Sun Pearl
+-- Parsed Creature quest for Creature id : 55021 
+DELETE FROM `creature_questender` WHERE `Id` = 55021 ;
+INSERT INTO `creature_questender` VALUES ( '55021', '29662'); -- Stronger Than Reeds
+-- Parsed Creature quest for Creature id : 55477 
+DELETE FROM `creature_questender` WHERE `Id` = 55477 ;
+INSERT INTO `creature_questender` VALUES ( '55477', '29680'); -- The Source of Our Livelihood
+INSERT INTO `creature_questender` VALUES ( '55477', '29768'); -- Missing Mallet
+INSERT INTO `creature_questender` VALUES ( '55477', '29769'); -- Rascals
+INSERT INTO `creature_questender` VALUES ( '55477', '29772'); -- Raucous Rousing
+INSERT INTO `creature_questender` VALUES ( '55477', '29774'); -- Not In the Face!
+-- Parsed Creature quest for Creature id : 55478 
+DELETE FROM `creature_questender` WHERE `Id` = 55478 ;
+INSERT INTO `creature_questender` VALUES ( '55478', '29771'); -- Stronger Than Wood
+-- Parsed Creature quest for Creature id : 55479 
+DELETE FROM `creature_questender` WHERE `Id` = 55479 ;
+INSERT INTO `creature_questender` VALUES ( '55479', '29770'); -- Still Good!-- Parsed Creature quest for Creature id : 55583 
+DELETE FROM `creature_questender` WHERE `Id` = 55583 ;
+INSERT INTO `creature_questender` VALUES ( '55583', '29776'); -- Morning Breeze Village
+INSERT INTO `creature_questender` VALUES ( '55583', '29778'); -- Rewritten Wisdoms
+INSERT INTO `creature_questender` VALUES ( '55583', '29779'); -- The Direct Solution
+INSERT INTO `creature_questender` VALUES ( '55583', '29780'); -- Do No Evil
+INSERT INTO `creature_questender` VALUES ( '55583', '29781'); -- Monkey Advisory Warning
+-- Parsed Creature quest for Creature id : 55585 
+DELETE FROM `creature_questender` WHERE `Id` = 55585 ;
+INSERT INTO `creature_questender` VALUES ( '55585', '29782'); -- Stronger Than Bone
+INSERT INTO `creature_questender` VALUES ( '55585', '29783'); -- Stronger Than Stone
+-- Parsed Creature quest for Creature id : 55586 
+DELETE FROM `creature_questender` WHERE `Id` = 55586 ;
+INSERT INTO `creature_questender` VALUES ( '55586', '29786'); -- Battle for the Skies
+-- Parsed Creature quest for Creature id : 55588 
+DELETE FROM `creature_questender` WHERE `Id` = 55588 ;
+INSERT INTO `creature_questender` VALUES ( '55588', '29777'); -- Tools of the Enemy
+-- Parsed Creature quest for Creature id : 55592 
+DELETE FROM `creature_questender` WHERE `Id` = 55592 ;
+INSERT INTO `creature_questender` VALUES ( '55592', '29785'); -- Dafeng, the Spirit of Air
+-- Parsed Creature quest for Creature id : 55595 
+DELETE FROM `creature_questender` WHERE `Id` = 55595 ;
+INSERT INTO `creature_questender` VALUES ( '55595', '29784'); -- Balanced Perspective
+-- Parsed Creature quest for Creature id : 55672 
+DELETE FROM `creature_questender` WHERE `Id` = 55672 ;
+INSERT INTO `creature_questender` VALUES ( '55672', '29787'); -- Worthy of Passing
+INSERT INTO `creature_questender` VALUES ( '55672', '29788'); -- Unwelcome Nature
+INSERT INTO `creature_questender` VALUES ( '55672', '29789'); -- Small, But Significant-- Parsed Creature quest for Creature id : 55940 
+DELETE FROM `creature_questender` WHERE `Id` = 55940 ;
+INSERT INTO `creature_questender` VALUES ( '55940', '29665'); -- From Bad to Worse
+-- Parsed Creature quest for Creature id : 55942 
+DELETE FROM `creature_questender` WHERE `Id` = 55942 ;
+INSERT INTO `creature_questender` VALUES ( '55942', '29793'); -- Evil from the Seas
+-- Parsed Creature quest for Creature id : 55943 
+DELETE FROM `creature_questender` WHERE `Id` = 55943 ;
+INSERT INTO `creature_questender` VALUES ( '55943', '29792'); -- Bidden to Greatness
+INSERT INTO `creature_questender` VALUES ( '55943', '30591'); -- Preying on the Predators
+-- Parsed Creature quest for Creature id : 55944 
+DELETE FROM `creature_questender` WHERE `Id` = 55944 ;
+INSERT INTO `creature_questender` VALUES ( '55944', '29794'); -- None Left Behind
+INSERT INTO `creature_questender` VALUES ( '55944', '29796'); -- Urgent News
+INSERT INTO `creature_questender` VALUES ( '55944', '29797'); -- Medical Supplies
+-- Parsed Creature quest for Creature id : 56012 
+DELETE FROM `creature_questender` WHERE `Id` = 56012 ;
+INSERT INTO `creature_questender` VALUES ( '56012', '29791'); -- The Suffering of Shen-zin Su
+-- Parsed Creature quest for Creature id : 56013 
+DELETE FROM `creature_questender` WHERE `Id` = 56013 ;
+INSERT INTO `creature_questender` VALUES ( '56013', '29800'); -- New Allies
+-- Parsed Creature quest for Creature id : 56416 
+DELETE FROM `creature_questender` WHERE `Id` = 56416 ;
+INSERT INTO `creature_questender` VALUES ( '56416', '29798'); -- An Ancient Evil-- Parsed Creature quest for Creature id : 56418 
+DELETE FROM `creature_questender` WHERE `Id` = 56418 ;
+INSERT INTO `creature_questender` VALUES ( '56418', '29799'); -- The Healing of Shen-zin Su
+INSERT INTO `creature_questender` VALUES ( '56418', '30767'); -- Risking It All
+-- Parsed Creature quest for Creature id : 56662 
+DELETE FROM `creature_questender` WHERE `Id` = 56662 ;
+INSERT INTO `creature_questender` VALUES ( '56662', '29790'); -- Passing Wisdom-- Parsed Creature quest for Creature id : 60042 
+DELETE FROM `creature_questender` WHERE `Id` = 60042 ;
+INSERT INTO `creature_questender` VALUES ( '60042', '29795'); -- Stocking Stalks
+-- Parsed Creature quest for Creature id : 60055 
+DELETE FROM `creature_questender` WHERE `Id` = 60055 ;
+INSERT INTO `creature_questender` VALUES ( '60055', '30589'); -- Wrecking the Wreck
+INSERT INTO `creature_questender` VALUES ( '60055', '30590'); -- Handle With Care
---
+++ sql/updates/world/2015_07_17_04_Quest_start_wandering_island.sql
@@ -0,0 +1,126 @@
+-- Parsed Creature quest for Creature id : 53566 
+DELETE FROM `creature_queststarter` WHERE `Id` = 53566 ;
+INSERT INTO `creature_queststarter` VALUES ( '53566', '29406'); -- The Lesson of the Sandy Fist
+INSERT INTO `creature_queststarter` VALUES ( '53566', '29408'); -- The Lesson of the Burning Scroll
+INSERT INTO `creature_queststarter` VALUES ( '53566', '29409'); -- The Disciple's Challenge
+INSERT INTO `creature_queststarter` VALUES ( '53566', '29410'); -- Aysa of the Tushui
+INSERT INTO `creature_queststarter` VALUES ( '53566', '29524'); -- The Lesson of Stifled Pride
+INSERT INTO `creature_queststarter` VALUES ( '53566', '30027'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_queststarter` VALUES ( '53566', '30033'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_queststarter` VALUES ( '53566', '30034'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_queststarter` VALUES ( '53566', '30035'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_queststarter` VALUES ( '53566', '30036'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_queststarter` VALUES ( '53566', '30037'); -- The Lesson of the Iron Bough
+INSERT INTO `creature_queststarter` VALUES ( '53566', '30038'); -- The Lesson of the Iron Bough-- Parsed Creature quest for Creature id : 54135 
+DELETE FROM `creature_queststarter` WHERE `Id` = 54135 ;
+INSERT INTO `creature_queststarter` VALUES ( '54135', '29421'); -- Only the Worthy Shall Pass
+INSERT INTO `creature_queststarter` VALUES ( '54135', '29422'); -- Huo, the Spirit of Fire
+INSERT INTO `creature_queststarter` VALUES ( '54135', '29664'); -- The Challenger's Fires
+-- Parsed Creature quest for Creature id : 54567 
+DELETE FROM `creature_queststarter` WHERE `Id` = 54567 ;
+INSERT INTO `creature_queststarter` VALUES ( '54567', '29414'); -- The Way of the Tushui
+-- Parsed Creature quest for Creature id : 54568 
+DELETE FROM `creature_queststarter` WHERE `Id` = 54568 ;
+INSERT INTO `creature_queststarter` VALUES ( '54568', '29417'); -- The Way of the Huojin
+INSERT INTO `creature_queststarter` VALUES ( '54568', '29418'); -- Kindling the Fire
+INSERT INTO `creature_queststarter` VALUES ( '54568', '29523'); -- Fanning the Flames
+-- Parsed Creature quest for Creature id : 54608 
+DELETE FROM `creature_queststarter` WHERE `Id` = 54608 ;
+INSERT INTO `creature_queststarter` VALUES ( '54608', '29522'); -- Ji of the Huojin
+-- Parsed Creature quest for Creature id : 54609 
+DELETE FROM `creature_queststarter` WHERE `Id` = 54609 ;
+INSERT INTO `creature_queststarter` VALUES ( '54609', '29420'); -- The Spirit's Guardian
+-- Parsed Creature quest for Creature id : 54786 
+DELETE FROM `creature_queststarter` WHERE `Id` = 54786 ;
+INSERT INTO `creature_queststarter` VALUES ( '54786', '29521'); -- The Singing Pools
+INSERT INTO `creature_queststarter` VALUES ( '54786', '29776'); -- Morning Breeze Village
+-- Parsed Creature quest for Creature id : 54787 
+DELETE FROM `creature_queststarter` WHERE `Id` = 54787 ;
+INSERT INTO `creature_queststarter` VALUES ( '54787', '29423'); -- The Passion of Shen-zin Su-- Parsed Creature quest for Creature id : 54943 
+DELETE FROM `creature_queststarter` WHERE `Id` = 54943 ;
+INSERT INTO `creature_queststarter` VALUES ( '54943', '29419'); -- The Missing Driver
+INSERT INTO `creature_queststarter` VALUES ( '54943', '29424'); -- Items of Utmost Importance
+-- Parsed Creature quest for Creature id : 54975 
+DELETE FROM `creature_queststarter` WHERE `Id` = 54975 ;
+INSERT INTO `creature_queststarter` VALUES ( '54975', '29661'); -- The Lesson of Dry Fur
+INSERT INTO `creature_queststarter` VALUES ( '54975', '29663'); -- The Lesson of the Balanced Rock
+INSERT INTO `creature_queststarter` VALUES ( '54975', '29676'); -- Finding an Old Friend
+INSERT INTO `creature_queststarter` VALUES ( '54975', '29680'); -- The Source of Our Livelihood-- Parsed Creature quest for Creature id : 55020 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55020 ;
+INSERT INTO `creature_queststarter` VALUES ( '55020', '29666'); -- The Sting of Learning
+INSERT INTO `creature_queststarter` VALUES ( '55020', '29677'); -- The Sun Pearl
+INSERT INTO `creature_queststarter` VALUES ( '55020', '29678'); -- Shu, the Spirit of Water
+-- Parsed Creature quest for Creature id : 55021 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55021 ;
+INSERT INTO `creature_queststarter` VALUES ( '55021', '29662'); -- Stronger Than Reeds
+-- Parsed Creature quest for Creature id : 55477 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55477 ;
+INSERT INTO `creature_queststarter` VALUES ( '55477', '29768'); -- Missing Mallet
+INSERT INTO `creature_queststarter` VALUES ( '55477', '29769'); -- Rascals
+INSERT INTO `creature_queststarter` VALUES ( '55477', '29772'); -- Raucous Rousing
+INSERT INTO `creature_queststarter` VALUES ( '55477', '29774'); -- Not In the Face!
+INSERT INTO `creature_queststarter` VALUES ( '55477', '29775'); -- The Spirit and Body of Shen-zin Su
+-- Parsed Creature quest for Creature id : 55478 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55478 ;
+INSERT INTO `creature_queststarter` VALUES ( '55478', '29771'); -- Stronger Than Wood
+-- Parsed Creature quest for Creature id : 55479 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55479 ;
+INSERT INTO `creature_queststarter` VALUES ( '55479', '29770'); -- Still Good!-- Parsed Creature quest for Creature id : 55583 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55583 ;
+INSERT INTO `creature_queststarter` VALUES ( '55583', '29778'); -- Rewritten Wisdoms
+INSERT INTO `creature_queststarter` VALUES ( '55583', '29779'); -- The Direct Solution
+INSERT INTO `creature_queststarter` VALUES ( '55583', '29780'); -- Do No Evil
+INSERT INTO `creature_queststarter` VALUES ( '55583', '29781'); -- Monkey Advisory Warning
+-- Parsed Creature quest for Creature id : 55585 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55585 ;
+INSERT INTO `creature_queststarter` VALUES ( '55585', '29783'); -- Stronger Than Stone
+-- Parsed Creature quest for Creature id : 55586 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55586 ;
+INSERT INTO `creature_queststarter` VALUES ( '55586', '29787'); -- Worthy of Passing
+-- Parsed Creature quest for Creature id : 55588 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55588 ;
+INSERT INTO `creature_queststarter` VALUES ( '55588', '29777'); -- Tools of the Enemy-- Parsed Creature quest for Creature id : 55595 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55595 ;
+INSERT INTO `creature_queststarter` VALUES ( '55595', '29785'); -- Dafeng, the Spirit of Air
+INSERT INTO `creature_queststarter` VALUES ( '55595', '29786'); -- Battle for the Skies
+-- Parsed Creature quest for Creature id : 55672 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55672 ;
+INSERT INTO `creature_queststarter` VALUES ( '55672', '29788'); -- Unwelcome Nature
+INSERT INTO `creature_queststarter` VALUES ( '55672', '29789'); -- Small, But Significant
+INSERT INTO `creature_queststarter` VALUES ( '55672', '29790'); -- Passing Wisdom-- Parsed Creature quest for Creature id : 55940 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55940 ;
+INSERT INTO `creature_queststarter` VALUES ( '55940', '29665'); -- From Bad to Worse
+INSERT INTO `creature_queststarter` VALUES ( '55940', '29798'); -- An Ancient Evil
+-- Parsed Creature quest for Creature id : 55942 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55942 ;
+INSERT INTO `creature_queststarter` VALUES ( '55942', '29793'); -- Evil from the Seas
+INSERT INTO `creature_queststarter` VALUES ( '55942', '29796'); -- Urgent News
+-- Parsed Creature quest for Creature id : 55943 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55943 ;
+INSERT INTO `creature_queststarter` VALUES ( '55943', '30591'); -- Preying on the Predators
+-- Parsed Creature quest for Creature id : 55944 
+DELETE FROM `creature_queststarter` WHERE `Id` = 55944 ;
+INSERT INTO `creature_queststarter` VALUES ( '55944', '29794'); -- None Left Behind
+INSERT INTO `creature_queststarter` VALUES ( '55944', '29797'); -- Medical Supplies
+-- Parsed Creature quest for Creature id : 56012 
+DELETE FROM `creature_queststarter` WHERE `Id` = 56012 ;
+INSERT INTO `creature_queststarter` VALUES ( '56012', '29792'); -- Bidden to Greatness
+-- Parsed Creature quest for Creature id : 56013 
+DELETE FROM `creature_queststarter` WHERE `Id` = 56013 ;
+INSERT INTO `creature_queststarter` VALUES ( '56013', '31450'); -- A New Fate
+-- Parsed Creature quest for Creature id : 56416 
+DELETE FROM `creature_queststarter` WHERE `Id` = 56416 ;
+INSERT INTO `creature_queststarter` VALUES ( '56416', '30767'); -- Risking It All-- Parsed Creature quest for Creature id : 56418 
+DELETE FROM `creature_queststarter` WHERE `Id` = 56418 ;
+INSERT INTO `creature_queststarter` VALUES ( '56418', '29799'); -- The Healing of Shen-zin Su
+-- Parsed Creature quest for Creature id : 56662 
+DELETE FROM `creature_queststarter` WHERE `Id` = 56662 ;
+INSERT INTO `creature_queststarter` VALUES ( '56662', '29791'); -- The Suffering of Shen-zin Su-- Parsed Creature quest for Creature id : 57739 
+DELETE FROM `creature_queststarter` WHERE `Id` = 57739 ;
+INSERT INTO `creature_queststarter` VALUES ( '57739', '29800'); -- New Allies-- Parsed Creature quest for Creature id : 60042 
+DELETE FROM `creature_queststarter` WHERE `Id` = 60042 ;
+INSERT INTO `creature_queststarter` VALUES ( '60042', '29795'); -- Stocking Stalks
+INSERT INTO `creature_queststarter` VALUES ( '60042', '30589'); -- Wrecking the Wreck
+-- Parsed Creature quest for Creature id : 60055 
+DELETE FROM `creature_queststarter` WHERE `Id` = 60055 ;
+INSERT INTO `creature_queststarter` VALUES ( '60055', '30590'); -- Handle With Care
