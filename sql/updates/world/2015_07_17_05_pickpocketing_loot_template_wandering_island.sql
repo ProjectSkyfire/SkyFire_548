668d2d8e2a89307a46a5351e35ceef165ea3b75a
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-07-17 16:56:57 +0200

    DB/ fix mistake & re-calculation  of pct chance drop for common drop

--- sql/updates/world/2015_07_17_05_pickpocketing_loot_template_wandering_island.sql
+++ sql/updates/world/2015_07_17_05_pickpocketing_loot_template_wandering_island.sql
@@ -1,6 +1,6 @@
 -- Parsed Creature loot for Creature id : 56360 
 DELETE FROM `pickpocketing_loot_template` WHERE `Entry` = 56360;
-UPDATE `creature_template` SET `pickpocketloot`= `56360`;
+UPDATE `creature_template` SET `pickpocketloot`= 56360 WHERE `entry`= 56360;
 INSERT INTO `pickpocketing_loot_template` VALUES ( '56360', '117', '0.6968641', '1', '0', '1', '1');
 INSERT INTO `pickpocketing_loot_template` VALUES ( '56360', '118', '1.045296', '1', '0', '1', '1');
 INSERT INTO `pickpocketing_loot_template` VALUES ( '56360', '4540', '1.219512', '1', '0', '1', '1');
