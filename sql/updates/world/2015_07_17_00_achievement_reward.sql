a4bc7188e5f89f9afc8ad7a3f06b8f0023d02fb9
AriDEV <aridev666@gmail.com>
2015-07-17 02:08:15 +0200

    Database/Achievement: Added Both-Side Achievement Title Rewards That Were Added In Mists Of Pandaria Expansion.
    -[ci skip]

---
+++ sql/updates/world/2015_07_17_00_achievement_reward.sql
@@ -0,0 +1,84 @@
+-- Achievement: 60 Exalted Reputations, Title: "the Beloved"
+DELETE FROM `achievement_reward` WHERE entry = 6742;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(6742, 308, 308, 0, 0, '', '');
+
+-- Achievement: Challenge Conqueror: Bronze, Title: "the Undaunted"
+DELETE FROM `achievement_reward` WHERE entry = 6374;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(6374, 314, 314, 0, 0, '', '');
+
+-- Achievement: Chapter V: Judgment of the Black Prince, Title: ", Legend of Pandaria"
+DELETE FROM `achievement_reward` WHERE entry = 8325;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(8325, 444, 444, 0, 0, '', '');
+
+-- Achievement: Fire-Watcher, Title: "Fire-Watcher"
+DELETE FROM `achievement_reward` WHERE entry = 8721;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(8721, 389, 389, 0, 0, '', '');
+
+-- Achievement: Heroic: Lei Shen, Title: ", Storm's End"
+DELETE FROM `achievement_reward` WHERE entry = 8067;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(8067, 342, 342, 0, 0, '', '');
+
+-- Achievement: Heroic: Sha of Fear, Title: "the Fearless"
+DELETE FROM `achievement_reward` WHERE entry = 6734;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(6734, 309, 309, 0, 0, '', '');
+
+-- Achievement: Heroic: Will of the Emperor, Title: ", Delver of the Vaults"
+DELETE FROM `achievement_reward` WHERE entry = 6724;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(6724, 317, 317, 0, 0, '', '');
+
+-- Achievement: Is Another Man's Treasure, Title: "the Relic Hunter"
+DELETE FROM `achievement_reward` WHERE entry = 7284;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(7284, 311, 311, 0, 0, '', '');
+
+-- Achievement: Master of Deepwind Gorge, Title: "Gorgeous"
+DELETE FROM `achievement_reward` WHERE entry = 8360;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(8360, 350, 350, 0, 0, '', '');
+
+-- Achievement: Master of Pandaren Cooking, Title: ", Master of the Ways"
+DELETE FROM `achievement_reward` WHERE entry = 7306;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(7306, 315, 315, 0, 0, '', '');
+
+-- Achievement: Stormbreaker, Title: "the Stormbreaker"
+DELETE FROM `achievement_reward` WHERE entry = 8121;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(8121, 341, 341, 0, 0, '', '');
+
+-- Achievement: Taming Azeroth, Title: "Tamer"
+DELETE FROM `achievement_reward` WHERE entry = 6607;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(6607, 320, 320, 0, 0, '', '');
+
+-- Achievement: The Seat of Knowledge, Title: ", Seeker of Knowledge"
+DELETE FROM `achievement_reward` WHERE entry = 7612;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(7612, 321, 321, 0, 0, '', '');
+
+-- Achievement: The Shado-Master, Title: "Shado-Master"
+DELETE FROM `achievement_reward` WHERE entry = 7479;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(7479, 318, 318, 0, 0, '', '');
+
+-- Achievement: Tranquil Master, Title: "the Tranquil Master"
+DELETE FROM `achievement_reward` WHERE entry = 6926;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(6926, 316, 316, 0, 0, '', '');
+
+-- Achievement: Wakener, Title: "the Wakener"
+DELETE FROM `achievement_reward` WHERE entry = 8023;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(8023, 338, 338, 0, 0, '', '');
+
+-- Achievement: World Safari, Title: "Zookeeper"
+DELETE FROM `achievement_reward` WHERE entry = 6590;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES
+(6590, 310, 310, 0, 0, '', '');
