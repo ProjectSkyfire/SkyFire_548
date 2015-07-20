dd43b8c5068b73b4ed0222fdb0947a766e43fdac
Software Engineer <aridev666@gmail.com>
2015-07-20 22:38:55 +0200

    SQL: Fixed @projectcore codestyle
    and removed creature spawn that is allready in database

--- sql/updates/world/2015_07_20_07_quest_14126.sql
+++ sql/updates/world/2015_07_20_07_quest_14126.sql
@@ -1,10 +1,7 @@
-DELETE FROM `spell_linked_spell` WHERE spell_trigger=74100;
-INSERT INTO `spell_linked_spell` VALUES (91847, 74100, 0, 'Life Savings: Teleport & Bind to the Lost Isles');
+DELETE FROM `spell_linked_spell` WHERE spell_trigger=91847;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES
+(91847, 74100, 'Life Savings: Teleport & Bind to the Lost Isles');
+
 DELETE FROM `spell_target_position` WHERE id=74100;
-INSERT INTO spell_target_position VALUES (74100, 0, 648, 534.5, 3273.4, 0.23, 4.79);
-INSERT INTO spell_target_position VALUES (74100, 1, 648, 534.5, 3273.4, 0.23, 4.79);
-
-
-DELETE FROM `creature` WHERE `guid` BETWEEN @CGUID+0 AND @CGUID+0;
-INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `MovementType`) VALUES
-(@CGUID+0, 35222, 648, 1, 1, -7854.503, 1847.41, 7.218571, 5.427974, 120, 0, 0);
+INSERT INTO `spell_target_position` (`id`, `effIndex`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES
+(74100, 1, 648, 534.5, 3273.4, 0.23, 4.79);
