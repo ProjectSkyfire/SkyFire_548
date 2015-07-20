0f167200417740d134c16b7026c958b7b94e3bc0
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-07-20 18:43:48 +0200

    DB/Update Creature_queststarter / ender for the Lost isles

---
+++ sql/updates/world/2015_07_20_03_update_creature_questender_&_starter_lost_isles.sql
@@ -0,0 +1,280 @@
+-- Parsed Creature quest for Creature id : 35650 
+DELETE FROM `creature_queststarter` WHERE `Id` = 35650 ;
+INSERT INTO `creature_queststarter` VALUES ( '35650', '14014'); -- Get Our Stuff Back!
+INSERT INTO `creature_queststarter` VALUES ( '35650', '14234'); -- The Enemy of My Enemy
+INSERT INTO `creature_queststarter` VALUES ( '35650', '14248'); -- Help Wanted
+
+-- Parsed Creature quest for Creature id : 35758 
+DELETE FROM `creature_queststarter` WHERE `Id` = 35758 ;
+INSERT INTO `creature_queststarter` VALUES ( '35758', '14019'); -- Monkey Business
+
+-- Parsed Creature quest for Creature id : 35769 
+DELETE FROM `creature_queststarter` WHERE `Id` = 35769 ;
+INSERT INTO `creature_queststarter` VALUES ( '35769', '14021'); -- Miner Troubles
+INSERT INTO `creature_queststarter` VALUES ( '35769', '14031'); -- Capturing the Unknown
+
+-- Parsed Creature quest for Creature id : 35786 
+DELETE FROM `creature_queststarter` WHERE `Id` = 35786 ;
+INSERT INTO `creature_queststarter` VALUES ( '35786', '14473'); -- It's Our Problem Now
+
+-- Parsed Creature quest for Creature id : 35837 
+DELETE FROM `creature_queststarter` WHERE `Id` = 35837 ;
+INSERT INTO `creature_queststarter` VALUES ( '35837', '14233'); -- Orcs Can Write?
+
+-- Parsed Creature quest for Creature id : 35875 
+DELETE FROM `creature_queststarter` WHERE `Id` = 35875 ;
+INSERT INTO `creature_queststarter` VALUES ( '35875', '14235'); -- The Vicious Vale
+INSERT INTO `creature_queststarter` VALUES ( '35875', '14237'); -- Forward Movement
+
+-- Parsed Creature quest for Creature id : 35893 
+DELETE FROM `creature_queststarter` WHERE `Id` = 35893 ;
+INSERT INTO `creature_queststarter` VALUES ( '35893', '14236'); -- Weed Whacker
+INSERT INTO `creature_queststarter` VALUES ( '35893', '14303'); -- Back to Aggra
+
+-- Parsed Creature quest for Creature id : 35917 
+DELETE FROM `creature_queststarter` WHERE `Id` = 35917 ;
+INSERT INTO `creature_queststarter` VALUES ( '35917', '14238'); -- Infrared = Infradead
+INSERT INTO `creature_queststarter` VALUES ( '35917', '14240'); -- To the Cliffs
+
+-- Parsed Creature quest for Creature id : 36112 
+DELETE FROM `creature_queststarter` WHERE `Id` = 36112 ;
+INSERT INTO `creature_queststarter` VALUES ( '36112', '14241'); -- Get to the Gyrochoppa!
+
+-- Parsed Creature quest for Creature id : 36127 
+DELETE FROM `creature_queststarter` WHERE `Id` = 36127 ;
+INSERT INTO `creature_queststarter` VALUES ( '36127', '14242'); -- Precious Cargo
+
+-- Parsed Creature quest for Creature id : 36145 
+DELETE FROM `creature_queststarter` WHERE `Id` = 36145 ;
+INSERT INTO `creature_queststarter` VALUES ( '36145', '14326'); -- Meet Me Up Top
+
+-- Parsed Creature quest for Creature id : 36161 
+DELETE FROM `creature_queststarter` WHERE `Id` = 36161 ;
+INSERT INTO `creature_queststarter` VALUES ( '36161', '14243'); -- Warchief's Revenge
+
+-- Parsed Creature quest for Creature id : 36188 
+DELETE FROM `creature_queststarter` WHERE `Id` = 36188 ;
+INSERT INTO `creature_queststarter` VALUES ( '36188', '14445'); -- Farewell, For Now
+
+-- Parsed Creature quest for Creature id : 36425 
+DELETE FROM `creature_queststarter` WHERE `Id` = 36425 ;
+INSERT INTO `creature_queststarter` VALUES ( '36425', '14244'); -- Up, Up & Away!
+
+-- Parsed Creature quest for Creature id : 36470 
+DELETE FROM `creature_queststarter` WHERE `Id` = 36470 ;
+INSERT INTO `creature_queststarter` VALUES ( '36470', '14245'); -- It's a Town-In-A-Box
+
+-- Parsed Creature quest for Creature id : 36471 
+DELETE FROM `creature_queststarter` WHERE `Id` = 36471 ;
+INSERT INTO `creature_queststarter` VALUES ( '36471', '24945'); -- Three Little Pygmies
+INSERT INTO `creature_queststarter` VALUES ( '36471', '27139'); -- Hobart Needs You
+
+-- Parsed Creature quest for Creature id : 36600 
+DELETE FROM `creature_queststarter` WHERE `Id` = 36600 ;
+INSERT INTO `creature_queststarter` VALUES ( '36600', '14001'); -- Goblin Escape Pods
+INSERT INTO `creature_queststarter` VALUES ( '36600', '14474'); -- Goblin Escape Pods
+
+-- Parsed Creature quest for Creature id : 38120 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38120 ;
+INSERT INTO `creature_queststarter` VALUES ( '38120', '24671'); -- Cluster Cluck
+INSERT INTO `creature_queststarter` VALUES ( '38120', '24744'); -- The Biggest Egg Ever
+INSERT INTO `creature_queststarter` VALUES ( '38120', '24816'); -- Who's Top of the Food Chain Now?
+INSERT INTO `creature_queststarter` VALUES ( '38120', '24856'); -- Invasion Imminent!
+INSERT INTO `creature_queststarter` VALUES ( '38120', '24954'); -- Children of a Turtle God
+INSERT INTO `creature_queststarter` VALUES ( '38120', '24958'); -- Volcanoth!
+INSERT INTO `creature_queststarter` VALUES ( '38120', '25214'); -- Escape Velocity
+
+-- Parsed Creature quest for Creature id : 38122 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38122 ;
+INSERT INTO `creature_queststarter` VALUES ( '38122', '24741'); -- Trading Up
+
+-- Parsed Creature quest for Creature id : 38124 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38124 ;
+INSERT INTO `creature_queststarter` VALUES ( '38124', '24817'); -- A Goblin in Shark's Clothing
+INSERT INTO `creature_queststarter` VALUES ( '38124', '24946'); -- Rockin' Powder
+INSERT INTO `creature_queststarter` VALUES ( '38124', '25110'); -- Kaja'Cola Gives You IDEAS! (TM)
+INSERT INTO `creature_queststarter` VALUES ( '38124', '25200'); -- Shredder Shutdown
+INSERT INTO `creature_queststarter` VALUES ( '38124', '25204'); -- Release the Valves
+INSERT INTO `creature_queststarter` VALUES ( '38124', '25213'); -- The Slave Pits
+
+-- Parsed Creature quest for Creature id : 38381 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38381 ;
+INSERT INTO `creature_queststarter` VALUES ( '38381', '24859'); -- Naga Hide
+
+-- Parsed Creature quest for Creature id : 38387 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38387 ;
+INSERT INTO `creature_queststarter` VALUES ( '38387', '24901'); -- Town-In-A-Box: Under Attack
+INSERT INTO `creature_queststarter` VALUES ( '38387', '24924'); -- Oomlot Village
+INSERT INTO `creature_queststarter` VALUES ( '38387', '24940'); -- Up the Volcano
+INSERT INTO `creature_queststarter` VALUES ( '38387', '25058'); -- Mine Disposal, the Goblin Way
+INSERT INTO `creature_queststarter` VALUES ( '38387', '25066'); -- The Pride of Kezan
+INSERT INTO `creature_queststarter` VALUES ( '38387', '25098'); -- The Warchief Wants You
+INSERT INTO `creature_queststarter` VALUES ( '38387', '25243'); -- She Loves Me, She Loves Me NOT!
+INSERT INTO `creature_queststarter` VALUES ( '38387', '25244'); -- What Kind of Name is Candy, Anyway?
+INSERT INTO `creature_queststarter` VALUES ( '38387', '25251'); -- Final Confrontation
+INSERT INTO `creature_queststarter` VALUES ( '38387', '25265'); -- Victory!
+
+-- Parsed Creature quest for Creature id : 38432 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38432 ;
+INSERT INTO `creature_queststarter` VALUES ( '38432', '24858'); -- Bilgewater Cartel Represent
+INSERT INTO `creature_queststarter` VALUES ( '38432', '24864'); -- Irresistible Pool Pony
+INSERT INTO `creature_queststarter` VALUES ( '38432', '24868'); -- Surrender or Else!
+INSERT INTO `creature_queststarter` VALUES ( '38432', '24897'); -- Get Back to Town
+
+-- Parsed Creature quest for Creature id : 38441 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38441 ;
+INSERT INTO `creature_queststarter` VALUES ( '38441', '25203'); -- What Kind of Name is Chip, Anyway?
+
+-- Parsed Creature quest for Creature id : 38517 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38517 ;
+INSERT INTO `creature_queststarter` VALUES ( '38517', '25109'); -- The Gallywix Labor Mine
+
+-- Parsed Creature quest for Creature id : 38647 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38647 ;
+INSERT INTO `creature_queststarter` VALUES ( '38647', '24925'); -- Free the Captives
+INSERT INTO `creature_queststarter` VALUES ( '38647', '24929'); -- Send a Message
+INSERT INTO `creature_queststarter` VALUES ( '38647', '24937'); -- Oomlot Dealt With
+INSERT INTO `creature_queststarter` VALUES ( '38647', '25202'); -- The Fastest Way to His Heart
+
+-- Parsed Creature quest for Creature id : 38738 
+DELETE FROM `creature_queststarter` WHERE `Id` = 38738 ;
+INSERT INTO `creature_queststarter` VALUES ( '38738', '24942'); -- Zombies vs. Super Booster Rocket Boots
+INSERT INTO `creature_queststarter` VALUES ( '38738', '24952'); -- Rocket Boot Boost
+INSERT INTO `creature_queststarter` VALUES ( '38738', '25201'); -- The Ultimate Footbomb Uniform
+
+-- Parsed Creature quest for Creature id : 35650 
+DELETE FROM `creature_questender` WHERE `Id` = 35650 ;
+INSERT INTO `creature_questender` VALUES ( '35650', '14001'); -- Goblin Escape Pods
+INSERT INTO `creature_questender` VALUES ( '35650', '14014'); -- Get Our Stuff Back!
+INSERT INTO `creature_questender` VALUES ( '35650', '14031'); -- Capturing the Unknown
+INSERT INTO `creature_questender` VALUES ( '35650', '14233'); -- Orcs Can Write?
+INSERT INTO `creature_questender` VALUES ( '35650', '14474'); -- Goblin Escape Pods
+
+-- Parsed Creature quest for Creature id : 35758 
+DELETE FROM `creature_questender` WHERE `Id` = 35758 ;
+INSERT INTO `creature_questender` VALUES ( '35758', '14019'); -- Monkey Business
+
+-- Parsed Creature quest for Creature id : 35769 
+DELETE FROM `creature_questender` WHERE `Id` = 35769 ;
+INSERT INTO `creature_questender` VALUES ( '35769', '14021'); -- Miner Troubles
+INSERT INTO `creature_questender` VALUES ( '35769', '14248'); -- Help Wanted
+
+-- Parsed Creature quest for Creature id : 35786 
+DELETE FROM `creature_questender` WHERE `Id` = 35786 ;
+INSERT INTO `creature_questender` VALUES ( '35786', '14473'); -- It's Our Problem Now
+
+-- Parsed Creature quest for Creature id : 35875 
+DELETE FROM `creature_questender` WHERE `Id` = 35875 ;
+INSERT INTO `creature_questender` VALUES ( '35875', '14234'); -- The Enemy of My Enemy
+INSERT INTO `creature_questender` VALUES ( '35875', '14303'); -- Back to Aggra
+
+-- Parsed Creature quest for Creature id : 35893 
+DELETE FROM `creature_questender` WHERE `Id` = 35893 ;
+INSERT INTO `creature_questender` VALUES ( '35893', '14235'); -- The Vicious Vale
+INSERT INTO `creature_questender` VALUES ( '35893', '14236'); -- Weed Whacker
+
+-- Parsed Creature quest for Creature id : 35917 
+DELETE FROM `creature_questender` WHERE `Id` = 35917 ;
+INSERT INTO `creature_questender` VALUES ( '35917', '14237'); -- Forward Movement
+INSERT INTO `creature_questender` VALUES ( '35917', '14238'); -- Infrared = Infradead
+
+-- Parsed Creature quest for Creature id : 36112 
+DELETE FROM `creature_questender` WHERE `Id` = 36112 ;
+INSERT INTO `creature_questender` VALUES ( '36112', '14240'); -- To the Cliffs
+
+-- Parsed Creature quest for Creature id : 36127 
+DELETE FROM `creature_questender` WHERE `Id` = 36127 ;
+INSERT INTO `creature_questender` VALUES ( '36127', '14241'); -- Get to the Gyrochoppa!
+
+-- Parsed Creature quest for Creature id : 36145 
+DELETE FROM `creature_questender` WHERE `Id` = 36145 ;
+INSERT INTO `creature_questender` VALUES ( '36145', '14242'); -- Precious Cargo
+
+-- Parsed Creature quest for Creature id : 36161 
+DELETE FROM `creature_questender` WHERE `Id` = 36161 ;
+INSERT INTO `creature_questender` VALUES ( '36161', '14326'); -- Meet Me Up Top
+
+-- Parsed Creature quest for Creature id : 36188 
+DELETE FROM `creature_questender` WHERE `Id` = 36188 ;
+INSERT INTO `creature_questender` VALUES ( '36188', '14243'); -- Warchief's Revenge
+
+-- Parsed Creature quest for Creature id : 36425 
+DELETE FROM `creature_questender` WHERE `Id` = 36425 ;
+INSERT INTO `creature_questender` VALUES ( '36425', '14445'); -- Farewell, For Now
+
+-- Parsed Creature quest for Creature id : 36470 
+DELETE FROM `creature_questender` WHERE `Id` = 36470 ;
+INSERT INTO `creature_questender` VALUES ( '36470', '14244'); -- Up, Up & Away!
+
+-- Parsed Creature quest for Creature id : 36471 
+DELETE FROM `creature_questender` WHERE `Id` = 36471 ;
+INSERT INTO `creature_questender` VALUES ( '36471', '14245'); -- It's a Town-In-A-Box
+INSERT INTO `creature_questender` VALUES ( '36471', '24945'); -- Three Little Pygmies
+
+-- Parsed Creature quest for Creature id : 38120 
+DELETE FROM `creature_questender` WHERE `Id` = 38120 ;
+INSERT INTO `creature_questender` VALUES ( '38120', '24671'); -- Cluster Cluck
+INSERT INTO `creature_questender` VALUES ( '38120', '24744'); -- The Biggest Egg Ever
+INSERT INTO `creature_questender` VALUES ( '38120', '24817'); -- A Goblin in Shark's Clothing
+INSERT INTO `creature_questender` VALUES ( '38120', '24952'); -- Rocket Boot Boost
+INSERT INTO `creature_questender` VALUES ( '38120', '24954'); -- Children of a Turtle God
+INSERT INTO `creature_questender` VALUES ( '38120', '25214'); -- Escape Velocity
+INSERT INTO `creature_questender` VALUES ( '38120', '27139'); -- Hobart Needs You
+
+-- Parsed Creature quest for Creature id : 38122 
+DELETE FROM `creature_questender` WHERE `Id` = 38122 ;
+INSERT INTO `creature_questender` VALUES ( '38122', '24741'); -- Trading Up
+
+-- Parsed Creature quest for Creature id : 38124 
+DELETE FROM `creature_questender` WHERE `Id` = 38124 ;
+INSERT INTO `creature_questender` VALUES ( '38124', '24816'); -- Who's Top of the Food Chain Now?
+INSERT INTO `creature_questender` VALUES ( '38124', '24946'); -- Rockin' Powder
+INSERT INTO `creature_questender` VALUES ( '38124', '25109'); -- The Gallywix Labor Mine
+INSERT INTO `creature_questender` VALUES ( '38124', '25110'); -- Kaja'Cola Gives You IDEAS! (TM)
+INSERT INTO `creature_questender` VALUES ( '38124', '25184'); -- Wild Mine Cart Ride
+INSERT INTO `creature_questender` VALUES ( '38124', '25200'); -- Shredder Shutdown
+INSERT INTO `creature_questender` VALUES ( '38124', '25207'); -- Good-bye, Sweet Oil
+
+-- Parsed Creature quest for Creature id : 38381 
+DELETE FROM `creature_questender` WHERE `Id` = 38381 ;
+INSERT INTO `creature_questender` VALUES ( '38381', '24859'); -- Naga Hide
+
+-- Parsed Creature quest for Creature id : 38387 
+DELETE FROM `creature_questender` WHERE `Id` = 38387 ;
+INSERT INTO `creature_questender` VALUES ( '38387', '24897'); -- Get Back to Town
+INSERT INTO `creature_questender` VALUES ( '38387', '24901'); -- Town-In-A-Box: Under Attack
+INSERT INTO `creature_questender` VALUES ( '38387', '24937'); -- Oomlot Dealt With
+INSERT INTO `creature_questender` VALUES ( '38387', '25058'); -- Mine Disposal, the Goblin Way
+INSERT INTO `creature_questender` VALUES ( '38387', '25066'); -- The Pride of Kezan
+INSERT INTO `creature_questender` VALUES ( '38387', '25213'); -- The Slave Pits
+INSERT INTO `creature_questender` VALUES ( '38387', '25243'); -- She Loves Me, She Loves Me NOT!
+INSERT INTO `creature_questender` VALUES ( '38387', '25244'); -- What Kind of Name is Candy, Anyway?
+INSERT INTO `creature_questender` VALUES ( '38387', '25251'); -- Final Confrontation
+
+-- Parsed Creature quest for Creature id : 38432 
+DELETE FROM `creature_questender` WHERE `Id` = 38432 ;
+INSERT INTO `creature_questender` VALUES ( '38432', '24856'); -- Invasion Imminent!
+INSERT INTO `creature_questender` VALUES ( '38432', '24858'); -- Bilgewater Cartel Represent
+INSERT INTO `creature_questender` VALUES ( '38432', '24864'); -- Irresistible Pool Pony
+INSERT INTO `creature_questender` VALUES ( '38432', '24868'); -- Surrender or Else!
+
+-- Parsed Creature quest for Creature id : 38441 
+DELETE FROM `creature_questender` WHERE `Id` = 38441 ;
+INSERT INTO `creature_questender` VALUES ( '38441', '25203'); -- What Kind of Name is Chip, Anyway?
+
+-- Parsed Creature quest for Creature id : 38517 
+DELETE FROM `creature_questender` WHERE `Id` = 38517 ;
+INSERT INTO `creature_questender` VALUES ( '38517', '25100'); -- Let's Ride
+
+-- Parsed Creature quest for Creature id : 38647 
+DELETE FROM `creature_questender` WHERE `Id` = 38647 ;
+INSERT INTO `creature_questender` VALUES ( '38647', '24924'); -- Oomlot Village
+INSERT INTO `creature_questender` VALUES ( '38647', '24925'); -- Free the Captives
+INSERT INTO `creature_questender` VALUES ( '38647', '24929'); -- Send a Message
+INSERT INTO `creature_questender` VALUES ( '38647', '25202'); -- The Fastest Way to His Heart
+
+-- Parsed Creature quest for Creature id : 38738 
+DELETE FROM `creature_questender` WHERE `Id` = 38738 ;
+INSERT INTO `creature_questender` VALUES ( '38738', '24940'); -- Up the Volcano
+INSERT INTO `creature_questender` VALUES ( '38738', '24942'); -- Zombies vs. Super Booster Rocket Boots
+INSERT INTO `creature_questender` VALUES ( '38738', '25201'); -- The Ultimate Footbomb Uniform
