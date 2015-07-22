75a5cd36f601f802ce52b504a11c8f2e04abb3c2
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-07-23 00:19:37 +0200

    DB/Update creature_equip_template 51346

---
+++ sql/updates/world/2015_07_22_03_creature_equip_template.sql
@@ -0,0 +1,2 @@
+DELETE FROM `creature_equip_template` WHERE `entry`= 51346;
+INSERT INTO `creature_equip_template` (`entry`, `id`, `itemEntry1`, `itemEntry2`, `itemEntry3`) VALUES (51346, 1, 5289, 0, 2507);
