51a7c190ab3ae639923059f4f0fc67799a3b2ed8
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-07-23 01:04:37 +0200

    DB/Update somes creatures_model & template

---
+++ sql/updates/world/2015_07_23_02_update_creature_template.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `faction_H`=14 WHERE `entry`= 69136;
+UPDATE `creature_template` SET `faction_H`=35 WHERE `entry`= 69068;
+UPDATE `creature_template` SET `faction_H`=1924 WHERE `entry`= 69362;
+UPDATE `creature_template` SET `faction_H`=2478 WHERE `entry`= 68410;
