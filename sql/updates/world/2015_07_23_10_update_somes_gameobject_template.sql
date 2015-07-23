2ef48e5bccf52b3473cff1980484bdfcf7f7beed
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-07-23 03:27:45 +0200

    DB/update fix my shit :)

---
+++ sql/updates/world/2015_07_23_10_update_somes_gameobject_template.sql
@@ -0,0 +1,11 @@
+DELETE FROM `gameobject_template` WHERE `entry`= 212955;
+DELETE FROM `gameobject_template` WHERE `entry`= 213160;
+DELETE FROM `gameobject_template` WHERE `entry`= 213162;
+DELETE FROM `gameobject_template` WHERE `entry`= 213167;
+DELETE FROM `gameobject_template` WHERE `entry`= 216355;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `data24`, `data25`, `data26`, `data27`, `data28`, `data29`, `data30`, `data31`, `unkInt32`, `AIName`, `ScriptName`, `WDBVerified`) VALUES 
+(212955, 5, 11873, 'Mogu Statue Base, Large', '', '', '', 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 18414),
+(213160, 5, 11917, 'Spider Web Hanging Arch 3', '', '', '', 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 18414),
+(213162, 5, 8334, 'Spider Web Straight Rope 1', '', '', '', 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 18414),
+(213167, 5, 11919, 'Spider Web Round Ground Wrap', '', '', '', 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 18414),
+(216355, 0, 13418, 'Doodad_PA_Alliance_Arena_Gate004', '', '', '', 0, 0, 0.895977, 0, 0, 0, 0, 0, 0, 0, 5773332, 3000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', '', 18414);
