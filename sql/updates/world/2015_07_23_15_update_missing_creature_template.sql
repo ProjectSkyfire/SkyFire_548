dadeb1c7235d96ad75e119178955b9835fc2bacc
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-07-23 05:47:23 +0200

    DB/Update missing creature data

---
+++ sql/updates/world/2015_07_23_15_update_missing_creature_template.sql
@@ -0,0 +1,242 @@
+INSERT IGNORE INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `exp_unk`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `type_flags2`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `Health_mod`, `Mana_mod`, `Mana_mod_extra`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES 
+(68392, 0, 0, 0, 0, 0, 28127, 0, 0, 0, 'King Varian Wrynn', 'King of Stormwind', '', 0, 3, 3, -1, 0, 1733, 1733, 0, 1, 1.14286, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 68, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1200, 20, 1, 1, 0, 0, 0, 0, 0, 0, 0, 144, 1, 0, 0, '', 18414),
+(69433, 0, 0, 0, 0, 0, 47529, 0, 0, 0, 'Magistrix Saia', 'Illusions and Transformations', '', 0, 90, 90, 4, 0, 2123, 2123, 0, 1, 1.14286, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, '', 18414),
+(70297, 0, 0, 0, 0, 0, 41792, 0, 0, 0, 'Taoshi', '', '', 0, 90, 90, 4, 0, 2491, 2491, 0, 1, 1.14286, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, '', 18414),
+(70371, 0, 0, 0, 0, 0, 17122, 0, 0, 0, 'Lor\'themar Theron', 'Regent Lord of Quel\'Thalas', '', 0, 3, 3, -1, 0, 2110, 2110, 0, 1, 1.14286, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1200, 30, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, '', 18414),
+(73335, 0, 0, 0, 0, 0, 49124, 0, 0, 0, 'Fading Breath', '', '', 0, 90, 90, 4, 0, 35, 35, 0, 1, 1.14286, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 1611923600, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, '', 18414);
+
+UPDATE `creature_template` SET `npcflag`= 131 WHERE `entry`= 384;
+UPDATE `creature_template` SET `npcflag`= 131 WHERE `entry`= 1261;
+UPDATE `creature_template` SET `npcflag`= 131 WHERE `entry`= 3362;
+UPDATE `creature_template` SET `npcflag`= 131 WHERE `entry`= 3685;
+UPDATE `creature_template` SET `npcflag`= 131 WHERE `entry`= 4730;
+UPDATE `creature_template` SET `npcflag`= 131 WHERE `entry`= 7952;
+UPDATE `creature_template` SET `npcflag`= 131 WHERE `entry`= 17584;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 40855;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 41530;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 58038;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 55438;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 55497;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 56113;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 56366;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 56467;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 56490;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 58384;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 58385;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 58386;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 58718;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 58814;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 59189;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 59517;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 59594;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 61021;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 61534;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 61636;
+UPDATE `creature_template` SET `npcflag`= 65539 WHERE `entry`= 62996;
+UPDATE `creature_template` SET `npcflag`= 643 WHERE `entry`= 63013;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 63067;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 63197;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 64013;
+UPDATE `creature_template` SET `npcflag`= 643 WHERE `entry`= 64041;
+UPDATE `creature_template` SET `npcflag`= 211 WHERE `entry`= 64058;
+UPDATE `creature_template` SET `npcflag`= 4307 WHERE `entry`= 64085;
+UPDATE `creature_template` SET `npcflag`= 643 WHERE `entry`= 64101;
+UPDATE `creature_template` SET `npcflag`= 65539 WHERE `entry`= 64149;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 64566;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 64572;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 64610;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 64672;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 64842;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 64901;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 65454;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 65669;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 65670;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 65671;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 65673;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 65674;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 65679;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 65724;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 65975;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66352;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66412;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66436;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66442;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66452;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66478;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66512;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66515;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66518;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66520;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66550;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66551;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66552;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66553;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66574;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66635;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66636;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66638;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 66639;
+UPDATE `creature_template` SET `npcflag`= 643 WHERE `entry`= 66998;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67153;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67660;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67662;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67663;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67673;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67983;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67984;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67985;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67986;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67989;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 67990;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 68274;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 69371;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 69389;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 69418;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 69425;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 69551;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 69572;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 69742;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 70160;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 70316;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 70520;
+UPDATE `creature_template` SET `npcflag`= 3 WHERE `entry`= 73348;
+
+INSERT IGNORE INTO `creature_model_info` (`modelid`, `bounding_radius`, `combat_reach`, `gender`, `modelid_other_gender`) VALUES 
+(46209, 0, 0, 0, 0),
+(44005, 0, 0, 0, 0),
+(47127, 0, 0, 0, 0),
+(47128, 0, 0, 0, 0),
+(47129, 0, 0, 0, 0),
+(47130, 0, 0, 0, 0),
+(47601, 0, 0, 0, 0),
+(48188, 0, 0, 0, 0),
+(47511, 0, 0, 0, 0),
+(46367, 0, 0, 0, 0),
+(47867, 0, 0, 0, 0),
+(48810, 0, 0, 0, 0),
+(47866, 0, 0, 0, 0),
+(44111, 0, 0, 0, 0),
+(48742, 0, 0, 0, 0),
+(46598, 0, 0, 0, 0),
+(47135, 0, 0, 0, 0),
+(47143, 0, 0, 0, 0),
+(46597, 0, 0, 0, 0),
+(46664, 0, 0, 0, 0),
+(47599, 0, 0, 0, 0),
+(48293, 0, 0, 0, 0),
+(50753, 0, 0, 0, 0),
+(47664, 0, 0, 0, 0),
+(49124, 0, 0, 0, 0),
+(47270, 0, 0, 0, 0),
+(46522, 0, 0, 0, 0),
+(46523, 0, 0, 0, 0),
+(46524, 0, 0, 0, 0),
+(46525, 0, 0, 0, 0),
+(47223, 0, 0, 0, 0),
+(48039, 0, 0, 0, 0),
+(48097, 0, 0, 0, 0),
+(49809, 0, 0, 0, 0),
+(47529, 0, 0, 0, 0),
+(47663, 0, 0, 0, 0),
+(47484, 0, 0, 0, 0),
+(46352, 0, 0, 0, 0),
+(46707, 0, 0, 0, 0),
+(46462, 0, 0, 0, 0),
+(47067, 0, 0, 0, 0),
+(47068, 0, 0, 0, 0),
+(47069, 0, 0, 0, 0),
+(47070, 0, 0, 0, 0),
+(46418, 0, 0, 0, 0),
+(46749, 0, 0, 0, 0),
+(46591, 0, 0, 0, 0),
+(46592, 0, 0, 0, 0),
+(46188, 0, 0, 0, 0),
+(46209, 0, 0, 0, 0),
+(46209, 0, 0, 0, 0),
+(48044, 0, 0, 0, 0),
+(47138, 0, 0, 0, 0),
+(46481, 0, 0, 0, 0),
+(48053, 0, 0, 0, 0),
+(47668, 0, 0, 0, 0),
+(47599, 0, 0, 0, 0),
+(46351, 0, 0, 0, 0),
+(46350, 0, 0, 0, 0),
+(47489, 0, 0, 0, 0),
+(48518, 0, 0, 0, 0),
+(48542, 0, 0, 0, 0),
+(47606, 0, 0, 0, 0),
+(46873, 0, 0, 0, 0),
+(42932, 0, 0, 0, 0),
+(47246, 0, 0, 0, 0),
+(46589, 0, 0, 0, 0),
+(48188, 0, 0, 0, 0),
+(46808, 0, 0, 0, 0),
+(47975, 0, 0, 0, 0),
+(47530, 0, 0, 0, 0),
+(48164, 0, 0, 0, 0),
+(47468, 0, 0, 0, 0),
+(47139, 0, 0, 0, 0),
+(48293, 0, 0, 0, 0),
+(46419, 0, 0, 0, 0),
+(47090, 0, 0, 0, 0),
+(47108, 0, 0, 0, 0),
+(47118, 0, 0, 0, 0),
+(48045, 0, 0, 0, 0),
+(47665, 0, 0, 0, 0),
+(46595, 0, 0, 0, 0),
+(46883, 0, 0, 0, 0),
+(47753, 0, 0, 0, 0),
+(46589, 0, 0, 0, 0),
+(46590, 0, 0, 0, 0),
+(47962, 0, 0, 0, 0),
+(47486, 0, 0, 0, 0),
+(47952, 0, 0, 0, 0),
+(47666, 0, 0, 0, 0),
+(46714, 0, 0, 0, 0),
+(46807, 0, 0, 0, 0),
+(47696, 0, 0, 0, 0),
+(46911, 0, 0, 0, 0),
+(48186, 0, 0, 0, 0),
+(47435, 0, 0, 0, 0),
+(47554, 0, 0, 0, 0),
+(47260, 0, 0, 0, 0),
+(46605, 0, 0, 0, 0),
+(46606, 0, 0, 0, 0),
+(46607, 0, 0, 0, 0),
+(46608, 0, 0, 0, 0),
+(46600, 0, 0, 0, 0),
+(46564, 0, 0, 0, 0),
+(46419, 0, 0, 0, 0),
+(46419, 0, 0, 0, 0),
+(46427, 0, 0, 0, 0),
+(46576, 0, 0, 0, 0),
+(46578, 0, 0, 0, 0),
+(46577, 0, 0, 0, 0),
+(46579, 0, 0, 0, 0),
+(46568, 0, 0, 0, 0),
+(46617, 0, 0, 0, 0),
+(46819, 0, 0, 0, 0),
+(46479, 0, 0, 0, 0),
+(46413, 0, 0, 0, 0),
+(46749, 0, 0, 0, 0),
+(46707, 0, 0, 0, 0),
+(47137, 0, 0, 0, 0),
+(46479, 0, 0, 0, 0),
+(46339, 0, 0, 0, 0),
+(46339, 0, 0, 0, 0),
+(47551, 0, 0, 0, 0),
+(47384, 0, 0, 0, 0),
+(47492, 0, 0, 0, 0),
+(47437, 0, 0, 0, 0),
+(47438, 0, 0, 0, 0),
+(48293, 0, 0, 0, 0),
+(47621, 0, 0, 0, 0),
+(47485, 0, 0, 0, 0),
+(47599, 0, 0, 0, 0),
+(47418, 0, 0, 0, 0),
+(47963, 0, 0, 0, 0),
+(47963, 0, 0, 0, 0),
+(48111, 0, 0, 0, 0),
+(48854, 0, 0, 0, 0),
+(48741, 0, 0, 0, 0),
+(49585, 0, 0, 0, 0);
