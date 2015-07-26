e1699658f8d3d4829188b4308edf49ccd887392f
AriDEV <aridev666@gmail.com>
2015-07-26 22:20:57 +0200

    Server/Globals: Fixed an issue at FillItemDamageField where it loaded damagetype from database instead of delay.
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -2634,7 +2634,7 @@ void ObjectMgr::LoadItemTemplates()
 
             // cache item damage
             FillItemDamageFields(&itemTemplate.DamageMin, &itemTemplate.DamageMax, &itemTemplate.DPS, itemTemplate.ItemLevel,
-                                 itemTemplate.Class, itemTemplate.SubClass, itemTemplate.Quality, fields[71].GetUInt16(),
+                                 itemTemplate.Class, itemTemplate.SubClass, itemTemplate.Quality, fields[72].GetUInt16(),
                                  fields[131].GetFloat(), itemTemplate.InventoryType, itemTemplate.Flags2);
 
             itemTemplate.DamageType                = fields[71].GetUInt8();
