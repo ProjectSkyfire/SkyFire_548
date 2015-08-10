367a510bcbb9b737dec6f62c9c389625b4717159
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-08-10 16:46:54 +0200

    Create 2015_08_10_00_smart_scripts.sql
    Test my new smart_scripts generator for creature in timeless isle :
    72764, 72844, 72765, 72777, 72762 !
    suggestion ?

---
+++ sql/updates/world/2015_08_10_00_smart_scripts.sql
@@ -0,0 +1,43 @@
+
+-- Parsed Creatures SmartScripts for creatureID : 72764.
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`= 72764; -- Add SmartAI in creature_template table for creatureID : 72764
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72764 AND `action_param1` = 147568; -- Delete the old data in smart_scripts table for creatureID : 72764
+INSERT INTO `smart_scripts` VALUES ( '72764', '0', '0', '0', '0', '0', '100', '0', '2000', '10000', '35000', '45000', '11', '147568', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Snapping Bite');
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72764 AND `action_param1` = 147571; -- Delete the old data in smart_scripts table for creatureID : 72764
+INSERT INTO `smart_scripts` VALUES ( '72764', '0', '1', '0', '0', '0', '100', '0', '4000', '20000', '70000', '90000', '11', '147571', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Shell Spin');
+
+
+-- Parsed Creatures SmartScripts for creatureID : 72844.
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`= 72844; -- Add SmartAI in creature_template table for creatureID : 72844
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72844 AND `action_param1` = 147368; -- Delete the old data in smart_scripts table for creatureID : 72844
+INSERT INTO `smart_scripts` VALUES ( '72844', '0', '0', '0', '0', '0', '100', '0', '2000', '10000', '35000', '45000', '11', '147368', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Iron Fur');
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72844 AND `action_param1` = 147385; -- Delete the old data in smart_scripts table for creatureID : 72844
+INSERT INTO `smart_scripts` VALUES ( '72844', '0', '1', '0', '0', '0', '100', '0', '4000', '20000', '70000', '90000', '11', '147385', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Ox Charge');
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72844 AND `action_param1` = 147386; -- Delete the old data in smart_scripts table for creatureID : 72844
+INSERT INTO `smart_scripts` VALUES ( '72844', '0', '2', '0', '0', '0', '100', '0', '6000', '30000', '105000', '135000', '11', '147386', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Ox Charge');
+
+
+-- Parsed Creatures SmartScripts for creatureID : 72765.
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`= 72765; -- Add SmartAI in creature_template table for creatureID : 72765
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72765 AND `action_param1` = 147573; -- Delete the old data in smart_scripts table for creatureID : 72765
+INSERT INTO `smart_scripts` VALUES ( '72765', '0', '0', '0', '0', '0', '100', '0', '2000', '10000', '35000', '45000', '11', '147573', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Geyser');
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72765 AND `action_param1` = 147589; -- Delete the old data in smart_scripts table for creatureID : 72765
+INSERT INTO `smart_scripts` VALUES ( '72765', '0', '1', '0', '0', '0', '100', '0', '4000', '20000', '70000', '90000', '11', '147589', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Snapping Bite');
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72765 AND `action_param1` = 147590; -- Delete the old data in smart_scripts table for creatureID : 72765
+INSERT INTO `smart_scripts` VALUES ( '72765', '0', '2', '0', '0', '0', '100', '0', '6000', '30000', '105000', '135000', '11', '147590', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Shell Spin');
+
+
+-- Parsed Creatures SmartScripts for creatureID : 72777.
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`= 72777; -- Add SmartAI in creature_template table for creatureID : 72777
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72777 AND `action_param1` = 147654; -- Delete the old data in smart_scripts table for creatureID : 72777
+INSERT INTO `smart_scripts` VALUES ( '72777', '0', '0', '0', '0', '0', '100', '0', '2000', '10000', '35000', '45000', '11', '147654', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Toxic Skin');
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72777 AND `action_param1` = 147655; -- Delete the old data in smart_scripts table for creatureID : 72777
+INSERT INTO `smart_scripts` VALUES ( '72777', '0', '1', '0', '0', '0', '100', '0', '4000', '20000', '70000', '90000', '11', '147655', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Gulp Frog Toxin');
+
+
+-- Parsed Creatures SmartScripts for creatureID : 72762.
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`= 72762; -- Add SmartAI in creature_template table for creatureID : 72762
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72762 AND `action_param1` = 147306; -- Delete the old data in smart_scripts table for creatureID : 72762
+INSERT INTO `smart_scripts` VALUES ( '72762', '0', '0', '0', '0', '0', '100', '0', '2000', '10000', '35000', '45000', '11', '147306', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Windfeather');
+DELETE FROM `smart_scripts` WHERE `entryorguid`= 72762 AND `action_param1` = 147310; -- Delete the old data in smart_scripts table for creatureID : 72762
+INSERT INTO `smart_scripts` VALUES ( '72762', '0', '1', '0', '0', '0', '100', '0', '4000', '20000', '70000', '90000', '11', '147310', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '@Gust of Wind');
