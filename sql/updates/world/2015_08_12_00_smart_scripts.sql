86aa366d900d8910e7ca70697f27dacf54ef6d98
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-08-12 16:43:48 +0200

    DB/update fix quest 8346 Thirst Unending
    Close #608

---
+++ sql/updates/world/2015_08_12_00_smart_scripts.sql
@@ -0,0 +1,10 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 15274;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(15274, 0, 0, 0, 8, 0, 100, 1, 28730, 0, 0, 0, 11, 61314, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Mana Wyrm - On Spellhit \'Arcane Torrent\' - Cast \'Quest Credit 8346\' (No Repeat)'),
+(15274, 0, 1, 0, 8, 0, 100, 1, 25046, 0, 0, 0, 11, 61314, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Mana Wyrm - On Spellhit \'Arcane Torrent\' - Cast \'Quest Credit 8346\' (No Repeat)'),
+(15274, 0, 2, 0, 8, 0, 100, 1, 50613, 0, 0, 0, 11, 61314, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Mana Wyrm - On Spellhit \'Arcane Torrent\' - Cast \'Quest Credit 8346\' (No Repeat)'),
+(15274, 0, 3, 0, 8, 0, 100, 1, 69179, 0, 0, 0, 11, 61314, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Mana Wyrm - On Spellhit \'Arcane Torrent\' - Cast \'Quest Credit 8346\' (No Repeat)'),
+(15274, 0, 4, 0, 8, 0, 100, 1, 80483, 0, 0, 0, 11, 61314, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Mana Wyrm - On Spellhit \'Arcane Torrent\' - Cast \'Quest Credit 8346\' (No Repeat)'),
+(15274, 0, 5, 0, 8, 0, 100, 1, 155145, 0, 0, 0, 11, 61314, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Mana Wyrm - On Spellhit \'Arcane Torrent\' - Cast \'Quest Credit 8346\' (No Repeat)'),
+(15274, 0, 6, 0, 8, 0, 100, 1, 129597, 0, 0, 0, 11, 61314, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Mana Wyrm - On Spellhit \'Arcane Torrent\' - Cast \'Quest Credit 8346\' (No Repeat)'),
+(15274, 0, 7, 0, 0, 0, 100, 0, 5000, 8000, 12000, 12000, 11, 25602, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Mana Wyrm - In Combat - Cast \'Faerie Fire\'');
