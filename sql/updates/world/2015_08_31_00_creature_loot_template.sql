596b132bf29840ae1b3083c90ac12b44a1481760
ProjectCoreDevs <projectcore.devs@gmail.com>
2015-08-31 23:05:12 +0200

    Update creature_loot_template for npc 23945
    close #638

---
+++ sql/updates/world/2015_08_31_00_creature_loot_template.sql
@@ -0,0 +1,15 @@
+-- Parsed Creature loot for Creature id : 23945 
+DELETE FROM `creature_loot_template` WHERE `Entry` = 23945;
+INSERT INTO `creature_loot_template` VALUES ( '23945', '17056', '0', '100', '1', '0', '3', '6');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '33238', '0', '-82.79791', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36473', '0', '1.567944', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36487', '0', '1.74216', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36515', '0', '2.61324', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36543', '0', '2.787457', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36571', '0', '2.090592', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36599', '0', '1.219512', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36641', '0', '1.393728', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36655', '0', '4.703833', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36669', '0', '4.878049', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ( '23945', '36683', '0', '1.74216', '1', '0', '1', '1');
+UPDATE `creature_template` SET `lootid`= 23945 WHERE `entry`= 23945;
