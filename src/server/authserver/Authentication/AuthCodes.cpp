cd848abbe8e627e64e3921bfaacd43b94c752577
SkyFire <admin@projectskyfire.org>
2016-01-01 13:39:59 -0600

    Update copyrights. Happy New Year 2016.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/authserver/Authentication/AuthCodes.cpp
+++ src/server/authserver/Authentication/AuthCodes.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2011-2014 Project SkyFire <http://www.projectskyfire.org/>
- * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2011-2016 Project SkyFire <http://www.projectskyfire.org/
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
--- src/server/scripts/Pandaria/boss_ordos.cpp
+++ src/server/scripts/Pandaria/boss_ordos.cpp
@@ -1,7 +1,7 @@
 /*
-* Copyright (C) 2011-2015 Project SkyFire <http://www.projectskyfire.org/>
-* Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
-* Copyright (C) 2005-2015 MaNGOS <http://getmangos.com/>
+* Copyright (C) 2011-2016 Project SkyFire <http://www.projectskyfire.org/>
+* Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+* Copyright (C) 2005-2016 MaNGOS <http://getmangos.com/>
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
