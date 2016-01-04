4356967892b88a975e16e24c5194bfbb35b781aa
Shoxxo <webmaster@god-network.org>
2016-01-04 14:32:12 +0100

    Fix Travix and Cmake...

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -1,34 +1,6 @@
-# Copyright (C) 2011-2016 Project SkyFire <http://www.projectskyfire.org/
-# Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
-#
-# This file is free software; as a special exception the author gives
-# unlimited permission to copy and/or distribute it, with or without
-# modifications, as long as this notice is preserved.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-# Platform-specfic options
-option(USE_MYSQL_SOURCES "Use included MySQL-sources to build libraries" 1)
-
 # Package overloads
-set(ACE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/acelite)
-set(ACE_LIBRARY "ace")
 set(BZIP2_LIBRARIES "bzip2")
 set(ZLIB_LIBRARIES "zlib")
-
-if(MSVC_VERSION EQUAL 1900)
-  set( USE_MYSQL_SOURCES 0 )
-  message(STATUS "MySQL: Disabled supplied MySQL sources")
-endif(MSVC_VERSION EQUAL 1900)
-
-if( USE_MYSQL_SOURCES )
-  set(MYSQL_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/mysqllite/include)
-  set(MYSQL_LIBRARY "libmysql")
-  set( MYSQL_FOUND 1 )
-  message(STATUS "Using supplied MySQL sources")
-endif()
 
 # check the CMake preload parameters (commented out by default)
 
