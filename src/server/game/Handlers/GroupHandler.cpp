b28fba8b10481cf9bdefe4d18637b1dcc9960a2f
Darkloveir <peymanyousefi1986@gmail.com>
2016-02-06 20:13:46 +0330

    Cleaner code

--- src/server/game/Handlers/GroupHandler.cpp
+++ src/server/game/Handlers/GroupHandler.cpp
@@ -604,25 +604,9 @@ void WorldSession::HandleLootRoll(WorldPacket& recvData)
     recvData >> itemSlot;
     recvData >> rollType;              // 0: pass, 1: need, 2: greed
 
-    guid[7] = recvData.ReadBit();
-    guid[1] = recvData.ReadBit();
-    guid[2] = recvData.ReadBit();
-    guid[0] = recvData.ReadBit();
-    guid[6] = recvData.ReadBit();
-    guid[3] = recvData.ReadBit();
-    guid[4] = recvData.ReadBit();
-    guid[5] = recvData.ReadBit();
-
+    recvData.ReadGuidMask(guid, 7, 1, 2, 0, 6, 3, 4, 5);
     recvData.FlushBits();
-
-    recvData.ReadByteSeq(guid[0]);
-    recvData.ReadByteSeq(guid[2]);
-    recvData.ReadByteSeq(guid[7]);
-    recvData.ReadByteSeq(guid[3]);
-    recvData.ReadByteSeq(guid[1]);
-    recvData.ReadByteSeq(guid[5]);
-    recvData.ReadByteSeq(guid[4]);
-    recvData.ReadByteSeq(guid[6]);
+    recvData.ReadGuidBytes(guid, 0, 2, 7, 3, 1, 5, 4, 6);
 
     Group* group = GetPlayer()->GetGroup();
     if (!group)
