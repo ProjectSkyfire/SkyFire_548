2cd4d3f666600cb426faeb00d214d5488663c3b5
cybrann <trulex8@gmail.com>
2017-05-19 16:29:52 +0200

    Core/Scripts - Fix bloodthirst, deep wounds and T16 2P DPS bonus for warriors

---
+++ sql/updates/world/9999_99_99_99_spell_script_names.sql
@@ -0,0 +1,7 @@
+DELETE FROM `spell_script_names` WHERE IN ('spell_warr_bloodthirst', 'spell_warr_bloodthirst_heal', 'spell_warr_deep_wounds');
+INSERT INTO `spell_script_names` VALUES
+(23881, 'spell_warr_bloodthirst'),
+(6343, 'spell_warr_deep_wounds'),
+(12294, 'spell_warr_deep_wounds'),
+(23881, 'spell_warr_deep_wounds'),
+(20243, 'spell_warr_deep_wounds');
