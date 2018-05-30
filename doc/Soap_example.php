328aa740e115fe82af3b3e0c4f4b848312c9861c
sasin91 <jonas.kerwin.hansen@gmail.com>
2018-05-30 19:42:45 +0200

    Updated URN
    It seems the namespace is registered as TC in src/server/worldserver/SFSoap/SFSoap.cpp:158

--- doc/Soap_example.php
+++ doc/Soap_example.php
@@ -14,7 +14,7 @@ $command = "server info";
 $client = new SoapClient(NULL,
 array(
     "location" => "http://$host:$soapport/",
-    "uri" => "urn:Oregon",
+    "uri" => "urn:TC",
     "style" => SOAP_RPC,
     'login' => $username,
     'password' => $password
