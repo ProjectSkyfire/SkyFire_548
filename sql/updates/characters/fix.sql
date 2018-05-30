ceb6f3ff1dc5fa466ba7f0ca1434cecc08679334
Shoxxo <cnw1000@ok.de>
2018-05-30 19:02:30 +0200

    [DB / Misc] Small Typo

--- sql/updates/characters/fix.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `characters`
-  DROP `instance_mode_mask`,
-  ADD `dungeonDifficulty` tinyint(3) unsigned NOT NULL DEFAULT '1' AFTER `instance_id`,
-  ADD `raidDifficulty` tinyint(3) unsigned NOT NULL DEFAULT '3' AFTER `dungeonDifficulty`;
-  
-ALTER TABLE `groups`
-  CHANGE `difficulty` `difficulty` tinyint(3) unsigned NOT NULL DEFAULT '1' AFTER `groupType`,
-  CHANGE `raiddifficulty` `raidDifficulty` tinyint(3) unsigned NOT NULL DEFAULT '14' AFTER `difficulty`;
