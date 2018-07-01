006e8fb37e62f61c928d401be55db011b7f31547
AriDEV <aridev666@gmail.com>
2018-07-01 06:59:55 +0200

    Server/PacketIO: Update CMSG_GUILD_REQUEST_PARTY_STATE
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- src/server/game/Handlers/GuildHandler.cpp
+++ src/server/game/Handlers/GuildHandler.cpp
@@ -830,23 +830,8 @@ void WorldSession::HandleGuildRequestPartyState(WorldPacket& recvPacket)
 
     ObjectGuid guildGuid;
 
-    guildGuid[0] = recvPacket.ReadBit();
-    guildGuid[6] = recvPacket.ReadBit();
-    guildGuid[7] = recvPacket.ReadBit();
-    guildGuid[3] = recvPacket.ReadBit();
-    guildGuid[5] = recvPacket.ReadBit();
-    guildGuid[1] = recvPacket.ReadBit();
-    guildGuid[2] = recvPacket.ReadBit();
-    guildGuid[4] = recvPacket.ReadBit();
-
-    recvPacket.ReadByteSeq(guildGuid[6]);
-    recvPacket.ReadByteSeq(guildGuid[3]);
-    recvPacket.ReadByteSeq(guildGuid[2]);
-    recvPacket.ReadByteSeq(guildGuid[1]);
-    recvPacket.ReadByteSeq(guildGuid[5]);
-    recvPacket.ReadByteSeq(guildGuid[0]);
-    recvPacket.ReadByteSeq(guildGuid[7]);
-    recvPacket.ReadByteSeq(guildGuid[4]);
+    recvPacket.ReadGuidMask(guildGuid, 1, 5, 7, 2, 6, 3, 0, 4);
+    recvPacket.ReadGuidBytes(guildGuid, 2, 5, 4, 6, 1, 0, 7, 3);
 
     if (Guild* guild = sGuildMgr->GetGuildByGuid(guildGuid))
         guild->HandleGuildPartyRequest(this);
