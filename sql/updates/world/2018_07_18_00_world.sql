e4d07aa94593208199c0f7dbf34b2b9a8648616c
AriDEV <aridev666@gmail.com>
2018-07-18 02:10:09 +0200

    DB/Spawns: Editor didnt save changes in last commit.
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- sql/updates/world/2018_07_18_00_world.sql
+++ sql/updates/world/2018_07_18_00_world.sql
@@ -1,3 +1,3 @@
 -- Black Temple SpawnMask
-UPDATE creature SET spawnMask=8 WHERE map = 564;
-UPDATE gameobject SET spawnMask=8 WHERE map = 564;
+UPDATE creature SET spawnMask=16 WHERE map = 564;
+UPDATE gameobject SET spawnMask=16 WHERE map = 564;
