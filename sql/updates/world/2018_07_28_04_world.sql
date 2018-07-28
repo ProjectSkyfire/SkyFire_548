8d57d8eb782634650324b11901476eff48205d26
AriDEV <aridev666@gmail.com>
2018-07-29 01:09:22 +0200

    Database: Update some Spell Click Spells.
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2018_07_28_04_world.sql
@@ -0,0 +1,338 @@
+DELETE FROM npc_spellclick_spells WHERE npc_entry=24992 AND spell_id=45008;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=25334 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=25968 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=26048 AND spell_id=46407;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=26191 AND spell_id=46978;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=26503 AND spell_id=47121;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=26809 AND spell_id=47416;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=27061 AND spell_id=47920;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=27213 AND spell_id=43671;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=27354 AND spell_id=43671;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=27409 AND spell_id=48678;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=27587 AND spell_id=49078;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=27587 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=27923 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=28161 AND spell_id=51037;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=28468 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=29857 AND spell_id=55460;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=29884 AND spell_id=55430;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=30108 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=30134 AND spell_id=43671;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=30134 AND spell_id=56603;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=30272 AND spell_id=57401;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=30388 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=30393 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=31050 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=31432 AND spell_id=59091;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=31749 AND spell_id=61421;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=31840 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=31856 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=32152 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=32513 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=33432 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=33651 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=34146 AND spell_id=43671;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=34150 AND spell_id=43671;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=34151 AND spell_id=43671;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=34282 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=34567 AND spell_id=65684;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=34568 AND spell_id=65686;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=34796 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35111 AND spell_id=66778;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35317 AND spell_id=43671;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35410 AND spell_id=43671;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35413 AND spell_id=66245;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35415 AND spell_id=66245;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35417 AND spell_id=66245;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35419 AND spell_id=68503;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35421 AND spell_id=68503;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35427 AND spell_id=43671;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35429 AND spell_id=68458;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35431 AND spell_id=66245;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35431 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35433 AND spell_id=66245;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35433 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35436 AND spell_id=66245;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=35491 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36059 AND spell_id=68291;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36059 AND spell_id=68293;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36287 AND spell_id=68597;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36288 AND spell_id=68598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36289 AND spell_id=68596;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36357 AND spell_id=66245;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36358 AND spell_id=66245;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36437 AND spell_id=68726;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36440 AND spell_id=68735;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36459 AND spell_id=68743;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36540 AND spell_id=94654;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36812 AND spell_id=69342;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36839 AND spell_id=70510;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36917 AND spell_id=81432;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=36972 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=37139 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=37198 AND spell_id=70036;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=37227 AND spell_id=70104;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=37598 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=37966 AND spell_id=69342;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=37980 AND spell_id=69342;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=38150 AND spell_id=71238;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=38615 AND spell_id=72472;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=38755 AND spell_id=72767;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=38933 AND spell_id=73123;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=38936 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=39294 AND spell_id=80343;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=39867 AND spell_id=94656;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=40825 AND spell_id=69063;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42204 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42205 AND spell_id=63313;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42246 AND spell_id=63313;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42247 AND spell_id=63313;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42255 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42268 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42314 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42399 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42400 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42548 AND spell_id=79276;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42548 AND spell_id=79277;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42576 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42644 AND spell_id=79936;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42646 AND spell_id=80017;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42671 AND spell_id=79568;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42673 AND spell_id=79545;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42716 AND spell_id=80690;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=42839 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43032 AND spell_id=80393;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43044 AND spell_id=80436;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43046 AND spell_id=80436;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43048 AND spell_id=80446;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43115 AND spell_id=56685;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43182 AND spell_id=84178;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43195 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43209 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43259 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43747 AND spell_id=81877;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43952 AND spell_id=82261;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=43984 AND spell_id=82359;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=44049 AND spell_id=82456;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=44126 AND spell_id=82535;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=44427 AND spell_id=56685;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=44429 AND spell_id=56685;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=44928 AND spell_id=72767;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=45005 AND spell_id=84071;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=45191 AND spell_id=84499;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=45344 AND spell_id=84754;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=46185 AND spell_id=86106;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=46208 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=46449 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=47297 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=47747 AND spell_id=89960;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48338 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48351 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48457 AND spell_id=90102;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48520 AND spell_id=90287;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48639 AND spell_id=90644;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48719 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48721 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48803 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48804 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48805 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48806 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48820 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48821 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48913 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=48939 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=49072 AND spell_id=69063;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=49520 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=51033 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=51101 AND spell_id=41020;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=51102 AND spell_id=41020;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=51103 AND spell_id=41020;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=51391 AND spell_id=95284;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=51395 AND spell_id=95285;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=51396 AND spell_id=95284;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=51397 AND spell_id=95285;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=52358 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=52359 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=52363 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=52682 AND spell_id=97400;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=52809 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=52812 AND spell_id=46598;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=53493 AND spell_id=101376;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=53517 AND spell_id=99325;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=53517 AND spell_id=99328;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=53526 AND spell_id=99356;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=53526 AND spell_id=99357;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=53561 AND spell_id=99471;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=53561 AND spell_id=99473;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=54348 AND spell_id=90102;
+DELETE FROM npc_spellclick_spells WHERE npc_entry=55967 AND spell_id=46598;
+
+INSERT INTO npc_spellclick_spells (npc_entry, spell_id, cast_flags, user_type) VALUES 
+(24992, 45008, 0, 0),
+(25334, 46598, 1, 0),
+(25968, 46598, 1, 0),
+(26048, 46407, 0, 0),
+(26191, 46978, 2, 0),
+(26503, 47121, 0, 0),
+(26809, 47416, 0, 0),
+(27061, 47920, 1, 0),
+(27213, 43671, 0, 0),
+(27354, 43671, 1, 0),
+(27409, 48678, 1, 0),
+(27587, 49078, 1, 0),
+(27587, 46598, 1, 1),
+(27923, 46598, 1, 1),
+(28161, 51037, 0, 0),
+(28468, 46598, 1, 0),
+(29857, 55460, 3, 0),
+(29884, 55430, 1, 0),
+(30108, 46598, 1, 0),
+(30134, 43671, 1, 1),
+(30134, 56603, 1, 0),
+(30272, 57401, 1, 0),
+(30388, 46598, 1, 0),
+(30393, 46598, 1, 0),
+(31050, 46598, 1, 0),
+(31432, 59091, 1, 0),
+(31749, 61421, 1, 0),
+(31840, 46598, 1, 0),
+(31856, 46598, 1, 0),
+(32152, 46598, 1, 0),
+(32513, 46598, 1, 0),
+(33432, 46598, 1, 1),
+(33651, 46598, 1, 1),
+(34146, 43671, 1, 0),
+(34150, 43671, 1, 0),
+(34151, 43671, 1, 0),
+(34282, 46598, 0, 0),
+(34567, 65684, 1, 0),
+(34568, 65686, 1, 0),
+(34796, 46598, 0, 0),
+(35111, 66778, 1, 0),
+(35317, 43671, 1, 0),
+(35410, 43671, 1, 0),
+(35413, 66245, 1, 0),
+(35415, 66245, 1, 0),
+(35417, 66245, 1, 0),
+(35419, 68503, 1, 0),
+(35421, 68503, 1, 0),
+(35427, 43671, 1, 0),
+(35429, 68458, 1, 0),
+(35431, 66245, 1, 0),
+(35431, 46598, 1, 0),
+(35433, 66245, 1, 0),
+(35433, 46598, 1, 0),
+(35436, 66245, 1, 0),
+(35491, 46598, 0, 0),
+(36059, 68291, 3, 0),
+(36059, 68293, 1, 0),
+(36287, 68597, 1, 0),
+(36288, 68598, 1, 0),
+(36289, 68596, 1, 0),
+(36357, 66245, 1, 0),
+(36358, 66245, 1, 0),
+(36437, 68726, 1, 0),
+(36440, 68735, 1, 0),
+(36459, 68743, 1, 0),
+(36540, 94654, 1, 0),
+(36812, 69342, 1, 0),
+(36839, 70510, 1, 0),
+(36917, 81432, 1, 0),
+(36972, 46598, 0, 0),
+(37139, 46598, 1, 0),
+(37198, 70036, 0, 0),
+(37227, 70104, 3, 0),
+(37598, 46598, 1, 0),
+(37966, 69342, 1, 0),
+(37980, 69342, 1, 0),
+(38150, 71238, 1, 0),
+(38615, 72472, 1, 0),
+(38755, 72767, 1, 0),
+(38933, 73123, 1, 0),
+(38936, 46598, 0, 0),
+(39294, 80343, 1, 0),
+(39867, 94656, 1, 0),
+(40825, 69063, 1, 0),
+(42204, 46598, 0, 0),
+(42205, 63313, 0, 0),
+(42246, 63313, 0, 0),
+(42247, 63313, 0, 0),
+(42255, 46598, 0, 0),
+(42268, 46598, 0, 0),
+(42314, 46598, 0, 0),
+(42399, 46598, 0, 0),
+(42400, 46598, 0, 0),
+(42548, 79276, 1, 0),
+(42548, 79277, 2, 0),
+(42576, 46598, 0, 0),
+(42644, 79936, 1, 0),
+(42646, 80017, 1, 0),
+(42671, 79568, 1, 0),
+(42673, 79545, 1, 0),
+(42716, 80690, 1, 0),
+(42839, 46598, 0, 0),
+(43032, 80393, 1, 0),
+(43044, 80436, 1, 0),
+(43046, 80436, 1, 0),
+(43048, 80446, 1, 0),
+(43115, 56685, 1, 0),
+(43182, 84178, 1, 0),
+(43195, 46598, 0, 0),
+(43209, 46598, 0, 0),
+(43259, 46598, 0, 0),
+(43747, 81877, 1, 0),
+(43952, 82261, 1, 0),
+(43984, 82359, 1, 0),
+(44049, 82456, 1, 0),
+(44126, 82535, 1, 0),
+(44427, 56685, 1, 0),
+(44429, 56685, 1, 0),
+(44928, 72767, 1, 0),
+(45005, 84071, 1, 0),
+(45191, 84499, 1, 0),
+(45344, 84754, 1, 0),
+(46185, 86106, 0, 0),
+(46208, 46598, 0, 0),
+(46449, 46598, 0, 0),
+(47297, 46598, 1, 0),
+(47747, 89960, 1, 1),
+(48338, 46598, 1, 0),
+(48351, 46598, 1, 0),
+(48457, 90102, 1, 1),
+(48520, 90287, 1, 0),
+(48639, 90644, 1, 0),
+(48719, 46598, 1, 0),
+(48721, 46598, 1, 0),
+(48803, 46598, 1, 0),
+(48804, 46598, 1, 0),
+(48805, 46598, 1, 0),
+(48806, 46598, 1, 0),
+(48820, 46598, 1, 0),
+(48821, 46598, 1, 0),
+(48913, 46598, 1, 0),
+(48939, 46598, 1, 0),
+(49072, 69063, 1, 0),
+(49520, 46598, 1, 0),
+(51033, 46598, 0, 0),
+(51101, 41020, 1, 0),
+(51102, 41020, 1, 0),
+(51103, 41020, 1, 0),
+(51391, 95284, 3, 0),
+(51395, 95285, 3, 0),
+(51396, 95284, 3, 0),
+(51397, 95285, 3, 0),
+(52358, 46598, 0, 0),
+(52359, 46598, 0, 0),
+(52363, 46598, 1, 0),
+(52682, 97400, 1, 0),
+(52809, 46598, 0, 0),
+(52812, 46598, 0, 0),
+(53493, 101376, 1, 0),
+(53517, 99325, 1, 0),
+(53517, 99328, 2, 0),
+(53526, 99356, 1, 0),
+(53526, 99357, 2, 0),
+(53561, 99471, 1, 0),
+(53561, 99473, 2, 0),
+(54348, 90102, 1, 0),
+(55967, 46598, 1, 0);
