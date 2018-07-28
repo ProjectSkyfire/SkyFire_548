1476ae6b3435a8b4a8e0630aee8b053e9a820f60
AriDEV <aridev666@gmail.com>
2018-07-29 01:38:30 +0200

    Database: Added some Missing faction change items.
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2018_07_28_06_world.sql
@@ -0,0 +1,50 @@
+DELETE FROM player_factionchange_items WHERE alliance_id=62473 AND horde_id=62454;
+DELETE FROM player_factionchange_items WHERE alliance_id=62474 AND horde_id=62455;
+DELETE FROM player_factionchange_items WHERE alliance_id=62475 AND horde_id=62456;
+DELETE FROM player_factionchange_items WHERE alliance_id=62476 AND horde_id=62457;
+DELETE FROM player_factionchange_items WHERE alliance_id=62477 AND horde_id=62458;
+DELETE FROM player_factionchange_items WHERE alliance_id=62478 AND horde_id=62459;
+DELETE FROM player_factionchange_items WHERE alliance_id=62479 AND horde_id=62460;
+DELETE FROM player_factionchange_items WHERE alliance_id=62468 AND horde_id=62463;
+DELETE FROM player_factionchange_items WHERE alliance_id=62469 AND horde_id=62464;
+DELETE FROM player_factionchange_items WHERE alliance_id=62470 AND horde_id=62465;
+DELETE FROM player_factionchange_items WHERE alliance_id=62471 AND horde_id=62466;
+DELETE FROM player_factionchange_items WHERE alliance_id=62472 AND horde_id=62467;
+DELETE FROM player_factionchange_items WHERE alliance_id=68739 AND horde_id=68740;
+DELETE FROM player_factionchange_items WHERE alliance_id=63355 AND horde_id=64996;
+DELETE FROM player_factionchange_items WHERE alliance_id=63141 AND horde_id=64997;
+DELETE FROM player_factionchange_items WHERE alliance_id=64998 AND horde_id=64999;
+DELETE FROM player_factionchange_items WHERE alliance_id=63039 AND horde_id=65356;
+DELETE FROM player_factionchange_items WHERE alliance_id=63391 AND horde_id=64995;
+DELETE FROM player_factionchange_items WHERE alliance_id=65175 AND horde_id=62176;
+DELETE FROM player_factionchange_items WHERE alliance_id=63144 AND horde_id=64994;
+DELETE FROM player_factionchange_items WHERE alliance_id=63145 AND horde_id=64993;
+DELETE FROM player_factionchange_items WHERE alliance_id=63379 AND horde_id=63378;
+DELETE FROM player_factionchange_items WHERE alliance_id=63377 AND horde_id=63376;
+DELETE FROM player_factionchange_items WHERE alliance_id=63517 AND horde_id=63518;
+
+INSERT INTO player_factionchange_items (race_A, alliance_id, commentA, race_H, horde_id, commentH) VALUES
+(0, 62473, 'Blade of the Fearless', 0, 62454, 'Blade of the Fearless'),
+(0, 62474, 'Spear of Trailing Shadows', 0, 62455, 'Spear of Trailing Shadows'),
+(0, 62475, 'Dagger of Restless Nights', 0, 62456, 'Dagger of Restless Nights'),
+(0, 62476, 'Ravening Slicer', 0, 62457, 'Ravening Slicer'),
+(0, 62477, 'Insidious Staff', 0, 62458, 'Insidious Staff'),
+(0, 62478, 'Shimmering Morningstar', 0, 62459, 'Shimmering Morningstar'),
+(0, 62479, 'Sky Piercer', 0, 62460, 'Sky Piercer'),
+(0, 62468, 'Unsolvable Riddle', 0, 62463, 'Unsolvable Riddle'),
+(0, 62469, 'Impatience of Youth', 0, 62464, 'Impatience of Youth'),
+(0, 62470, 'Stump of Time', 0, 62465, 'Stump of Time'),
+(0, 62471, 'Mirror of Broken Images', 0, 62466, 'Mirror of Broken Images'),
+(0, 62472, 'Mandala of Stirring Patterns', 0, 62467, 'Mandala of Stirring Patterns'),
+(0, 68739, 'Darkheart Hacker', 0, 68740, 'Darkheart Hacker'),
+(0, 63355, 'Rustberg Gull', 0, 64996, 'Rustberg Gull'),
+(0, 63141, 'Tol Barad Searchlight', 0, 64997, 'Tol Barad Searchlight'),
+(0, 64998, 'Reins of the Spectral Steed', 0, 64999, 'Reins of the Spectral Wolf'),
+(0, 63039, 'Reins of the Drake of the West Wind', 0, 65356, 'Reins of the Drake of the West Wind'),
+(0, 63391, 'Baradin\'s Wardens Bandage', 0, 64995, 'Hellscream\'s Reach Bandage'),
+(0, 65175, 'Baradin Footman\'s Tags', 0, 62176, 'Baradin Grunt\'s Talisman'),
+(0, 63144, 'Baradin\'s Wardens Healing Potion', 0, 64994, 'Hellscream\'s Reach Healing Potion'),
+(0, 63145, 'Baradin\'s Wardens Mana Potion', 0, 64993, 'Hellscream\'s Reach Mana Potion'),
+(0, 63379, 'Baradin\'s Wardens Tabard', 0, 63378, 'Hellscream\'s Reach Tabard'),
+(0, 63377, 'Baradin\'s Wardens Battle Standard', 0, 63376, 'Hellscream\'s Reach Battle Standard'),
+(0, 63517, 'Baradin\'s Wardens Commendation', 0, 63518, 'Hellscream\'s Reach Commendation');
