9343401abb877fb351cf6dcd879f7a7c0112eadb
AriDEV <aridev666@gmail.com>
2018-07-28 23:17:49 +0200

    Fixup last commit. editor didn't save changes.

--- sql/updates/world/2018_07_28_03_world.sql
+++ sql/updates/world/2018_07_28_03_world.sql
@@ -1,2 +1,2 @@
-DELETE FROM creature_addon WHERE path_id=0 AND mount=0 AND bytes1=0 AND bytes2=0 AND emote=0;
-DELETE FROM creature_template_addon WHERE path_id=0 AND mount=0 AND bytes1=0 AND bytes2=0 AND emote=0;
+DELETE FROM creature_addon WHERE path_id=0 AND mount=0 AND bytes1=0 AND bytes2=0 AND emote=0 AND auras="";
+DELETE FROM creature_template_addon WHERE path_id=0 AND mount=0 AND bytes1=0 AND bytes2=0 AND emote=0 AND auras="";
