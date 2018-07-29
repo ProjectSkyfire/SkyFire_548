88b9c585bd24be86dd6e0fe51fdfb932977c59b0
AriDEV <aridev666@gmail.com>
2018-07-29 02:05:22 +0200

    Database: Add some Fishing base Levels.
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2018_07_28_08_world.sql
@@ -0,0 +1,8 @@
+DELETE FROM `skill_fishing_base_level` WHERE `entry` IN (1497, 4710, 4722, 4656, 4987, 4100);
+INSERT INTO `skill_fishing_base_level` (`entry`, `skill`) VALUES 
+(1497, -20),
+(4710, 480),
+(4722, 430),
+(4656, 430),
+(4987, 480),
+(4100, 450);
