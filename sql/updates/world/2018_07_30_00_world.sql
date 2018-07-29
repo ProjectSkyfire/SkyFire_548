c9392fe070ae92e3fa50bcedd0e912c82721eacc
AriDEV <aridev666@gmail.com>
2018-07-29 23:02:40 +0200

    Database: Remove creature spawns for Traveling Traders.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2018_07_30_00_world.sql
@@ -0,0 +1,17 @@
+-- Grand Expedition Yak
+-- Cousin Slowhands <Traveling Trader>
+DELETE FROM creature WHERE id=62822;
+-- Mystic Birdhat <Transmogrifier>
+DELETE FROM creature WHERE id=64515;
+
+-- Traveler's Tundra Mammoth (Horde)
+-- Drix Blackwrench <The Fixer>
+DELETE FROM creature WHERE id=32641;
+-- Mojodishu <Traveling Trader>
+DELETE FROM creature WHERE id=32642;
+
+-- Traveler's Tundra Mammoth (Alliance)
+-- Hakmud of Argus <Traveling Trader>
+DELETE FROM creature WHERE id=32638;
+-- Gnimo <Adventurous Tinker>
+DELETE FROM creature WHERE id=32639;
