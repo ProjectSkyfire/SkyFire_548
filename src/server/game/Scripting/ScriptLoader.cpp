797b202e6d5976d4ed4e6bc651884a50c243c7d3
Arci <Arcidev@users.noreply.github.com>
2018-08-21 08:29:54 +0200

    Build:: removed empty script files

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -252,28 +252,16 @@ void AddSC_boss_venoxis();
 void AddSC_boss_wushoolay();
 void AddSC_boss_zanzil();
 void AddSC_instance_zulgurub();
-//void AddSC_alterac_mountains();
 void AddSC_arathi_highlands();
 void AddSC_blasted_lands();
-void AddSC_burning_steppes();
-void AddSC_duskwood();
 void AddSC_eastern_plaguelands();
 void AddSC_eversong_woods();
 void AddSC_ghostlands();
 void AddSC_hinterlands();
-void AddSC_ironforge();
 void AddSC_isle_of_queldanas();
-void AddSC_loch_modan();
-void AddSC_redridge_mountains();
 void AddSC_silverpine_forest();
-void AddSC_stormwind_city();
 void AddSC_stranglethorn_vale();
-void AddSC_swamp_of_sorrows();
-void AddSC_tirisfal_glades();
 void AddSC_undercity();
-void AddSC_western_plaguelands();
-void AddSC_westfall();
-void AddSC_wetlands();
 void AddSC_elwynn_forest();
 
 //kalimdor
@@ -351,26 +339,18 @@ void AddSC_instance_firelands();
 void AddSC_boss_alysrazor();
 
 void AddSC_ashenvale();
-void AddSC_azshara();
 void AddSC_azuremyst_isle();
 void AddSC_bloodmyst_isle();
-void AddSC_darkshore();
 void AddSC_desolace();
 void AddSC_durotar();
 void AddSC_dustwallow_marsh();
-void AddSC_felwood();
-void AddSC_feralas();
 void AddSC_moonglade();
 void AddSC_mulgore();
-void AddSC_orgrimmar();
 void AddSC_silithus();
-void AddSC_stonetalon_mountains();
 void AddSC_tanaris();
 void AddSC_teldrassil();
 void AddSC_the_barrens();
-void AddSC_thousand_needles();
 void AddSC_thunder_bluff();
-void AddSC_ungoro_crater();
 void AddSC_winterspring();
 
 // Northrend
@@ -943,28 +923,16 @@ void AddEasternKingdomsScripts()
     AddSC_boss_zanzil();
     AddSC_instance_zulgurub();
 
-    //AddSC_alterac_mountains();
     AddSC_arathi_highlands();
     AddSC_blasted_lands();
-    AddSC_burning_steppes();
-    AddSC_duskwood();
     AddSC_eastern_plaguelands();
     AddSC_eversong_woods();
     AddSC_ghostlands();
     AddSC_hinterlands();
-    AddSC_ironforge();
     AddSC_isle_of_queldanas();
-    AddSC_loch_modan();
-    AddSC_redridge_mountains();
     AddSC_silverpine_forest();
-    AddSC_stormwind_city();
     AddSC_stranglethorn_vale();
-    AddSC_swamp_of_sorrows();
-    AddSC_tirisfal_glades();
     AddSC_undercity();
-    AddSC_western_plaguelands();
-    AddSC_westfall();
-    AddSC_wetlands();
 	AddSC_elwynn_forest();
 #endif
 }
@@ -1040,26 +1008,18 @@ void AddKalimdorScripts()
     AddSC_instance_zulfarrak();
 
     AddSC_ashenvale();
-    AddSC_azshara();
     AddSC_azuremyst_isle();
     AddSC_bloodmyst_isle();
-    AddSC_darkshore();
     AddSC_desolace();
     AddSC_durotar();
     AddSC_dustwallow_marsh();
-    AddSC_felwood();
-    AddSC_feralas();
     AddSC_moonglade();
     AddSC_mulgore();
-    AddSC_orgrimmar();
     AddSC_silithus();
-    AddSC_stonetalon_mountains();
     AddSC_tanaris();
     AddSC_teldrassil();
     AddSC_the_barrens();
-    AddSC_thousand_needles();
     AddSC_thunder_bluff();
-    AddSC_ungoro_crater();
     AddSC_winterspring();
 
     AddSC_instance_halls_of_origination();
