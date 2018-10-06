bfd31600a5e33f80a0dca40320aec4cec928161e
AriDEV <aridev666@gmail.com>
2018-10-06 04:37:40 +0200

    Server/Logging: add some casts

--- src/server/shared/Logging/Log.cpp
+++ src/server/shared/Logging/Log.cpp
@@ -287,14 +287,8 @@ std::string Log::GetTimestampStr()
     time_t t = time(NULL);
     tm aTm;
     ACE_OS::localtime_r(&t, &aTm);
-    //       YYYY   year
-    //       MM     month (2 digits 01-12)
-    //       DD     day (2 digits 01-31)
-    //       HH     hour (2 digits 00-23)
-    //       MM     minutes (2 digits 00-59)
-    //       SS     seconds (2 digits 00-59)
     char buf[20];
-    snprintf(buf, sizeof(buf), "%04d-%02d-%02d_%02d-%02d-%02d", aTm.tm_year + 1900, aTm.tm_mon + 1, aTm.tm_mday, aTm.tm_hour, aTm.tm_min, aTm.tm_sec);
+    snprintf(buf, sizeof(buf), "%u-%u-%u_%u-%u-%u", uint32(aTm.tm_year + 1900), uint32(aTm.tm_mon + 1), uint32(aTm.tm_mday), uint32(aTm.tm_hour), uint32(aTm.tm_min), uint32(aTm.tm_sec));
     return std::string(buf);
 }
 
