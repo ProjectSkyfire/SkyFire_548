f6ce201fe354dc362dec876925728adf1fe344e6
AriDEV <aridev666@gmail.com>
2018-11-23 00:00:38 +0100

    Dependencies: Update ACE to v6.5.3 (Windows only)
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- dep/acelite/ace/Event.cpp
+++ dep/acelite/ace/Event.cpp
@@ -18,13 +18,13 @@ ACE_BEGIN_VERSIONED_NAMESPACE_DECL
 ACE_ALLOC_HOOK_DEFINE_Tc(ACE_Event_T)
 
 template <class TIME_POLICY>
-ACE_Event_T<TIME_POLICY>::ACE_Event_T(int manual_reset,
-    int initial_state,
-    int type,
-    const ACE_TCHAR *name,
-    void *arg,
-    LPSECURITY_ATTRIBUTES sa)
-    : ACE_Event_Base()
+ACE_Event_T<TIME_POLICY>::ACE_Event_T (int manual_reset,
+                                       int initial_state,
+                                       int type,
+                                       const ACE_TCHAR *name,
+                                       void *arg,
+                                       LPSECURITY_ATTRIBUTES sa)
+  : ACE_Event_Base ()
 {
   ACE_Condition_Attributes_T<TIME_POLICY> cond_attr (type);
   if (ACE_OS::event_init (&this->handle_,
