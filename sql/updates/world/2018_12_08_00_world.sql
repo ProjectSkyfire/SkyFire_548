e0542e7f3abd764d2bcd2bd53babf09e5a026e9f
AriDEV <aridev666@gmail.com>
2018-12-08 01:10:46 +0100

    Database/Disables: Disabled some quests.
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2018_12_08_00_world.sql
@@ -0,0 +1,18 @@
+DELETE FROM disables WHERE entry IN
+(7361, 7362, 7364, 7365, 7366, 6702, 7401, 7402, 7421, 7422, 7423, 7424, 7425, 7427, 7428);
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 7361, 'Disable Quest 7361: Favor Amongst the Darkspear'),
+(1, 7362, 'Disable Quest 7362: Ally of the Tauren'),
+(1, 7364, 'Disable Quest 7364: Gnomeregan Bounty'),
+(1, 7365, 'Disable Quest 7365: Staghelm\'s Requiem'),
+(1, 7366, 'Disable Quest 7366: The Archbishop\'s Mercy'),
+(1, 6702, 'Disable Quest 6702: [DEPRECATED] <TXT> SF, RFK, GNOMER, BF'),
+(1, 7401, 'Disable Quest 7401: Wanted: DWARVES!'),
+(1, 7402, 'Disable Quest 7402: WANTED: Orcs!'),
+(1, 7421, 'Disable Quest 7421: Darkspear Defense'),
+(1, 7422, 'Disable Quest 7422: Tuft it Out'),
+(1, 7423, 'Disable Quest 7423: I\'ve Got A Fever For More Bone Chips'),
+(1, 7424, 'Disable Quest 7424: What the Hoof?'),
+(1, 7425, 'Disable Quest 7425: Staghelm\'s Mojo Jamboree'),
+(1, 7427, 'Disable Quest 7427: Wanted: MORE DWARVES!'),
+(1, 7428, 'Disable Quest 7428: Wanted: MORE ORCS!');
