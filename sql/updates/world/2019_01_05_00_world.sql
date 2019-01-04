9c0ce8769ac5ae0dd46ad9d6df5233334dfe3116
AriDEV <aridev666@gmail.com>
2019-01-04 23:58:42 +0100

    Database/Creature: Update Creature Template for Heroic Lord Godfrey in Shadowfang
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_01_05_00_world.sql
@@ -0,0 +1,2 @@
+-- Update Creature Template for Heroic Lord Godfrey in Shadowfang
+UPDATE creature_template SET minlevel=87, maxlevel=87, faction_A=16, faction_H=16, speed_run=1.42857, baseattacktime=2000, rangeattacktime=2000, unit_class=2, unit_flags=32832 WHERE entry=49712;
