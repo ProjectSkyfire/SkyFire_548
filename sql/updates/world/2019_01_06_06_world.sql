9eabdf2b3843d830f629345409f2fc67e634098d
AriDEV <aridev666@gmail.com>
2019-01-06 22:42:45 +0100

    Database/Loot: Add Gameobject Loot for Object: Crate of Candles (1586) - Item: Candle of Beckoning (3080)
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_01_06_06_world.sql
@@ -0,0 +1,4 @@
+-- Add Gameobject Loot for Object: Crate of Candles (1586) - Item: Candle of Beckoning (3080) 
+DELETE FROM gameobject_loot_template WHERE entry=28239 AND item=3080
+INSERT INTO gameobject_loot_template (entry, item, ChanceOrQuestChance) VALUES
+(28239, 3080, -100);
