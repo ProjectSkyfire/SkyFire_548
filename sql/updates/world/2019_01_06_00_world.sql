7770a921a2d2c352d2fdbec6f9b210b9fea638b3
AriDEV <aridev666@gmail.com>
2019-01-06 17:01:57 +0100

    Database/Engine: Change Engines to MyISAM
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_01_06_00_world.sql
@@ -0,0 +1,10 @@
+-- Change Engines to MyISAM
+ALTER TABLE battle_pet_breed ENGINE=MyISAM;
+ALTER TABLE battle_pet_quality ENGINE=MyISAM;
+ALTER TABLE locales_quest_objective ENGINE=MyISAM;
+ALTER TABLE phase_area ENGINE=MyISAM;
+ALTER TABLE quest_objective ENGINE=MyISAM;
+ALTER TABLE quest_objective_effects ENGINE=MyISAM;
+ALTER TABLE terrain_phase_info ENGINE=MyISAM;
+ALTER TABLE terrain_swap_defaults ENGINE=MyISAM;
+ALTER TABLE terrain_worldmap ENGINE=MyISAM;
