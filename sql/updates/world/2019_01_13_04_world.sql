198cafe77c5420aad0d810af607fe3656b340706
AriDEV <aridev666@gmail.com>
2019-01-13 13:33:20 +0100

    Database/Areatrigger: Added AreaTriggers for Inn's: Tavern in the Mists & Dawnchaser Retreat.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_01_13_04_world.sql
@@ -0,0 +1,9 @@
+-- Dawnchaser Retreat - Inn
+DELETE FROM areatrigger_tavern WHERE id=8107;
+INSERT INTO areatrigger_tavern (id, name) VALUES
+(8107, 'Dawnchaser Retreat - Inn');
+
+-- Tavern in the Mists - Inn
+DELETE FROM areatrigger_tavern WHERE id=8442;
+INSERT INTO areatrigger_tavern (id, name) VALUES
+(8442, 'Tavern in the Mists - Inn');
