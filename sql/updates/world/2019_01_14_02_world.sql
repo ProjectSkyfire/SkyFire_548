926762b42a2da5f6779281fbd05b2ddb5b77d722
AriDEV <aridev666@gmail.com>
2019-01-14 14:09:43 +0100

    Database/Spells: Fixed Target Positions for some Portals.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_01_14_02_world.sql
@@ -0,0 +1,74 @@
+-- Portal Effect: Exodar(Trigger:32268) - Portal: Exodar(121850)
+DELETE FROM spell_target_position WHERE id IN (32268, 121850);
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121850, 0, 530, -4029.93, -11572.2, -138.296, 2.43);
+
+-- Portal Effect: Silvermoon(Trigger:32270) - Portal Effect: Silvermoon(121855)
+DELETE FROM spell_target_position WHERE id IN (32270, 121855);
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121855, 0, 530, 9998.49, -7106.78, 47.7055, 2.44);
+
+-- Portal Effect: Shattrath(Trigger:33728) - Portal: Shattrath(121853)
+DELETE FROM spell_target_position WHERE id IN (33728, 121853 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121853 , 0, 530, -1823.71, 5416.97, -12.4274, 2.79036);
+
+-- Portal Effect: Shattrath(Trigger:35718) - Portal: Shattrath(121854)
+DELETE FROM spell_target_position WHERE id IN (35718, 121854 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121854, 0, 530, -1904.76, 5443.43, -12.4272, 5.96);
+
+-- Portal Effect: Dalaran(Trigger:53141) - Portal: Dalaran(121847)
+DELETE FROM spell_target_position WHERE id IN (53141, 121847 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121847, 0, 571, 5807.75, 588.347, 661.505, 1.663);
+
+-- Portal Effect: Tol Barad(Trigger:88341) - Portal: Tol Barad(121861)
+DELETE FROM spell_target_position WHERE id IN (88341, 121861 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121861, 0, 732, -603.724, 1387.62, 22.0498, 0.469644);
+
+-- Portal Effect: Ironforge(Trigger:44089) - Portal: Ironforge(121851)
+DELETE FROM spell_target_position WHERE id IN (44089, 121851 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121851, 0, 0, -4613.71, -915.287, 501.062, 0);
+
+-- Portal Effect: Stormwind(Trigger:17334) - Portal: Stormwind(121857)
+DELETE FROM spell_target_position WHERE id IN (17334, 121857 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121857, 0, 0, -8998.14, 861.254, 29.6206, 2.25);
+
+-- Portal Effect: Theramore(Trigger:49362) - Portal: Theramore(121858)
+DELETE FROM spell_target_position WHERE id IN (49362, 121858 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121858, 0, 1, -3748.11, -4440.21, 30.5688, 3.95172);
+
+-- Portal Effect: Orgrimmar(Trigger:17609) - Portal: Orgrimmar(121852)
+DELETE FROM spell_target_position WHERE id IN (17609, 121852 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121852, 0, 1, 1776.5, -4338.8, -7.48, 3.55349);
+
+-- Portal Effect: Tol Barad(Trigger:88339) - Portal: Tol Barad(121860)
+DELETE FROM spell_target_position WHERE id IN (88339, 121860 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121860, 0, 732, -369.208, 1058.73, 21.7719, 0.634577);
+
+-- Portal Effect: Darnassus(Trigger:17608) - Portal: Darnassus(121849)
+DELETE FROM spell_target_position WHERE id IN (17608, 121849 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121849, 0, 1, 9656.54, 2518.26, 1331.66, 0);
+
+-- Portal Effect: Thunder Bluff(Trigger:17610) - Portal: Thunder Bluff(121859)
+DELETE FROM spell_target_position WHERE id IN (17610, 121859 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121859, 0, 1, -967.375, 284.82, 110.773, 3.19999);
+
+-- Portal Effect: Undercity(Trigger:17611) - Portal: Undercity(121862)
+DELETE FROM spell_target_position WHERE id IN (17611, 121862 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121862, 0, 0, 1773.42, 61.7391, -46.3215, 0);
+
+-- Portal Effect: Stonard(Trigger:49363) - Portal: Stonard(121856)
+DELETE FROM spell_target_position WHERE id IN (49363, 121856 );
+INSERT INTO spell_target_position (id, effIndex, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(121856, 0, 0, -10469, -3331.54, 25.4716, 0.98606);
