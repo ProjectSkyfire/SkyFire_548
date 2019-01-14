ae0e9419269b62e41ea7f16f4d130e24321b5369
AriDEV <aridev666@gmail.com>
2019-01-14 14:28:53 +0100

    Database/Spells: Update effectIndex of some teleport spells in spell_target_position.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_01_14_03_world.sql
@@ -0,0 +1,13 @@
+UPDATE spell_target_position SET effIndex=1 WHERE effIndex=0 AND id IN
+(67835, -- Sholazar Basin
+ 67836, -- Icecrown
+ 67837, -- Storm Peaks
+ 67834, -- Borean Tundra
+ 68081, -- Underground
+ 84073, -- Deepholm Intro Teleport
+ 109984, -- Anti-Exploit LOS Check
+ 100268, -- Teleport to Abyssal Breach
+ 100335, -- Teleport to Therazane's Throne
+ 100508, -- Teleport to Firelands
+ 100509, -- Teleport to Uldum
+ 100523); -- Teleport to Hyjal
