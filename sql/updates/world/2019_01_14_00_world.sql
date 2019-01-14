18b4d211b610cf04fc24e6943ed5197f5a7787a7
AriDEV <aridev666@gmail.com>
2019-01-14 11:37:50 +0100

    Database/Disables: Disabled some Obsolete/Deprecated Quests.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_01_14_00_world.sql
@@ -0,0 +1,54 @@
+-- Disable Quest 9941: Tracking Down the Culprits
+DELETE FROM disables WHERE sourceType=1 AND entry=9941;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9941, 'Disable Quest 9941: Tracking Down the Culprits');
+
+-- Disable Quest 9950: A Bird's-Eye View
+DELETE FROM disables WHERE sourceType=1 AND entry=9950;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9950, 'Disable Quest 9950: A Bird''s-Eye View');
+
+-- Disable Quest 9958: Scouting the Defenses
+DELETE FROM disables WHERE sourceType=1 AND entry=9958;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9958, 'Disable Quest 9958: Scouting the Defenses');
+
+-- Disable Quest 9963: Seeking Help from the Source
+DELETE FROM disables WHERE sourceType=1 AND entry=9963;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9963, 'Disable Quest 9963: Seeking Help from the Source');
+
+-- Disable Quest 9964: Seeking Help from the Source
+DELETE FROM disables WHERE sourceType=1 AND entry=9964;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9964, 'Disable Quest 9964: Seeking Help from the Source');
+
+-- Disable Quest 9965: A Show of Good Faith
+DELETE FROM disables WHERE sourceType=1 AND entry=9965;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9965, 'Disable Quest 9965: A Show of Good Faith');
+
+-- Disable Quest 9966: A Show of Good Faith
+DELETE FROM disables WHERE sourceType=1 AND entry=9966;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9966, 'Disable Quest 9966: A Show of Good Faith');
+
+-- Disable Quest 9974: The Final Reagents
+DELETE FROM disables WHERE sourceType=1 AND entry=9974;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9974, 'Disable Quest 9974: The Final Reagents');
+
+-- Disable Quest 9348: Ravager Roundup
+DELETE FROM disables WHERE sourceType=1 AND entry=9348;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9348, 'Disable Quest 9348: Ravager Roundup');
+
+-- Disable Quest 10925: Evil Draws Near
+DELETE FROM disables WHERE sourceType=1 AND entry=10925;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10925, 'Disable Quest 10925: Evil Draws Near');
+
+-- Disable Quest 10088: When This Mine's a-Rockin'
+DELETE FROM disables WHERE sourceType=1 AND entry=10088;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10088, 'Disable Quest 10088: When This Mine''s a-Rockin''');
