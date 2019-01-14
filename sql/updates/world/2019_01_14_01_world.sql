52d14d6be16ac53c89239a64f991c8fb4070ae41
AriDEV <aridev666@gmail.com>
2019-01-14 12:13:28 +0100

    Database/Quests: Update some quest_templates with RequiredSkillIds corresponding to their ZoneOrSort
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_01_14_01_world.sql
@@ -0,0 +1,17 @@
+-- RequiredSkillId to SKILL_FISHING(356) for QUEST_SORT_FISHING(101) 
+UPDATE quest_template SET RequiredSkillId=356 WHERE ZoneOrSort=-101;
+
+-- RequiredSkillId to SKILL_COOKING(185) for QUEST_SORT_COOKING(304) 
+UPDATE quest_template SET RequiredSkillId=185 WHERE ZoneOrSort=-304;
+
+-- RequiredSkillId to SKILL_JEWELCRAFTING(755) for QUEST_SORT_JEWELCRAFTING(373) 
+UPDATE quest_template SET RequiredSkillId=755 WHERE ZoneOrSort=-373;
+
+-- RequiredSkillId to SKILL_ENGINEERING(202) for QUEST_SORT_ENGINEERING(201) 
+UPDATE quest_template SET RequiredSkillId=202 WHERE ZoneOrSort=-201;
+
+-- RequiredSkillId to SKILL_LEATHERWORKING(165) for QUEST_SORT_LEATHERWORKING(182) 
+UPDATE quest_template SET RequiredSkillId=165 WHERE ZoneOrSort=-182;
+
+-- RequiredSkillId to SKILL_TAILORING(197) for QUEST_SORT_TAILORING(264) 
+UPDATE quest_template SET RequiredSkillId=197 WHERE ZoneOrSort=-264;
