7a7f089ee0cdfea52cbafbd9bd93d625582ef98b
AriDEV <aridev666@gmail.com>
2019-01-16 08:55:04 +0100

    Database/Disables: Disabled some Obsolete Quests.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_01_16_00_world.sql
@@ -0,0 +1,184 @@
+-- Disable Quest 9510: Bristlehide Clefthoof Hides
+DELETE FROM disables WHERE sourceType=1 AND entry=9510;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9510, 'Disable Quest 9510: Bristlehide Clefthoof Hides');
+
+-- Disable Quest 10084: Assault on Mageddon
+DELETE FROM disables WHERE sourceType=1 AND entry=10084;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10084, 'Disable Quest 10084: Assault on Mageddon');
+
+-- Disable Quest 10100: The Mastermind
+DELETE FROM disables WHERE sourceType=1 AND entry=10100;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10100, 'Disable Quest 10100: The Mastermind');
+
+-- Disable Quest 9342: Marauding Crust Bursters
+DELETE FROM disables WHERE sourceType=1 AND entry=9342;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9342, 'Disable Quest 9342: Marauding Crust Bursters');
+
+-- Disable Quest 9930: The Missing Merchant
+DELETE FROM disables WHERE sourceType=1 AND entry=9930;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9930, 'Disable Quest 9930: The Missing Merchant');
+
+-- Disable Quest 7426: One Man's Love
+DELETE FROM disables WHERE sourceType=1 AND entry=7426;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 7426, 'Disable Quest 7426: One Man''s Love');
+
+-- Disable Quest 6706: [DEPRECATED] <TXT> ZUL, ST, MAR
+DELETE FROM disables WHERE sourceType=1 AND entry=6706;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 6706, 'Disable Quest 6706: [DEPRECATED] <TXT> ZUL, ST, MAR');
+
+-- Disable Quest 6062: Taming the Beast
+DELETE FROM disables WHERE sourceType=1 AND entry=6062;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 6062, 'Disable Quest 6062: Taming the Beast');
+
+-- Disable Quest 6067: The Hunter's Path
+DELETE FROM disables WHERE sourceType=1 AND entry=6067;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 6067, 'Disable Quest 6067: The Hunter''s Path');
+
+-- Disable Quest 6081: Training the Beast
+DELETE FROM disables WHERE sourceType=1 AND entry=6081;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 6081, 'Disable Quest 6081: Training the Beast');
+
+-- Disable Quest 6082: Taming the Beast
+DELETE FROM disables WHERE sourceType=1 AND entry=6082;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 6082, 'Disable Quest 6082: Taming the Beast');
+
+-- Disable Quest 6083: Taming the Beast
+DELETE FROM disables WHERE sourceType=1 AND entry=6083;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 6083, 'Disable Quest 6083: Taming the Beast');
+
+-- Disable Quest 6708: [DEPRECATED] <TXT> BRD, DM, BRS
+DELETE FROM disables WHERE sourceType=1 AND entry=6708;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 6708, 'Disable Quest 6708: [DEPRECATED] <TXT> BRD, DM, BRS');
+
+-- Disable Quest 10083: The Legion's Plans
+DELETE FROM disables WHERE sourceType=1 AND entry=10083;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10083, 'Disable Quest 10083: The Legion''s Plans');
+
+-- Disable Quest 9947: Return to Rokag
+DELETE FROM disables WHERE sourceType=1 AND entry=9947;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9947, 'Disable Quest 9947: Return to Rokag');
+
+-- Disable Quest 9949: A Bird's-Eye View
+DELETE FROM disables WHERE sourceType=1 AND entry=9949;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9949, 'Disable Quest 9949: A Bird''s-Eye View');
+
+-- Disable Quest 9346: When Helboars Fly
+DELETE FROM disables WHERE sourceType=1 AND entry=9346;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9346, 'Disable Quest 9346: When Helboars Fly');
+
+-- Disable Quest 9969: The Final Reagents
+DELETE FROM disables WHERE sourceType=1 AND entry=9969;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9969, 'Disable Quest 9969: The Final Reagents');
+
+-- Disable Quest 9929: The Missing Merchant
+DELETE FROM disables WHERE sourceType=1 AND entry=9929;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9929, 'Disable Quest 9929: The Final Reagents');
+
+-- Disable Quest 9959: Scouting the Defenses
+DELETE FROM disables WHERE sourceType=1 AND entry=9959;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9959, 'Disable Quest 9959: Scouting the Defenses');
+
+-- Disable Quest 9953: Lookout Nodak
+DELETE FROM disables WHERE sourceType=1 AND entry=9953;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9953, 'Disable Quest 9953: Lookout Nodak');
+
+-- Disable Quest 9943: Return to Thander
+DELETE FROM disables WHERE sourceType=1 AND entry=9943;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9943, 'Disable Quest 9943: Return to Thander');
+
+-- Disable Quest 9344: A Hasty Departure
+DELETE FROM disables WHERE sourceType=1 AND entry=9344;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9344, 'Disable Quest 9344: A Hasty Departure');
+
+-- Disable Quest 10089: Forge Camps of the Legion
+DELETE FROM disables WHERE sourceType=1 AND entry=10089;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10089, 'Disable Quest 10089: Forge Camps of the Legion');
+
+-- Disable Quest 10149: Mission: End All, Be All
+DELETE FROM disables WHERE sourceType=1 AND entry=10149;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10149, 'Disable Quest 10149: Mission: End All, Be All');
+
+-- Disable Quest 9952: Mission: End All, Be All
+DELETE FROM disables WHERE sourceType=1 AND entry=9952;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 9952, 'Disable Quest 9952: Prospector Balmoral');
+
+-- Disable Quest 10090: The Legion's Plans
+DELETE FROM disables WHERE sourceType=1 AND entry=10090;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10090, 'Disable Quest 10090: The Legion''s Plans');
+
+-- Disable Quest 10219: Walk the Dog
+DELETE FROM disables WHERE sourceType=1 AND entry=10219;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10219, 'Disable Quest 10219: Walk the Dog');
+
+-- Disable Quest 10398: Return to Honor Hold
+DELETE FROM disables WHERE sourceType=1 AND entry=10398;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10398, 'Disable Quest 10398: Return to Honor Hold');
+
+-- Disable Quest 10401: Mission: End All, Be All
+DELETE FROM disables WHERE sourceType=1 AND entry=10401;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10401, 'Disable Quest 10401: Mission: End All, Be All');
+
+-- Disable Quest 10841: The Vengeful Harbinger
+DELETE FROM disables WHERE sourceType=1 AND entry=10841;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10841, 'Disable Quest 10841: The Vengeful Harbinger');
+
+-- Disable Quest 10631: Back In Business
+DELETE FROM disables WHERE sourceType=1 AND entry=10631;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10631, 'Disable Quest 10631: Back In Business');
+
+-- Disable Quest 13150: ZZOLDLost: Widget
+DELETE FROM disables WHERE sourceType=1 AND entry=13150;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 13150, 'Disable Quest 13150: ZZOLDLost: Widget');
+
+-- Disable Quest 13299: ZZOLDWisp Cloak
+DELETE FROM disables WHERE sourceType=1 AND entry=13299;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 13299, 'Disable Quest 13299: ZZOLDWisp Cloak');
+
+-- Disable Quest 10080: Forge Camps of the Legion
+DELETE FROM disables WHERE sourceType=1 AND entry=10080;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 10080, 'Disable Quest 10080: Forge Camps of the Legion');
+
+-- Disable Quest 13303: The Panchurian Candidate
+DELETE FROM disables WHERE sourceType=1 AND entry=13303;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 13303, 'Disable Quest 13303: The Panchurian Candidate');
+
+-- Disable Quest 29260: The Elements Cry Out
+DELETE FROM disables WHERE sourceType=1 AND entry=29260;
+INSERT INTO disables (sourceType, entry, comment) VALUES
+(1, 29260, 'Disable Quest 29260: The Elements Cry Out');
