b132753c0f97b4546a7880b8ef947682bc2f5c8c
AriDEV <aridev666@gmail.com>
2019-04-28 18:12:15 +0200

    Fix Typo
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- sql/updates/world/2019_04_23_00_world.sql
+++ sql/updates/world/2019_04_23_00_world.sql
@@ -1,6 +1,6 @@
 -- GameObjectLoot for Quest: "Stealing is Bad... Re-Stealing is OK"
 SET @QITEM := 210768;
-SET @GD1 := 41405
+SET @GD1 := 41405;
 DELETE FROM gameobject_loot_template WHERE entry=@GD1;
 INSERT INTO gameobject_loot_template (entry, item, ChanceOrQuestChance) VALUES
 (@GD1, @QITEM, -100);
