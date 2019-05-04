a3471aacfe48637ef9ad814a742ea8d604a2e9a8
AriDEV <aridev666@gmail.com>
2019-05-04 02:51:39 +0200

    Scripts/Commands: Remove Arena Team Commands.
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/auth/2019_05_04_00_logs.sql
@@ -0,0 +1,5 @@
+DELETE FROM rbac_permissions WHERE id IN
+(232, 233, 234, 235, 236, 237, 238);
+
+DELETE FROM rbac_linked_permissions WHERE linkedId IN
+(232, 233, 234, 235, 236, 237, 238);
--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -42,7 +42,6 @@ void AddSC_SmartSCripts();
 //Commands
 void AddSC_account_commandscript();
 void AddSC_achievement_commandscript();
-void AddSC_arena_commandscript();
 void AddSC_ban_commandscript();
 void AddSC_bf_commandscript();
 void AddSC_cast_commandscript();
@@ -705,7 +704,6 @@ void AddCommandScripts()
 {
     AddSC_account_commandscript();
     AddSC_achievement_commandscript();
-    AddSC_arena_commandscript();
     AddSC_ban_commandscript();
     AddSC_bf_commandscript();
     AddSC_cast_commandscript();
