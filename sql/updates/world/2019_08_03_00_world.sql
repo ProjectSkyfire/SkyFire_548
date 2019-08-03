0f94180938af45ab257abebdea4bee081261c25a
AriDEV <aridev666@gmail.com>
2019-08-03 10:21:56 +0200

    Database/Misc: Added Missing exploration_basexp for cataclysm and pandaria levels.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2019_08_03_00_world.sql
@@ -0,0 +1,12 @@
+DELETE FROM exploration_basexp WHERE level IN (89, 88, 87, 86, 85, 84, 83, 82, 81, 80);
+INSERT INTO exploration_basexp (level, basexp) VALUES
+(89, 2006),
+(88, 1968),
+(87, 1930),
+(86, 1892),
+(85, 1852),
+(84, 1800),
+(83, 1780),
+(82, 1750),
+(81, 1700),
+(80, 1670);
