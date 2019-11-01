3bdcec25d0f173e901f0b3b46671459ef42eb03d
AriDEV <aridev666@gmail.com>
2019-11-01 03:08:37 +0100

    Fix a typo.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- sql/updates/world/2019_11_01_02_world.sql
+++ sql/updates/world/2019_11_01_02_world.sql
@@ -3,7 +3,7 @@ INSERT INTO phase_area (AreaId, PhaseId, Comment) VALUES (4756, 170, 'Gilneas Ph
 
 DELETE FROM conditions WHERE SourceTypeOrReferenceId=26 AND SourceGroup=170 AND ConditionValue1=14078;
 INSERT INTO conditions (SourceTypeOrReferenceId, SourceGroup, SourceEntry, SourceId, ElseGroup, ConditionTypeOrReference, ConditionTarget, ConditionValue1, ConditionValue2, ConditionValue3, NegativeCondition, ErrorType, ErrorTextId, ScriptName, Comment) VALUES
-(26, 170, 4756, 0, 1, 8, 0, 14078, 0, 0, 0, 0, 0, '', 'Set Phase 170 in area 4756 if q14078 rewarded');
+(26, 170, 4756, 0, 1, 8, 0, 14078, 0, 0, 0, 0, 0, '', 'Set Phase 170 in area 4756 if q14078 rewarded'),
 (26, 170, 4756, 0, 2, 28, 0, 14078, 0, 0, 0, 0, 0, '', 'Set Phase 170 in area 4756 if q14078 complete');
 
 SET @CGUID := 237139;
