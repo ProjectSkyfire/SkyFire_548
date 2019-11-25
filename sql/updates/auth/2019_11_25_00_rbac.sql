291ff43f1bb9260e2ee63609f6ddb006f31a08f5
Economy - V <10685816+TheNeoTV@users.noreply.github.com>
2019-11-25 20:25:20 +0100

    Create 2019_11_25_00_rbac.sql
    -- old unused arena commands. Removed in https://github.com/ProjectSkyfire/SkyFire_548/commit/a3471aacfe48637ef9ad814a742ea8d604a2e9a8

---
+++ sql/updates/auth/2019_11_25_00_rbac.sql
@@ -0,0 +1,2 @@
+-- old unused arena commands. Removed in https://github.com/ProjectSkyfire/SkyFire_548/commit/a3471aacfe48637ef9ad814a742ea8d604a2e9a8
+DELETE FROM `rbac_permissions` WHERE `id` IN (232, 233, 234, 235, 236, 237, 238);
