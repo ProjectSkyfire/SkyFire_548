10530b91bc022f43f0605a38ab3efba312dd61de
AriDEV <aridev666@gmail.com>
2020-01-14 06:28:09 +0100

    CMake: Added new OpenSSL root path.
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- cmake/macros/FindOpenSSL.cmake
+++ cmake/macros/FindOpenSSL.cmake
@@ -44,6 +44,7 @@ SET(_OPENSSL_ROOT_HINTS
 IF(PLATFORM EQUAL 64)
   SET(_OPENSSL_ROOT_PATHS
     "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (64-bit)_is1;InstallLocation]"
+    "C:/Program Files/OpenSSL-Win64/"
     "C:/OpenSSL-Win64/"
     "C:/OpenSSL/"
   )
@@ -51,6 +52,7 @@ ELSE()
   SET(_OPENSSL_ROOT_PATHS
     "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;InstallLocation]"
     "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OpenSSL (32-bit)_is1;InstallLocation]"
+    "C:/Program Files/OpenSSL-Win32/"
     "C:/OpenSSL-Win32/"
     "C:/OpenSSL/"
   )
