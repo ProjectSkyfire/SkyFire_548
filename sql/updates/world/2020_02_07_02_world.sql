294222f76871ccb2be3b96bf33e0f2511da76b1f
AriDEV <aridev666@gmail.com>
2020-02-07 05:36:07 +0100

    Database/Quests: Update quest titles and objective text to frost nova, got changed from arcane missiles in pandaria. -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2020_02_07_02_world.sql
@@ -0,0 +1,16 @@
+-- Worgen Frost Nova
+UPDATE `quest_template` SET `Title`='Frost Nova', `Objectives`='Reach level 3 to learn Frost Nova. Use it against a Bloodfang Worgen.' WHERE `Id`=14281;
+-- Draenei Frost Nova
+UPDATE `quest_template` SET `Title`='Frost Nova', `Objectives`='Reach level 3 to learn Frost Nova. Find a Training Dummy on the northwest side of the Crash Site and practice using it 2 times.' WHERE `Id`=26968;
+-- Orc Frost Nova
+UPDATE `quest_template` SET `Title`='Frost Nova', `Objectives`='Reach level 3 to learn Frost Nova. Locate a Training Dummy and practice using Frost Nova 2 times.' WHERE `Id`=25149;
+-- Gnome Frost Nova
+UPDATE `quest_template` SET `Title`='Frost Nova', `Objectives`='Reach level 3 to learn Frost Nova. Once you've learned the spell, successfully cast it twice at a Training Dummy in New Tinkertown.' WHERE `Id`=26198;
+-- Blood Elf Frost Nova
+UPDATE `quest_template` SET `Title`='Frost Nova', `Objectives`='Reach level 3 to learn Frost Nova. Locate a Training Dummy outside the Sunspire and practice using it 2 times.' WHERE `Id`=10068;
+-- Dwarf Frost Nova
+UPDATE `quest_template` SET `Title`='Frost Nova', `Objectives`='Reach level 3 to learn Frost Nova. Successfully cast it 2 times on the Training Dummies outside the entrance to Anvilmar.' WHERE `Id`=24526;
+-- Undead Frost Nova
+UPDATE `quest_template` SET `Title`='Frost Nova', `Objectives`='Reach Level 3 to learn Frost Nova, then practice using it 2 times against a Training Dummy.' WHERE `Id`=24965;
+-- Troll Frost Nova
+UPDATE `quest_template` SET `Title`='Frost Nova', `Objectives`='Reach level 3 to learn Frost Nova. Practice using it 2 times against a Tiki Target.' WHERE `Id`=24752;
