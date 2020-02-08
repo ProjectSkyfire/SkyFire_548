7c2c9c4b586d74bd5ede48843f14925bbbfce03f
SkyFire <admin@projectskyfire.org>
2020-02-08 17:57:01 -0600

    SQL Syntax. Fix an error.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- sql/updates/world/2020_01_23_00_world.sql
+++ sql/updates/world/2020_01_23_00_world.sql
@@ -1,3 +1,3 @@
-DELETE FROM `spell_linked_spell` FROM `spell_linked_spell` WHERE `spell_trigger`=103958 AND `spell_effect`=103965;
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=103958 AND `spell_effect`=103965;
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
 ('103958', '103965', '2', 'Metamorphosis');
