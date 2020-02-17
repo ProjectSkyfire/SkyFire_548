f8973366cab4352e98b470091931d47970ffb3d4
AriDEV <aridev666@gmail.com>
2020-02-17 09:28:54 +0100

    Database/Creature: Update creature_template for heroic version of Isiset. -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2020_02_17_03_world.sql
@@ -0,0 +1,2 @@
+-- Normal: 39587 Heroic: 48710 Isiset
+UPDATE `creature_template` SET `minlevel`='87', `maxlevel`='87', `exp`='3', `faction_A`='16', `faction_H`='16', `speed_walk`='2', `baseattacktime`='2000', `rangeattacktime`='2000', `unit_class`='2', `unit_flags`='32832', `flags_extra`='1' WHERE  `entry`=48710;
