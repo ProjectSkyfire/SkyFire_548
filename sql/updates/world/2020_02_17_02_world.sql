96db0ab4891adb79e47b5ffbbabc92f0fa071f18
AriDEV <aridev666@gmail.com>
2020-02-17 09:17:13 +0100

    Database/Creature: Update creature template for heroic version of Crimsonborne Seer <Servant of Deathwing> -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2020_02_17_02_world.sql
@@ -0,0 +1,2 @@
+-- Normal: 39405 Heroic: 48652 Crimsonborne Seer <Servant of Deathwing>
+UPDATE `creature_template` SET `minlevel`='85', `maxlevel`='85', `faction_A`='1771', `faction_H`='1771', `baseattacktime`='2000', `rangeattacktime`='2000', `unit_class`='8' WHERE `entry`=48652;
