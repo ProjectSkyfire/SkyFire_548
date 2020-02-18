b0384a09279902de2752c31a0e5626db64aca56a
AriDEV <aridev666@gmail.com>
2020-02-18 19:35:35 +0100

    Database/Instance: link some instances with scripts.
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2020_02_18_00_world.sql
@@ -0,0 +1,27 @@
+DELETE FROM `instance_template` WHERE `map`=558;
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES
+(558, 530, 'instance_auchenai_crypts', 0);
+
+DELETE FROM `instance_template` WHERE `map`=557;
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES
+(557, 530, 'instance_mana_tombs', 0);
+
+DELETE FROM `instance_template` WHERE `map`=547;
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES
+(547, 530, 'instance_the_slave_pens', 0);
+
+DELETE FROM `instance_template` WHERE `map`=546;
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES
+(546, 530, 'instance_the_underbog', 0);
+
+DELETE FROM `instance_template` WHERE `map`=600;
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES
+(600, 571, 'instance_drak_tharon_keep', 1);
+
+DELETE FROM `instance_template` WHERE `map`=624;
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES
+(624, 571, 'instance_vault_of_archavon', 0);
+
+DELETE FROM `instance_template` WHERE `map`=429;
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES
+(429, 1, 'instance_dire_maul', 0);
