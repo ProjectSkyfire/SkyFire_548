93db3fd87917e22faef168d85bd1689704e49f9f
AriDEV <aridev666@gmail.com>
2020-02-22 04:58:37 +0100

    Database/Terrain: Added alliance terrain swap for jade forest. -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2020_02_22_01_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `terrain_swap_defaults` WHERE `MapId`=870 AND `TerrainSwapMap`=971;
+INSERT INTO `terrain_swap_defaults` (`MapId`, `TerrainSwapMap`, `Comment`) VALUES
+(870, 971, 'Jade Forest Alliance Starting Terrain');
