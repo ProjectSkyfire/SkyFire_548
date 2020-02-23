8c66b22b6882c2433fef0b5ad9dd9297ba5a1872
AriDEV <aridev666@gmail.com>
2020-02-23 17:11:46 +0100

    lol -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- sql/updates/world/2020_02_23_00_world.sql
+++ sql/updates/world/2020_02_23_00_world.sql
@@ -1,3 +1,3 @@
-DELETE FROM `disables` WHERE `sourceType`=1 AND `entry=5`;
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=5;
 INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
 ('1', '5', 'Disable Quest 5: Jitters'' Growling Gut');
