feed252abf25e8f15b2a804792e81ae5eb250eb3
AriDEV <aridev666@gmail.com>
2020-02-26 19:53:41 +0100

    Server/Pet: Drop obsolete HappynessState enum.
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- src/server/game/Entities/Pet/PetDefines.h
+++ src/server/game/Entities/Pet/PetDefines.h
@@ -39,13 +39,6 @@ enum PetSaveMode
     PET_SAVE_NOT_IN_SLOT       =  100                       // for avoid conflict with stable size grow will use 100
 };
 
-enum HappinessState
-{
-    UNHAPPY = 1,
-    CONTENT = 2,
-    HAPPY   = 3
-};
-
 enum PetSpellState
 {
     PETSPELL_UNCHANGED = 0,
