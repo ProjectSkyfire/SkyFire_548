d7835fff99133c21b9195751c80f8072085ba796
Nightprince <keramatjokar8855@gmail.com>
2020-02-28 11:33:42 +0330

    DB/Quest: Fix Quest 11129 #842
    * Delete duplicate Kyle the Frenzied
    * Fix Tender Strider Meat chance drop
    * Fix waypoints Kyle the Frenzied
    * Move from script to DB

--- src/server/scripts/Kalimdor/zone_mulgore.cpp
+++ src/server/scripts/Kalimdor/zone_mulgore.cpp
@@ -21,13 +21,12 @@
 /* ScriptData
 SDName: Mulgore
 SD%Complete: 100
-SDComment: Support for quest: 11129, 861
+SDComment: Support for quest: 861
 SDCategory: Mulgore
 EndScriptData */
 
 /* ContentData
 npc_skorn_whitecloud
-npc_kyle_frenzied
 EndContentData */
 
 #include "ScriptMgr.h"
@@ -71,139 +70,7 @@ public:
 
 };
 
-/*#####
-# npc_kyle_frenzied
-######*/
-
-enum KyleFrenzied
-{
-    EMOTE_SEE_LUNCH         = 0,
-    EMOTE_EAT_LUNCH         = 1,
-    EMOTE_DANCE             = 2,
-
-    SPELL_LUNCH             = 42222,
-    NPC_KYLE_FRENZIED       = 23616,
-    NPC_KYLE_FRIENDLY       = 23622,
-    POINT_ID                = 1
-};
-
-class npc_kyle_frenzied : public CreatureScript
-{
-public:
-    npc_kyle_frenzied() : CreatureScript("npc_kyle_frenzied") { }
-
-    CreatureAI* GetAI(Creature* creature) const OVERRIDE
-    {
-        return new npc_kyle_frenziedAI (creature);
-    }
-
-    struct npc_kyle_frenziedAI : public ScriptedAI
-    {
-        npc_kyle_frenziedAI(Creature* creature) : ScriptedAI(creature) { }
-
-        bool EventActive;
-        bool IsMovingToLunch;
-        uint64 PlayerGUID;
-        uint32 EventTimer;
-        uint8 EventPhase;
-
-        void Reset() OVERRIDE
-        {
-            EventActive = false;
-            IsMovingToLunch = false;
-            PlayerGUID = 0;
-            EventTimer = 5000;
-            EventPhase = 0;
-
-            if (me->GetEntry() == NPC_KYLE_FRIENDLY)
-                me->UpdateEntry(NPC_KYLE_FRENZIED);
-        }
-
-        void SpellHit(Unit* Caster, SpellInfo const* Spell)
-        {
-            if (!me->GetVictim() && !EventActive && Spell->Id == SPELL_LUNCH)
-            {
-                if (Caster->GetTypeId() == TypeID::TYPEID_PLAYER)
-                    PlayerGUID = Caster->GetGUID();
-
-                if (me->GetMotionMaster()->GetCurrentMovementGeneratorType() == WAYPOINT_MOTION_TYPE)
-                {
-                    me->GetMotionMaster()->MovementExpired();
-                    me->GetMotionMaster()->MoveIdle();
-                    me->StopMoving();
-                }
-
-                EventActive = true;
-                Talk(EMOTE_SEE_LUNCH);
-                me->SetUInt32Value(UNIT_FIELD_NPC_EMOTESTATE, EMOTE_ONESHOT_CREATURE_SPECIAL);
-            }
-        }
-
-        void MovementInform(uint32 type, uint32 pointId) OVERRIDE
-        {
-            if (type != POINT_MOTION_TYPE || !EventActive)
-                return;
-
-            if (pointId == POINT_ID)
-                IsMovingToLunch = false;
-        }
-
-        void UpdateAI(uint32 diff) OVERRIDE
-        {
-            if (EventActive)
-            {
-                if (IsMovingToLunch)
-                    return;
-
-                if (EventTimer <= diff)
-                {
-                    EventTimer = 5000;
-                    ++EventPhase;
-
-                    switch (EventPhase)
-                    {
-                        case 1:
-                            if (Unit* unit = ObjectAccessor::GetUnit(*me, PlayerGUID))
-                            {
-                                if (GameObject* go = unit->GetGameObject(SPELL_LUNCH))
-                                {
-                                    IsMovingToLunch = true;
-                                    me->GetMotionMaster()->MovePoint(POINT_ID, go->GetPositionX(), go->GetPositionY(), go->GetPositionZ());
-                                }
-                            }
-                            break;
-                        case 2:
-                            Talk(EMOTE_EAT_LUNCH);
-                            me->SetUInt32Value(UNIT_FIELD_NPC_EMOTESTATE, EMOTE_STATE_USE_STANDING);
-                            break;
-                        case 3:
-                            if (Player* unit = ObjectAccessor::GetPlayer(*me, PlayerGUID))
-                                unit->TalkedToCreature(me->GetEntry(), me->GetGUID());
-
-                            me->UpdateEntry(NPC_KYLE_FRIENDLY);
-                            break;
-                        case 4:
-                            EventTimer = 30000;
-                            Talk(EMOTE_DANCE);
-                            me->SetUInt32Value(UNIT_FIELD_NPC_EMOTESTATE, EMOTE_STATE_DANCESPECIAL);
-                            break;
-                        case 5:
-                            me->SetUInt32Value(UNIT_FIELD_NPC_EMOTESTATE, EMOTE_STATE_NONE);
-                            Reset();
-                            me->GetMotionMaster()->Clear();
-                            break;
-                    }
-                }
-                else
-                    EventTimer -= diff;
-            }
-        }
-    };
-
-};
-
 void AddSC_mulgore()
 {
     new npc_skorn_whitecloud();
-    new npc_kyle_frenzied();
 }
