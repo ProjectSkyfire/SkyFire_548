a3d39bed55fc84a4f41389df068cf13ba52782b0
AriDEV <aridev666@gmail.com>
2020-03-05 01:11:42 +0100

    Database/Creature: Random movement for manifestation/vestige of pride. -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2020_03_05_03_world.sql
@@ -0,0 +1,2 @@
+UPDATE `creature` SET `MovementType`=1, `spawndist`=5 WHERE `id`=72279; -- Vestige of Pride Movement
+UPDATE `creature` SET `MovementType`=1, `spawndist`=5 WHERE `id`=72280; -- Manifestation of Pride Movement
