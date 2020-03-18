f4af571237085b04594168f751297e9171a0bc86
SkyFire <admin@projectskyfire.org>
2020-03-18 08:12:22 -0500

    Update missed copyright headers. No change to any functions.
    [ci-skip]
    
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/scripts/EasternKingdoms/ZulGurub/instance_zulgurub.h
+++ src/server/scripts/EasternKingdoms/ZulGurub/instance_zulgurub.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2011-2019 Project SkyFire <http://www.projectskyfire.org/
- * Copyright (C) 2008-2019 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2011-2020 Project SkyFire <http://www.projectskyfire.org/
+ * Copyright (C) 2008-2020 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
  * Copyright (C) 2006-2014 ScriptDev2 <https://github.com/scriptdev2/scriptdev2/>
  *
--- src/server/scripts/Spells/spell_monk.cpp
+++ src/server/scripts/Spells/spell_monk.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2011-2019 Project SkyFire <http://www.projectskyfire.org/>
- * Copyright (C) 2008-2019 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2011-2020 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2008-2020 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2005-2019 MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify it
