3c7818f9f83a657627575e0d63722fa7df6d8eca
AriDEV <aridev666@gmail.com>
2020-05-29 03:04:58 +0200

    Database/Creature: Update Huolon's level and inhabittype. -[ci skip]
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2020_05_29_00_world.sql
@@ -0,0 +1,5 @@
+-- Crimsonscale Firestorm
+UPDATE creature_template SET InhabitType=4 WHERE entry=72876;
+
+-- Huolon
+UPDATE creature_template SET minlevel=93, maxlevel=93, InhabitType=4 WHERE entry=73167;
