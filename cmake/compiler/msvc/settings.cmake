a45d9e92e69188d0d118cc0f234d5cb6c3e4c9b9
AriDEV <aridev666@gmail.com>
2020-07-02 07:32:24 +0200

    CMake: Update visual studio expected version and set upper limit for pch memory allocation to 50% aka (37.5mb) instead of 500% aka (375mb), also drop coverity bat files.
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- cmake/compiler/msvc/settings.cmake
+++ cmake/compiler/msvc/settings.cmake
@@ -14,7 +14,7 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 
 include(CheckCXXCompilerFlag)
 
-set(MSVC_EXPECTED_VERSION 19.26.28619.0)
+set(MSVC_EXPECTED_VERSION 19.26.28806.0)
 if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS MSVC_EXPECTED_VERSION)
   message(FATAL_ERROR "MSVC: SkyFire requires version ${MSVC_EXPECTED_VERSION} (MSVC 2019) to build but found ${CMAKE_CXX_COMPILER_VERSION}")
 endif()
@@ -86,7 +86,7 @@ endif()
 # Fixes a compiler-problem when using PCH - the /Ym flag is adjusted by the compiler in MSVC2012, hence we need to set an upper limit with /Zm to avoid discrepancies)
 # (And yes, this is a verified , unresolved bug with MSVC... *sigh*)
 string(REGEX REPLACE "/Zm[0-9]+ *" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zm500" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zm50" CACHE STRING "" FORCE)
 
 # Enable and treat as errors the following warnings to easily detect virtual function signature failures:
 # 'function' : member function does not override any base class virtual member function
