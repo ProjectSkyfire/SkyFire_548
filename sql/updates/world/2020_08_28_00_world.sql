5944c43c2145cefdd6ecd9641a46bdb11b9b7fd5
AriDEV <aridev666@gmail.com>
2020-08-28 05:25:17 +0200

    Database/Achievement: Remove obsolete Achievement rewards.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2020_08_28_00_world.sql
@@ -0,0 +1,38 @@
+-- Achievement: [Pilgrim]
+DELETE FROM `achievement_reward` WHERE `entry`=3656;
+UPDATE `achievement_reward` SET `title_H`=168 WHERE `entry`=3478;
+
+-- Achievement: [Noble Gardener]
+DELETE FROM `achievement_reward` WHERE `entry`=2797;
+UPDATE `achievement_reward` SET `title_A`=155 WHERE `entry`=2798; 
+
+-- Achievement: [What A Long, Strange Trip It's Been]
+DELETE FROM `achievement_reward` WHERE `entry`=2145; 
+
+-- Achievement: [Brewmaster]
+DELETE FROM `achievement_reward` WHERE `entry`=1684;
+UPDATE `achievement_reward` SET `title_A`=133 WHERE `entry`=2798; 
+
+-- Achievement: [Merrymaker]
+DELETE FROM `achievement_reward` WHERE `entry`=1692;
+UPDATE `achievement_reward` SET `title_A`=134 WHERE `entry`=1691; 
+
+-- Achievement: [Fool For Love]
+DELETE FROM `achievement_reward` WHERE `entry`=1707;
+UPDATE `achievement_reward` SET `title_A`=135 WHERE `entry`=1693; 
+
+-- Achievement: [Hail to the Chef]
+DELETE FROM `achievement_reward` WHERE `entry`=1784;
+UPDATE `achievement_reward` SET `title_H`=84 WHERE `entry`=1563; 
+
+-- Achievement: [The Loremaster]
+DELETE FROM `achievement_reward` WHERE `entry`=1681;
+DELETE FROM `achievement_reward` WHERE `entry`=1682;
+
+-- Achievement: [Hallowed Be Thy Name]
+DELETE FROM `achievement_reward` WHERE `entry`=1657;
+UPDATE `achievement_reward` SET `title_H`=124 WHERE `entry`=1656; 
+
+-- Achievement: [Chapter V: Judgment of the Black Prince]
+-- The title was added in Warlords of Draenor.
+DELETE FROM `achievement_reward` WHERE `entry`=8325;
