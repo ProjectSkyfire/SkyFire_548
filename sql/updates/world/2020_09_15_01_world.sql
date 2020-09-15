dcefd477afefa80ef1fb3e2d8e401b25b76f0b97
AriDEV <aridev666@gmail.com>
2020-09-15 21:31:19 +0200

    Database/Spells: Added missing spell_script_names for master of subtlety.
    Closes #941
    
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2020_09_15_01_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (1784, 31666);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(1784, 'spell_rog_stealth'),
+(31666, 'spell_rog_master_of_subtlety');
