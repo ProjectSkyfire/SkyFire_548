d3becab9e7b439c6d1076494e5f3f4f2a22b449d
ARIDEV <AriDEV666@gmail.com>
2021-01-04 21:24:39 +0100

    Database/Quest: Beating Them Back! -[ci skip]

---
+++ sql/updates/world/2021_01_04_01_world.sql
@@ -0,0 +1,7 @@
+-- Quests: Beating Them Back!
+UPDATE quest_template SET
+RequiredRaces=RequiredRaces|1,
+OfferRewardText='You've bought us a little time, $N, but we've got even bigger problems to deal with now.',
+RequestItemsText='Have you beaten back the Blackrock battle worgs?',
+WDBVerified='18414' WHERE Id IN
+(28766, 28762, 28767, 28764, 28763, 29078, 28757, 28765, 31139);
