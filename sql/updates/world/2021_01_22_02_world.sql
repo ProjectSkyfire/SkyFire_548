efdae252ac9d02523c868c9dc6ec7776302d47f6
ARIDEV <AriDEV666@gmail.com>
2021-01-22 06:37:22 +0100

    Database/Quest: Update The Rear is Clear quests -[ci skip]

---
+++ sql/updates/world/2021_01_22_02_world.sql
@@ -0,0 +1,5 @@
+UPDATE quest_template SET
+Method=0,
+RequiredRaces=1,
+OfferRewardText='With your help we have managed to secure the northern and western sectors of Northshire. We still have a rather large contingency of Blackrock orcs to the east and they\'ve begun burning down the forest!' WHERE Id IN
+(28823, 28819, 28817, 28821, 28820, 29083, 28818, 28822, 31145;
