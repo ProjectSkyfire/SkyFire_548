82e88dd07404b046fd0c9c2edbc12358ce202adb
ARIDEV <AriDEV666@gmail.com>
2021-01-24 10:35:03 +0100

    Database/Movement: Added some random movement to some creatures in Valley of four winds. -[ci skip]

---
+++ sql/updates/world/2021_01_24_01_world.sql
@@ -0,0 +1,24 @@
+-- Mist Incarnation Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=56740;
+-- Wild Shaghorn Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=58892;
+-- Bandicoon Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=64782;
+-- Whitefisher Crane Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=56762;
+-- Softshell Snapling Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=64789;
+-- Razorback Snapper Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=56832;
+-- Wyrmhorn Turtle Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=56256;
+-- Adolescent Mushan Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=56239;
+-- Marsh Fiddler Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=64786;
+-- Woolly Sheep Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=56033;
+-- Riverbank Barbshell Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=66099;
+-- Krasari Hissing Wasp Random Movement
+UPDATE creature SET spawndist=15, MovementType=1 WHERE id=65802;
