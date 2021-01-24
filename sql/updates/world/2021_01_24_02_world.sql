f7bb81b769beda7d3f448c7ca5c9bacd4edfc81f
ARIDEV <AriDEV666@gmail.com>
2021-01-24 11:53:04 +0100

    Database/Creature: Update some levels for vendors on timeless isle. -[ci skip]

---
+++ sql/updates/world/2021_01_24_02_world.sql
@@ -0,0 +1,6 @@
+UPDATE creature_template SET minlevel=90, maxlevel=90, npcflag=npcflag|128 WHERE entry=73305;
+UPDATE creature_template SET minlevel=90, maxlevel=90, npcflag=npcflag|128 WHERE entry=73306;
+UPDATE creature_template SET minlevel=90, maxlevel=90, npcflag=npcflag|128 WHERE entry=73657;
+UPDATE creature_template SET minlevel=90, maxlevel=90, npcflag=npcflag|128 WHERE entry=73812;
+UPDATE creature_template SET minlevel=88, maxlevel=88, npcflag=npcflag|128 WHERE entry=73010;
+UPDATE creature_template SET minlevel=90, maxlevel=90 WHERE entry=73648;
