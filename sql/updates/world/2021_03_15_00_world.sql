340c1b204533f78bfabed26f7734ad574043bdde
ARIDEV <AriDEV666@gmail.com>
2021-03-15 12:24:22 +0100

    Database/Terrain: Added worldmap swap for krasarang wilds terrain swaps. -[ci skip]

---
+++ sql/updates/world/2021_03_15_00_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `terrain_worldmap` WHERE `WorldMapArea`=910;
+INSERT INTO `terrain_worldmap` (`TerrainSwapMap`, `WorldMapArea`, `Comment`) VALUES
+(1061, 910, 'KrasarangHorde'),
+(1062, 910, 'KrasarangAlliance');
