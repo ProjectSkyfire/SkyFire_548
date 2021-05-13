0900e4a28054f64249899e0eff1533808d78748b
AriDEV <aridev666@gmail.com>
2021-05-13 17:46:09 +0200

    Database: Added suffixes for Ornate Band.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2021_05_13_08_world.sql
@@ -0,0 +1,22 @@
+DELETE FROM `item_enchantment_template` WHERE `entry`=442;
+INSERT INTO `item_enchantment_template` (`entry`, `ench`, `chance`) VALUES
+(442, 137, 5), -- of the Windstorm
+(442, 136, 5), -- of the Zephyr
+(442, 135, 5), -- of the Windflurry
+(442, 134, 5), -- of the Galeburst
+(442, 133, 5), -- of the Stormblast
+(442, 138, 5), -- of the Feverflare
+(442, 132, 5), -- of the Wavecrest
+(442, 131, 5), -- of the Undertow
+(442, 130, 5), -- of the Fireflash
+(442, 129, 5), -- of the Wildfire
+(442, 114, 5), -- of the Flameblaze
+(442, 128, 5), -- of the Rockslab
+(442, 127, 5), -- of the Bouldercrag
+(442, 125, 5), -- of the Bedrock
+(442, 124, 5), -- of the Mountainbed
+(442, 123, 5), -- of the Earthbreaker
+(442, 122, 5), -- of the Earthfall
+(442, 121, 5), -- of the Landslide
+(442, 120, 5), -- of the Earthshaker
+(442, 118, 5); -- of the Faultline
