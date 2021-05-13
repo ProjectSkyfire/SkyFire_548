3efc02c7a36d486c5899b388d2e754c0f03eb62b
AriDEV <aridev666@gmail.com>
2021-05-13 07:32:21 +0200

    Database: Added loot templates for uncommon yellow pandaria gems (Sunstone).
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2021_05_13_04_world.sql
@@ -0,0 +1,29 @@
+-- Fractured Sunstone
+DELETE FROM `spell_loot_template` WHERE `entry`=107640;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107640, 76568, 75, 1, 1, 1, 1),
+(107640, 76634, 25, 1, 1, 1, 1);
+
+-- Mystic Sunstone
+DELETE FROM `spell_loot_template` WHERE `entry`=107641;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107641, 76569, 75, 1, 1, 1, 1),
+(107641, 76635, 25, 1, 1, 1, 1);
+
+-- Quick Sunstone
+DELETE FROM `spell_loot_template` WHERE `entry`=107642;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107642, 76567, 75, 1, 1, 1, 1),
+(107642, 76633, 25, 1, 1, 1, 1);
+
+-- Smooth Sunstone
+DELETE FROM `spell_loot_template` WHERE `entry`=107643;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107643, 76565, 75, 1, 1, 1, 1),
+(107643, 76631, 25, 1, 1, 1, 1);
+
+-- Subtle Sunstone
+DELETE FROM `spell_loot_template` WHERE `entry`=107644;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107644, 76566, 75, 1, 1, 1, 1),
+(107644, 76632, 25, 1, 1, 1, 1);
