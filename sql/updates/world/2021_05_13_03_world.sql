36a8561ed4d0aeede11df2dcd40f0ea67d611061
AriDEV <aridev666@gmail.com>
2021-05-13 07:19:43 +0200

    Database: Added loot templates for uncommon purple pandaria gems (Roguestone).
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2021_05_13_03_world.sql
@@ -0,0 +1,71 @@
+-- Accurate Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107627;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107627, 76549, 75, 1, 1, 1, 1),
+(107627, 76615, 25, 1, 1, 1, 1);
+
+-- Defender's Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107628;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107628, 76558, 75, 1, 1, 1, 1),
+(107628, 76624, 25, 1, 1, 1, 1);
+
+-- Etched Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107630;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107630, 76552, 75, 1, 1, 1, 1),
+(107630, 76618, 25, 1, 1, 1, 1);
+
+-- Glinting Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107631;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107631, 76548, 75, 1, 1, 1, 1),
+(107631, 76614, 25, 1, 1, 1, 1);
+
+-- Guardian's Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107632;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107632, 76556, 75, 1, 1, 1, 1),
+(107632, 76622, 25, 1, 1, 1, 1);
+
+-- Mysterious Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107633;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107633, 76553, 75, 1, 1, 1, 1),
+(107633, 76619, 25, 1, 1, 1, 1);
+
+-- Purified Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107634;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107634, 76554, 75, 1, 1, 1, 1),
+(107634, 76620, 25, 1, 1, 1, 1);
+
+-- Retaliating Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107635;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107635, 76551, 75, 1, 1, 1, 1),
+(107635, 76617, 25, 1, 1, 1, 1);
+
+-- Shifting Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107636;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107636, 76555, 75, 1, 1, 1, 1),
+(107636, 76621, 25, 1, 1, 1, 1);
+
+-- Sovereign Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107637;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107637, 76559, 75, 1, 1, 1, 1),
+(107637, 76625, 25, 1, 1, 1, 1);
+
+-- Timeless Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107638;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107638, 76557, 75, 1, 1, 1, 1),
+(107638, 76623, 25, 1, 1, 1, 1);
+
+-- Veiled Roguestone
+DELETE FROM `spell_loot_template` WHERE `entry`=107639;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107639, 76550, 75, 1, 1, 1, 1),
+(107639, 76616, 25, 1, 1, 1, 1);
