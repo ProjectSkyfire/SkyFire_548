3edfd055beb7a013b62028b98fc79189118cede8
AriDEV <aridev666@gmail.com>
2021-05-13 06:25:41 +0200

    Database: Added loot templates for uncommon red pandaria gems (Pandarian Garnet).
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2021_05_13_02_world.sql
@@ -0,0 +1,29 @@
+-- Bold Pandarian Garnet
+DELETE FROM `spell_loot_template` WHERE `entry`=107622;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107622, 76564, 75, 1, 1, 1, 1),
+(107622, 76630, 25, 1, 1, 1, 1);
+
+-- Brilliant Pandarian Garnet
+DELETE FROM `spell_loot_template` WHERE `entry`=107623;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107623, 76562, 75, 1, 1, 1, 1),
+(107623, 76628, 25, 1, 1, 1, 1);
+
+-- Delicate Pandarian Garnet
+DELETE FROM `spell_loot_template` WHERE `entry`=107624;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107624, 76560, 75, 1, 1, 1, 1),
+(107624, 76626, 25, 1, 1, 1, 1);
+
+-- Flashing Pandarian Garnet
+DELETE FROM `spell_loot_template` WHERE `entry`=107625;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107625, 76563, 75, 1, 1, 1, 1),
+(107625, 76629, 25, 1, 1, 1, 1);
+
+-- Precise Pandarian Garnet
+DELETE FROM `spell_loot_template` WHERE `entry`=107626;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107626, 76561, 75, 1, 1, 1, 1),
+(107626, 76627, 25, 1, 1, 1, 1);
