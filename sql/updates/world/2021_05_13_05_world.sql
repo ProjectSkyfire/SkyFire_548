5f8712eca7987cdfb6748535a1f8f5b2c7d83cf8
AriDEV <aridev666@gmail.com>
2021-05-13 08:09:47 +0200

    Database: Added loot templates for uncommon orange pandaria gems (Tiger Opal).
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2021_05_13_05_world.sql
@@ -0,0 +1,131 @@
+-- Adept Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107645;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107645, 76538, 75, 1, 1, 1, 1),
+(107645, 76604, 25, 1, 1, 1, 1);
+
+-- Artful Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107646;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107646, 76540, 75, 1, 1, 1, 1),
+(107646, 76606, 25, 1, 1, 1, 1);
+
+-- Champion's Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107647;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107647, 76533, 75, 1, 1, 1, 1),
+(107647, 76599, 25, 1, 1, 1, 1);
+
+-- Crafty Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107648;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107648, 76527, 75, 1, 1, 1, 1),
+(107648, 76593, 25, 1, 1, 1, 1);
+
+-- Deadly Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107649;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107649, 76526, 75, 1, 1, 1, 1),
+(107649, 76592, 25, 1, 1, 1, 1);
+
+-- Deft Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107650;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107650, 76534, 75, 1, 1, 1, 1),
+(107650, 76600, 25, 1, 1, 1, 1);
+
+-- Fierce Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107651;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107651, 76537, 75, 1, 1, 1, 1),
+(107651, 76603, 25, 1, 1, 1, 1);
+
+-- Fine Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107652;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107652, 76541, 75, 1, 1, 1, 1),
+(107652, 76607, 25, 1, 1, 1, 1);
+
+-- Inscribed Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107653;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107653, 76529, 75, 1, 1, 1, 1),
+(107653, 76595, 25, 1, 1, 1, 1);
+
+-- Keen Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107654;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107654, 76539, 75, 1, 1, 1, 1),
+(107654, 76605, 25, 1, 1, 1, 1);
+
+-- Lucent Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107655;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107655, 76543, 75, 1, 1, 1, 1),
+(107655, 76609, 25, 1, 1, 1, 1);
+
+-- Polished Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107656;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107656, 76530, 75, 1, 1, 1, 1),
+(107656, 76596, 25, 1, 1, 1, 1);
+
+-- Potent Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107657;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107657, 76528, 75, 1, 1, 1, 1),
+(107657, 76594, 25, 1, 1, 1, 1);
+
+-- Reckless Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107658;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107658, 76536, 75, 1, 1, 1, 1),
+(107658, 76602, 25, 1, 1, 1, 1);
+
+-- Resolute Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107659;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107659, 76531, 75, 1, 1, 1, 1),
+(107659, 76597, 25, 1, 1, 1, 1);
+
+-- Resplendent Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107660;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107660, 76547, 75, 1, 1, 1, 1),
+(107660, 76613, 25, 1, 1, 1, 1);
+
+-- Skillful Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107661;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107661, 76542, 75, 1, 1, 1, 1),
+(107661, 76608, 25, 1, 1, 1, 1);
+
+-- Splendid Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107662;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107662, 76546, 75, 1, 1, 1, 1),
+(107662, 76612, 25, 1, 1, 1, 1);
+
+-- Stalwart Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107663;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107663, 76532, 75, 1, 1, 1, 1),
+(107663, 76598, 25, 1, 1, 1, 1);
+
+-- Tenuous Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107665;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107665, 76544, 75, 1, 1, 1, 1),
+(107665, 76610, 25, 1, 1, 1, 1);
+
+-- Wicked Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107666;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107666, 76535, 75, 1, 1, 1, 1),
+(107666, 76601, 25, 1, 1, 1, 1);
+
+-- Willful Tiger Opal
+DELETE FROM `spell_loot_template` WHERE `entry`=107667;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107667, 76545, 75, 1, 1, 1, 1),
+(107667, 76611, 25, 1, 1, 1, 1);
