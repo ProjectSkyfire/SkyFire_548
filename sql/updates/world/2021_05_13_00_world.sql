0cbb363d58e4ac7e6bf2c1b90ca4dabfbc024e35
AriDEV <aridev666@gmail.com>
2021-05-13 05:39:50 +0200

    Database: Added loot templates for uncommon green pandaria gems (Alexandrite).
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

---
+++ sql/updates/world/2021_05_13_00_world.sql
@@ -0,0 +1,107 @@
+-- Balanced Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107598;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107598, 76513, 75, 1, 1, 1, 1),
+(107598, 76580, 25, 1, 1, 1, 1);
+
+-- Effulgent Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107599;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107599, 76511, 75, 1, 1, 1, 1),
+(107599, 76578, 25, 1, 1, 1, 1);
+
+-- Energized Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107600;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107600, 76519, 75, 1, 1, 1, 1),
+(107600, 76585, 25, 1, 1, 1, 1);
+
+-- Forceful Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107601;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107601, 76522, 75, 1, 1, 1, 1),
+(107601, 76588, 25, 1, 1, 1, 1);
+
+-- Jagged Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107602;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107602, 76520, 75, 1, 1, 1, 1),
+(107602, 76586, 25, 1, 1, 1, 1);
+
+-- Lightning Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107604;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107604, 76509, 75, 1, 1, 1, 1),
+(107604, 76576, 25, 1, 1, 1, 1);
+
+-- Misty Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107605;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107605, 76507, 75, 1, 1, 1, 1),
+(107605, 76574, 25, 1, 1, 1, 1);
+
+-- Nimble Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107606;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107606, 93706, 75, 1, 1, 1, 1),
+(107606, 93707, 25, 1, 1, 1, 1);
+
+-- Piercing Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107607;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107607, 76508, 75, 1, 1, 1, 1),
+(107607, 76575, 25, 1, 1, 1, 1);
+
+-- Puissant Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107608;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107608, 76524, 75, 1, 1, 1, 1),
+(107608, 76590, 25, 1, 1, 1, 1);
+
+-- Radiant Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107609;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107609, 76517, 75, 1, 1, 1, 1),
+(107609, 76583, 25, 1, 1, 1, 1);
+
+-- Regal Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107610;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107610, 76521, 75, 1, 1, 1, 1),
+(107610, 76587, 25, 1, 1, 1, 1);
+
+-- Sensei's Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107611;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107611, 76510, 75, 1, 1, 1, 1),
+(107611, 76577, 25, 1, 1, 1, 1);
+
+-- Shattered Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107612;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107612, 76518, 75, 1, 1, 1, 1),
+(107612, 76584, 25, 1, 1, 1, 1);
+
+-- Steady Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107613;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107613, 76525, 75, 1, 1, 1, 1),
+(107613, 76591, 25, 1, 1, 1, 1);
+
+-- Turbid Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107614;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107614, 76515, 75, 1, 1, 1, 1),
+(107614, 76582, 25, 1, 1, 1, 1);
+
+-- Vivid Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107615;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107615, 76514, 75, 1, 1, 1, 1),
+(107615, 76581, 25, 1, 1, 1, 1);
+
+-- Zen Alexandrite
+DELETE FROM `spell_loot_template` WHERE `entry`=107616;
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(107616, 76512, 75, 1, 1, 1, 1),
+(107616, 76579, 25, 1, 1, 1, 1);
