7c9643865f1b39657fbde885944b1868f50ef49c
AriDEV <aridev666@gmail.com>
2021-05-14 12:02:28 +0200

    Database: Added rank 1 to previous rank commit.
    -[ci skip]
    
    Signed-off-by: AriDEV <aridev666@gmail.com>

--- sql/updates/world/2021_05_13_06_world.sql
+++ sql/updates/world/2021_05_13_06_world.sql
@@ -4,6 +4,7 @@ INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillv
 
 DELETE FROM `spell_ranks` WHERE `first_spell_id`=25245;
 INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `spell_rank`) VALUES
+(25245, 25245, 1),
 (25245, 25246, 2),
 (25245, 28896, 3),
 (25245, 28899, 4),
