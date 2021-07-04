dbd2215a46d081c8b4146858f1b093df5dcc7300
Crypticaz <nick.lop6@gmail.com>
2021-07-04 18:35:39 -0500

    Burst of Speed fix

---
+++ sql/updates/world/2021_07_04_00_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=108212 AND `spell_effect`=137573 AND `type`=0;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (108212, 137573, 0, 'Burst of Spell - Rogue Talent');
