4d5caa87ecd47596b65938f96e111d1487226878
Crypticaz <nick.lop6@gmail.com>
2021-07-05 11:38:25 -0500

    fix (Core/DB) wrong spell_trigger id for prowl

--- sql/updates/world/2021_07_04_01_world.sql
+++ sql/updates/world/2021_07_04_01_world.sql
@@ -5,7 +5,7 @@ DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=1850 AND `spell_effect`=7
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (1850, 768, 0, 'Dash into catform');
 
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=5212 AND `spell_effect`=768 AND `type`=0;
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (5212, 768, 0, 'Prowl into catform');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (5215, 768, 0, 'Prowl into catform');
 
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=102280 AND `spell_effect`=768 AND `type`=0;
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (102280, 768, 0, 'Displacer Beast into catform');
