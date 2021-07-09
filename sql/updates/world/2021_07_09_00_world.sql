e427657b1c701adc8428f3af6a2e91a37efd0cbf
KylePancamo <50267605+KylePancamo@users.noreply.github.com>
2021-07-09 15:42:35 -0500

    fix (DB/Spells) Hypothermia debuff on ice block cast

---
+++ sql/updates/world/2021_07_09_00_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=45438 AND `spell_effect`=41425 AND `type`=0;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (45438, 41425, 0, 'Hypothermia');
