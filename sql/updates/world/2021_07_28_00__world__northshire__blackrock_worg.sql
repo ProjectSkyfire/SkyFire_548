82095c1e5d011747071048336d3c5bbb3925a449
darksectordds <darksectordds@gmail.com>
2021-07-28 18:52:11 +0300

    fix `blackrock worg` faction in Northshire

---
+++ sql/updates/world/2021_07_28_00__world__northshire__blackrock_worg.sql
@@ -0,0 +1,2 @@
+/* Set fraction for the worg that player can hit */
+UPDATE `creature_template` SET `name` = 'Blackrock Worg',`faction_A` = '25', `faction_H` = '25' WHERE `creature_template`.`entry` = 49871;
