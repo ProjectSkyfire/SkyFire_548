694615b5edf47e5381aae47c9434cf3f1f769ff6
Crypticaz <nick.lop6@gmail.com>
2021-08-03 07:25:46 -0500

    Fix stampeding roar and prowl on catform remove (#1054)
    * fix (Core/Spells) Stampeding roar
    
    - Now triggers bear form appropriately
    - Now removes root/snares
    
    * fix (Core/Spells) Remove prowl when leaving catform
    
    * Use GetShapeshiftForm instead of HasAura
    
    * fix (Core/Spells) few druid spells
    
    - Bear Hug now activates bearform
    - Might of Ursoc now activates bear form and calculates proper health increase amount
    
    Co-authored-by: Kyle <kyle.pancamo@gmail.com>
    (cherry picked from commit a6b62f678b9d65bbee934796c147f1b6070e60d7)

---
+++ sql/updates/world/2021_08_02_00_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=106922 AND `ScriptName`='spell_dru_might_of_ursoc';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (106922, 'spell_dru_might_of_ursoc');
