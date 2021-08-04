15642fa5eccd3a12ed532416a5b533b0ab8165e9
darksectordds <darksectordds@gmail.com>
2021-08-04 21:19:54 +0300

    fix `Gug Fatcandle` behaviour

---
+++ sql/updates/world/2021_08_04_02__world__northshire__gug_fatcandle.sql
@@ -0,0 +1,18 @@
+SET @ENTRY := 62;
+SET @FIREBOLT := 20793;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `spell1` = @FIREBOLT WHERE `entry` = @ENTRY;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = @ENTRY;
+INSERT INTO `smart_scripts`(
+`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, 
+`event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, 
+`action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`,
+ `target_type`, `target_param1`, `target_param2`, `target_param3`, 
+ `target_x`, `target_y`, `target_z`, `target_o`, `comment`) 
+ VALUES 
+(
+@ENTRY, 0, 0, 0, 0, 0, 100, 
+0, 0, 0, 3400, 4800, 
+11, @FIREBOLT, 1, 0, 0, 0, 0, 
+2, 0, 0, 0, 0, 0, 0, 0, 'Gug Fatcandle - In Combat - Cast Firebolt');
