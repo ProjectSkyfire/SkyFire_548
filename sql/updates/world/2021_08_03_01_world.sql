18c3e7caf7f396bf5b7e69ce63a37fd98eb2dfd8
Crypticaz <nick.lop6@gmail.com>
2021-08-03 21:44:33 -0500

    fix (Core/Spells) tiger's fury
    - Fix usability outside catform
    - Fix usability with berserk active

---
+++ sql/updates/world/2021_08_03_01_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=5217 AND `ScriptName`='spell_dru_tigers_fury';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (5217, 'spell_dru_tigers_fury');
