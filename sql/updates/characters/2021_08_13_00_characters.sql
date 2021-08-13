b4383e3594100b6bfd89f0668daec9d5dcf5e903
UnknownDev <10685816+iCry1337@users.noreply.github.com>
2021-08-13 10:03:08 +0200

    Update 2021_08_13_00_characters.sql

--- sql/updates/characters/2021_08_13_00_characters.sql
+++ sql/updates/characters/2021_08_13_00_characters.sql
@@ -1,5 +1,5 @@
 -- Fix SQL(p): REPLACE INTO item_instance (itemEntry, owner_guid, creatorGuid, giftCreatorGuid, count, duration, charges, flags, enchantments, randomPropertyId, reforgeID, transmogrifyId, durability, playedTime, text, guid) VALUES (35, 3, 0, 0, 1, 0, '0 0 0 0 0 ', 0, '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ', 0, 4261216296, 0, 40, 69, '#
- [ERROR]: [1264] Out of range value for column 'reforgeID' at row 1
+-- [ERROR]: [1264] Out of range value for column 'reforgeID' at row 1
 ALTER TABLE `item_instance`
 	CHANGE COLUMN `guid` `guid` INT(11) UNSIGNED NOT NULL DEFAULT '0' FIRST,
 	CHANGE COLUMN `owner_guid` `owner_guid` INT(11) UNSIGNED NOT NULL DEFAULT '0' AFTER `itemEntry`,
