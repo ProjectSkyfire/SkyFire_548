ea21db497562e880473319b9820c0fddae2ed767
MDIC <joshua.lee.betts@gmail.com>
2022-06-24 13:33:21 -0400

    update (sql): corrected sql midding collumns

--- sql/updates/characters/2022_06_23_00_anticheat.sql
+++ sql/updates/characters/2022_06_23_00_anticheat.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `daily_players_reports`;
 CREATE TABLE IF NOT EXISTS `daily_players_reports` (
   `guid` int unsigned NOT NULL DEFAULT 0,
   `creation_time` int unsigned NOT NULL DEFAULT 0,
@@ -9,11 +10,15 @@ CREATE TABLE IF NOT EXISTS `daily_players_reports` (
   `waterwalk_reports` bigint unsigned NOT NULL DEFAULT 0,
   `teleportplane_reports` bigint unsigned NOT NULL DEFAULT 0,
   `climb_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `teleport_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `ignorecontrol_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `zaxis_reports` bigint unsigned NOT NULL DEFAULT 0,
   `antiswim_reports` bigint unsigned NOT NULL DEFAULT 0,
   `gravity_reports` bigint unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+DROP TABLE IF EXISTS `players_reports_status`;
 CREATE TABLE IF NOT EXISTS `players_reports_status` (
   `guid` int unsigned NOT NULL DEFAULT 0,
   `creation_time` int unsigned NOT NULL DEFAULT 0,
@@ -25,6 +30,9 @@ CREATE TABLE IF NOT EXISTS `players_reports_status` (
   `waterwalk_reports` bigint unsigned NOT NULL DEFAULT 0,
   `teleportplane_reports` bigint unsigned NOT NULL DEFAULT 0,
   `climb_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `teleport_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `ignorecontrol_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `zaxis_reports` bigint unsigned NOT NULL DEFAULT 0,
   `antiswim_reports` bigint unsigned NOT NULL DEFAULT 0,
   `gravity_reports` bigint unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`guid`)
