dea4f62c4dbe873e64a29e726b936eddd48d9e8f
ArivanaDEV <michalik.andrej.aridev@gmail.com>
2022-06-26 19:35:04 +0200

    Scripts/Spells: Implement Selfless Healer Talent.

---
+++ sql/updates/world/2022_06_26_02_world.sql
@@ -0,0 +1,6 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (19750, 82326, 82327, 85804);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(19750, 'spell_pal_flash_of_light'),
+(82326, 'spell_pal_divine_light'),
+(82327, 'spell_pal_holy_radiance'),
+(85804, 'spell_pal_selfless_healer');
