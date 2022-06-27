f55d549e2c8fa0be1c742a709e234480600cc78c
ArivanaDEV <michalik.andrej.aridev@gmail.com>
2022-06-27 22:50:52 +0200

    Server/Spells: Drop old code for furor.

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -1765,19 +1765,14 @@ void AuraEffect::HandleAuraModShapeshift(AuraApplication const* aurApp, uint8 mo
 
             if (form == FORM_CAT || form == FORM_BEAR)
             {
-                // get furor proc chance
-                int32 FurorChance = 0;
-                if (AuraEffect const* dummy = target->GetDummyAuraEffect(SPELLFAMILY_DRUID, 238, 0))
-                    FurorChance = std::max(dummy->GetAmount(), 0);
-
                 if (form == FORM_CAT)
                 {
-                    int32 basePoints = std::min<int32>(oldPower, FurorChance);
                     target->SetPower(POWER_ENERGY, 0);
-                    target->CastCustomSpell(target, 17099, &basePoints, NULL, NULL, true, NULL, this);
                 }
-                else if (roll_chance_i(FurorChance))
+                else
+                {
                     target->CastSpell(target, 17057, true);
+                }
             }
         }
         // stop handling the effect if it was removed by linked event
