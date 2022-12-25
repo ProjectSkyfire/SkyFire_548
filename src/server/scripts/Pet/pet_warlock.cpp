d4c29abb47ad08a780c2f5d2ae081ba31cb3bd4c
AlterEgo <admin@projectskyfire.org>
2022-12-25 12:18:54 -0600

    Missed a few headers
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

--- src/server/scripts/Pet/pet_warlock.cpp
+++ src/server/scripts/Pet/pet_warlock.cpp
@@ -1,21 +1,7 @@
 /*
- * Copyright (C) 2011-2015 Project SkyFire <http://www.projectskyfire.org/>
- * Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2005-2015 MaNGOS <http://getmangos.com/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+* This file is part of Project SkyFire https://www.projectskyfire.org. 
+* See LICENSE.md file for Copyright information
+*/
 
 /*
  * Ordered alphabetically using scriptname.
