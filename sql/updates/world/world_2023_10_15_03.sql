53e27b7a87ecd435cc0b91ba54075373a49ae2e1
AriDEV <aridev@projectskyfire.org>
2023-10-16 20:44:14 +0200

    typo

--- sql/updates/world/world_2023_10_15_03.sql
+++ sql/updates/world/world_2023_10_15_03.sql
@@ -3,4 +3,4 @@ UPDATE `creature_template` SET `trainer_type`=2 WHERE `entry`=66980;
 DELETE FROM `npc_trainer` WHERE `entry`=66980;
 INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES
 (66980, -200404, 0, 0, 0, 0),
-(66980, 110415, 600000, 182, 500, 80),
+(66980, 110415, 600000, 182, 500, 80);
