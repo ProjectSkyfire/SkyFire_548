74bddc47f8367c1e9e7cc97e047ec96d8f57a49e
AriDEV <aridev@projectskyfire.org>
2023-10-18 17:33:47 +0200

    Database/Quests: Fixed a bug where Brother Paxton did not offer or offer a wrong quest for monks and warriors.

---
+++ sql/updates/world/world_2023_10_18_00.sql
@@ -0,0 +1,7 @@
+-- Quest: Fear no Evil
+-- Warrior
+UPDATE `quest_template` SET `RequiredClasses`=1 WHERE `Id`=28813;
+-- Monk
+UPDATE `quest_template` SET `RequiredClasses`=512 WHERE `Id`=29082;
+
+
