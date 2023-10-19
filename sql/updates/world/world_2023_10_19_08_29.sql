840f2fb339e018f1651c5b77304694296c5b3c09
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 11:27:11 +0330

    Fix Achievement [Challenge Master: Shado-Pan Monastery] reward

---
+++ sql/updates/world/world_2023_10_19_08_29.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8432;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8432, 371, 371, 0, 0, NULL, NULL);
