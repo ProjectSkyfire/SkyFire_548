67f475cc8f0879ffcdf9a4670d097d544d1130e5
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 11:28:17 +0330

    DB/World: Fix Achievement [Challenge Master: Mogu'shan Palace] reward

---
+++ sql/updates/world/world_2023_10_19_08_30.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8433;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8433, 367, 367, 0, 0, NULL, NULL);
