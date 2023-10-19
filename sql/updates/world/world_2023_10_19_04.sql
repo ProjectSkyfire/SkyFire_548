91cc58827f7c742731a23de321a7f7f07efb4ced
AriDEV <aridev@projectskyfire.org>
2023-10-19 14:03:26 +0200

    Database/Quests: Fixed required race for 'Encrypted Letter' and 'The Deepest Cut'

---
+++ sql/updates/world/world_2023_10_19_04.sql
@@ -0,0 +1,4 @@
+-- Fixed required race for rogue version of 'Encrypted Letter' where human race mask was missing.
+UPDATE `quest_template` SET `RequiredRaces`=2097153 WHERE  `Id`=3102;
+-- Fixed required race for rogue version of 'The Deepest Cut' where human race mask was missing.
+UPDATE `quest_template` SET `RequiredRaces`=2097153 WHERE  `Id`=26915;
