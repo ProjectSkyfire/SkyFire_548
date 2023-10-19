feeae029b8bac2bdabc4e3ca0ad48056e31e087f
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 20:08:38 +0330

    DB/World: Fix Achievement [Challenge Master: Siege of Niuzao Temple] reward (#1147)

---
+++ sql/updates/world/world_2023_10_19_08_40.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8439;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8439, 372, 372, 0, 0, NULL, NULL);
