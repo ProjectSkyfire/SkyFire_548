c9fa3252a9f76c6f4552da41ccaeb99195a43486
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 20:11:11 +0330

    DB/World: Fix Achievement [Challenge Master: Temple of the Jade Serpent] reward (#1139)

---
+++ sql/updates/world/world_2023_10_19_08_25.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8430;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8430, 374, 374, 0, 0, NULL, NULL);
