5c93a4993f36dc760d26e2b345b53fce6971a3f6
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 20:09:46 +0330

    DB/World: Fix Achievement [Challenge Master: Scarlet Monastery] reward (#1145)

---
+++ sql/updates/world/world_2023_10_19_08_36.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8437;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8437, 369, 369, 0, 0, NULL, NULL);
