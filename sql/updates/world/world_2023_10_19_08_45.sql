e2bf7c73af7103a342e81e74884cc482f72b4164
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 19:50:53 +0330

    Update world_2023_10_19_08_45.sql

--- sql/updates/world/world_2023_10_19_08_45.sql
+++ sql/updates/world/world_2023_10_19_08_45.sql
@@ -1,2 +1,2 @@
 DELETE FROM `achievement_reward` WHERE `entry`=8454;
-INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8454, 0, 0, 104208, 0, 32216, 'Reins of Galakras', NULL);
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8454, 0, 0, 104208, 32216, 'Reins of Galakras', NULL);
