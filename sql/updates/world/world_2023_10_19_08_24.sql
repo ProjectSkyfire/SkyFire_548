ee5dbe9f659789f54777fa3bb0a88d42e804c264
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 11:24:08 +0330

    DB/World: Fix Achievement [Ahead of the Curve: Garrosh Hellscream (10 player)] reward

---
+++ sql/updates/world/world_2023_10_19_08_24.sql
@@ -0,0 +1,4 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8398;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8398, 0, 0, 104246, 0, 32216, 'Reins of the Kor\'kron War Wolf', NULL);
+DELETE FROM `achievement_reward` WHERE `entry`=8399;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8399, 0, 0, 104246, 0, 32216, 'Reins of the Kor\'kron War Wolf', NULL);
