0e8fcee8b901793a9bd8165f0d42d768faf9f9a2
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 20:10:01 +0330

    DB/World: Fix Achievement [Challenge Master: Gate of the Setting Sun] reward (#1143)

---
+++ sql/updates/world/world_2023_10_19_08_32.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8434;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8434, 366, 366, 0, 0, NULL, NULL);
