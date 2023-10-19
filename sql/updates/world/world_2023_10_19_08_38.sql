5663418dd84be1c289c92e862cf4cd5b59b26b3e
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 20:09:03 +0330

    DB/World: Fix Achievement [Challenge Master: Scholomance] reward (#1146)

---
+++ sql/updates/world/world_2023_10_19_08_38.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8438;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8438, 370, 370, 0, 0, NULL, NULL);
