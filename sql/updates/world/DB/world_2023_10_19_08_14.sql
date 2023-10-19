a0b83c3b2720a1727ba3f1d6fee969712cde9c04
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 11:17:42 +0330

    DB/World: Fix Achievement [Having a Brawl (Season 1)] reward

---
+++ sql/updates/world/DB/world_2023_10_19_08_14.sql
@@ -0,0 +1,4 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8335;
+INSERT INTO `achievement_reward` (`entry`, `item`, `sender`, `subject`) VALUES ('8335', '93195', '32216', 'Brawler\'s Pass');
+DELETE FROM `achievement_reward` WHERE `entry`=8337;
+INSERT INTO `achievement_reward` (`entry`, `item`, `sender`, `subject`) VALUES ('8337', '93228', '32216', 'Brawler\'s Pass');
