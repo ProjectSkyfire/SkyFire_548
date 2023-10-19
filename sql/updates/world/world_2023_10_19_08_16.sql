3e6c51f7b2d5ab0b9063c2b228dc89baf59007d9
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 11:18:43 +0330

    Create world_2023_10_19_08_16.sql
    DB/World: Fix Achievement [Deck Your Collection (Season 1)]

---
+++ sql/updates/world/world_2023_10_19_08_16.sql
@@ -0,0 +1,4 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8340;
+INSERT INTO `achievement_reward` (`entry`, `item`, `sender`, `subject`) VALUES ('8340', '98543', '32216', 'Wraps of the Blood-Soaked Brawler');
+DELETE FROM `achievement_reward` WHERE `entry`=8343;
+INSERT INTO `achievement_reward` (`entry`, `item`, `sender`, `subject`) VALUES ('8343', '98543', '32216', 'Wraps of the Blood-Soaked Brawler');
