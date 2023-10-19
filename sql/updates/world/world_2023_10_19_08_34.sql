08dbe2b004fa416358467382ae94c7fba058100d
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 20:09:20 +0330

    DB/World: Fix Achievement [Challenge Master: Scarlet Halls] reward (#1144)

---
+++ sql/updates/world/world_2023_10_19_08_34.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8436;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8436, 368, 368, 0, 0, NULL, NULL);
