3f26e6bb5932c4365cd0f48eac5b4f15f6db890b
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-19 11:26:19 +0330

    DB/World: Fix Achievement [Challenge Master: Stormstout Brewery] reward

---
+++ sql/updates/world/world_2023_10_19_08_27.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8431;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`, `text`) VALUES (8431, 373, 373, 0, 0, NULL, NULL);
