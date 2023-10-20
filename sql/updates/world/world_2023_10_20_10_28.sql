19db1cc611fbf5dd6052c27e528de0fc0d3354dd
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-20 11:40:18 +0330

    update to new system reward

---
+++ sql/updates/world/world_2023_10_20_10_28.sql
@@ -0,0 +1,4 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8398;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8398, 0, 0, 0, -148396, 0, NULL, NULL);
+DELETE FROM `achievement_reward` WHERE `entry`=8399;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8399, 0, 0, 0, -148396, 0, NULL, NULL);
