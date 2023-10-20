cb2d07e11fe420784bfb5b1d2b7819a5d8a897aa
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-20 12:40:49 +0330

    DB/World: Fix Achievement [Having a Brawl (Season 1)] reward (#1134)
    * DB/World: Fix Achievement [Having a Brawl (Season 1)] reward
    
    * Delete sql/updates/world/DB/world_2023_10_19_08_14.sql
    
    * Create world_2023_10_20_10_52.sql

---
+++ sql/updates/world/world_2023_10_20_10_52.sql
@@ -0,0 +1,5 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8335;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8335, 0, 0, 93195, 0, 67260, 'Another Brawler\'s Pass', 'You keep pumping out those wins down here at Brawl\'gar!\r\n\r\nHere\'s another invitation.  Invite some of your friends to come join in the carnage.\r\n\r\nOr, you can just sell it to the highest bidder.  That\'s what I\'d do.\r\n\r\n- Boss Bazzelflange');
+
+DELETE FROM `achievement_reward` WHERE `entry`=8337;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8337, 0, 0, 93228, 0, 67260, 'Another Brawler\'s Pass', 'You keep pumping out those wins down here at Brawl\'gar!\r\n\r\nHere\'s another invitation.  Invite some of your friends to come join in the carnage.\r\n\r\nOr, you can just sell it to the highest bidder.  That\'s what I\'d do.\r\n\r\n- Boss Bazzelflange');
