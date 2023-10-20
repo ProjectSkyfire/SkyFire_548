eeb7b1f129313d9f61931b876fd480dcb779abf8
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-20 12:18:54 +0330

    DB/World: Fix Achievement [Deck Your Collection (Season 1)] (#1135)
    * Create world_2023_10_19_08_16.sql
    
    DB/World: Fix Achievement [Deck Your Collection (Season 1)]
    
    * Update world_2023_10_19_08_16.sql
    
    * Update world_2023_10_19_08_16.sql

---
+++ sql/updates/world/world_2023_10_19_08_16.sql
@@ -0,0 +1,5 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8340;
+INSERT INTO `achievement_reward` (`entry`, `item`, `sender`, `subject`, `text`) VALUES (8340, 98543, 67260, 'Wraps of the Blood-Soaked Brawler', 'Let\'s take a quick inventory: You\'ve defeated a shark, a robotic crocolisk, a pile of ooze, a pair of penguins, and a whole roomful of deadly brawlers from around the world.\r\n\r\nYeah. I\'d say you earned a shirt.');
+DELETE FROM `achievement_reward` WHERE `entry`=8343;
+INSERT INTO `achievement_reward` (`entry`, `item`, `sender`, `subject`, `text`) VALUES (8343, 98543, 67260, 'Wraps of the Blood-Soaked Brawler', 'Let\'s take a quick inventory: You\'ve defeated a shark, a robotic crocolisk, a pile of ooze, a pair of penguins, and a whole roomful of deadly brawlers from around the world.\r\n\r\nYeah. I\'d say you earned a shirt.');
+
