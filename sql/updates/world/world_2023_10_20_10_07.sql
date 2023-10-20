9484348d48b775d9b24578d2b8c22e173c054edc
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-20 10:34:21 +0330

    Update world_2023_10_20_10_07.sql

--- sql/updates/world/world_2023_10_20_10_07.sql
+++ sql/updates/world/world_2023_10_20_10_07.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `achievement_reward`
-	ADD COLUMN `spell` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0' AFTER `item`;
+	CHANGE COLUMN `spell` `spell` MEDIUMINT(8) NOT NULL DEFAULT '0' AFTER `item`;
