a2e7d017d0b4f35af3e1afa6546937256d807b05
AriDEV <aridev@projectskyfire.org>
2023-10-20 16:59:52 +0200

    Database/Quest: Update Offer reward text for Fear no Evil.

---
+++ sql/updates/world/2023_10_20_world_01.sql
@@ -0,0 +1,11 @@
+UPDATE `quest_template` SET `OfferRewardText`='I think you now understand the power of the Light. The Light giveth hope, $g brother:sister; and the Light taketh from the darkness! BLESSED BE THE LIGHT!' WHERE  `Id`=28809;
+
+UPDATE `quest_template` SET `OfferRewardText`='I think you now understand the power of the Light. The Light giveth hope, $g brother:sister; and the Light taketh from the darkness! BLESSED BE THE LIGHT!' WHERE  `Id`=28810;
+
+UPDATE `quest_template` SET `OfferRewardText`='I think you now understand the power of the Light. The Light giveth hope, $g brother:sister; and the Light taketh from the darkness! BLESSED BE THE LIGHT!' WHERE  `Id`=28811;
+
+UPDATE `quest_template` SET `OfferRewardText`='I think you now understand the power of the Light. The Light giveth hope, $g brother:sister; and the Light taketh from the darkness! BLESSED BE THE LIGHT!' WHERE  `Id`=28812;
+
+UPDATE `quest_template` SET `OfferRewardText`='I think you now understand the power of the Light. The Light giveth hope, $g brother:sister; and the Light taketh from the darkness! BLESSED BE THE LIGHT!' WHERE  `Id`=28813;
+
+UPDATE `quest_template` SET `OfferRewardText`='I think you now understand the power of the Light. The Light giveth hope, $g brother:sister; and the Light taketh from the darkness! BLESSED BE THE LIGHT!' WHERE  `Id`=29082;
