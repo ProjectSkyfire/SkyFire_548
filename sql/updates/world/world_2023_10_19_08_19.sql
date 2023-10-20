f7343a145babe16781e2bdbe473800c63a82142f
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-20 11:07:33 +0330

    Update text mail

--- sql/updates/world/world_2023_10_19_08_19.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `achievement_reward` WHERE `entry`=8345;
-INSERT INTO `achievement_reward` (`entry`, `item`, `sender`, `subject`) VALUES ('8345', '98618', '32216', 'Hearthsteed');
---
+++ sql/updates/world/world_2023_10_20_10_22.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8345;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8345, 0, 0, 98618, 0, 32216, 'Hearthsteed', '$n,\r\n\r\nThis fine magical construct arrived\r\nonly yesterday, and you were the first\r\nadventurer I thought of.\r\n\r\nEnjoy!\r\n\r\nMei Francis\r\nExotic Mounts\r\nDalaran');
