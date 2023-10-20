0811ee26a053777d267f87fb081f8d3b3bc8978a
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-20 10:50:18 +0330

    DB/World: Fix Achievement [Glory of the Orgrimmar Raider] reward (#1148)
    * DB/World: Fix Achievement [Glory of the Orgrimmar Raider] reward
    
    * Update world_2023_10_19_08_45.sql
    
    * set spell id after new reward system
    
    * Rename world_2023_10_19_08_45.sql to world_2023_10_20_10_10.sql

---
+++ sql/updates/world/world_2023_10_20_10_10.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8454;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8454, 0, 0, 0, -148392, 0, NULL, NULL);
