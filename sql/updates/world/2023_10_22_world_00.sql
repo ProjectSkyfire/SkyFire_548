4fd7d52ccd927c0ed44747053b7432531e48a185
AriDEV <aridev@projectskyfire.org>
2023-10-22 07:26:31 +0200

    Database/Creature: Drop old invis auras from template.

---
+++ sql/updates/world/2023_10_22_world_00.sql
@@ -0,0 +1,7 @@
+UPDATE `creature_template_addon` SET `auras`='29266' WHERE `entry`=237;
+UPDATE `creature_template_addon` SET `auras`='29266' WHERE `entry`=238;
+UPDATE `creature_template_addon` SET `auras`='29266' WHERE `entry`=582;
+UPDATE `creature_template_addon` SET `auras`='' WHERE `entry`=42308;
+UPDATE `creature_template_addon` SET `auras`='' WHERE `entry`=42309;
+UPDATE `creature_template_addon` SET `auras`='' WHERE `entry`=42311;
+
