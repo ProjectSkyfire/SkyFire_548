78f857bde5a3e05dc663d518d3cdfdc3130f53c4
AriDEV <aridev@projectskyfire.org>
2023-10-29 15:53:20 +0100

    Database: Fix database errors.
    -[ci skip]

---
+++ sql/updates/world/2023_10_29_world_02.sql
@@ -0,0 +1,17 @@
+-- Savannah Prowler
+UPDATE `creature_template` SET `skinloot`=3425 WHERE `entry`=3425;
+
+-- Echeyakee
+UPDATE `creature_template` SET `skinloot`=3475 WHERE `entry`=3475;
+
+-- Raging Agam'ar
+DELETE FROM `creature_template` WHERE `entry`=4514;
+
+-- Ravasaur
+UPDATE `creature_template` SET `skinloot`=6505 WHERE `entry`=6505;
+
+-- Sian-Rotam
+UPDATE `creature_template` SET `skinloot`=10741 WHERE `entry`=10741;
+
+-- Elder Springpaw
+UPDATE `creature_template` SET `skinloot`=15652 WHERE `entry`=15652;
