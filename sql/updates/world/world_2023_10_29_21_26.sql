fda4a4e177046639df251c11ee1e7a880c0c7543
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-31 02:21:19 +0330

    DB/World: Fix Achievement [Raiding with Leashes II: Attunement Edition] reward (#1170)
    [ci-skip]

---
+++ sql/updates/world/world_2023_10_29_21_26.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8293;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (8293, 0, 0, 97558, 0, 71965, 'Tito\'s Basket', 'Congrats!');
