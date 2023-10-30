47ddaf675a3e868a04d0c478935a7c3113279111
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-30 22:36:16 +0330

    DB/World: Fix Achievement [That's a Lot of Pet Food] reward

---
+++ sql/updates/world/world_2023_10_29_21_32.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=7501;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (7501, 0, 0, 89736, 0, 71965, 'Venus', 'Congrats!');
