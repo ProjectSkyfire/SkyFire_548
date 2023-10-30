9e26679632a5827805d08552b358520d08b0fdfb
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-31 02:23:09 +0330

    DB/World: Fix Achievement [Conqueror of Orgrimmar] reward (#1165)
    [ci-skip]

---
+++ sql/updates/world/world_2023_10_29_21_24.sql
@@ -0,0 +1,4 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8679;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`) VALUES (8679, 382, 0);
+DELETE FROM `achievement_reward` WHERE `entry`=8680;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`) VALUES (8680, 0, 383);
