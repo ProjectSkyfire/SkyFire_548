f80fd4d826517b99460d9bfcca2fbe51d32dcfc0
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-30 22:36:56 +0330

    DB/World: Fix Achievement [Going to Need More Leashes] reward

---
+++ sql/updates/world/world_2023_10_29_21_34.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=7500;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (7500, 0, 0, 85578, 0, 71965, 'Feral Vermling', 'Congrats!');
