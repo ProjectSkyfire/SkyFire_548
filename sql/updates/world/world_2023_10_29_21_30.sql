144040ce400c5019bc21fa9abb97442fbc60ac22
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-30 22:34:14 +0330

    DB/World: Fix Achievement [We're Going to Need More Saddles] reward

---
+++ sql/updates/world/world_2023_10_29_21_30.sql
@@ -0,0 +1,4 @@
+DELETE FROM `achievement_reward` WHERE `entry`=7860;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`) VALUES (7860, 0, 0, 0, -133023);
+DELETE FROM `achievement_reward` WHERE `entry`=7862;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`) VALUES (7862, 0, 0, 0, -133023);
