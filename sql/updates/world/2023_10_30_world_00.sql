a60b7e121a19e99932bef138b37dae063935a434
AriDEV <aridev@projectskyfire.org>
2023-10-30 10:50:29 +0100

    Database: Added some jewelcrafting loot.

---
+++ sql/updates/world/2023_10_30_world_00.sql
@@ -0,0 +1,25 @@
+DELETE FROM `spell_loot_template` WHERE `entry` IN
+(53854, 53857, 53859, 53860, 53861, 53865, 53869, 53870, 53871);
+INSERT INTO `spell_loot_template` (`entry`, `item`) VALUES
+(53854, 39915), -- Rigid Chalcedony
+(53857, 39917), -- Mystic Sun Crystal
+(53859, 39934), -- Sovereign Shadow Crystal
+(53860, 39935), -- Shifting Shadow Crystal
+(53861, 39942), -- Glinting Shadow Crystal
+(53865, 39945), -- Mysterious Shadow Crystal
+(53869, 39939), -- Defender's Shadow Crystal
+(53870, 39933), -- Jagged Dark Jade
+(53871, 39940); -- Guardian's Shadow Crystal
+
+DELETE FROM `spell_loot_template` WHERE `entry` IN
+(53872, 53873, 53874, 53875, 53876, 53877, 53879, 53880, 53882);
+INSERT INTO `spell_loot_template` (`entry`, `item`) VALUES
+(53872, 39947), -- Inscribed Huge Citrine
+(53873, 39948), -- Etched Shadow Crystal
+(53874, 39949), -- Champion's Huge Citrine
+(53875, 39950), -- Resplendent Huge Citrine
+(53876, 39951), -- Fierce Huge Citrine
+(53877, 39952), -- Deadly Huge Citrine
+(53879, 39954), -- Lucent Huge Citrine
+(53880, 39955), -- Deft Huge Citrine
+(53882, 39956); -- Potent Huge Citrine
