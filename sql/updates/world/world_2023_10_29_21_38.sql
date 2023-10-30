dd74f549bee830133dd37591ffafc79a570b8342
Keramat Jokar <keramatjokar8855@gmail.com>
2023-10-30 22:42:09 +0330

    DB/World: Fix Achievement [Prideful Combatant] reward

---
+++ sql/updates/world/world_2023_10_29_21_38.sql
@@ -0,0 +1,4 @@
+DELETE FROM `achievement_reward` WHERE `entry`=8641;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`) VALUES (8641, 0, 0, 103533, 10540, 'Vicious Saddle');
+DELETE FROM `achievement_reward` WHERE `entry`=8642;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `sender`, `subject`) VALUES (8642, 0, 0, 103533, 29611, 'Vicious Saddle');
