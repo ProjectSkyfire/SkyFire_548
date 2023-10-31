7c79857b2dfed61693a0e6ce0657151b35fd469e
AriDEV <aridev@projectskyfire.org>
2023-10-31 18:52:20 +0100

    Database: Added Adolescent Mushan SAI
    -[ci skip]

---
+++ sql/updates/world/2023_10_31_world_07.sql
@@ -0,0 +1,5 @@
+-- Adolescent Mushan SAI
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=56239;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=56239;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(56239, 0, 1, 0, 0, 0, 100, 0, 8000, 9000, 22000, 23000, 11, 125384, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Adolescent Mushan - In Combat - Cast Belly Flop');
