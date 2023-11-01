512bf8c6d1cfbc35c4534b4e5fb1b4a5a23f6696
AriDEV <aridev@projectskyfire.org>
2023-11-01 11:06:35 +0100

    Database: Add some quest items to gameobject loot.

---
+++ sql/updates/world/2023_11_01_world_00.sql
@@ -0,0 +1,27 @@
+-- Defias Gunpowder
+DELETE FROM `gameobject_loot_template` WHERE `entry`=2882;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`) VALUES
+(2882, 17155);
+
+-- Rattan Switch
+DELETE FROM `gameobject_loot_template` WHERE `entry`=40371;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES
+(40371, 72926, -100);
+
+-- Vault of Forbidden Treasures
+UPDATE `gameobject_template` SET `name`='Vault of Forbidden Treasures' WHERE `entry`=233029;
+
+-- Ancient Dynastic Tablet
+DELETE FROM `gameobject_loot_template` WHERE `entry`=41387;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES
+(41387, 79713, -100);
+
+-- Mutilated Remains
+DELETE FROM `gameobject_loot_template` WHERE `entry`=27780;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES
+(27780, 49945, -100);
+
+-- Stolen Watermelon
+DELETE FROM `gameobject_loot_template` WHERE `entry`=40518;
+INSERT INTO `sf_world`.`gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES
+(40518, 76298, -100);
