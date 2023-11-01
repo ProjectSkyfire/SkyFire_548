03332942b81cea0e36c0c35b5547ebfee42b86fb
AriDEV <aridev@projectskyfire.org>
2023-11-01 13:15:02 +0100

    Database: Added Jewelcrafting Loot
    -[ci skip]

---
+++ sql/updates/world/2023_11_01_world_03.sql
@@ -0,0 +1,33 @@
+DELETE FROM `spell_loot_template` WHERE `entry` IN
+(53883, 53884, 53885, 53891, 53892, 53893, 53894, 53917);
+INSERT INTO `spell_loot_template` (`entry`, `item`) VALUES
+(53883, 39957), -- Veiled Shadow Crystal
+(53884, 39958), -- Willful Huge Citrine
+(53885, 39959), -- Reckless Huge Citrine
+(53891, 39965), -- Stalwart Huge Citrine
+(53892, 39966), -- Accurate Shadow Crystal
+(53893, 39967), -- Resolute Huge Citrine
+(53894, 39968), -- Timeless Shadow Crystal
+(53917, 39975); -- Nimble Dark Jade
+
+DELETE FROM `spell_loot_template` WHERE `entry` IN
+(53918, 53919, 53920, 53921, 53922, 53923, 53924, 53925);
+INSERT INTO `spell_loot_template` (`entry`, `item`) VALUES
+(53918, 39976), -- Regal Dark Jade
+(53919, 39977), -- Steady Dark Jade
+(53920, 39978), -- Forceful Dark Jade
+(53921, 39979), -- Purified Shadow Crystal
+(53922, 39980), -- Misty Dark Jade
+(53923, 39981), -- Lightning Dark Jade
+(53924, 39982), -- Turbid Dark Jade
+(53925, 39983); -- Energized Dark Jade
+
+DELETE FROM `spell_loot_template` WHERE `entry` IN
+(53932, 53933, 53934, 53941, 53943, 54017);
+INSERT INTO `spell_loot_template` (`entry`, `item`) VALUES
+(53932, 39991), -- Radiant Dark Jade
+(53933, 39992), -- Shattered Dark Jade
+(53934, 39919), -- Solid Chalcedony
+(53941, 39927), -- Sparkling Chalcedony
+(53943, 39932), -- Stormy Chalcedony
+(54017, 39910); -- Precise Bloodstone
