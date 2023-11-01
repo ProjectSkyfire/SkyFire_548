1d5cdedb83783824e0ed9c3f223134307bcc3191
AriDEV <aridev@projectskyfire.org>
2023-11-01 16:26:32 +0100

    Database: Fix WotLK Perfect Cut Gems.

---
+++ sql/updates/world/2023_11_01_world_04.sql
@@ -0,0 +1,87 @@
+DELETE FROM `spell_loot_template` WHERE `entry` IN
+(53854, 53857, 53859, 53860, 53861, 53865, 53869, 53870, 53871, 53872, 53873, 53874, 53875, 53876, 53877, 53879, 53880, 53882);
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(53854, 39915, 90, 1, 1, 1, 1), -- Rigid Chalcedony
+(53854, 41447, 10, 1, 1, 1, 1), -- Perfect Rigid Chalcedony
+(53857, 39917, 90, 1, 1, 1, 1), -- Mystic Sun Crystal
+(53857, 41445, 10, 1, 1, 1, 1), -- Perfect Mystic Sun Crystal
+(53859, 39934, 90, 1, 1, 1, 1), -- Sovereign Shadow Crystal
+(53859, 41461, 10, 1, 1, 1, 1), -- Perfect Sovereign Shadow Crystal
+(53860, 39935, 90, 1, 1, 1, 1), -- Shifting Shadow Crystal
+(53860, 41450, 10, 1, 1, 1, 1), -- Perfect Shifting Shadow Crystal
+(53861, 39942, 90, 1, 1, 1, 1), -- Glinting Shadow Crystal
+(53861, 41462, 10, 1, 1, 1, 1), -- Perfect Glinting Shadow Crystal
+(53865, 39945, 90, 1, 1, 1, 1), -- Mysterious Shadow Crystal
+(53865, 41455, 10, 1, 1, 1, 1), -- Perfect Mysterious Shadow Crystal
+(53869, 39939, 90, 1, 1, 1, 1), -- Defender's Shadow Crystal
+(53869, 41451, 10, 1, 1, 1, 1), -- Perfect Defender's Shadow Crystal
+(53870, 39933, 90, 1, 1, 1, 1), -- Jagged Dark Jade
+(53870, 41468, 10, 1, 1, 1, 1), -- Perfect Jagged Dark Jade
+(53871, 39940, 90, 1, 1, 1, 1), -- Guardian's Shadow Crystal
+(53871, 41453, 10, 1, 1, 1, 1), -- Perfect Guardian's Shadow Crystal
+(53872, 39947, 90, 1, 1, 1, 1), -- Inscribed Huge Citrine
+(53872, 41492, 10, 1, 1, 1, 1), -- Perfect Inscribed Huge Citrine
+(53873, 39948, 90, 1, 1, 1, 1), -- Etched Shadow Crystal
+(53873, 41488, 10, 1, 1, 1, 1), -- Perfect Etched Shadow Crystal
+(53874, 39949, 90, 1, 1, 1, 1), -- Champion's Huge Citrine
+(53874, 41483, 10, 1, 1, 1, 1), -- Perfect Champion's Huge Citrine
+(53875, 39950, 90, 1, 1, 1, 1), -- Resplendent Huge Citrine
+(53875, 41499, 10, 1, 1, 1, 1), -- Perfect Resplendent Huge Citrine
+(53876, 39951, 90, 1, 1, 1, 1), -- Fierce Huge Citrine
+(53876, 41489, 10, 1, 1, 1, 1), -- Perfect Fierce Huge Citrine
+(53877, 39952, 90, 1, 1, 1, 1), -- Deadly Huge Citrine
+(53877, 41484, 10, 1, 1, 1, 1), -- Perfect Deadly Huge Citrine
+(53879, 39954, 90, 1, 1, 1, 1), -- Lucent Huge Citrine
+(53879, 41493, 10, 1, 1, 1, 1), -- Perfect Lucent Huge Citrine
+(53880, 39955, 90, 1, 1, 1, 1), -- Deft Huge Citrine
+(53880, 41485, 10, 1, 1, 1, 1), -- Perfect Deft Huge Citrine
+(53882, 39956, 90, 1, 1, 1, 1), -- Potent Huge Citrine
+(53882, 41495, 10, 1, 1, 1, 1); -- Perfect Potent Huge Citrine
+
+DELETE FROM `spell_loot_template` WHERE `entry` IN
+(53883, 53884, 53885, 53891, 53892, 53893, 53894, 53917, 53918, 53919, 53920, 53921, 53922, 53923, 53924, 53925, 53932, 53933, 53934, 53941, 53943, 54017);
+INSERT INTO `spell_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(53883, 39957, 90, 1, 1, 1, 1), -- Veiled Shadow Crystal
+(53883, 41502, 10, 1, 1, 1, 1), -- Perfect Veiled Shadow Crystal
+(53884, 39958, 90, 1, 1, 1, 1), -- Willful Huge Citrine
+(53884, 41486, 10, 1, 1, 1, 1), -- Perfect Willful Huge Citrine
+(53885, 39959, 90, 1, 1, 1, 1), -- Reckless Huge Citrine
+(53885, 41497, 10, 1, 1, 1, 1), -- Perfect Reckless Huge Citrine
+(53891, 39965, 90, 1, 1, 1, 1), -- Stalwart Huge Citrine
+(53891, 41490, 10, 1, 1, 1, 1), -- Perfect Stalwart Huge Citrine
+(53892, 39966, 90, 1, 1, 1, 1), -- Accurate Shadow Crystal
+(53892, 41482, 10, 1, 1, 1, 1), -- Perfect Accurate Shadow Crystal
+(53893, 39967, 90, 1, 1, 1, 1), -- Resolute Huge Citrine
+(53893, 41498, 10, 1, 1, 1, 1), -- Perfect Resolute Huge Citrine
+(53894, 39968, 90, 1, 1, 1, 1), -- Timeless Shadow Crystal
+(53894, 41452, 10, 1, 1, 1, 1), -- Perfect Timeless Shadow Crystal
+(53917, 39975, 90, 1, 1, 1, 1), -- Nimble Dark Jade
+(53917, 41481, 10, 1, 1, 1, 1), -- Perfect Nimble Dark Jade
+(53918, 39976, 90, 1, 1, 1, 1), -- Regal Dark Jade
+(53918, 41464, 10, 1, 1, 1, 1), -- Perfect Regal Dark Jade
+(53919, 39977, 90, 1, 1, 1, 1), -- Steady Dark Jade
+(53919, 41476, 10, 1, 1, 1, 1), -- Perfect Steady Dark Jade
+(53920, 39978, 90, 1, 1, 1, 1), -- Forceful Dark Jade
+(53920, 41466, 10, 1, 1, 1, 1), -- Perfect Forceful Dark Jade
+(53921, 39979, 90, 1, 1, 1, 1), -- Purified Shadow Crystal
+(53921, 41473, 10, 1, 1, 1, 1), -- Perfect Purified Shadow Crystal
+(53922, 39980, 90, 1, 1, 1, 1), -- Misty Dark Jade
+(53922, 41470, 10, 1, 1, 1, 1), -- Perfect Misty Dark Jade
+(53923, 39981, 90, 1, 1, 1, 1), -- Lightning Dark Jade
+(53923, 41475, 10, 1, 1, 1, 1), -- Perfect Lightning Dark Jade
+(53924, 39982, 90, 1, 1, 1, 1), -- Turbid Dark Jade
+(53924, 41480, 10, 1, 1, 1, 1), -- Perfect Turbid Dark Jade
+(53925, 39983, 90, 1, 1, 1, 1), -- Energized Dark Jade
+(53925, 41467, 10, 1, 1, 1, 1), -- Perfect Energized Dark Jade
+(53932, 39991, 90, 1, 1, 1, 1), -- Radiant Dark Jade
+(53932, 41478, 10, 1, 1, 1, 1), -- Perfect Radiant Dark Jade
+(53933, 39992, 90, 1, 1, 1, 1), -- Shattered Dark Jade
+(53933, 41474, 10, 1, 1, 1, 1), -- Perfect Shattered Dark Jade
+(53934, 39919, 90, 1, 1, 1, 1), -- Solid Chalcedony
+(53934, 41441, 10, 1, 1, 1, 1), -- Perfect Solid Chalcedony
+(53941, 39927, 90, 1, 1, 1, 1), -- Sparkling Chalcedony
+(53941, 41440, 10, 1, 1, 1, 1), -- Perfect Sparkling Chalcedony
+(53943, 39932, 90, 1, 1, 1, 1), -- Stormy Chalcedony
+(53943, 41443, 10, 1, 1, 1, 1), -- Perfect Stormy Chalcedony
+(54017, 39910, 90, 1, 1, 1, 1), -- Precise Bloodstone
+(54017, 41437, 10, 1, 1, 1, 1); -- Perfect Precise Bloodstone
