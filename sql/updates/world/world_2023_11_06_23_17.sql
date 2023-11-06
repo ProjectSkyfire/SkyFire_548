4c530f96fd7dbb6335f55d30c3ada2269feb4840
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-06 23:18:27 +0330

    DB/World: Fix other start action bar for Monk class

---
+++ sql/updates/world/world_2023_11_06_23_17.sql
@@ -0,0 +1,49 @@
+DELETE FROM `playercreateinfo_action` WHERE `race`=1 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES 
+(1, 10, 81, 59752, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=2 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) 
+VALUES (2, 10, 81, 33702, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=3 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(3, 10, 81, 20594, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=4 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(4, 10, 81, 58984, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=5 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(5, 10, 81, 20577, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=6 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(6, 10, 81, 20549, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=7 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(7, 10, 81, 20589, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=8 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(8, 10, 81, 26297, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=9 AND `class`=10 AND `button`=81;
+DELETE FROM `playercreateinfo_action` WHERE `race`=9 AND `class`=10 AND `button`=82;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(9, 10, 81, 69070, 0),
+(9, 10, 82, 69041, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=10 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(10, 10, 81, 28730, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=11 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(11, 10, 81, 59548, 0);
+
+DELETE FROM `playercreateinfo_action` WHERE `race`=24 AND `class`=10 AND `button`=81;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(24, 10, 81, 107079, 0);
