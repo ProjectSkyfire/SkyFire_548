899b8d293ef3e20cfce11cad9018640f628cc9dc
Nightprince <keramatjokar8855@gmail.com>
2023-11-06 20:33:40 +0330

    DB/World: Fix page text gameobject "Path of the Tushui"

---
+++ sql/updates/world/world_2023_11_06_20_32.sql
@@ -0,0 +1,4 @@
+UPDATE `gameobject_template` SET `unk1`='', `faction`=33 WHERE `entry`=214409;
+DELETE FROM `page_text` WHERE `entry`=4605;
+INSERT INTO `page_text` (`entry`, `text`, `next_page`, `WDBVerified`) VALUES
+(4605, 'Nodding, the General withdrew a coin from his pocket. "Let us see how the winds blow!" he said, tossing the coin into the air. "If it is heads, our defense will hold. If it is tails, the wall will be overrun."$B$BBy now, many of his men had gathered to see the outcome, and a crowd of soldiers eagerly pressed forward to watch the coin land. It bounced, spun, and came to rest. Heads! Cheers erupted.$B$BThe next day battle was fought. The mantid swarmed and the defenders prevailed. Outnumbered thirty to one, the defenders were victorious.', 0, 1);
