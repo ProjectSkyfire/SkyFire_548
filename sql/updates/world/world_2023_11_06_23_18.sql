2376df86012b2f692d853cfe5406203f0a94636e
AriDEV <aridev@projectskyfire.org>
2023-11-06 21:43:17 +0100

    Database: Fix Racials for monks.

---
+++ sql/updates/world/world_2023_11_06_23_18.sql
@@ -0,0 +1,24 @@
+DELETE FROM `playercreateinfo_action` WHERE `class`=10 AND `action` IN
+(59752, 20572, 20594, 58984, 20577, 20549, 20589, 28730, 59542, 107079);
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES 
+(1, 10, 9, 59752, 0), -- Every Man for Himself (Racial)
+(2, 10, 9, 20572, 0), -- Blood Fury (Racial) 
+(3, 10, 9, 20594, 0), -- Stoneform (Racial)
+(4, 10, 9, 58984, 0), -- Shadowmeld (Racial)
+(5, 10, 9, 20577, 0), -- Cannibalize (Racial)
+(6, 10, 9, 20549, 0), -- War Stomp (Racial)
+(7, 10, 9, 20589, 0), -- Escape Artist (Racial)
+(10, 10, 9, 28730, 0), -- Arcane Torrent (Racial)
+(11, 10, 9, 59542, 0), -- Gift of the Naaru (Racial)
+(24, 10, 9, 107079, 0), -- Quaking Palm (Racial)
+(1, 10, 81, 59752, 0), -- Every Man for Himself (Racial)
+(2, 10, 81, 20572, 0), -- Blood Fury (Racial) 
+(3, 10, 81, 20594, 0), -- Stoneform (Racial)
+(4, 10, 81, 58984, 0), -- Shadowmeld (Racial)
+(5, 10, 81, 20577, 0), -- Cannibalize (Racial)
+(6, 10, 81, 20549, 0), -- War Stomp (Racial)
+(7, 10, 81, 20589, 0), -- Escape Artist (Racial)
+(10, 10, 81, 28730, 0), -- Arcane Torrent (Racial)
+(11, 10, 81, 59542, 0), -- Gift of the Naaru (Racial)
+(24, 10, 81, 107079, 0); -- Quaking Palm (Racial)
+
