84d5d19c21c061d0ebbbbe2edb4c9723832ae7ea
AriDEV <aridev@projectskyfire.org>
2023-11-07 07:07:35 +0100

    Database: Added Gnome Racial 'Escape Artist' to action bars.

---
+++ sql/updates/world/2023_11_07_world_00.sql
@@ -0,0 +1,11 @@
+DELETE FROM `playercreateinfo_action` WHERE `action`=20589;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES 
+(7, 1, 9, 20589, 0),
+(7, 1, 81, 20589, 0),
+(7, 4, 9, 20589, 0),
+(7, 5, 9, 20589, 0),
+(7, 6, 9, 20589, 0),
+(7, 8, 9, 20589, 0),
+(7, 9, 9, 20589, 0),
+(7, 10, 9, 20589, 0),
+(7, 10, 81, 20589, 0);
