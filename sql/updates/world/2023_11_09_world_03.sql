4a5ed8d3e427396d7007156d0f06adb8d68becfc
AriDEV <aridev@projectskyfire.org>
2023-11-09 20:06:36 +0100

    Database: Add RequestItemsText and OfferRewardText for quest 'The Lesson of the Burning Scroll'
    -[ci skip]

---
+++ sql/updates/world/2023_11_09_world_03.sql
@@ -0,0 +1,5 @@
+-- The Lesson of the Burning Scroll - RequestItemsText
+UPDATE `quest_template` SET `RequestItemsText`='There will come time when you must trust your instincts and simply act.' WHERE `Id`=29408;
+
+-- The Lesson of the Burning Scroll - OfferRewardText
+UPDATE `quest_template` SET `OfferRewardText`='The fact that you were able to snatch the flame so easily is no small feat.\r\n\r\nThe Edict of Temperance is a scroll of wisdom passed down from my elders\' elders. Wisdom from a more peaceful time.\r\n\r\nEvery lesson has its time and place, and with darkness on the horizon, the time for this particular wisdom has passed.\r\n\r\nThe burning of the scroll is an acceptance of tidings to come and a promise to action. Your hand carried the flame, and i suspect that it will continue to do so in the future.' WHERE `Id`=29408;
