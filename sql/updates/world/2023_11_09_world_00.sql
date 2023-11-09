f99bc31d918ed890bdf527ef11d27c16c2a6c3a5
AriDEV <aridev@projectskyfire.org>
2023-11-09 18:26:59 +0100

    Database: Add RequestItemsText for quest 'The Lesson of the Iron Bough'
    -[ci skip]

---
+++ sql/updates/world/2023_11_09_world_00.sql
@@ -0,0 +1,2 @@
+-- The Lesson of the Iron Bough - RequestItemsText
+UPDATE `quest_template` SET `RequestItemsText`='Picking up your first weapon is also a symbolic gesture. Every $c that leaves these training grounds must be ready to take up arms in defense of their fellow $r.' WHERE  `Id` IN (30027, 30033, 30034, 30035, 30036, 30037, 30038);
