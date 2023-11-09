58dac53cf99684e94334f71ef6ca4fe885cd4756
AriDEV <aridev@projectskyfire.org>
2023-11-09 19:04:12 +0100

    Database: Add RequestItemText and OfferRewardText for 'The Lesson of the Sandy Fist'
    -[ci skip]

---
+++ sql/updates/world/2023_11_09_world_01.sql
@@ -0,0 +1,4 @@
+-- The Lesson of the Sandy Fist - RequestItemsText
+UPDATE `quest_template` SET `RequestItemsText`='The training targets are just down the hill. Come back to me once you\'re finished.' WHERE `Id`=29406;
+-- The Lesson of the Sandy Fist - OfferRewardText
+UPDATE `quest_template` SET `OfferRewardText`='Not bad, $n. You may indeed have chosen the proper path.' WHERE `Id`=29406;
