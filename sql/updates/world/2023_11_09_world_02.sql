b069f8bbcdd0f9b7a3eaf61c23ca37a517d17fe4
AriDEV <aridev@projectskyfire.org>
2023-11-09 19:31:26 +0100

    Database: Added RequestItemsText and OfferRewardText for quest 'The Lesson of Stifled Pride'
    -[ci skip]

---
+++ sql/updates/world/2023_11_09_world_02.sql
@@ -0,0 +1,6 @@
+-- The Lesson of Stifled Pride - RequestItemsText
+UPDATE`quest_template` SET `RequestItemsText`='There is a time for meditation and contemplation, and there is a time for action. Diving the balance between the two is one of the greatest wisdoms we seek.' WHERE `Id`=29524;
+
+-- The Lesson of Stifled Pride - OfferRewardText
+UPDATE `quest_template` SET `OfferRewardText`='Intriguing, my young pupil.\r\n\r\nMost of the other trainees have been here for quite some time, but you are able to match them even within this first hour.\r\n\r\nThis speaks well of you, but there are yet other lessons I would see you learn.' WHERE `Id`=29524;
+
