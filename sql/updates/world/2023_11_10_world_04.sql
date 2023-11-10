c621fa16fd1b37ba2679b52e94bdf65dc4471a8c
AriDEV <aridev@projectskyfire.org>
2023-11-10 12:56:33 +0100

    Database: Fix reward in last commit

--- sql/updates/world/2023_11_10_world_04.sql
+++ sql/updates/world/2023_11_10_world_04.sql
@@ -1,2 +1,3 @@
 -- The Lesson of the Iron Bough (Mage)
 UPDATE `quest_template` SET `RequiredSourceItemId2`=76392 WHERE `Id`=30033;
+UPDATE `quest_template` SET `RewardItemId2`=76392 WHERE `Id`=30033;
