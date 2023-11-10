4fa07020c5b705ccebd77c99484b065b5b08a45e
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-10 13:20:01 +0330

    DB/World: Fix missing pandaren warrior action bar

---
+++ sql/updates/world/world_2023_11_10_13_19.sql
@@ -0,0 +1,4 @@
+DELETE FROM `playercreateinfo_action` WHERE `race`=24 AND `class`=1;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(24, 1, 0, 6603, 0),
+(24, 1, 9, 107079, 0);
