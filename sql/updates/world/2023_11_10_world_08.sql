dbd0f5cea74832de83c3615dbbea66cce33d592e
AriDEV <aridev@projectskyfire.org>
2023-11-10 20:00:51 +0100

    Database: Add some OfferRewardText and RequestItemsText
    -[ci skip]

---
+++ sql/updates/world/2023_11_10_world_08.sql
@@ -0,0 +1,72 @@
+UPDATE `quest_template` SET `OfferRewardText`='Anotha newblood, eh?\r\n\r\nYa actually look ta be in shape though.\r\nSteady hands. Fierce eyes. I maybe able ta turn ya into a real $c.\r\n\r\nLets not waste anymore time \'den.\r\n' WHERE `Id`=24607;
+UPDATE `quest_template` SET `OfferRewardText`='Anotha newblood, eh?\r\n\r\nYa actually look ta be in shape though.\r\nSteady hands. Fierce eyes. I maybe able ta turn ya into a real $c.\r\n\r\nLets not waste anymore time \'den.\r\n' WHERE `Id`=24750;
+UPDATE `quest_template` SET `OfferRewardText`='Anotha newblood, eh?\r\n\r\nYa actually look ta be in shape though.\r\nSteady hands. Fierce eyes. I maybe able ta turn ya into a real $c.\r\n\r\nLets not waste anymore time \'den.\r\n' WHERE `Id`=24758;
+UPDATE `quest_template` SET `OfferRewardText`='Anotha newblood, eh?\r\n\r\nYa actually look ta be in shape though.\r\nSteady hands. Fierce eyes. I maybe able ta turn ya into a real $c.\r\n\r\nLets not waste anymore time \'den.\r\n' WHERE `Id`=24764;
+UPDATE `quest_template` SET `OfferRewardText`='Anotha newblood, eh?\r\n\r\nYa actually look ta be in shape though.\r\nSteady hands. Fierce eyes. I maybe able ta turn ya into a real $c.\r\n\r\nLets not waste anymore time \'den.\r\n' WHERE `Id`=24770;
+UPDATE `quest_template` SET `OfferRewardText`='Anotha newblood, eh?\r\n\r\nYa actually look ta be in shape though.\r\nSteady hands. Fierce eyes. I maybe able ta turn ya into a real $c.\r\n\r\nLets not waste anymore time \'den.\r\n' WHERE `Id`=24782;
+UPDATE `quest_template` SET `OfferRewardText`='Anotha newblood, eh?\r\n\r\nYa actually look ta be in shape though.\r\nSteady hands. Fierce eyes. I maybe able ta turn ya into a real $c.\r\n\r\nLets not waste anymore time \'den.\r\n' WHERE `Id`=26272;
+UPDATE `quest_template` SET `OfferRewardText`='Anotha newblood, eh?\r\n\r\nYa actually look ta be in shape though.\r\nSteady hands. Fierce eyes. I maybe able ta turn ya into a real $c.\r\n\r\nLets not waste anymore time \'den.\r\n' WHERE `Id`=31159;
+
+UPDATE `quest_template` SET `RequestItemsText`='Strength isn\'t da end all of defeating ya enemy, and it most certainly isn\'t tha end all of stayin\' alive. Fightin\' is about balance. Everytin has its time and place.' WHERE `Id`=24639;
+UPDATE `quest_template` SET `RequestItemsText`='Strength isn\'t da end all of defeating ya enemy, and it most certainly isn\'t tha end all of stayin\' alive. Fightin\' is about balance. Everytin has its time and place.' WHERE `Id`=24751;
+UPDATE `quest_template` SET `RequestItemsText`='Strength isn\'t da end all of defeating ya enemy, and it most certainly isn\'t tha end all of stayin\' alive. Fightin\' is about balance. Everytin has its time and place.' WHERE `Id`=24759;
+UPDATE `quest_template` SET `RequestItemsText`='Strength isn\'t da end all of defeating ya enemy, and it most certainly isn\'t tha end all of stayin\' alive. Fightin\' is about balance. Everytin has its time and place.' WHERE `Id`=24765;
+UPDATE `quest_template` SET `RequestItemsText`='Strength isn\'t da end all of defeating ya enemy, and it most certainly isn\'t tha end all of stayin\' alive. Fightin\' is about balance. Everytin has its time and place.' WHERE `Id`=24771;
+UPDATE `quest_template` SET `RequestItemsText`='Strength isn\'t da end all of defeating ya enemy, and it most certainly isn\'t tha end all of stayin\' alive. Fightin\' is about balance. Everytin has its time and place.' WHERE `Id`=24783;
+UPDATE `quest_template` SET `RequestItemsText`='Strength isn\'t da end all of defeating ya enemy, and it most certainly isn\'t tha end all of stayin\' alive. Fightin\' is about balance. Everytin has its time and place.' WHERE `Id`=26273;
+UPDATE `quest_template` SET `RequestItemsText`='Strength isn\'t da end all of defeating ya enemy, and it most certainly isn\'t tha end all of stayin\' alive. Fightin\' is about balance. Everytin has its time and place.' WHERE `Id`=24777;
+
+UPDATE `quest_template` SET `OfferRewardText`='Not bad, mon. Ya have a natural flow to ya punches - you may have a talent for dis.\r\n\r\nSome rough edges ta be sure, but we\'ll get dose ironed out here and I\'ll teach ya a few new tings as we go.' WHERE `Id`=24639;
+UPDATE `quest_template` SET `OfferRewardText`='Not bad, mon. Ya have a natural flow to ya punches - you may have a talent for dis.\r\n\r\nSome rough edges ta be sure, but we\'ll get dose ironed out here and I\'ll teach ya a few new tings as we go.' WHERE `Id`=24751;
+UPDATE `quest_template` SET `OfferRewardText`='Not bad, mon. Ya have a natural flow to ya punches - you may have a talent for dis.\r\n\r\nSome rough edges ta be sure, but we\'ll get dose ironed out here and I\'ll teach ya a few new tings as we go.' WHERE `Id`=24759;
+UPDATE `quest_template` SET `OfferRewardText`='Not bad, mon. Ya have a natural flow to ya punches - you may have a talent for dis.\r\n\r\nSome rough edges ta be sure, but we\'ll get dose ironed out here and I\'ll teach ya a few new tings as we go.' WHERE `Id`=24765;
+UPDATE `quest_template` SET `OfferRewardText`='Not bad, mon. Ya have a natural flow to ya punches - you may have a talent for dis.\r\n\r\nSome rough edges ta be sure, but we\'ll get dose ironed out here and I\'ll teach ya a few new tings as we go.' WHERE `Id`=24771;
+UPDATE `quest_template` SET `OfferRewardText`='Not bad, mon. Ya have a natural flow to ya attacks - you may have a talent for dis.$B$BSome rough edges ta be sure, but we\'ll get dose ironed out here and I\'ll teach ya a few new tings as we go.' WHERE `Id`=24783;
+UPDATE `quest_template` SET `OfferRewardText`='Not bad, mon. Ya have a natural flow to ya attacks - you may have a talent for dis.$B$BSome rough edges ta be sure, but we\'ll get dose ironed out here and I\'ll teach ya a few new tings as we go.' WHERE `Id`=26273;
+
+UPDATE `quest_template` SET `OfferRewardText`='Dese are some fine pelts, $G boy:girl;. We could make some hefty cloaks outta dese.' WHERE `Id`=24641;
+UPDATE `quest_template` SET `OfferRewardText`='Dese are some fine pelts, $G boy:girl;. We could make some hefty cloaks outta dese.' WHERE `Id`=24753;
+UPDATE `quest_template` SET `OfferRewardText`='Dese are some fine pelts, $G boy:girl;. We could make some hefty cloaks outta dese.' WHERE `Id`=24761;
+UPDATE `quest_template` SET `OfferRewardText`='Dese are some fine pelts, $G boy:girl;. We could make some hefty cloaks outta dese.' WHERE `Id`=24767;
+UPDATE `quest_template` SET `OfferRewardText`='Dese are some fine pelts, $G boy:girl;. We could make some hefty cloaks outta dese.' WHERE `Id`=24773;
+UPDATE `quest_template` SET `OfferRewardText`='Dese are some fine pelts, $G boy:girl;. We could make some hefty cloaks outta dese.' WHERE `Id`=24785;
+UPDATE `quest_template` SET `OfferRewardText`='Dese are some fine pelts, $G boy:girl;. We could make some hefty cloaks outta dese.' WHERE `Id`=26275;
+
+UPDATE `quest_template` SET `RequestItemsText`='I doubt dey too much of a challenge, mon, but they\'ll still claw up ya ankles if ya don\'t stay quick footed.' WHERE `Id`=24761;
+UPDATE `quest_template` SET `RequestItemsText`='I doubt dey too much of a challenge, mon, but they\'ll still claw up ya ankles if ya don\'t stay quick footed.' WHERE `Id`=24773;
+UPDATE `quest_template` SET `RequestItemsText`='I doubt dey too much of a challenge, mon, but they\'ll still claw up ya ankles if ya don\'t stay quick footed.' WHERE `Id`=24785;
+UPDATE `quest_template` SET `RequestItemsText`='I doubt dey too much of a challenge, mon, but they\'ll still claw up ya ankles if ya don\'t stay quick footed.' WHERE `Id`=26275;
+
+UPDATE `quest_template` SET `RequestItemsText`='All $rs gotta prove demselves before we let them move past da training grounds.' WHERE `Id`=24642;
+UPDATE `quest_template` SET `RequestItemsText`='All $rs gotta prove demselves before we let them move past da training grounds.' WHERE `Id`=24780;
+UPDATE `quest_template` SET `RequestItemsText`='All $rs gotta prove demselves before we let them move past da training grounds.' WHERE `Id`=24754;
+UPDATE `quest_template` SET `RequestItemsText`='All $rs gotta prove demselves before we let them move past da training grounds.' WHERE `Id`=24762;
+UPDATE `quest_template` SET `RequestItemsText`='All $rs gotta prove demselves before we let them move past da training grounds.' WHERE `Id`=24768;
+UPDATE `quest_template` SET `RequestItemsText`='All $rs gotta prove demselves before we let them move past da training grounds.' WHERE `Id`=24774;
+UPDATE `quest_template` SET `RequestItemsText`='All $rs gotta prove demselves before we let them move past da training grounds.' WHERE `Id`=24786;
+UPDATE `quest_template` SET `RequestItemsText`='All $rs gotta prove demselves before we let them move past da training grounds.' WHERE `Id`=26276;
+
+UPDATE `quest_template` SET `OfferRewardText`='Ya handle yerself quite well. Ya gonna be quite powerful some day.' WHERE `Id`=24642;
+UPDATE `quest_template` SET `OfferRewardText`='Ya handle yerself quite well. Ya gonna be quite powerful some day.' WHERE `Id`=24754;
+UPDATE `quest_template` SET `OfferRewardText`='Ya handle yerself quite well. Ya gonna be quite powerful some day.' WHERE `Id`=24762;
+UPDATE `quest_template` SET `OfferRewardText`='Ya handle yerself quite well. Ya gonna be quite powerful some day.' WHERE `Id`=24768;
+UPDATE `quest_template` SET `OfferRewardText`='Ya handle yerself quite well. Ya gonna be quite powerful some day.' WHERE `Id`=24774;
+UPDATE `quest_template` SET `OfferRewardText`='Ya handle yerself quite well. Ya gonna be quite powerful some day.' WHERE `Id`=24786;
+UPDATE `quest_template` SET `OfferRewardText`='Ya handle yerself quite well. Ya gonna be quite powerful some day.' WHERE `Id`=26276;
+
+UPDATE `quest_template` SET `OfferRewardText`='Ya tha new recruit everyone\'s talkin\' about, eh? Welcome ta tha Darkspear.' WHERE `Id`=24643;
+UPDATE `quest_template` SET `OfferRewardText`='Ya tha new recruit everyone\'s talkin\' about, eh? Welcome ta tha Darkspear.' WHERE `Id`=24755;
+UPDATE `quest_template` SET `OfferRewardText`='Ya tha new recruit everyone\'s talkin\' about, eh? Welcome ta tha Darkspear.' WHERE `Id`=24763;
+UPDATE `quest_template` SET `OfferRewardText`='Ya tha new recruit everyone\'s talkin\' about, eh? Welcome ta tha Darkspear.' WHERE `Id`=24769;
+UPDATE `quest_template` SET `OfferRewardText`='Ya tha new recruit everyone\'s talkin\' about, eh? Welcome ta tha Darkspear.' WHERE `Id`=24775;
+UPDATE `quest_template` SET `OfferRewardText`='Ya tha new recruit everyone\'s talkin\' about, eh? Welcome ta tha Darkspear.' WHERE `Id`=24787;
+UPDATE `quest_template` SET `OfferRewardText`='Ya tha new recruit everyone\'s talkin\' about, eh? Welcome ta tha Darkspear.' WHERE `Id`=26277;
+
+UPDATE `quest_template` SET `NextQuestIdChain`=25064 WHERE `Id`=24643;
+UPDATE `quest_template` SET `NextQuestIdChain`=25064 WHERE `Id`=24755;
+UPDATE `quest_template` SET `NextQuestIdChain`=25064 WHERE `Id`=24763;
+UPDATE `quest_template` SET `NextQuestIdChain`=25064 WHERE `Id`=24769;
+UPDATE `quest_template` SET `NextQuestIdChain`=25064 WHERE `Id`=24775;
+UPDATE `quest_template` SET `NextQuestIdChain`=25064 WHERE `Id`=24781;
+UPDATE `quest_template` SET `NextQuestIdChain`=25064 WHERE `Id`=24787;
+UPDATE `quest_template` SET `NextQuestIdChain`=25064 WHERE `Id`=26277;
