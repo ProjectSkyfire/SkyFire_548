07695046824ad49b6bf7a999f353670d18789f3d
AriDEV <aridev@projectskyfire.org>
2023-11-10 08:52:50 +0100

    Database: Add RequestItemsText and OfferRewardText for quest 'The Disciple's Challenge'
    -[ci skip]

---
+++ sql/updates/world/2023_11_10_world_00.sql
@@ -0,0 +1,5 @@
+-- The Disciple's Challenge - RequestItemsText
+UPDATE `quest_template` SET `RequestItemsText`='Our greatest glory is not in never falling, but in getting up every time we do.\r\nJaomin Ro knows this. He finds strength in every defeat.\r\n\r\nYou may yet learn that lesson.' WHERE `Id`=29409;
+
+-- The Disciple's Challenge - OfferRewardText
+UPDATE `quest_template` SET `OfferRewardText`='You truly impress, $n. It seems certain that the path of the $c is indeed the path for you.' WHERE `Id`=29409;
