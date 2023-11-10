f98a62c0aafd5bf6d5ae571fa309a5f7925d49fc
AriDEV <aridev@projectskyfire.org>
2023-11-10 12:17:01 +0100

    Database: Added RequestItemsText and OfferRewardText for monk version of quests 'The Basics: Hitting Things' and 'Proving Pit'
    -[ci skip]

---
+++ sql/updates/world/2023_11_10_world_03.sql
@@ -0,0 +1,11 @@
+-- The Basics: Hitting Things - Monk RequestItemsText
+UPDATE `quest_template` SET `RequestItemsText`='I doubt dey too much of a challenge, mon, but they\'ll still claw up ya ankles if ya don\'t stay quick footed.' WHERE  `Id`=31160;
+
+-- The Basics: Hitting Things - Monk OfferRewardText
+UPDATE `quest_template` SET `OfferRewardText`='Dese are some fine pelts, $G boy:girl;. We could make some hefty cloaks outta dese.' WHERE `Id`=31160;
+
+-- Proving Pit - Monk RequestItemsText
+UPDATE `quest_template` SET `RequestItemsText`='All $rs gotta prove demselves before we let them move past da training grounds.' WHERE  `Id`=31161;
+
+-- Proving Pit - Monk OfferRewardText
+UPDATE `quest_template` SET `OfferRewardText`='Ya handle yerself quite well. Ya gonna be quite powerful some day.' WHERE  `Id`=31161;
