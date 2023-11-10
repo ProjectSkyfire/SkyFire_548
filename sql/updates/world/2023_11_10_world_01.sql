5a9f2754d8f5ca71e782f20e34c5da5d5aa7003f
AriDEV <aridev@projectskyfire.org>
2023-11-10 11:21:21 +0100

    Database: Add RequestItemsText for monk quest version 'The Rise of the Darkspear'
    -[ci skip]

---
+++ sql/updates/world/2023_11_10_world_01.sql
@@ -0,0 +1,2 @@
+-- The Rise of the Darkspear - Monk RequestItemsText
+UPDATE `quest_template` SET `OfferRewardText`='Anotha newblood, eh?\r\n\r\nYa actually look ta be in shape though.\r\nSteady hands. Fierce eyes. I maybe able ta turn ya into a read $c.\r\n\r\nLets not waste anymore time \'den.\r\n' WHERE `Id`=31159;
