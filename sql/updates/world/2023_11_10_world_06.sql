0e5823e34848bb3164fa9f5d77267fc0bcbf2057
AriDEV <aridev@projectskyfire.org>
2023-11-10 17:02:32 +0100

    Database: Add NextQuestIdChain and OfferRewardText for monk version of quest 'More Than Expecteed'
    -[ci skip]

---
+++ sql/updates/world/2023_11_10_world_06.sql
@@ -0,0 +1,3 @@
+-- More Than Expected - Monk OfferRewardText
+UPDATE `quest_template` SET `OfferRewardText`='Ya tha new recruit everyone\'s talkin\' about, eh? Welcome ta tha Darkspear.' WHERE `Id`=31163;
+UPDATE `quest_template` SET `NextQuestIdChain`=25064 WHERE `Id`=31163;
