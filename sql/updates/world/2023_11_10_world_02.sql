6040246fb435e7952d19365f06d25944a15a0b5c
AriDEV <aridev@projectskyfire.org>
2023-11-10 11:43:36 +0100

    Database: Fix query
    - [ci skip]

--- sql/updates/world/2023_11_10_world_02.sql
+++ sql/updates/world/2023_11_10_world_02.sql
@@ -2,4 +2,4 @@
 UPDATE `quest_template` SET `RequestItemsText`='Strength isn\'t da end all of defeating ya enemy, and it most certainly isn\'t tha end all of stayin\' alive. Fightin\' is about balance. Everytin has its time and place.' WHERE `Id`=31158;
 
 -- The Basics: Hitting Things - Monk OfferRewardText
-UPDATE `sf_world`.`quest_template` SET `OfferRewardText`='Not bad, mon. Ya have a natural flow to ya punches - you may have a talent for dis.\r\n\r\nSome rough edges ta be sure, but we\'ll get dose ironed out here and I\'ll teach ya a few new tings as we go.' WHERE `Id`=31158;
+UPDATE `quest_template` SET `OfferRewardText`='Not bad, mon. Ya have a natural flow to ya punches - you may have a talent for dis.\r\n\r\nSome rough edges ta be sure, but we\'ll get dose ironed out here and I\'ll teach ya a few new tings as we go.' WHERE `Id`=31158;
