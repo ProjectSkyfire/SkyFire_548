b0524002f82976f94edef9811ab9772eeaee2e1b
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-10 13:36:37 +0330

    DB/World: Fix missing 'Quaking Palm' pandaren mage action bar

---
+++ sql/updates/world/world_2023_11_10_13_34.sql
@@ -0,0 +1,3 @@
+DELETE FROM `playercreateinfo_action` WHERE `race`=24 AND `class`=8 AND `button`=9;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(24, 8, 9, 107079, 0);
