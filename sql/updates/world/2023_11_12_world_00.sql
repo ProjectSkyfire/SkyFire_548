5c9a52da5a9587de5f0e260dd85fb945b4ff00b9
AriDEV <aridev@projectskyfire.org>
2023-11-12 10:39:50 +0100

    Database: Add OfferRewardText and RequestItemsText for quests in orc start area.
    -[ci skip]

---
+++ sql/updates/world/2023_11_12_world_00.sql
@@ -0,0 +1,41 @@
+UPDATE `quest_template` SET `OfferRewardText`='Another one of Kaltunk\'s recruits, hmm?$B$BA sorry state of affairs we find ourselves in if this is the best the Horde can produce. No matter. By the time we think you\'re ready to leave the Valley, you\'ll be a proud $C of the Horde.' WHERE `Id`=4641;
+UPDATE `quest_template` SET `OfferRewardText`='Another one of Kaltunk\'s recruits, hmm?$b$bA sorry state of affairs we find ourselves in if this is the best the Horde can produce. No matter. By the time we think you\'re ready to leave the Valley, you\'ll be a proud warrior of the Horde.' WHERE `Id`=25152;
+
+UPDATE `quest_template` SET `RequestItemsText`='I hope you haven\'t returned to try and convince me that you have completed your task, $n? No, of course not. I would think better of you.' WHERE `Id`=25126;
+
+UPDATE `quest_template` SET `OfferRewardText`='Hmmm, not bad, $n. But don\'t let it get to your head...you\'ll fight tougher than boars in your career.$b$bNonetheless, you\'ve proven yourself well, and your next trial will be against a considerably more dangerous opponent, so you\'ll need some extra protection.' WHERE  `Id`=25126;
+
+UPDATE `quest_template` SET `RequestItemsText`='Quickly, $n. Dispatch of these humans in our valley.' WHERE `Id`=25172;
+UPDATE `quest_template` SET `OfferRewardText`='Executed with quickness and discretion. A job well done.' WHERE `Id`=25172;
+
+UPDATE `quest_template` SET `RequestItemsText`='The carapace of a scorpid isn\'t so thick that the strength of a determined $C will be deterred. Strike strongly and without doubt, and the scorpids should prove easy prey.' WHERE `Id`=25127;
+UPDATE `quest_template` SET `OfferRewardText`='There is an important lesson that you must take away from fighting scorpids. The smallest or largest of opponents can still send you to your doom. In fierce combat, any number of things can prove your downfall.$b$bI have no more to teach you, $n. You have done well, and I will watch your progress with interest.' WHERE `Id`=25127;
+
+UPDATE `quest_template` SET `RequestItemsText`='To prove yourself against the Burning Blade, you must first defeat its Vile Familiars. Return to me when you have done this.' WHERE `Id`=25131;
+
+UPDATE `quest_template` SET `RequestItemsText`='No good lazy...$b$b Eh? Do you have my blackjack? Did you catch any peons sleeping on the job?!' WHERE `Id`=25134;
+
+UPDATE `quest_template` SET `OfferRewardText`='<Hana\'zua groans in pain.>' WHERE `Id`=25128;
+
+UPDATE `quest_template` SET `RequestItemsText`='Ahhh... my father allways said I would never amount to much, and here, lying beneath a tree as life seeps away, I\'m afraid it looks like he was right.$b$bAt least I wish to die knowing that my last enemy in life lays dead.' WHERE `Id`=25129;
+UPDATE `quest_template` SET `OfferRewardText`='My blow was not enough to kill him, but looking at the damage I inflicted gives me some small measure of pride. That small measure will be all i have to sustain myself if I die, and in that light the short list of my life\'s accomplishments fills me with anger.' WHERE `Id`=25129;
+
+UPDATE `quest_template` SET `OfferRewardText`='From your description of the beast, I believe you must be speaking of Sarkoth! It is no wonder that Hana\'zua was overtaken by it. Aid will be dispatched to him immediately, trouble yourself no more with Hana\'zua\'s plight.$b$bHowever, I must say, I am most impressed to hear that you brought Sarkoth to death. It is a feat to be proud of, $n. And that you would fight for a stranger\'s honor, while other tasks occupied your time, your own honor is heightened.' WHERE `Id`=25130;
+
+UPDATE `quest_template` SET `OfferRewardText`='You have done well, $n.$b$bAlthough the vile familiars were merely pets of the darker powers within the Burning Blade, your success against them foretells greater deeds ahead.' WHERE `Id`=25131;
+
+UPDATE `quest_template` SET `OfferRewardText`='Wonderful, $n! As promised, here is your cactus apple surprise plus a little something extra.' WHERE `Id`=25136;
+
+UPDATE `quest_template` SET `OfferRewardText`='Good. good. Maybe they\'ll think twice before slacking next time! Thanks for the help!' WHERE `Id`=25134;
+
+UPDATE `quest_template` SET `RequestItemsText`='Is your task complete, $n? Do you have the Burning Blade Medallion?' WHERE `Id`=25132;
+
+UPDATE `quest_template` SET `RequestItemsText`='Have you entered the Burning Blade Coven, $n? Did you find my pick?' WHERE `Id`=25135;
+
+UPDATE `quest_template` SET `OfferRewardText`='Great, you got it! Thanks a lot, $n. This is my favorite pick! Now if my peons ever finish chopping down these trees, maybe we can find a nice cave to mine!' WHERE `Id`=25135;
+
+UPDATE `quest_template` SET `OfferRewardText`='You retrieved it! Well done!$b$bYour efforts within the Burning Blade Coven are key in rooting out this cult in the Valley of Trials. But I fear they have further aims in our land.$b$bWe have not seen the end of them.' WHERE `Id`=25132;
+
+UPDATE `quest_template` SET `OfferRewardText`='Hmm... your report comes at a bad time. The Burning Blade is not seen here in Sen\'jin, but their evil has taken seed off the coast, on the Echo Isles.$b$bThe orcs are friends of the Darkspear Trolls. Honorable friends. We want to help the orcs, but... we need help too.$b' WHERE `Id`=805;
+UPDATE `quest_template` SET `OfferRewardText`='Hmm... your report comes at a bad time. The Burning Blade is not seen here in Sen\'jin, but their evil has taken seed off the coast, on the Echo Isles.$b$bThe orcs are friends of the Darkspear Trolls. Honorable friends. We want to help the orcs, but... we need help too.$b' WHERE `Id`=25133;
+
