3f1c4c2fc946410aebde398adb653240698b49db
AriDEV <aridev@projectskyfire.org>
2023-11-13 15:54:40 +0100

    Database: Fix spawn timers for some herbs and veins and dberrors.
    -[ci skip]

---
+++ sql/updates/world/2023_11_13_world_01.sql
@@ -0,0 +1,4 @@
+UPDATE `gameobject` SET `spawntimesecs`=900 WHERE `id`=209328; -- Rich Ghost Iron Deposit
+UPDATE `gameobject` SET `spawntimesecs`=900 WHERE `id`=209311; -- Ghost Iron Deposit
+UPDATE `gameobject` SET `spawntimesecs`=600 WHERE `id`=209353; -- Rain Poppy
+UPDATE `gameobject` SET `spawntimesecs`=600 WHERE `id`=209349; -- Green Tea Leaf
