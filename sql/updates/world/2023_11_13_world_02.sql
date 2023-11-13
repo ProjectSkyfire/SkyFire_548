b3dd313141b095d6f3d7dac6c990cd8099c91d65
AriDEV <aridev@projectskyfire.org>
2023-11-13 16:29:37 +0100

    Database: Disable some quests.
    -[ci skip]

---
+++ sql/updates/world/2023_11_13_world_02.sql
@@ -0,0 +1,43 @@
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9413;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9413, 'Disable Quest 9413: <UNUSED>Crystal Flake Throat Lozenge');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9414;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9414, 'Disable Quest 9414: <UNUSED>Infused Bandages');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9507;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9507, 'Disable Quest 9507: <DEPRECATED>From the Desk of Mogul Razdunk');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9613;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9613, 'Disable Quest 9613: Deprecated: Keanna\'s Will');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9652;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9652, 'Disable Quest 9652: Deprecated: Physical Enhancement');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9653;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9653, 'Disable Quest 9653: Deprecated: Physical Enhancement');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9658;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9658, 'Disable Quest 9658: Deprecated: Offensive Channeling');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9659;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9659, 'Disable Quest 9659: Deprecated: Healing Amplification');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9661;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9661, 'Disable Quest 9661: Deprecated: A Well Rounded Defense');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=9695;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 9695, 'Disable Quest 9695: The Sun King\'s Command');
+
+DELETE FROM `disables` WHERE `sourceType`=1 AND `entry`=14020;
+INSERT INTO `disables` (`sourceType`, `entry`, `comment`) VALUES
+(1, 14020, 'Disable Quest 14020: The Curse');
