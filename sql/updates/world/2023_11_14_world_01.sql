ad4b99a89d4082d888056492866b64fda5d40f15
AriDEV <aridev@projectskyfire.org>
2023-11-14 13:15:50 +0100

    Database: Add some RequestItemsText and OfferRewardText for some quests in jade forest.
    -[ci skip]

---
+++ sql/updates/world/2023_11_14_world_01.sql
@@ -0,0 +1,22 @@
+UPDATE `quest_template` SET `OfferRewardText`='That\'s a whole Horde base down there!$BWhere was our intel!$B$BWell $c, it\'s time to see what this upgraded gunship is capable of.' WHERE `Id`=29548;
+
+UPDATE `quest_template` SET `RequestItemsText`='What are you waiting for?! Scorch the ground beneath them!' WHERE `Id`=31732;
+
+UPDATE `quest_template` SET `OfferRewardText`='A proper welcome for the Horde to this new land!' WHERE `Id`=31732;
+
+UPDATE `quest_template` SET `OfferRewardText`='\'Bout time you showed up, lassie! Let\'s do some damage!' WHERE `Id`=31733;
+
+UPDATE `quest_template` SET `OfferRewardText`='Great work! If you ever decide to give up your life of adventure, the SI:7 could use more talent like you...' WHERE `Id`=30069;
+
+UPDATE `quest_template` SET `RequestItemsText`='Did "The Pickle" send you with a mission?' WHERE `Id`=31734;
+
+UPDATE `quest_template` SET `OfferRewardText`='With those wagons our of commission, we can turn our attention to the commander.' WHERE `Id`=31734;
+
+UPDATE `quest_template` SET `RequestItemsText`='We will complete our mission, but right now our priority is that warlock!' WHERE `Id`=31735;
+
+UPDATE `quest_template` SET `OfferRewardText`='We\'ll catch up with him aggain. I\'m sure of it.' WHERE `Id`=31735;
+
+UPDATE `quest_template` SET `OfferRewardText`='I am glad you took responsibility for your actions. Left unchecked, negative emotions can quickly overwhelm an individual here, just as they started to overwhelm the one you call "Rell."' WHERE `Id`=31737;
+
+UPDATE `quest_template` SET `OfferRewardText`='When the mist fell, we knew it would only be a matter of time before visitors arrived.$B$BWe could not have predicted it would be like this.' WHERE `Id`=31736;
+
