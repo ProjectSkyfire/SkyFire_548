a8114601ea918e576245115c4109740f2417213c
AriDEV <aridev@projectskyfire.org>
2023-11-14 18:39:20 +0100

    Database: Added some missing RequestItemsText for worgen and dwarf start area
    -[ci skip]

---
+++ sql/updates/world/2023_11_14_world_02.sql
@@ -0,0 +1,13 @@
+UPDATE `quest_template` SET `RequestItemsText`='Have the $r been dealt with?' WHERE `Id`=14093;
+
+UPDATE `quest_template` SET `RequestItemsText`='Have you succeeded, $n?' WHERE `Id`=14098;
+
+UPDATE `creature_template` SET `unit_flags2`=1|2048 WHERE `entry`=35912;
+
+UPDATE `quest_template` SET `RequestItemsText`='What is it then, $n?' WHERE `Id`=24930;
+
+UPDATE `quest_template` SET `RequestItemsText`='How many of \'em did ya get?' WHERE `Id`=24469;
+
+UPDATE `quest_template` SET `RequestItemsText`='Well, what\'re ye waitin\' for, $c?' WHERE `Id`=24470;
+
+UPDATE `quest_template` SET `RequestItemsText`='Thanks for yer help, $n.' WHERE `Id`=24471;
