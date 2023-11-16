212120039b1bef9fc4bbb90ba26837dc089fcd88
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-16 14:26:59 +0330

    DB/World: Fix 'Path of the Huojin' page text

---
+++ sql/updates/world/world_2023_11_16_14_24.sql
@@ -0,0 +1,3 @@
+DELETE FROM `page_text` WHERE `entry`=4610;
+INSERT INTO `page_text` (`entry`, `text`, `next_page`, `WDBVerified`) VALUES
+(4610, 'At that moment, thunder and lightning tore open the sky as if it were a sheet, and a great rain poured down. The little boat soon became swamped and flipped over. Casting off his overcoat, the Angler began swimming for the shore.$B$B"Help us!" the monks called out, as the swells began to crash around them. "We cannot swim!"The Angler answered: "It would seem to me that you have wasted your lives!"', 0, 18414);
