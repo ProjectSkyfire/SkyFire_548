5e691754245c242924f0f375cfe9be063b8ea128
AriDEV <aridev@projectskyfire.org>
2023-11-16 12:28:00 +0100

    Database: Added OfferRewardText and PrevQuestId for quest Much to Learn.
    -[ci skip]

---
+++ sql/updates/world/2023_11_16_world_00.sql
@@ -0,0 +1,21 @@
+UPDATE `quest_template` SET `PrevQuestId`=30039 WHERE `Id`=30027;
+UPDATE `quest_template` SET `PrevQuestId`=30040 WHERE `Id`=30033;
+UPDATE `quest_template` SET `PrevQuestId`=30041 WHERE `Id`=30034;
+UPDATE `quest_template` SET `PrevQuestId`=30042 WHERE `Id`=30035;
+UPDATE `quest_template` SET `PrevQuestId`=30043 WHERE `Id`=30036;
+UPDATE `quest_template` SET `PrevQuestId`=30044 WHERE `Id`=30037;
+UPDATE `quest_template` SET `PrevQuestId`=30045 WHERE `Id`=30038;
+UPDATE `quest_template` SET `PrevQuestId`=29404 WHERE `Id`=29405;
+
+-- Much to Learn (Warrior)
+UPDATE `quest_template` SET `OfferRewardText`='Today is the day when your fate will be decided. Do you have the strength and hardiness to be a $c? Or are you destined to a life working the fields?$B$BEach has its place, but i can sense that you aspire to greatness.$B$BLet us begin our training, my newest pupil.' WHERE  `Id`=30045;
+-- Much to Learn (Hunter)
+UPDATE `quest_template` SET `OfferRewardText`='Today is the day when your fate will be decided. Do you have the compassion and keen eye of a $c? Or are you destined to a life tending to the livestock?$B$BEach has its place, but I can sense that you aspire to greatness.$B$BLet us begin our training, my newest pupil.' WHERE  `Id`=30041;
+-- Much to Learn (Priest)
+UPDATE `quest_template` SET `OfferRewardText`='Today is the day when your fate will be decided. Do you have the awareness and wisdom necessary to be a $c? Or are you destined to a life scribing in the libraries?$B$BEach has its place, but I can sense that you aspire to greatness.$B$BLet us begin our training, my newest pupil.' WHERE  `Id`=30042;
+-- Much to Learn (Shaman)
+UPDATE `quest_template` SET `OfferRewardText`='Today is the day when your fate will be decided. Do you have the strength of will to be a $c? Or are you destined to a life of tending to the shrines?$B$BEach has its place, but i can sense that you aspire to greatness.$B$BLet us begin our training, my newest pupil.' WHERE  `Id`=30044;
+-- Much to Learn (Mage)
+UPDATE `quest_template` SET `OfferRewardText`='Today is the day when your fate will be decided. Do you have the intellect and force of will necessary to be a $c? Or are you destined to a life scribing in the dusty libraries?$B$BEach has its place, but i can sense that you aspire to greatness.$B$BLet us begin our training, my newest pupil.' WHERE  `Id`=30040;
+-- Much to Learn (Monk)
+UPDATE `quest_template` SET `OfferRewardText`='Today is the day when your fate will be decided. Do you have the strength and cunning to be a $c? Or are you destined to a life working in the fields?$B$BEach has its place, but I can sense that you aspire to greatness.$B$BLet us begin our training, my newest pupil.' WHERE  `Id`=30039;
