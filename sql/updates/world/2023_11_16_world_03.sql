8b8575198febb0c287e22739e9719de0e1a37428
AriDEV <aridev@projectskyfire.org>
2023-11-16 22:08:38 +0100

    Database: Fixed phase after quest reward.

--- sql/updates/world/2023_11_16_world_03.sql
+++ sql/updates/world/2023_11_16_world_03.sql
@@ -21,5 +21,5 @@ INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spe
 
 DELETE FROM `spell_area` WHERE `spell`=102395;
 INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES
-(102395, 5846, 0, 29419, 0, 0, 2, 1, 64, 2),
-(102395, 5825, 0, 29419, 0, 0, 2, 1, 64, 2);
+(102395, 5846, 0, 29419, 0, 0, 2, 1, 64, 66),
+(102395, 5825, 0, 29419, 0, 0, 2, 1, 64, 66);
