7635e89e0cf9718f1ba963fcb383aff0d2ff37fb
AriDEV <aridev@projectskyfire.org>
2023-11-17 20:44:55 +0100

    Database: Added Aysa's Meditation Bar if quest incomplete.
    -[ci skip]

---
+++ sql/updates/world/2023_11_17_world_04.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_area` WHERE `spell`=116421;
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES
+(116421, 5848, 29414, 0, 0, 0, 2, 1, 8, 11);
