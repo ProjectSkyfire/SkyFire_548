5b7f73a51764591c32a1868a6747b3654f1d6d10
AriDEV <aridev@projectskyfire.org>
2023-11-18 20:02:25 +0100

    Database: Added OfferRewardText for quest 'Ji of the Huojin'

---
+++ sql/updates/world/2023_11_18_world_04.sql
@@ -0,0 +1,1 @@
+UPDATE `quest_template` SET `OfferRewardText`='Hello! You look poisoned and confident... I like that!$B$BI\'ve heard some rather impressive whispers about you from the training grounds. If you\'re half as capable as they say, I think you and I are going to be good friends!' WHERE `Id`=29522;
