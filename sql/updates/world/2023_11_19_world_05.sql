66ba101e9c11f9dcd341c7de4f56705d35333cd4
AriDEV <aridev@projectskyfire.org>
2023-11-19 20:24:33 +0100

    Database: Fix gameobject for quest 'The Challenger's Fires'
    -[ci skip]

---
+++ sql/updates/world/2023_11_19_world_05.sql
@@ -0,0 +1,10 @@
+UPDATE `gameobject_template` SET `name`='Brazier of the Flickering Flame' WHERE `entry`=209369;
+UPDATE `gameobject_template` SET `name`='Brazier of the Violet Flame' WHERE `entry`=209803;
+UPDATE `gameobject_template` SET `name`='Brazier of the Blue Flame' WHERE `entry`=209802;
+UPDATE `gameobject_template` SET `name`='Brazier of the Red Flame' WHERE `entry`=209801;
+
+UPDATE `gameobject_template` SET `name`='Brazier of the Red Flame' WHERE `entry`=209374;
+UPDATE `gameobject_template` SET `name`='Brazier of the Violet Flame' WHERE `entry`=209372;
+UPDATE `gameobject_template` SET `name`='Brazier of the Blue Flame' WHERE `entry`=209373;
+
+UPDATE `gameobject_template` SET `type`=22, `data0`=105151, `data5`=13004 WHERE `entry`=209369;
