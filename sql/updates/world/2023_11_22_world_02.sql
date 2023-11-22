16623783e019adfe78cff1fdd341ca4b8d974e34
AriDEV <aridev@projectskyfire.org>
2023-11-22 13:52:52 +0100

    Database: Add gossip flag to aysa and lamplighter dang

---
+++ sql/updates/world/2023_11_22_world_02.sql
@@ -0,0 +1,5 @@
+UPDATE `creature_template` SET `npcflag`=1 WHERE `entry`=64929;
+
+UPDATE `quest_template` SET `PrevQuestId`=29419 WHERE `Id`=29414;
+
+UPDATE `creature_template` SET `npcflag`=1|2 WHERE `entry`=64929;
