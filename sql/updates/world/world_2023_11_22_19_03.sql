b2c0d2f0cc60dd46599470c66e844035eb8eef8c
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-22 19:04:15 +0330

    DB/World: Fix achievement reward "Rock Lover"
    https://www.wowhead.com/achievement=5449/rock-lover#comments

---
+++ sql/updates/world/world_2023_11_22_19_03.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5449;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(5449, 0, 0, 60869, 0, 34337, 'Recovered Item', 'We recovered a lost item in the Twisting Nether and noted that it was yours.$b$bPlease find said object enclosed.');
