d71a895d621b19f751cfa0bd2a58cec03fee5dc7
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-22 19:00:33 +0330

    DB/World: Fix achievement reward "Legacy of Leyara"

---
+++ sql/updates/world/world_2023_11_22_18_59.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5859;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(5859, 0, 0, 69854, 0, 52825, 'Left Behind', 'I found this locket in the Igneous Depths, near the lava\'s edge. It is not mine, and I know that it does not belong to either of the Arch Druids. Perhaps it belonged to one of the fire druids.$b$bNo matter who its owner, I have no use for it. Please take it as a small token of our appreciation for all you\'ve done for us.');
