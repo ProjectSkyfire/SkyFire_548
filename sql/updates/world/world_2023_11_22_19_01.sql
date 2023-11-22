a3a05958789370ca50fbc01be4ff7011757595dc
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-22 19:02:12 +0330

    DB/World: Fix achievement reward "The Molten Front Offensive"

---
+++ sql/updates/world/world_2023_11_22_19_01.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5866;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(5866, 0, 0, 69213, 0, 52135, 'A Gift From the Arch Druid', 'Never again will Hyjal shudder beneath the endless forces of flame. Never again shall the mortal races of the world tremble before the minions of Ragnaros.$b$bNow, thanks to your efforts, they will tremble before us.$b$bYour heroism on the Molten Front has been instrumental in the fight against the Firelord. You have gathered allies from all over Kalimdor and turned them into an army. You have fought the minions of Ragnaros... And prevailed. And for that, you have our deepest thanks.');
