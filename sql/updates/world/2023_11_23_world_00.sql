bb5facad0023efef4fbfa0f6b1de3f895966dcdc
AriDEV <aridev@projectskyfire.org>
2023-11-23 14:19:57 +0100

    Database: Added script name
    -[ci skip]

--- sql/updates/world/2023_11_23_world_00.sql
+++ sql/updates/world/2023_11_23_world_00.sql
@@ -2,3 +2,7 @@ DELETE FROM `creature_text` WHERE `entry` IN (54567, 54943);
 INSERT INTO `creature_text` (`entry`, `text`, `type`, `probability`) VALUES
 (54567, 'A challenge in compassion and humility. By helping Merchant Lorvo, it would seem you\'ve passed, $c. Master Shang would be pleased.', 12, 100),
 (54943, 'Shhhh! Come quietly. She\'s practicing.', 12, 100);
+
+DELETE FROM `areatrigger_scripts` WHERE `entry`=7748;
+INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES
+(7748, 'at_fus_pond');
