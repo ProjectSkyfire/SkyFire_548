b0caef1bf5dfb40f01f5806918fd11a7d618ee5b
AriDEV <aridev@projectskyfire.org>
2023-11-24 13:07:43 +0100

    Database: Fixed quest chains.

---
+++ sql/updates/world/2023_11_24_world_04.sql
@@ -0,0 +1,3 @@
+UPDATE `quest_template` SET `PrevQuestId`=29410 WHERE `Id`=29424;
+UPDATE `quest_template` SET `PrevQuestId`=29410 WHERE `Id`=29419;
+UPDATE `quest_template` SET `PrevQuestId`=29414 WHERE `Id`=29522;
