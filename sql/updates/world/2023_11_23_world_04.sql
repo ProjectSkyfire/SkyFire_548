ddb513fc38b87c7a5b381173c5c76cf02175fbb9
AriDEV <aridev@projectskyfire.org>
2023-11-24 08:27:25 +0100

    Database: Fixed import errors.

--- sql/updates/world/2023_11_23_world_04.sql
+++ sql/updates/world/2023_11_23_world_04.sql
@@ -2,6 +2,6 @@ DELETE FROM `areatrigger_scripts` WHERE `entry`=7747;
 INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES
 (7747, 'at_dawning_valley2');
 
-DELETE FROM `creature_text` WHERE `entry`=60244
+DELETE FROM `creature_text` WHERE `entry`=60244;
 INSERT INTO `creature_text` (`entry`, `text`, `type`, `probability`) VALUES
 (60244, 'You\'re departing so soon?  I am envious of your prowess, $c. Good luck to you.', 12, 100);
