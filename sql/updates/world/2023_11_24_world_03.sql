f1ad7a52e683a6db2146c3c9ea16f4fed280520b
AriDEV <aridev@projectskyfire.org>
2023-11-24 12:40:20 +0100

    Database: Add some OfferRewardText and RequestItemsText.
    -[ci skip]

---
+++ sql/updates/world/2023_11_24_world_03.sql
@@ -0,0 +1,7 @@
+UPDATE `quest_template` SET `OfferRewardText`='You came for Aysa? You... you really shouldn\'t interrupt her until she finishes her exercises. She doesn\'t speak to anyone until her routine is done.$B$BIn the meantime, could you maybe help me? I had some bad luck with forest sprites.' WHERE `Id`=29410;
+UPDATE `quest_template` SET `RequestItemsText`='I should have been more careful.' WHERE `Id`=29419;
+UPDATE `quest_template` SET `RequestItemsText`='You needn\'t worry about hurting the sprites. They\'re really just living plants when it comes down to it - they\'ll resprout later.' WHERE `Id`=29424;
+UPDATE `quest_template` SET `OfferRewardText`='You are too kind, $c.' WHERE `Id`=29419;
+UPDATE `quest_template` SET `OfferRewardText`='Thank you! You\'re an honorable $c. They\'ve taught you well.' WHERE `Id`=29424;
+
+
