59b66bd6b96bf0bb2390ee0030861137ed2487f1
AriDEV <aridev@projectskyfire.org>
2023-11-26 13:16:31 +0100

    Database: fixed some quest chains in dailo farm
    -[ci skip]

---
+++ sql/updates/world/2023_11_26_world_03.sql
@@ -0,0 +1,3 @@
+UPDATE `quest_template` SET `PrevQuestId`=29770 WHERE `Id`=29768;
+UPDATE `quest_template` SET `PrevQuestId`=29768 WHERE `Id`=29772;
+UPDATE `quest_template` SET `PrevQuestId`=29772 WHERE `Id`=29774;
