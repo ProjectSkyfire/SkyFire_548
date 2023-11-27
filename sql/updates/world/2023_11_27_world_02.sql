a13c16a099eb041284be155567120abfa167204a
AriDEV <aridev@projectskyfire.org>
2023-11-27 16:27:07 +0100

    Database: Added Pandaren Warrior Heroic Strike to action bars.
    -[ci skip]

---
+++ sql/updates/world/2023_11_27_world_02.sql
@@ -0,0 +1,6 @@
+DELETE FROM `playercreateinfo_action` WHERE `race`=24 AND `class`=1 AND `button`=1;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(24, 1, 1, 78, 0);
+DELETE FROM `playercreateinfo_action` WHERE `race`=24 AND `class`=1 AND `button`=73;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES
+(24, 1, 73, 78, 0);
