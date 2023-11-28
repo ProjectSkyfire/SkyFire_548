976008baddf77dabc2f31f1fce3db9eeacc6048d
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:20:22 +0330

    DB/World: Fix achievement reward "Heroic: Nefarian"

---
+++ sql/updates/world/2023_11_28_11_48.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5116;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(5116, 229, 229, 0, 0, 0, NULL, NULL);
