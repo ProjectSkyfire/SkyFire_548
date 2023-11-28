fd8e0a541cd205847935891fa395435cb27ede99
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:22:51 +0330

    DB/World: Fix achievement reward "Heroic: Al'Akir"

---
+++ sql/updates/world/2023_11_28_11_52.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5123;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (5123, 226, 226, 0, 0, 0, NULL, NULL);
