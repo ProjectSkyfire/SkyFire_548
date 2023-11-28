4608604649f9b5622914466bc883e2bb77a06982
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:18:41 +0330

    DB/World: Fix achievement reward "Glory of the Cataclysm Raider"

---
+++ sql/updates/world/2023_11_28_11_46.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=4853;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+  (4853, 0, 0, 62901, 0, 34337, 'Recovered item', 'We recovered a lost item in the twisting nether and noted that it was yours. Please find said object enclosed.');
