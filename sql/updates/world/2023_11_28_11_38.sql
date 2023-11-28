d23387efe2e639a2cfcde7645cdb649710fef8b2
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:12:23 +0330

    DB/World: Fix achievement reward "Gladiator"

---
+++ sql/updates/world/2023_11_28_11_38.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=2091;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(2091, 42, 42, 0, 0, 0, NULL, NULL);
