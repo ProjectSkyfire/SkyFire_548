b106864b1efd8ee055c3496bfa20f71a8278230a
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:10:23 +0330

    DB/World: Fix achievement reward "Veteran of the Molten Front"

---
+++ sql/updates/world/2023_11_27_11_28.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5879;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(5879, 276, 276, 0, 0, 0, NULL, NULL);
