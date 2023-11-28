8fc9be54c97743cdf5c6e93ff07b8917e7a4ee8a
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:21:30 +0330

    DB/World: Fix achievement reward "Heroic: Sinestra"

---
+++ sql/updates/world/2023_11_28_11_50.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5121;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(5121, 228, 228, 0, 0, 0, NULL, NULL);
