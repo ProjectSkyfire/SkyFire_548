b25daa130faf0dab01206e61ef0b5314ee78ca50
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:25:16 +0330

    DB/World: Fix achievement reward "Heroic: Ragnaros"

---
+++ sql/updates/world/2023_11_28_11_54.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5803;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (5803, 278, 278, 0, 0, 0, NULL, NULL);
