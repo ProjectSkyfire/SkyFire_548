936c11916ff65ad76689eee0b0154ba83da8026f
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:17:41 +0330

    DB/World: Fix achievement reward "Glory of the Cataclysm Hero"

---
+++ sql/updates/world/2023_11_28_11_42.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=4845;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+  (4845, 0, 0, 62900, 0, 34337, 'Recovered item', 'We recovered a lost item in the twisting nether and noted that it was yours. Please find said object enclosed.');
