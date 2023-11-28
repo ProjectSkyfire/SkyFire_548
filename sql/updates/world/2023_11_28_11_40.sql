f5b51d534f05f1a3a15c645d4204fa2795616caa
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:14:17 +0330

    DB/World: Fix achievement reward "Defender of a Shattered World"

---
+++ sql/updates/world/2023_11_28_11_40.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5506;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(5506, 227, 227, 0, 0, 0, NULL, NULL);
