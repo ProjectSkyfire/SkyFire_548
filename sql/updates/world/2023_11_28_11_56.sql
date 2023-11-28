db4b1e57b2f7fc2997c4989bcd02b16008056bae
Keramat Jokar <keramatjokar8855@gmail.com>
2023-11-28 11:26:09 +0330

    DB/World: Fix achievement reward "Avengers of Hyjal"

---
+++ sql/updates/world/2023_11_28_11_56.sql
@@ -0,0 +1,2 @@
+DELETE FROM `achievement_reward` WHERE `entry`=5827;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES (5827, 267, 267, 0, 0, 0, NULL, NULL);
