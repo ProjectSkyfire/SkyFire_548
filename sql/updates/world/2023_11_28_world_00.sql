138750d6dd10d0e8e68052c54f92dde3423f6437
AlterEgo <admin@projectskyfire.org>
2023-12-03 12:06:18 -0600

    Fix syntax error in sql update.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

--- sql/updates/world/2023_11_28_world_00.sql
+++ sql/updates/world/2023_11_28_world_00.sql
@@ -5,7 +5,7 @@ INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `e
 (225865, 0, 0, 0, 0, 0, '60921'),
 (225866, 0, 0, 0, 0, 0, '60921');
 
-DELETE FROM `spell_area` WHERE `spell`=60922 AND `area=`5829;
+DELETE FROM `spell_area` WHERE `spell`=60922 AND `area`=5829;
 INSERT INTO `spell_area` (`spell`, `area`, `quest_end`, `autocast`, `quest_end_status`) VALUES
 (60922, 5829, 29786, 1, 66);
 
