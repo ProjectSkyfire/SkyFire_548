c3b17503f48030edaa718d190365446dc03ecb79
Keramat Jokar <keramatjokar8855@gmail.com>
2023-12-08 11:11:19 +0330

    DB/World: Fix achievement reward "Challenge Conqueror: Silver"

---
+++ sql/updates/world/world_2023_10_08_11_11.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=6375;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(6375, 0, 0, 90045, 0, 66973, 'In Recognition of Your Valiant Deeds', 'Congratulations,  Conquering the mighty challenges put before you is a laudable achievement, and in recognition of this mighty deed we entrust to you the egg of an Ancestral Phoenix. Visit me in the Vale of Eternal Blossoms and choose your boon companion.');
