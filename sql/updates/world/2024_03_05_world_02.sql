375c72c1da3091b2e492032abf8d5b1909dfaa3e
AriDEV3 <aridev@projectskyfire.org>
2024-03-05 17:37:27 +0100

    Database/Quests: Fix Quest Chains offered by Adana Thunderhorn in Red Cloud Mesa.

---
+++ sql/updates/world/2024_03_05_world_02.sql
@@ -0,0 +1,4 @@
+UPDATE `quest_template` SET `PrevQuestId`=14458 WHERE `Id`=14455;
+UPDATE `quest_template` SET `PrevQuestId`=14458 WHERE `Id`=14456;
+UPDATE `quest_template` SET `PrevQuestId`=14456 WHERE `Id`=14459;
+UPDATE `quest_template` SET `PrevQuestId`=14456 WHERE `Id`=14461;
