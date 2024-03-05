75904032b5767da8411eacec428750113ac56c40
AriDEV3 <aridev@projectskyfire.org>
2024-03-05 16:28:53 +0100

    Database/Quests: Add RequestItemsText for Quests in Camp Narache.

---
+++ sql/updates/world/2024_03_05_world_00.sql
@@ -0,0 +1,6 @@
+UPDATE `quest_template` SET `RequestItemsText`='Get in there, $n! We won\'t let any past. I want you in the midst of them, scattering them left and right!' WHERE `Id`=14452;
+UPDATE `quest_template` SET `RequestItemsText`='Not enough have returned, $n. Return and free more of our peopple. Be brave!' WHERE `Id`=24852;
+UPDATE `quest_template` SET `RequestItemsText`='The roots still thrive. You must sop as many of the thorncallers as you can.' WHERE `Id`=14455;
+UPDATE `quest_template` SET `RequestItemsText`='I can still see many of them from here, $n. More should fall.' WHERE `Id`=14459;
+UPDATE `quest_template` SET `RequestItemsText`='Have all the thoughs been burned?' WHERE `Id`=14461;
+UPDATE `quest_template` SET `RequestItemsText`='Do not be shy, $n. I am honored to have you lay the final offering.' WHERE `Id`=24861;
