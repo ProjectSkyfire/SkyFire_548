d9ccced2f481431eb5a703e39a855abd87b748dc
AriDEV3 <aridev@projectskyfire.org>
2024-03-10 12:01:06 +0100

    Database/Achievement: Fixed reward of 'Brewmaster' Title for alliance players.

---
+++ sql/updates/world/2024_03_10_world_08.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_reward` WHERE `entry`=1683;
+INSERT INTO `achievement_reward` (`entry`, `title_A`, `title_H`, `item`, `spell`, `sender`, `subject`, `text`) VALUES
+(1683, 133, 133, 0, 0, 0, '', '');
