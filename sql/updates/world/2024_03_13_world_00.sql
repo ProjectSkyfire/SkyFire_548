b6f3091c50c77ca8e1b4a7e3aec2fbe09231768e
AriDEV3 <aridev@projectskyfire.org>
2024-03-14 09:51:11 +0100

    Database/Gameobject: Change flags to 4. Fixes burning of scroll gameobject.

--- sql/updates/world/2024_03_13_world_00.sql
+++ sql/updates/world/2024_03_13_world_00.sql
@@ -6,6 +6,8 @@ DELETE FROM `smart_scripts` WHERE  `entryorguid`=53566 AND `id`=9;
 
 UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=-100 WHERE `entry`=54631 AND `item`=72112;
 
-UPDATE `gameobject_template` SET `flags`=0 WHERE `entry`=209326;
+UPDATE `gameobject_template` SET `flags`=4 WHERE `entry`=209326;
 
 UPDATE `gameobject_template` SET `flags`=4 WHERE `entry`=209973;
+
+UPDATE `gameobject_template` SET `flags`=4 WHERE `entry`=210986;
