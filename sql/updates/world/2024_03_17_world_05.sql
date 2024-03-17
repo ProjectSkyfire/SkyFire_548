4ebec3eebfc190ac05ee227d5f03e07aa9fe6c6d
AriDEV <aridev@projectskyfire.org>
2024-03-17 15:06:03 +0100

    Database/Gameobject: Fix flag for 'Servicable Arrow'

--- sql/updates/world/2024_03_17_world_05.sql
+++ sql/updates/world/2024_03_17_world_05.sql
@@ -4,5 +4,6 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseId`, `phaseGroup
 (100046, 8582, 1, 1, 0, 0, 0, 0, 1229.06, -2231.31, 91.7917, 1.58406, 120, 0, 0, 0, 0, 0, 0, 0, 0);
 -- Fix equipment of Kadrak
 DELETE FROM `creature_equip_template` WHERE `entry`=8582;
-INSERT INTO `creature_equip_template` (`entry`, `id`, `itemEntry1`, `itemEntry2`, `itemEntry3`) VALUES (8582, 1, 34638, 0, 0);
+INSERT INTO `creature_equip_template` (`entry`, `id`, `itemEntry1`, `itemEntry2`, `itemEntry3`) VALUES
+(8582, 1, 34638, 0, 0);
 
---
+++ sql/updates/world/2024_03_17_world_06.sql
@@ -0,0 +1,2 @@
+UPDATE `gameobject_template` SET `flags`=4 WHERE `entry`=194263;
+
