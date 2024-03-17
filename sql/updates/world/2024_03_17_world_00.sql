55631db5aa1c4a12bcd97e18c008537a7e781ac9
AriDEV <aridev@projectskyfire.org>
2024-03-17 11:25:32 +0100

    Database/SAI: Scripted Wayward Plainstriders for quest 'Unbidden Visitors'

---
+++ sql/updates/world/2024_03_17_world_00.sql
@@ -0,0 +1,77 @@
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-143353;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-143353, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-143353, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-143353, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, -94.2799, -3748.97, 16.8539, 5.6, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-143358;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-143358, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-143358, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-143358, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 38.46, -3770.507, 17.882, 5.68, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-143365;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-143365, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-143365, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-143365, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 38.46, -3770.507, 17.882, 5.68, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-143364;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-143364, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-143364, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-143364, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 38.46, -3770.507, 17.882, 5.68, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-143385;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-143385, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-143385, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-143385, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 38.46, -3770.507, 17.882, 5.68, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-141389;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-141389, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-141389, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-141389, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 220.85, -3758.12345, 16.33, 4.82, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-143391;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-143391, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-143391, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-143391, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 278.54, -3760.343, 16.30, 4.61, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-143389;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-143389, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-143389, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-143389, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 409.677, -3824.360, 21.06, 1.62, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-143407;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-143407, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-143407, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-143407, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 574.526, -3774.149, 16.82, 4.68, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-141362;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-141362, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-141362, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-141362, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 574.526, -3774.149, 16.82, 4.68, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-141396;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-141396, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-141396, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-141396, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 663.7949, -3811.1398, 20.27, 1.62, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-141408;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-141408, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-141408, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-141408, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 772.607, -3785.005, 16.62, 4.52, 'Wayward Plainstrider - Move to pos');
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=-141409;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(-141409, 0, 1, 2, 61, 0, 100, 1, 0, 0, 0, 0, 0, 33, 39336, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward Plainstrider - killcredit'),
+(-141409, 0, 2, 0, 61, 0, 100, 1, 0, 0, 0, 0, 0, 41, 2000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Wayward plainstrider despawn'),
+(-141409, 0, 0, 1, 2, 0, 100, 1, 0, 30, 0, 0, 0, 69, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 772.607, -3785.005, 16.62, 4.52, 'Wayward Plainstrider - Move to pos');
