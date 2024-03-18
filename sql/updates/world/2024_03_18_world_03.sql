9c7cd13516c3c110eedaaae13a4ed63a3721dd0c
Nightprince <keramatjokar8855@gmail.com>
2024-03-18 13:52:00 +0330

    DB/World: Add 'Nodd Codejack' talk to Player

---
+++ sql/updates/world/2024_03_18_world_03.sql
@@ -0,0 +1,2 @@
+DELETE FROM `creature_text` WHERE `entry`=54615;
+INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`) VALUES (54615, 0, 0, 'Over here, $n! Your gyrocopter\'s ready to go!', 12, 0, 100, 3, 0, 0, 'Nodd Codejack to Player');
