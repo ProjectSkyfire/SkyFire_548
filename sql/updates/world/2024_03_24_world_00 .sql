e9347caa2f1984b8c908a7d3f18b0e8266da4414
AriDEV3 <aridev@projectskyfire.org>
2024-03-24 18:41:45 +0100

    fix derp in last commit.

--- sql/updates/world/2024_03_24_world_00 .sql
+++ sql/updates/world/2024_03_24_world_00 .sql
@@ -1,5 +1,5 @@
 DELETE FROM `gameobject_template` WHERE `entry` IN (212091, 212092, 212093, 212094);
-INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name` `size`, `data1`, `data3`, `WDBVerified`) VALUES
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `size`, `data1`, `data3`, `WDBVerified`) VALUES
 (212091, 24, 11533, 'Orb of Power', 4, 121164, 121164, 18414),
 (212092, 24, 11533, 'Orb of Power', 4, 121175, 121175, 18414),
 (212093, 24, 11533, 'Orb of Power', 4, 121176, 121176, 18414),
