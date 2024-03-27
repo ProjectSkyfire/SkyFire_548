a98b00f5afc74d196a2355e9076e3904ae7ee315
AriDEV3 <aridev@projectskyfire.org>
2024-03-27 12:57:15 +0100

    Database/Vendor: Add vendor data to Torenda and Gora'tin on Echo Isles.

---
+++ sql/updates/world/2024_03_27_world_01.sql
@@ -0,0 +1,20 @@
+DELETE FROM `npc_vendor` WHERE `entry`=39032;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES
+(39032, 0, 4540, 0, 0, 0, 1),
+(39032, 1, 159, 0, 0, 0, 1),
+(39032, 2, 4496, 0, 0, 0, 1);
+
+DELETE FROM `npc_vendor` WHERE `entry`=39031;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`, `type`) VALUES 
+(39031, 0, 117, 0, 0, 0, 1),
+(39031, 1, 2287, 0, 0, 0, 1),
+(39031, 2, 3770, 0, 0, 0, 1),
+(39031, 3, 3771, 0, 0, 0, 1),
+(39031, 4, 4599, 0, 0, 0, 1),
+(39031, 5, 8952, 0, 0, 0, 1),
+(39031, 6, 159, 0, 0, 0, 1),
+(39031, 7, 1179, 0, 0, 0, 1),
+(39031, 8, 1205, 0, 0, 0, 1),
+(39031, 9, 1708, 0, 0, 0, 1),
+(39031, 10, 1645, 0, 0, 0, 1),
+(39031, 11, 8766, 0, 0, 0, 1);
