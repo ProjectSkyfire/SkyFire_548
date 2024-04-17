c6b6c7e58e4f65e5628f2fe2a24260509d6cd842
AriDEV <aridev@projectskyfire.org>
2024-04-17 17:35:49 +0200

    Server/Authserver: Fix Memory Leaks. Valgrind: 5,005 (224 direct, 4,781 indirect) bytes in 1 blocks are definitely lost in loss record 90 of 90    at 0x484280F: malloc (vg_replace_malloc.c:442)     by 0x51CB334: CRYPTO_zalloc (mem.c:197)     by 0x51DD4F6: provider_new.lto_priv.0 (provider_core.c:451)     by 0x52B4C34: ossl_provider_new.isra.0 (provider_core.c:562)     by 0x51DDAA4: OSSL_PROVIDER_try_load (provider.c:26)     by 0x40E42C: main (Main.cpp:130)

--- src/server/authserver/Main.cpp
+++ src/server/authserver/Main.cpp
@@ -125,15 +125,13 @@ extern int main(int argc, char** argv)
 
     SF_LOG_WARN("server.authserver", "%s (Library: %s)", OPENSSL_VERSION_TEXT, SSLeay_version(SSLEAY_VERSION));
 
-    if (!OSSL_PROVIDER_available(NULL, "legacy"))
-    {
-        if (!OSSL_PROVIDER_try_load(NULL, "legacy", 0))
-        {
-            SF_LOG_INFO("server.authserver", "Failed to load OpenSSL legacy provider.");
-            return 1;
-        }
-        SF_LOG_INFO("server.authserver", "Loaded OpenSSL legacy provider.");
-    }
+    OSSL_PROVIDER* legacy_provider = OSSL_PROVIDER_load(NULL, "legacy");
+    SF_LOG_INFO("server.authserver", "Loading legacy provider: (%s)", (legacy_provider == NULL || !OSSL_PROVIDER_available(NULL, "legacy")) ? "failed" : "succeeded");
+
+    if (legacy_provider == NULL)
+        return 1;
+        
+    OSSL_PROVIDER_unload(legacy_provider);
 
 #if defined (ACE_HAS_EVENT_POLL) || defined (ACE_HAS_DEV_POLL)
     ACE_Reactor::instance(new ACE_Reactor(new ACE_Dev_Poll_Reactor(ACE::max_handles(), 1), 1), true);
