7b816ee1e69828130839c7cbe5a2f949d48d617d
AriDEV <aridev@projectskyfire.org>
2024-04-18 22:11:17 +0200

    Database: Update Loremaster and Loremaster of Northrend Achievement Conditions

---
+++ sql/updates/world/2024_04_18_world_01.sql
@@ -0,0 +1,3 @@
+UPDATE `conditions` SET `ConditionValue1`=7520 WHERE `SourceTypeOrReferenceId`=15 AND `ConditionValue1`=1681; -- The Loremaster
+UPDATE `conditions` SET `ConditionValue1`=7520 WHERE `SourceTypeOrReferenceId`=15 AND `ConditionValue1`=1682; -- The Loremaster
+UPDATE `conditions` SET `ConditionValue1`=41 WHERE `SourceTypeOrReferenceId`=15 AND `ConditionValue1`=1360; -- Loremaster of Northrend
