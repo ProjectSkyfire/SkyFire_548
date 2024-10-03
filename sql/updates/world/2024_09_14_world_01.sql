99d3ad95c4ae2ae914bdfba0649bc4774faaba8d
Digid702 <brendan.inman@hotmail.com>
2024-10-02 19:10:54 -0700

    DB fixed update for RBAC commands

--- sql/updates/world/2024_09_14_world_01.sql
+++ sql/updates/world/2024_09_14_world_01.sql
@@ -1,5 +1,2 @@
 DELETE FROM `command` WHERE `name`='account set gmlevel';
 DELETE FROM `command` WHERE `name`='gm list';
-
-DELETE FROM `rbac_permissions` WHERE `id`=228;
-DELETE FROM `rbac_permissions` WHERE `id`=375;
