553626ed310536fcb06d8fc0da91fb73e1800315
AlterEgo <admin@projectskyfire.org>
2025-08-06 18:09:37 -0500

    CMAKE| Fix installing to defined folder after compile.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

--- cmake/platform/settings.cmake
+++ cmake/platform/settings.cmake
@@ -6,11 +6,5 @@
 # set installation prefix
 if( PREFIX )
   set(CMAKE_INSTALL_PREFIX "${PREFIX}")
-else()
-  if ( UNIX )
-    set(CMAKE_INSTALL_PREFIX "/usr/local/skyfire-server")
-  else()
-    set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/bin")
-  endif()
 endif()
 
