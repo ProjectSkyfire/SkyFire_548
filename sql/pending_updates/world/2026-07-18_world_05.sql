e5648b378765a288ca30bcc52c0404c0de1bc065
AriDEV3 <aridev@projectskyfire.org>
2026-07-18 21:00:03 +0200

    Database: Add OfferRewardTexts to some quests on Echo Island.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026-07-18_world_05.sql
@@ -0,0 +1,7 @@
+UPDATE `quest_template` SET `OfferRewardText`='Nice ta see new faces \'round here, mon. it\'s easy ta get ta feelin\' isolated since we left Orgrimmar.' WHERE `Id`=24622;
+
+UPDATE `quest_template` SET `OfferRewardText`='Thank ya so much! They can be vicious when provoked, but most of da time dey don\'t know betta.' WHERE `Id`=24623;
+
+UPDATE `quest_template` SET `OfferRewardText`='Thank you, $c.\r\n\r\nIt will be takin\' brave and determined people like you ta drive da Darkspear up again. I hope dere are more people even half as dedicated to come.' WHERE `Id`=24624;
+
+UPDATE `quest_template` SET `OfferRewardText`='Well done, $n! Dis naga has set us back quite some time, but nothin\' will be holdin\' us down foreva.\r\n\r\nIt\'s good ta be havin\' ya wit us, $n. Ya have a fierce heart within ya.' WHERE `Id`=24625;
