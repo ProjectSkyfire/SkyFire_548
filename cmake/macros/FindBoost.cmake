e73eb018e7e74ef19baa0f12b1905d49f73d1171
AriDEV3 <aridev@projectskyfire.org>
2026-07-18 07:39:57 +0200

    CMake: Add FindBoost Macro.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ cmake/macros/FindBoost.cmake
@@ -0,0 +1,10 @@
+ #
+# This file is part of Project SkyFire https://www.projectskyfire.org. 
+# See COPYRIGHT file for Copyright information
+#
+
+if( WIN32 )
+  SET(Boost_INCLUDE_DIRS
+    "C:/local/boost_1_91_0/"
+    )
+endif()
