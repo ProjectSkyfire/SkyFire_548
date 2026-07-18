5af48575b07b373f53054f0ef40eeaf6b8d143d9
AriDEV3 <aridev@projectskyfire.org>
2026-07-18 09:21:27 +0200

    CMake: add back boost code from last commits but into FindBoost macro

--- cmake/macros/FindBoost.cmake
+++ cmake/macros/FindBoost.cmake
@@ -8,3 +8,24 @@ if( WIN32 )
     "C:/local/boost_1_91_0/"
     )
 endif()
+
+if(BOOST_ROOT)
+  file(TO_CMAKE_PATH "${BOOST_ROOT}" BOOST_ROOT_HINT)
+elseif(DEFINED ENV{BOOST_ROOT})
+  file(TO_CMAKE_PATH "$ENV{BOOST_ROOT}" BOOST_ROOT_HINT)
+endif()
+
+if(BOOST_ROOT_HINT)
+  list(APPEND CMAKE_PREFIX_PATH "${BOOST_ROOT_HINT}")
+
+  if(NOT Boost_DIR)
+    file(GLOB BOOST_CONFIG_DIRS LIST_DIRECTORIES true
+      "${BOOST_ROOT_HINT}/lib*/cmake/Boost-${BOOST_EXPECTED_VERSION}"
+    )
+
+    if(BOOST_CONFIG_DIRS)
+      list(SORT BOOST_CONFIG_DIRS ORDER DESCENDING)
+      list(GET BOOST_CONFIG_DIRS 0 Boost_DIR)
+    endif()
+  endif()
+endif()
