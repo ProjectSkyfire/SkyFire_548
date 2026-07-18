3df68a93eaec34c98f02be12b4b6022303e6079a
AriDEV3 <aridev@projectskyfire.org>
2026-07-18 15:47:04 +0200

    Database: Added Loot Templates for some Pandarian Treasures.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026-07-18_world_01.sql
@@ -0,0 +1,29 @@
+-- Blade of the Poisoned Mind
+DELETE FROM `gameobject_loot_template` WHERE `entry`=42948;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`) VALUES
+(42948, 86527);
+
+-- Fragment of Dread
+DELETE FROM `gameobject_loot_template` WHERE `entry`=42936;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`) VALUES
+(42936, 86516);
+
+-- Sturdy Yaungol Spear
+DELETE FROM `gameobject_loot_template` WHERE `entry`=42927;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`) VALUES
+(42927, 88723);
+
+-- Terracotta Head
+DELETE FROM `gameobject_loot_template` WHERE `entry`=42918;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`) VALUES
+(42918, 86427);
+
+-- Ancient Jinyu Staff
+DELETE FROM `gameobject_loot_template` WHERE `entry`=42904;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`) VALUES
+(42904, 86196);
+
+-- Pandaren Fishing Spear
+DELETE FROM `gameobject_loot_template` WHERE `entry`=42902;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`) VALUES
+(42902, 86124);
