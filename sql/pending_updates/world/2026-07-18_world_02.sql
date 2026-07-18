6185549b62e29305a0039d66fbc45c757e3008a0
AriDEV3 <aridev@projectskyfire.org>
2026-07-18 18:52:06 +0200

    Database: Add RequestItemsText and OfferRewardText for some quests around pandaria.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026-07-18_world_02.sql
@@ -0,0 +1,38 @@
+UPDATE `quest_template` SET `OfferRewardText`='I see.\r\n\r\nWell, we gave it out all. It\'s time to evacuate, save lives, and reassess our strategy, before these mantid take over the entire valley.r the entire valley.' WHERE `Id`=30626;
+
+UPDATE `quest_template` SET `OfferRewardText`='You seek training?\r\n\r\n<The pandaren studies your physique for several seconds. without speaking.>\r\n\r\nAlright.' WHERE `Id`=29983;
+
+UPDATE `quest_template` SET `OfferRewardText`='Lin asked you to help us out? Rather presumptive of her.\r\n\r\nStill, we are short-manned. We won\'t turn down your help... or the girl\'s. You have our thanks, $c.' WHERE `Id`=29981;
+
+UPDATE `quest_template` SET `OfferRewardText`='I\'m glad to hear that Lin is well.\r\n\r\nOur defenses are holding, for now. The mantid continue to send their scouts over the wall, but they haven\'t gotten much farther than that.' WHERE `Id`=29990;
+
+UPDATE `quest_template` SET `RequestItemsText`='Kypari Vor was beautiful once. Rich with amber and surrounded by prime hunting grounds.' WHERE `Id`=31107;
+
+UPDATE `quest_template` SET `OfferRewardText`='They are large. They will hatch soon.\r\n\r\nGood. We may need them.' WHERE `Id`=31107;
+
+UPDATE `quest_template` SET `OfferRewardText`='In disciplined hands, the spear is the perfect weapon. Only the weak would augment it with sorcery.\r\n\r\nAnd we have no need for the weak.' WHERE `Id`=31689;
+
+UPDATE `quest_template` SET `OfferRewardText`='So the bugs have taken an interest in us have they? Well if they want somethin\' from us they\'ll have to come and earn it themselves!\r\n\r\nYou work for what you get here. That means you too!' WHERE `Id`=31727;
+
+UPDATE `quest_template` SET `OfferRewardText`='A few less traitors for us to feed to the kunchong when the dust settles.' WHERE `Id`=31007;
+
+UPDATE `quest_template` SET `OfferRewardText`='So they want us to leave do they? What a selfish request! We\'re only taking amber from one tree.\r\n\r\nWell, I\'m not going anywhere! I have brew to make.\r\n\r\nIf you help us maybe we can all come to a mutually beneficial agreement. I give them some brew, they stop trying to kill us.' WHERE `Id`=31730;
+
+UPDATE `quest_template` SET `OfferRewardText`='You\'ve got the makings of a fine brewgarden guard, $r. Let me know if you ever get tired of the whole "$c" thing.' WHERE `Id`=31133;
+
+UPDATE `quest_template` SET `OfferRewardText`='Their eggs should have basked in the warmth of the sun and the song of a million wings for ten more years.\r\n\r\nThe Empress will pay for her crimes.' WHERE `Id`=31660;
+
+UPDATE `quest_template` SET `OfferRewardText`='Well done. With access to this amber, we shall be able to craft weapons to our cause.' WHERE `Id`=31267;
+
+UPDATE `quest_template` SET `OfferRewardText`='I never liked spiders. Filthy, mindless insects.' WHERE `Id`=31231;
+
+UPDATE `quest_template` SET `RequestItemsText`='How many arrows do you have?' WHERE  `Id`=30459;
+
+UPDATE `quest_template` SET `OfferRewardText`='Thank you, $n. You\'ve saved our defensive line here.\r\n\r\nThis next attack wouldn\'t have been possible without you.' WHERE `Id`=30459;
+
+UPDATE `quest_template` SET `OfferRewardText`='Instructor Skythorn updated me on the situation at The Widow\'s Wall. You have done well.' WHERE `Id`=31706;
+
+UPDATE `quest_template` SET `OfferRewardText`='Thank goodness, they finally sent someone!\r\n\r\nThings have gotten out of control!' WHERE `Id`=30134;
+
+UPDATE `quest_template` SET `OfferRewardText`='Thank you, $c.\r\n\r\nI tried to stop the Slitherscale where I could, but I was no match for their numbers.\r\n\r\nYour work today will help preserve an entire generation of cloud serpents.' WHERE `Id`=30135;
+
