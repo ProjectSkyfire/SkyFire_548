065cefd5f901a267b6e38c0ef8633b4edfc74382
AriDEV3 <aridev@projectskyfire.org>
2026-07-18 19:59:02 +0200

    Database: Add RequestItemsText and OfferRewardText for quest "Joining the Fight".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026-07-18_world_04.sql
@@ -0,0 +1,3 @@
+UPDATE `quest_template` SET `RequestItemsText`='Welcome to the mess.' WHERE `Id`=30972;
+
+UPDATE `quest_template` SET `OfferRewardText`='Taran Zhu told me what became of my camp. I would have stopped it if I could, but the attacks from the bridge have not let up.\r\n\r\nThank you for bringing these arrows - my archers will make good use of them.' WHERE `Id`=30972;
