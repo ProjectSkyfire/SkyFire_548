ba84a7a11a127ac08e9b6abdc500b7d64e247f12
AriDEV3 <aridev@projectskyfire.org>
2026-07-19 10:52:34 +0200

    Database: Add OfferRewardTexts and RequestItemsTexts to some quests in Jade Forest and Valley of the four Winds.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_19_world_03.sql
@@ -0,0 +1,31 @@
+UPDATE `quest_template` SET `OfferRewardText`='if you\'ve come to speak with my fists, take your place in line. Otherwise, I could use your help.' WHERE `Id`=29576;
+
+UPDATE `quest_template` SET `PrevQuestId`=29576 WHERE `Id`=29578;
+
+UPDATE `quest_template` SET `OfferRewardText`='Nice work, but there\'s more to be done if we are to save this village.' WHERE `Id`=29578;
+
+UPDATE `quest_template` SET `OfferRewardText`='This is terrible...\r\n\r\nWe must find a way to undo what has been done here.' WHERE `Id`=30500;
+
+UPDATE `quest_template` SET `OfferRewardText`='You seek two lovers blind to the ambitions of their family names. They were here, seeking a way to hold to their path.\r\n\r\nSadly they did not find their answers among the trees of art.' WHERE `Id`=29993;
+
+UPDATE `quest_template` SET `RequestItemsText`='Have you been able to acquire some stingers? I would normally ask a monk for help with this but they all seem to be elsewhere at the moment.' WHERE `Id`=29882;
+
+UPDATE `quest_template` SET `OfferRewardText`='My thanks traveler. These will make a fine set of quills. Here is something for your trouble.' WHERE `Id`=29882;
+
+UPDATE `quest_template` SET `RequestItemsText`='You have a letter for me?' WHERE `Id`=31486;
+
+UPDATE `quest_template` SET `OfferRewardText`='My, that\'s so thoughtful. I love hearing from my old students.' WHERE `Id`=31486;
+
+UPDATE `quest_template` SET `RequestItemsText`='Do I smell turnips?' WHERE `Id`=29911;
+
+UPDATE `quest_template` SET `OfferRewardText`='Ho ho! Now that\'s what I like to see!' WHERE `Id`=29911;
+
+UPDATE `quest_template` SET `OfferRewardText`='Wha-? Who the...? My father sent you?\r\n\r\nPshh... he can be so nosy sometimes. I have everything under control here.' WHERE `Id`=29909;
+
+UPDATE `quest_template` SET `OfferRewardText`='It seems like there\'s not QUITE as many corrupted moths out there. At the very least, you\'ve bought me some more time before I get in big, big trouble.' WHERE `Id`=30059;
+
+UPDATE `quest_template` SET `OfferRewardText`='Thank you for your help stranger!\r\nHopefully we can deal with the rest on our own from here.' WHERE `Id`=30029;
+
+UPDATE `quest_template` SET `OfferRewardText`='Now that that\'s over I have some ideas on how to make a new concoction. We\'ll see.\r\n\r\nThank you for your help. Here is your reward.' WHERE `Id`=30030;
+
+UPDATE `quest_template` SET `OfferRewardText`='Well done $n.\r\n\r\nMy father travels across the sea to a new continent and all he can seem ta\' want to hunt is old borin\' wolves.\r\n\r\nMe, I like to hunt stuff with meat on its bones.' WHERE `Id`=30181;
