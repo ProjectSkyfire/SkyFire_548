d613147c8408c5a61307ed72599cc9dd5a9adad1
AriDEV3 <aridev@projectskyfire.org>
2026-07-19 14:44:54 +0200

    Database: Added OfferRewardText for "Thunder Calls" and "Snap Judgment" quests.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_19_world_03.sql
@@ -0,0 +1,6 @@
+UPDATE `quest_template` SET `OfferRewardText`='You have my gratitude, $n.\r\n\r\nPlease take this as a token of my appreciation.' WHERE `Id`=29600;
+
+-- Thunder Calls Alliance
+UPDATE `quest_template` SET `OfferRewardText`='We must move quickly, $n! The others have gone ahead of us, but i will brief you in the flight over.' WHERE `Id`=32679;
+-- Thunder Calls Horde
+UPDATE `quest_template` SET `OfferRewardText`='We must move quickly, $n! The others have gone ahead of us, but i will brief you in the flight over.' WHERE `Id`=32678;
