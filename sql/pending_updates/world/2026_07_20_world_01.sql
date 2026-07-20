3aa5b49d4b9337154fa5afeda46166fd02b4b2c6
AriDEV3 <aridev@projectskyfire.org>
2026-07-20 05:12:43 +0200

    Database: Added some OfferRewardTexts and RequestItemsTexts.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_20_world_01.sql
@@ -0,0 +1,11 @@
+UPDATE `quest_template` SET `OfferRewardText`='Greetings, $c. Welcome to the Silken Fields.' WHERE `Id`=32035;
+
+UPDATE `quest_template` SET `OfferRewardText`='I know I should be the one fighting them off. Maybe I\'m not cut out for shepherding after all.' WHERE `Id`=29917;
+
+UPDATE `quest_template` SET `OfferRewardText`='They... they\'re not stopping, but at least we fought back. Right?' WHERE `Id`=29916;
+
+UPDATE `quest_template` SET `OfferRewardText`='This place... it... it so SAD!\r\n\r\nKen-Ken never seen a place so sad.' WHERE `Id`=29873;
+
+UPDATE `quest_template` SET `RequestItemsText`='Cant\'t believe Ken-Ken did not think of this sooner. It\'s the ONE thing that can ruin his day...' WHERE `Id`=30081;
+
+UPDATE `quest_template` SET `OfferRewardText`='Pandaren got a case of the spooky-dooks!\r\n\r\nDooks get scared sometimes, don\'t wanna come out. Poor, fluffy mopey-dopes must be ready to explode by now!\r\n\r\nThis just the thing to bring \'em out. That make pandaren happy for sure!\r\n\r\nJust like ol\' gramma used to say: can\'t THROW if can\'t GO!' WHERE `Id`=30081;
