27e76cd6a5d3518638eb5847e8c351a2fc25fcb3
AriDEV3 <aridev@projectskyfire.org>
2026-07-20 15:37:45 +0200

    Database: Added OfferRewardText to quest "Supply and Demand".
    Closes: #1222
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_20_world_05.sql
@@ -0,0 +1,2 @@
+-- Supply and Demand
+UPDATE `quest_template` SET `OfferRewardText`='These skins should do. Their quality isn\'t too important, given that they\'ll make up the inside layer of the boots, but it\'s still important that they\'re comfortable.\r\n\r\nIf they\'re a bit too rigid, I\'ll use some tiger blood to soften it up. Tricks o\' the trade, you know?' WHERE `Id`=26343;
