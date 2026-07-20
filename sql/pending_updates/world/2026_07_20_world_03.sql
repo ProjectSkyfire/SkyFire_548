d1b51dcfe2f9a0dd254427033d49039df756c84c
AriDEV3 <aridev@projectskyfire.org>
2026-07-20 07:02:45 +0200

    Database: Remove obsolete spells from previous expansions from playercreateinfo.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_20_world_03.sql
@@ -0,0 +1,12 @@
+-- Obsolete: Strike from cataclysm expansion.
+DELETE FROM `playercreateinfo_spell` WHERE `Spell`=88161;
+-- unk
+DELETE FROM `playercreateinfo_spell` WHERE `Spell`=69001;
+-- Obsolete: Ravage! from cataclysm expansion
+DELETE FROM `playercreateinfo_spell` WHERE `Spell`=81170;
+-- Obsolete: Chaos Bolt Passive from wrath and cataclysm expansion
+DELETE FROM `playercreateinfo_spell` WHERE `Spell`=58284;
+-- Obsolete: Demonic Immolate from wrath and cataclysm expansion
+DELETE FROM `playercreateinfo_spell` WHERE `Spell`=75445;
+-- Obsolete: Pyroblase! cataclysm expansion
+DELETE FROM `playercreateinfo_spell` WHERE `Spell`=92315;
