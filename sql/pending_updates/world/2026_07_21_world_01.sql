2ad52c080a378db268a75b500d951f2bf468d9b9
AriDEV3 <aridev@projectskyfire.org>
2026-07-21 20:32:07 +0200

    Database: Add OfferRewardText and RequestItemsText for quest "Saving the Sutras"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_21_world_01.sql
@@ -0,0 +1,3 @@
+UPDATE `quest_template` SET `RequestItemsText`='How are the sutras? Are they damaged?' WHERE `Id`=30064;
+
+UPDATE `quest_template` SET `OfferRewardText`='Thank you! What a relief it is to see these texts are safe.' WHERE `Id`=30064;
