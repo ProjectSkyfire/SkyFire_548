364464aeb9a6304d3dae56ec5bf92875dec9f95e
AriDEV3 <aridev@projectskyfire.org>
2026-07-22 15:03:59 +0200

    Scripts: Added script for "Transient", "Homeless Stormwind Citizen" and "West Plains Drifter"

---
+++ sql/pending_updates/world/2026_07_22_world_03.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `ScriptName`='npc_transient' WHERE `entry`=42383;
+UPDATE `creature_template` SET `ScriptName`='npc_transient' WHERE `entry`=42384;
+UPDATE `creature_template` SET `ScriptName`='npc_transient' WHERE `entry`=42386;
+UPDATE `creature_template` SET `ScriptName`='npc_transient' WHERE `entry`=42391;
