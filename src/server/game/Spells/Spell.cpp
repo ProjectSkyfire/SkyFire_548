c2c06258430227ca7be46f58a2dce2634ee5b208
DigiD702 <poserbiker@live.com>
2026-07-22 22:09:48 -0700

    Fix MoP SPELL_START predicted-power field order for mana display.

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -4279,14 +4279,9 @@ void Spell::SendSpellStart()
 
     if (predictedPowerCount > 0)
     {
-        /*for (uint32 i = 0; i < powerUnitPowerCount; ++i)
-        {
-            data << int32(powerValue);
-            data << uint8(powerType);
-        }*/
-
-        data << uint8(m_powerType);
+        // SMSG_SPELL_START (5.4.8): Power value then PowerType (WPP V5_4_8).
         data << int32(m_caster->GetPower((Powers)m_powerType));
+        data << uint8(m_powerType);
     }
 
     data << uint32(castFlags);
@@ -4755,12 +4750,7 @@ void Spell::SendSpellGo()
 
     if (predictedPowerCount > 0)
     {
-        //for (uint32 i = 0; i < powerUnitPowerCount; ++i)
-        //{
-        //    data << int32(powerValue);
-        //    data << uint8(powerType);
-        //}
-
+        // SMSG_SPELL_GO (5.4.8): PowerType then Power value (opposite of SPELL_START).
         data << uint8(m_powerType);
         data << int32(m_caster->GetPower((Powers)m_powerType));
     }
