53420dffd5b614515b881e59d5b47a618bc810db
AriDEV3 <aridev@projectskyfire.org>
2026-07-25 18:24:07 +0200

    Database: Update class training quests in Valley of Trials.

---
+++ sql/pending_updates/world/2026_07_25_world_04.sql
@@ -0,0 +1,61 @@
+-- Simple Parchment
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=2383;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=2383;
+-- Charge
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=25147;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=25147;
+UPDATE `quest_template` SET `Objectives`='Reach level 3 to learn Charge. Locate a Training Dummy and practice using Charge.' WHERE `Id`=25147;
+UPDATE `quest_template` SET `Details`='I\'ve been instructed to prepare you for battle. Though, from the look of you, it looks like you\'ve already spent a little time out in the field. Good.$B$BAs you continue your journey, you\'ll discover that you can begin to use new techniques$B$BGo, get some more experience, and teach yourself how to Charge, then show me what you\'ve learned.' WHERE `Id`=25147;
+
+-- Etched Parchment
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=3087;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=3087;
+-- Steady Shot
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=25139;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=25139;
+UPDATE `quest_template` SET `Objectives`='Reach level 3 to learn Steady Shot. Locate a Training Dummy and practice using Steady Shot 5 times.' WHERE `Id`=25139;
+UPDATE `quest_template` SET `Details`='You already know how to fire an arcane shot. Good. But doing so depletes your focus. To gain your focus back you will need to switch to a second type of ranged attack. Go out and get some more experience. As you do, you\'ll learn some new abilities. The first of these will help you get your focus back.' WHERE `Id`=25139;
+
+-- Encrypted Parchment
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=3088;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=3088;
+UPDATE `quest_template` SET `PrevQuestId`=0 WHERE `Id`=3088;
+-- Eviscerate
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=25141;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=25141;
+UPDATE `quest_template` SET `Objectives`='Reach Level 3 to Learn Eviscerate, then locate a Training Dummy and practice using Eviscerate 3 times.' WHERE `Id`=25141;
+UPDATE `quest_template` SET `Details`='Let\'s start with the basics, shall we? I\'m the only rogue trainer in town, so you\'re going to have to learn it from me.$B$BGo get some more experience, figure out how to eviscerate your foes, and then show me on the training dummies here. I\'ll evaluate your form.' WHERE `Id`=25141;
+
+-- Rune-Inscribed Parchment
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=3089;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=3089;
+-- Primal Strike
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=25143;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=25143;
+UPDATE `quest_template` SET `Objectives`='Learn Primal Strike by reaching Level 3, then look for a training dummy near Shikrik and practice using Primal Strike 3 times.' WHERE `Id`=25143;
+UPDATE `quest_template` SET `Details`='As you grow in power by gaining levels, you will discover the power to use new abilities. The first you will discover on your journey is Primal Strike. Use it to deliver a strike with your weapon, infused with the power of the elements.$B$BIt is time to learn and master your new skill. Practice using it on the training dummies outside the Den.' WHERE `Id`=25143;
+
+-- Glyphic Parchment
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=25138;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=25138;
+UPDATE `quest_template` SET `OfferRewardText`='Ahh, where to start... introductions! I\'m Acrypha - the one who sent you that parchment - I\'ve spent the last several years in an Undercity library, my head in a book. I learned everything I could about the arcane, and now I\'ve returned home to share it with grunts like yourself.$B$BShall we begin?' WHERE `Id`=25138;
+-- Frost Nova
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=25149;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=25149;
+
+-- Tainted Parchment
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=3090;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=3090;
+-- Corruption
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=25145;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=25145;
+UPDATE `quest_template` SET `Title`='Corruption' WHERE `Id`=25145;
+UPDATE `quest_template` SET `Objectives`='Reach level 3 to learn Corruption. Locate a Training Dummy and practice casting Corruption 5 times.' WHERE `Id`=25145;
+UPDATE `quest_template` SET `Details`='It is my duty to train you, and I intend to do just that.$B$BAs you become more powerful, you will begin to hear the fel voices revealing new spells to you. Go, get more experience, learn a new spell, and then practice your new spell against one of the training dummies around here.' WHERE `Id`=25145;
+
+-- Calligraphed Parchment
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=31156;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=31156;
+-- Tiger Palm
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=31157;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=31157;
