9b36efd0adca1d4814adde44ef53af61eb975e44
AriDEV3 <aridev@projectskyfire.org>
2026-07-25 18:35:24 +0200

    Database: Update level ranges for class training quests in Sunstrider Isle and Ammen Vale.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_25_world_05.sql
@@ -0,0 +1,83 @@
+-- Warrior Training
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=8329;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=8329;
+-- Charge!
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=27091;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=27091;
+
+-- Paladin Training
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=9676;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=9676;
+-- Ways of the Light
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=10069;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=10069;
+
+-- Hunter Training
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=9393;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=9393;
+-- Steady Shot
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=10070;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=10070;
+
+-- Rogue Training
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=9392;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=9392;
+-- Evisceration
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=10071;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=10071;
+
+-- Priest Training
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=8564;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=8564;
+-- Learning the Word
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=10072;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=10072;
+
+-- Mage Training
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=8328;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=8328;
+-- Learning the Word
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=10068;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=10068;
+
+-- Warlock Training
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=8563;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=8563;
+-- Corruption
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=10073;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=10073;
+
+-- Monk Training
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=31170;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=31170;
+-- Tiger Palm
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=31171;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=31171;
+
+-- Your First Lesson
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=26958;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=26958;
+
+-- The Light's Power
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=26966;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=26966;
+
+-- Steadying Your Shot
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=26963;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=26963;
+
+-- Learning the Word
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=26970;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=26970;
+
+-- Primal Strike
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=26969;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=26969;
+
+-- Frost Nova
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=26968;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=26968;
+
+-- The Tiger Palm
+UPDATE `quest_template` SET `Level`=2 WHERE `Id`=31173;
+UPDATE `quest_template` SET `MinLevel`=2 WHERE `Id`=31173;
