35f338a0bd4f24795787b2b0a17df478d5cf2e55
AriDEV3 <aridev@projectskyfire.org>
2026-07-26 17:01:49 +0200

    Database: Update class training quest texts on Echo Island.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_26_world_07.sql
@@ -0,0 +1,25 @@
+ -- The Arts of a Warrior
+UPDATE `quest_template` SET `Objectives`='Reach level 3 to learn Charge. Practice using Charge against a Tiki Target.' WHERE `Id`=24640;
+UPDATE `quest_template` SET `Details`='As ya get bettah\' at fightin\', you\'ll start to find new tings that ya nevah knew you could do.$B$BWanna see? Go, train up some, and learn someting new.$B$B\'Den come back and ya can show me.' WHERE `Id`=24640;
+UPDATE `quest_template` SET `OfferRewardText`='Good work, mon! Dis stuff come easy to ya.' WHERE `Id`=24640;
+-- The Arts of a Hunter
+UPDATE `quest_template` SET `Objectives`='Reach level 3 to learn Steady Shot. Use Steady Shot 5 times on a Tiki Target.' WHERE `Id`=24778;
+UPDATE `quest_template` SET `Details`='As ya\' start to get da hang of tings, ya gonna figure out how ta do new tricks.$B$BOne of da\' first ya gonna learn is to keep a steady hand between shots, or else ya gonna find yaself exhausted and not really managin\' to kill much.$B$BGo get some more experience \'till ya figure it out, \'den come back and try it out. Let me know when ya tink you have it down.' WHERE `Id`=24778;
+-- The Arts of a Rogue
+UPDATE `quest_template` SET `Objectives`='Reach level 3 to learn Eviscerate, then use Eviscerate 3 times against a Tiki Target.' WHERE `Id`=24772;
+UPDATE `quest_template` SET `Details`='As ya fight ya enemies, ya will learn new tricks you use ta be even bettah\'. There\'s one ya should be learnin\' soon to let ya see dere weaknesses and set up for devastating combo attacks. After ya\'ve struck a few blows to weaken ya foe, finish dem off with a vicious evisceration attack. When ya learn it, try it out. Let me know when ya tink you have it down.' WHERE `Id`=24772;
+UPDATE `quest_template` SET `OfferRewardText`='Good work, mon! Dis stuff come easy to ya.' WHERE `Id`=24772;
+-- The Arts of a Priest
+UPDATE `quest_template` SET `Objectives`='Reach level 3 to learn Shadow Word: Pain. Use it 5 times on 5 Tiki training dummies.' WHERE `Id`=24784;
+UPDATE `quest_template` SET `Details`='As ya get more skill out there in tha\' world, you\'ll start to discovah\' new powahs.$B$BWhy dontcha go out an\' go get yaself stronger, and come back when ya learn sometin\' new. You can show me what ya learn.' WHERE `Id`=24784;
+UPDATE `quest_template` SET `OfferRewardText`='Good work, mon! Dis stuff come easy to ya.' WHERE `Id`=24784;
+-- The Arts of a Shaman
+UPDATE `quest_template` SET `Objectives`='Reach Level 3 to learn Primal Strike, and then use Primal Strike 3 times against a Tiki Target.' WHERE `Id`=24760;
+UPDATE `quest_template` SET `Details`='As ya grow in powah, you begin ta discovah the voices of da ancestahs speakin\' to ya, teachin\' you dere secrets. Tha first of \'dese is to focus tha power of da elements into yer weapon. It allows ya to unleash a quick, vicious attack on your enemies. Dis should be easy enough for ya to make a lot of use of it.$B$BTry it out. Let me know when ya tink you have it down.' WHERE `Id`=24760;
+UPDATE `quest_template` SET `OfferRewardText`='Good work, mon! Dis stuff come easy to ya.' WHERE `Id`=24760;
+-- The Arts of a Monk
+UPDATE `quest_template` SET `OfferRewardText`='Good work, mon! Dis stuff come easy to ya.' WHERE `Id`=31162;
+-- The Arts of a Druid
+UPDATE `quest_template` SET `Objectives`='Reach level 3 to learn Moonfire. Practice using it on a Tiki target in the Darkspear Training Grounds.' WHERE `Id`=24766;
+UPDATE `quest_template` SET `Details`='As ya grow in powah, ya ability to call on nature gets strongah, too. Ya wanna see? Go get some experience and show me what ya\' learn.' WHERE `Id`=24766;
+UPDATE `quest_template` SET `OfferRewardText`='Good work, mon! Dis stuff come easy to ya.' WHERE `Id`=24766;
