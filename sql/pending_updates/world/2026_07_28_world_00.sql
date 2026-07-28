acfb4a8a5f571748534214a6452a55ae424d68bf
AriDEV3 <aridev@projectskyfire.org>
2026-07-28 05:23:30 +0200

    Database: Added RequestItemsText and OfferRewardText to quest "Where Silk Comes From"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_28_world_00.sql
@@ -0,0 +1,2 @@
+UPDATE `quest_template` SET `RequestItemsText`='Have you found all of the items? Did you pay close attention to our process?' WHERE `Id`=30072;
+UPDATE `quest_template` SET `OfferRewardText`='Superb specimens. I will see to it they find their way to the right place. Please enjoy this free sample.' WHERE `Id`=30072;
