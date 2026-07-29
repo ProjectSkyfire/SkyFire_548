ba919240beb0d78c301ba78016d8c7d659870df7
AriDEV3 <aridev@projectskyfire.org>
2026-07-29 15:56:12 +0200

    Database: Add RequestItemsText and OfferRewardText for quest "What's Keeping Jessup?" and "Get Me Explosives Back!"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_29_world_04.sql
@@ -0,0 +1,6 @@
+-- What's Keeping Jessup?
+UPDATE quest_template` SET `OfferRewardText`='It won\'t be tough to collapse th\' tunnel, but I\'m goin\' to need my team an\' my equipment \'afore I can get to th\' task.' WHERE `Id`=26316;
+
+-- Get Me Explosives Back!
+UPDATE `quest_template` SET `RequestItemsText`='Well, did you recover me missing supplies?' WHERE `Id`=26285;
+UPDATE `quest_template` SET `OfferRewardText`='Well, now, that\'s more like it. We may get around to blowin\' up somethin\' after all.' WHERE `Id`=26285;
