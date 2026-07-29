ea381e76790d0163eb20d9376fd67548468fbcba
AriDEV3 <aridev@projectskyfire.org>
2026-07-29 18:18:51 +0200

    Database: Add RequestItemsText and OfferRewardText for quests "One More Thing" and "Crushcog's Minions"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_29_world_06.sql
@@ -0,0 +1,6 @@
+-- One More Thing
+UPDATE `quest_template` SET `RequestItemsText`='Welcome back, $n.' WHERE `Id`=26329;
+UPDATE `quest_template` SET `OfferRewardText`='<The high tinker reads Jessup\'s report.>$B$BSplendid news! With the troggs taken care of, we should be able to turn our attention to Crushcog\'s troublemaking.' WHERE `Id`=26329;
+
+-- Crushcog's Minions
+UPDATE `quest_template` SET `OfferRewardText`='That should set back Crushcog\'s plans a bit, but we can\'t rest until he is defeated and Chill Breeze Valley is secure.' WHERE `Id`=26331;
