1fe3c809a9687e066dd39ba2aa294b615aaadc5f
AriDEV3 <aridev@projectskyfire.org>
2026-07-29 20:36:35 +0200

    Database: Add OfferRewardText for quests "Staging in Brewnall" and "On to Kharanos"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_07_29_world_08.sql
@@ -0,0 +1,5 @@
+-- Staging in Brewnall
+UPDATE `quest_template` SET `OfferRewardText`='Ah, yes, Kelsey told me to expect you. There\'s still much to do before we can take on Crushcog.' WHERE `Id`=26339;
+
+-- On to Kharanos
+UPDATE `quest_template` SET `OfferRewardText`='If Gnomeregan Covert Operations is recommending you, then you must have made quite an impression. Your help will be most welcome here, as we\'ve a wealth of enemies and few enough mountaineers to face them.' WHERE `Id`=26373;
