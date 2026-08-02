5335c9fd87c955705f5417a63a40de8dae03a60a
AriDEV3 <aridev@projectskyfire.org>
2026-08-02 18:11:59 +0200

    Database: Add RequestItemsText and OfferRewardText for quests "Like A Fart in the Wind", "Render's Army" and "Hero's Call: Duskwood"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/updates/world/2026_08_02_world_05.sql
@@ -0,0 +1,12 @@
+-- Like A Fart in the Wind
+UPDATE `quest_template` SET `RequestItemsText`='Did you recover the supplies?' WHERE `Id`=26513;
+UPDATE `quest_template` SET `OfferRewardText`='Thank you, citizen. Without you, we would have starved or worse, been forced to go out and get food for ourselves.' WHERE `Id`=26513;
+
+-- Render's Army
+UPDATE `quest_template` SET `OfferRewardText`='You\'ve probably bought us another day, maybe two. Nice work, $n.' WHERE `Id`=26570;
+
+-- Hero's Call: Duskwood (Redridge Questgiver)
+UPDATE `quest_template` SET `OfferRewardText`='You are the help that we requested?$B$B<Althea sighs.>$B$BI suppose you will have to do.' WHERE `Id`=26728;
+
+-- Hero's Call: Duskwood (Hero's Call Board)
+UPDATE `quest_template` SET `OfferRewardText`='You are the help that we requested?$B$B<Althea sighs.>$B$BI suppose you will have to do.' WHERE `Id`=28564;
