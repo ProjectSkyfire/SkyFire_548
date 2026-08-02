3273d2fec253e99c2c5664635ce87d73b5bbb446
AriDEV3 <aridev@projectskyfire.org>
2026-08-02 19:00:31 +0200

    Database: Corrected quest order for quests "Ice and Fire" and "Don't Forget About Us", also add OfferRewardText for quest "Evisceratin' The Enemy".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_02_world_06.sql
@@ -0,0 +1,8 @@
+-- Ice and Fire
+UPDATE `quest_template` SET `PrevQuestId`=24489 WHERE `Id`=218;
+
+-- Don't Forget About Us
+UPDATE `quest_template` SET `PrevQuestId`=24491 WHERE `Id`=24493;
+
+-- Evisceratin' The Enemy
+UPDATE `quest_template` SET `OfferRewardText`='You\'ve got the hang of it.  Go get some practice out there... maybe take out a few of those troggs or trolls.  Or rabbits, I don\'t care.$B$BReturn after you\'ve gotten some practice, and I\'ll show you another trick of the trade.' WHERE `Id`=24532;
