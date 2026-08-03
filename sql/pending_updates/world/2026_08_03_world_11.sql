ff001e769247c56627f2a264234b514d28f6feed
AriDEV3 <aridev@projectskyfire.org>
2026-08-03 16:03:09 +0200

    Database: Update RequestItemsText and OfferRewardText to some quests in Duskwood.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_03_world_11.sql
@@ -0,0 +1,63 @@
+-- Wolves at Our Heels
+UPDATE `quest_template` SET `OfferRewardText`='Don\'t go thinking that was too easy. Far greater dangers lurk deeper in the woods.' WHERE `Id`=26618;
+
+-- The Night Watch
+UPDATE `quest_template` SET `OfferRewardText`='Splendid, $n. For your service to the people of Darkshire you shall be rewarded.' WHERE `Id`=26645;
+
+-- Bones That Walk
+UPDATE `quest_template` SET `OfferRewardText`='The people of Darkshire thank you, $n. You have proven yourself to be a great ally of The Night Watch.' WHERE `Id`=26686;
+
+-- Worgen in the Woods
+UPDATE `quest_template` SET `OfferRewardText`='Impressive, $n. It would seem that you are capable enough to handle yourself. Perhaps a more suitable challenge could be found for one of your abilities.' WHERE `Id`=26688;
+
+-- The Rotting Orchard
+UPDATE `quest_template` SET `OfferRewardText`='You performed well against the Shadow Weavers, $n. But even now, years after their first arrival, there seem to be so many to replace the ones we kill.$B$BI will put my faith in Master Carevin. No doubt he will get to the bottom of the problem.' WHERE `Id`=26689;
+
+-- Vile and Tainted
+UPDATE `quest_template` SET `OfferRewardText`='$n, to be honest with you, I did not believe that you would get this far, but you are clearly a $c to be reckoned with. In fact, if you wish to formally join Master Carevin\'s struggle, I will gladly write for you a letter of recommendation.' WHERE `Id`=26690;
+
+-- Worgen in the woods
+UPDATE `quest_template` SET `OfferRewardText`='Excuse me for my cold reception, $n, but as I\'m sure you can tell, I am an extremely busy man. I see that you have impressed Calor--and I assure you, that is no small feat--and that he has given his recommendation.$B$BThere are unsavory types afoot, $n, and we can use the help of all who have proven themselves. We battle against demons, the undead, and those who would provide them aid. Be vigilant, be wary, and trust none who would not give aid to our cause.$B$BGlory under the Light.' WHERE `Id`=26691;
+
+-- Seasoned Wolf Kabobs
+UPDATE `quest_template` SET `OfferRewardText`='Mmm-mmm! The skirtsteak\'s the best part. It\'s not an efficient way to cook, though.$B$BHere, have this recipe. It\'s a little different, but flank meat\'s easier to come by when you really need a meal.' WHERE `Id`=26620;
+
+-- Dusky Crab Cakes
+UPDATE `quest_template` SET `OfferRewardText`='Ah yes, a nice lump you have there! Let me just get this seasoned with my secret spices (no looking!) and get them to skittering on a skillet for a while...$B$BAnd, although Dusky Crab Cakes are my specialty and I won\'t give out the recipe, here\'s the recipe for a dish that\'s almost as good.' WHERE `Id`=26623;
+
+-- The Hermit
+UPDATE `quest_template` SET `OfferRewardText`='Ah, welcome, stranger. I\'d offer you something, but there\'s not much here...$B$BCould I beg you to run some errands for a poor old hermit?' WHERE `Id`=26627;
+
+-- Supplies from Darkshire
+UPDATE `quest_template` SET `OfferRewardText`='Ah, ghost hair thread is what you need, is it? I\'m afraid I have none in stock, but I can make some for you...if you can supply the ghost hair.' WHERE `Id`=26653;
+
+-- Ghost Hair Thread
+UPDATE `quest_template` SET `OfferRewardText`='What is this? A comb? It\'s lovely! And it glides through my hair as if it weren\'t the stiff, stringy horror that it is.$B$BOh, if only I had a mirror...' WHERE `Id`=26652;
+
+-- Return the Comb
+UPDATE `quest_template` SET `OfferRewardText`='I can make a spool of ghost hair thread with this, and have a few strands to spare. Here are some coins for those extra strands.' WHERE `Id`=26654;
+
+-- Deliver the Thread
+UPDATE `quest_template` SET `OfferRewardText`='Delightful! This will do splendidly...$B$BHere, good $n, take this as payment for your honorable deed.' WHERE `Id`=26655;
+
+-- Zombie Juice
+UPDATE `quest_template` SET `OfferRewardText`='You need some Zombie Juice, do you? Hmm...that\'s some strong stuff - I don\'t usually get requests for it.' WHERE `Id`=26660;
+
+-- Gather Rot Blossoms
+UPDATE `quest_template` SET `OfferRewardText`='Good, you got the rot blossoms.$B$BI\'ll whip up the zombie juice... it\'s strong stuff, I warn you.' WHERE `Id`=26661;
+UPDATE `quest_template` SET `RequestItemsText`='Hi, $n. Do you have those rot blossoms for me yet?' WHERE `Id`=26661;
+
+-- Juice Delivery
+UPDATE `quest_template` SET `OfferRewardText`='A thousand thanks, $n. You warm an old man\'s heart with your foolish-...I mean...with your kindness!$B$BHere you are, friend. Take this as a token of my gratitude.' WHERE `Id`=26676;
+
+-- A Deadly Vine
+UPDATE `quest_template` SET `OfferRewardText`='I thank you, $c... and I\'m sure the worgen that may be returned to sanity with the potion this will make would thank you too.' WHERE `Id`=26707;
+
+-- The Yorgen Worgen
+UPDATE `quest_template` SET `OfferRewardText`='Sounds like a close call! But he ran off without harming you further...$B$BHe might be still fighting it. Master Harris must know!' WHERE `Id`=26717;
+
+-- Vulgar Vul'Gol
+UPDATE `quest_template` SET `OfferRewardText`='I saw you drop a few of them from here. Nice work.$B$BHere\'s your reward, on behalf of the Night Watch.' WHERE `Id`=25235;
+
+-- Delivery to Master Harris
+UPDATE `quest_template` SET `OfferRewardText`='Welcome to our humble camp. I wouldn\'t call it pleasant, but it\'s at least somewhat private.$B$BBeg your pardon? A worgen at the farm?' WHERE `Id`=26719;
