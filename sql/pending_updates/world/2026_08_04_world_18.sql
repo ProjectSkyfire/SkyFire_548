36e3054b5068676d70c81aa397f526f20127953f
AriDEV3 <aridev@projectskyfire.org>
2026-08-04 12:54:07 +0200

    Database: Quest "Beer Basted Boar Ribs" is no longer a cooking quest.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_04_world_18.sql
@@ -0,0 +1,4 @@
+-- Beer Basted Boar Ribs
+UPDATE `quest_template` SET `RequiredSkillId`=0 WHERE `Id`=384;
+UPDATE `quest_template` SET `RequiredSkillPoints`=0 WHERE `Id`=384;
+UPDATE `quest_template` SET `RequestItemsText`='I\'m going to need those crag boar ribs and a mug of Rhapsody Malt, $n.' WHERE `Id`=384;
