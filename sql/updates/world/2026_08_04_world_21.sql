8ea4dcc5c198a4bb40eb4ddfe0d32af4c6f9a2ea
AriDEV3 <aridev@projectskyfire.org>
2026-08-05 09:00:26 +0200

    Database: Fix target in script.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

--- sql/updates/world/2026_08_04_world_21.sql
+++ sql/updates/world/2026_08_04_world_21.sql
@@ -94,4 +94,4 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=41052;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=41052;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(41052, 0, 0, 0, 54, 0, 100, 0, 0, 0, 0, 0, 0, 89, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Remote Observation Bot - On Respawn - Set Random Movement');
+(41052, 0, 0, 0, 54, 0, 100, 0, 0, 0, 0, 0, 0, 89, 10, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Remote Observation Bot - On Respawn - Set Random Movement');
