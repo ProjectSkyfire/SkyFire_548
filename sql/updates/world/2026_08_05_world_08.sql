7b302cd8330f66421fa9a1d91cfc295f49798061
Nightprince <keramatjokar8855@gmail.com>
2026-08-05 11:49:21 +0330

    DB/World: Add Missing Instance Exit Portals for "Gate of the Setting Sun" Dungeon. Close #1210

---
+++ sql/updates/world/2026_08_05_world_08.sql
@@ -0,0 +1,6 @@
+DELETE FROM `gameobject` WHERE `guid`=263875;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseId`, `phaseGroup`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (263875, 212899, 962, 262, 0, 0, 958.95, 2161.86, 296.106, 1.6117, 0, 0, 0, 1, 7200, 255, 1);
+DELETE FROM `gameobject` WHERE `guid`=263876;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseId`, `phaseGroup`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (263876, 212899, 962, 262, 0, 0, 721.646, 2097.76, 403.981, 1.6117, 0, 0, 0, 1, 7200, 255, 1);
+
+
