34a97a33f1ad733f7fd73498c56b5645fdf28e42
Nightprince <keramatjokar8855@gmail.com>
2026-08-05 11:36:23 +0330

    DB/World: Add Missing Instance Exit Portals for "Mogu'shan Vaults" Raid. Close #1207

---
+++ sql/updates/world/2026_08_05_world_06.sql
@@ -0,0 +1,2 @@
+DELETE FROM `gameobject` WHERE `guid`=263873;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseId`, `phaseGroup`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (263873, 214441, 1008, 248, 0, 0, 3848.87, 1044.63, 492.389, 0.0197337, 0, 0, 0, 1, 7200, 255, 1);
