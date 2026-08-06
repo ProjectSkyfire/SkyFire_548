827ef7b120fafc2f8df10650b6934e2a3e11d483
AriDEV3 <aridev@projectskyfire.org>
2026-08-06 19:22:17 +0200

    Database: Add RequestItemsText and OfferRewardText for quest "Trouble From the Ground Up"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_06_world_14.sql
@@ -0,0 +1,3 @@
+-- Trouble From the Ground Up
+UPDATE `quest_template` SET `RequestItemsText`='I am very busy, $c. This uncontrolled growth threatens to engulf the continent. Do you have any news?' WHERE `Id`=25028;
+UPDATE `quest_template` SET `OfferRewardText`='Ah, samples from different parts of the overgrowth? These will be immensely helpful, yes. Thank you, $c.' WHERE `Id`=25028;
