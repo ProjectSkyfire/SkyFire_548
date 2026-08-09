507776a86e4747c20be2f64e13353cac28008d0f
AriDEV3 <aridev@projectskyfire.org>
2026-08-09 11:19:17 +0200

    Database: Add OfferRewardText for quest "The Admiral Won't Back Down"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_09_world_00.sql
@@ -0,0 +1,2 @@
+-- The Admiral Won't Back Down
+UPDATE `quest_template` SET `OfferRewardText`='You\'ve ventured all the way here from Honor\'s Stand? Excellent, we could use your experience here.$B$BThey may have breached the walls, but Northwatch isn\'t going down without a fight.' WHERE `Id`=25197;
