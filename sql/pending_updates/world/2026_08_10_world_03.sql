bc19b8ddb6b9edc947c0e84aea9923f807f12765
AriDEV3 <aridev@projectskyfire.org>
2026-08-10 22:40:19 +0200

    Database: Update quest order, RequestItemsText and OfferRewardText for quest "Make 'em Squeal"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_10_world_03.sql
@@ -0,0 +1,4 @@
+-- Make 'em Squeal
+UPDATE `quest_template` SET `PrevQuestId`=25036 WHERE `Id`=25015;
+UPDATE `quest_template` SET `RequestItemsText`='I should mention, you\'re probably going to need a machete to hack through this mess.$B$BThis jungle came out of nowhere!' WHERE `Id`=25015;
+UPDATE `quest_template` SET `OfferRewardText`='<Corporal Teegan nods as you deliver your report, then grasps the Bramblestaff with both hands.>$B$BSo, this is what the quilboar are using against us? Do you think they used these to create this jungle?' WHERE `Id`=25015;
