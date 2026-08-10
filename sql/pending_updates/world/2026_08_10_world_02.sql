4a806de367c89a4f63014e0e4aab6615ec68cd0b
AriDEV3 <aridev@projectskyfire.org>
2026-08-10 21:03:29 +0200

    Database: Add RequestItemsText and OfferRewardText for quests "Scavenged" and "Teegan's Troubles".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_10_world_02.sql
@@ -0,0 +1,5 @@
+-- Scavenged
+UPDATE `quest_template` SET `RequestItemsText`='What? My supply caravan was sacked by those cold-blooded scissor-paw things?' WHERE `Id`=25002;
+UPDATE `quest_template` SET `OfferRewardText`='My supplies! Well, that showed some real initiative, $c. Those rap-saws or whatever they\'re called have torn lesser men to ribbons. Thank you!' WHERE `Id`=25002;
+-- Teegan's Troubles
+UPDATE `quest_template` SET `OfferRewardText`='So, Paxton says there\'s no jungle in the Barrens? What do you call THIS!?$B$BI\'ve got a pile of supplies to move to the interior and my camp has been overrun by a forest that - I swear - grows by the hour!' WHERE `Id`=25036;
