d9d956432c2319dd533277c7c5b7ebbd48c1088f
AriDEV3 <aridev@projectskyfire.org>
2026-08-12 13:31:36 +0200

    Database: Add "Cold Heart" Aura to "Grik'nir the Cold"
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_12_world_03.sql
@@ -0,0 +1,4 @@
+-- Grik'nir the Cold
+DELETE FROM `creature_template_addon` WHERE `entry`=808;
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(808, 0, 0, 0, 1, 0, '80631');
