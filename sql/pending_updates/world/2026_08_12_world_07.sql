789314d7916028e82be9c62585686dfecbe72b28
AriDEV3 <aridev@projectskyfire.org>
2026-08-12 15:19:32 +0200

    Database: Add SAI for "Frostmane Blade".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_12_world_07.sql
@@ -0,0 +1,14 @@
+-- Frostmane Blade
+DELETE FROM `creature_text` WHERE `entry`=37507;
+INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`) VALUES
+(37507, 0, 0, 'Killing you be easy.', 12, 0, 80, 0, 0, 0, 'SAY_AGGRO1'),
+(37507, 0, 1, 'You be dead soon!', 12, 0, 80, 0, 0, 0, 'SAY_AGGRO2'),
+(37507, 0, 2, 'My weapon be thirsty!', 12, 0, 80, 0, 0, 0, 'SAY_AGGRO3'),
+(37507, 0, 3, 'I gonna make you into mojo!', 12, 0, 80, 0, 0, 0, 'SAY_AGGRO4');
+
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=37507;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=37507;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(37507, 0, 0, 0, 4, 0, 100, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Frostmane Blade - On Aggro - Say 0'),
+(37507, 0, 1, 0, 9, 0, 100, 0, 0, 5, 15000, 20000, 0, 11, 70133, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'Frostmane Blade - Range 5 yards - Cast Poisoned Blade');
