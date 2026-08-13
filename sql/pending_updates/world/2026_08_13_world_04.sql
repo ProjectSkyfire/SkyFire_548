206880bcf33d0654ab3ef542bc56845cd9c9eac6
AriDEV3 <aridev@projectskyfire.org>
2026-08-13 19:48:48 +0200

    Database: Add RequestItemsText and OfferRewardText for quest "Demanding Answers".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_13_world_04.sql
@@ -0,0 +1,3 @@
+-- Demanding Answers
+UPDATE `quest_template` SET `RequestItemsText`='What is the meaning of this intrusion?' WHERE `Id`=26112;
+UPDATE `quest_template` SET `OfferRewardText`='And just what is this?$B$B<Moira takes the report and the color drains from her face as she reads.>$B$BThis is impossible!' WHERE `Id`=26112;
