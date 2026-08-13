9808c8802744bb7fc19cc7d0754a2cb37e863fe7
AriDEV3 <aridev@projectskyfire.org>
2026-08-13 17:23:24 +0200

    Database: Use summon spell for summoning Tombs.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

--- sql/updates/world/2026_08_13_world_02.sql
+++ sql/updates/world/2026_08_13_world_02.sql
@@ -7,8 +7,8 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=41763;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=41763;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(41763, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 12, 41768, 7, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Frozen Mountaineer - On Respawn - Summon'),
-(41763, 0, 1, 0, 38, 0, 100, 0, 1, 1, 0, 0, 0, 80, 4176300, 7, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Frozen Mountaineer - On Data Set - Run Script');
+(41763, 0, 0, 0, 11, 0, 100, 0, 0, 0, 0, 0, 0, 11, 77906, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Frozen Mountaineer - On Respawn - Summon'),
+(41763, 0, 1, 0, 38, 0, 100, 0, 1, 1, 0, 0, 0, 80, 4176300, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Frozen Mountaineer - On Data Set - Run Script');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=4176300;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
