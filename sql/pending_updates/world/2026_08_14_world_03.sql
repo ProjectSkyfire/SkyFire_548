b4bfe88b4e92315638e3026d96458a2310d2ff0f
AriDEV3 <aridev@projectskyfire.org>
2026-08-14 10:07:23 +0200

    Database: Add OfferRewardText for quest "Hero's Call: Loch Modan!".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_14_world_03.sql
@@ -0,0 +1,3 @@
+-- Hero's Call: Loch Modan!
+UPDATE `quest_template` SET `OfferRewardText`='Have our leaders finally heard my pleas? We\'re down to just a handful of pilots and mountaineers to defend Loch Modan.' WHERE `Id`=28567;
+
