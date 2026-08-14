b338212db39152aca41deb2d4b69bf422cb5fcbd
AriDEV3 <aridev@projectskyfire.org>
2026-08-14 10:44:04 +0200

    Database: Implement Gyrocopter flights to/from Ironforge Airfield.
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_14_world_05.sql
@@ -0,0 +1,13 @@
+-- Mathel's Flying Machine <Charter Flights to Ironforge Airfield>
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=41848;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=41848;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(41848, 0, 0, 0, 73, 0, 100, 0, 0, 0, 0, 0, 0, 52, 2201, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Mathel\'s Flying Machine - On Spellclick - Activate Taxi');
+
+-- Rixa's Flying Machine <Charter Flights to Ironforge Airfield>
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=42175;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=42175;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(42175, 0, 0, 0, 73, 0, 100, 0, 0, 0, 0, 0, 0, 52, 2207, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Mathel\'s Flying Machine - On Spellclick - Activate Taxi');
