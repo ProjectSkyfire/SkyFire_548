9b86bded681ee2aaab9608b4f7c99d260577c4b2
AriDEV3 <aridev@projectskyfire.org>
2026-08-18 15:32:31 +0200

    Database: Add RequestItemsText and OfferRewardText for quests "You Flicked a Fine Vine to Leaf Me", "Teegan's People" and "To the Front!".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_18_world_00.sql
@@ -0,0 +1,10 @@
+-- You Flicked a Fine Vine to Leaf Me
+UPDATE `quest_template` SET `RequestItemsText`='I can see why the Horde hates these little nuisances. Have you taken on Three-Tooth yet?' WHERE `Id`=25027;
+UPDATE `quest_template` SET `OfferRewardText`='...Torn limb from limb!? Well, I\'d almost feel bad but the burning hatred gets in the way. She got what she deserved.$B$B<Corporal Teegan takes the Bramblestaff and snaps it over his knee.>$B$BProblem solved.' WHERE `Id`=25027;
+
+-- Teegan's People
+UPDATE `quest_template` SET `RequestItemsText`='What is it, $c? This here camp ain\'t fit for sight-seein\'.' WHERE `Id`=25000;
+UPDATE `quest_template` SET `OfferRewardText`='Hey, you\'re tougher than you look. These terrortooth hides reflect arrows and turn axe-blades - they don\'t go down easy, but their hides are worth their weight in silver in these light-forsaken Barrens. Nice work.' WHERE `Id`=25000;
+
+-- To the Front!
+UPDATE `quest_template` SET `OfferRewardText`='Ah, $n! I\'ve been talking you up to the General here. Thank you again for saving my life.' WHERE `Id`=25034;
