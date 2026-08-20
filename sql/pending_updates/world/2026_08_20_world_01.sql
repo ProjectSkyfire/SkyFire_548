432cf0d04b9479adcd2bb10076f17412e330f984
AriDEV3 <aridev@projectskyfire.org>
2026-08-20 10:45:22 +0200

    Database: Add spawntime secs to some gameobjects in southern barrens.
    Closes #1298
    
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_20_world_01.sql
@@ -0,0 +1,17 @@
+-- Khadgar's Whisker
+UPDATE `gameobject` SET `spawntimesecs`=120, `animprogress`=255 WHERE `guid` IN (101358, 101357, 101356);
+
+-- Kingsblood
+UPDATE `gameobject` SET `spawntimesecs`=120, `animprogress`=255 WHERE `guid` IN (101360, 101359);
+
+-- Iron Deposit
+UPDATE `gameobject` SET `spawntimesecs`=120, `animprogress`=255 WHERE `guid`=101361;
+
+-- Gold Vein
+UPDATE `gameobject` SET `spawntimesecs`=120, `animprogress`=255 WHERE `guid`=101362;
+
+-- Goldthorn
+UPDATE `gameobject` SET `spawntimesecs`=120, `animprogress`=255 WHERE `guid`=101363;
+
+-- Northwatch Supply Crate
+UPDATE `gameobject` SET `spawntimesecs`=120, `animprogress`=255 WHERE `id`=202405;
