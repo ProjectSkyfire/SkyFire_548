caee7539bd9a03b88bf5e19dcc71be39bf6e7155
AriDEV3 <aridev@projectskyfire.org>
2026-08-20 18:37:13 +0200

    Database: Add RequestItemsText and OfferRewardText for quests "Hero's Call: Wetlands!", "The Stolen Keg", "Cleaning Hovel" and "Keg Run".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_20_world_04.sql
@@ -0,0 +1,13 @@
+-- Hero's Call: Wetlands!
+UPDATE `quest_template` SET `OfferRewardText`='We haven\'t gotten much in the way of reinforcements lately. We can use every pair of hands we can get.' WHERE `Id`=28565;
+
+-- The Stolen Keg
+UPDATE `quest_template` SET `OfferRewardText`='Thank the Makers! It\'s downright pristine. Those orcs couldn\'t figure out the tap-lock, it seems...and couldn\'t hack through the mastercraft work of Ironforge\'s keg-crafters with their crude weapons!' WHERE `Id`=25395;
+UPDATE `quest_template` SET `RequestItemsText`='Do you have it? Is the poor thing intact?' WHERE `Id`=25395;
+
+-- Cleaning Hovel
+UPDATE `quest_template` SET `OfferRewardText`='That\'ll show \'em!  Nice work!' WHERE `Id`=25211;
+
+-- Keg Run
+UPDATE `quest_template` SET `OfferRewardText`='Hah! Nice to see those addlebrained mountaineers knew to hand the ale off to a real man to get the job done.$B$BWelcome to my survey. If you\'re of the mind to help Ironforge by takin\' over the intended jobs of some useless, lazy sods that call themselves dwarves, you\'re in the right place!' WHERE `Id`=25770;
+UPDATE `quest_template` SET `RequestItemsText`='Aye? What\'ve you got for me there?' WHERE `Id`=25770;
