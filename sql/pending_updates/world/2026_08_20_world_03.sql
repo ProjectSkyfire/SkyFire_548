97f7fb9428e06be03cde6f3d63f940e2f777a400
AriDEV3 <aridev@projectskyfire.org>
2026-08-20 18:11:57 +0200

    Database: Add OfferRewardText for quest "Stormpike's Orders".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_20_world_03.sql
@@ -0,0 +1,2 @@
+-- Stormpike's Orders
+UPDATE `quest_template` SET `OfferRewardText`='Orders! Let\'s see what we have here... hah! He wants me to deal with the infestation of rat men. We\'re already in the middle of that, so no real deviation there.$B$BCare to lend a hand?' WHERE `Id`=13636;
