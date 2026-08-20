5ca354081fc4164a1125ea4f64b296946740cc57
AriDEV3 <aridev@projectskyfire.org>
2026-08-20 11:09:47 +0200

    Database: Add OfferRewardText for quest "A Failure to Communicate".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_20_world_02.sql
@@ -0,0 +1,2 @@
+-- A Failure to Communicate
+UPDATE `quest_template` SET `OfferRewardText`='Good work. Gaines passed through here not long ago on his way to Northwatch and a warm bath.$B$BThe mission wasn\'t a total loss, though. We\'ve learned a good deal about these quilboar and their leadership structure.$B$BNow we can handle things ... MY way.' WHERE `Id`=25022;
