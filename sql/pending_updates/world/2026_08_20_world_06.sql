66aa6a0f9b748a131a8a32d29ed67e7b7a9eaed2
AriDEV3 <aridev@projectskyfire.org>
2026-08-20 19:29:36 +0200

    Database: Add OfferRewardText for quest "Swiftgear Station".
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_20_world_06.sql
@@ -0,0 +1,2 @@
+-- Swiftgear Station
+UPDATE `quest_template` SET `OfferRewardText`='Stoutfist sent you, eh? He\'s a good captain, runs his men well. Just wish he\'d stop starin\' at my midriff.$B$BDon\'t let me catch you doin\' it either. We\'ve work to take care of here!' WHERE `Id`=26980;
