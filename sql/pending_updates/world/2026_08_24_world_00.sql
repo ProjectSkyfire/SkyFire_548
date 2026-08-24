2ace49c6bd5234c187da7ef340287c562a68296c
AriDEV3 <aridev@projectskyfire.org>
2026-08-24 10:51:23 +0200

    Database: Add RequiredRaces for quests "Mahka's Plea" and "Concern for Mankrik".
    Closes #1317
    
    Signed-off-by: AriDEV3 <aridev@projectskyfire.org>

---
+++ sql/pending_updates/world/2026_08_24_world_00.sql
@@ -0,0 +1,5 @@
+-- Mahka's Plea
+UPDATE `quest_template` SET `RequiredRaces`=2|16|32|128|256|512|33554432 WHERE `Id`=24633;
+
+-- Concern for Mankrik
+UPDATE `quest_template` SET `RequiredRaces`=2|16|32|128|256|512|33554432 WHERE `Id`=24604;
