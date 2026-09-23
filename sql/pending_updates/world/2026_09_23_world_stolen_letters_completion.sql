3c5d4e0aeb898b64a592dfdec6fed65e7d34bbe6
Digid702 <poserbiker@live.com>
2026-09-23 15:28:15 -0700

    Database: Restore quest 26667 completion text.
    Closes #1496

---
+++ sql/pending_updates/world/2026_09_23_world_stolen_letters_completion.sql
@@ -0,0 +1,2 @@
+-- The Stolen Letters (26667): restore missing completion text
+UPDATE `quest_template` SET `OfferRewardText`='By the Light... you actually went and got it?$B$BI''m shocked. I suppose I owe you thanks for returning it to the archives.' WHERE `Id`=26667;
